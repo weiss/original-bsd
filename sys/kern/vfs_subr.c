90509ee63dc23a24ec9337e08be123dd96dd5208
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1990-01-19 14:53:09 +0000

    cannot skip vgone when recycling VNON vnodes

--- sys/kern/vfs_subr.c
+++ sys/kern/vfs_subr.c
@@ -14,7 +14,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)vfs_subr.c	7.23 (Berkeley) 01/15/90
+ *	@(#)vfs_subr.c	7.24 (Berkeley) 01/19/90
  */
 
 /*
@@ -239,7 +239,7 @@ getnewvnode(tag, mp, vops, vpp)
 	vfreeh = vq;
 	vp->v_freef = NULL;
 	vp->v_freeb = NULL;
-	if (vp->v_type != VNON && vp->v_type != VBAD)
+	if (vp->v_type != VBAD)
 		vgone(vp);
 	vp->v_type = VNON;
 	vp->v_flag = 0;
