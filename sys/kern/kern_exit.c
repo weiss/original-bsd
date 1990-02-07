41d863f7fe1d16efd55e57ccba45111e42fc5e8e
marc <marc@65305748-3e16-e211-a854-8c89a5c6f760>
1990-02-07 15:26:01 +0000

    tsleep

--- sys/kern/kern_exit.c
+++ sys/kern/kern_exit.c
@@ -14,7 +14,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)kern_exit.c	7.13 (Berkeley) 12/19/89
+ *	@(#)kern_exit.c	7.14 (Berkeley) 02/07/90
  */
 
 #include "param.h"
@@ -381,6 +381,6 @@ loop:
 		u.u_eosys = RESTARTSYS;
 		return (0);
 	}
-	sleep((caddr_t)u.u_procp, PWAIT);
+	tsleep((caddr_t)u.u_procp, PWAIT, SLP_WAIT, 0);
 	goto loop;
 }
