ade21e6ebb6ee9958c6218cece79b087cd8b1389
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1990-02-17 10:48:33 +0000

    get rid of unneeded header files

--- sys/vax/vax/flp.c
+++ sys/vax/vax/flp.c
@@ -3,17 +3,15 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)flp.c	7.2 (Berkeley) 05/09/89
+ *	@(#)flp.c	7.3 (Berkeley) 02/17/90
  */
 
 #if VAX780
 #include "param.h"
 #include "systm.h"
 #include "conf.h"
-#include "dir.h"
 #include "user.h"
 #include "buf.h"
-#include "uio.h"
 
 #include "cons.h"
 #include "cpu.h"
