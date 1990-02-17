8379de4b66d981134b6f88af639db6cb77152fc3
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1990-02-17 10:22:02 +0000

    get rid of dir.h

--- sys/net/rtsock.c
+++ sys/net/rtsock.c
@@ -14,13 +14,12 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)rtsock.c	7.5 (Berkeley) 08/29/89
+ *	@(#)rtsock.c	7.6 (Berkeley) 02/17/90
  */
 
 #ifndef RTF_UP
 #include "param.h"
 #include "mbuf.h"
-#include "dir.h"
 #include "user.h"
 #include "proc.h"
 #include "socket.h"
