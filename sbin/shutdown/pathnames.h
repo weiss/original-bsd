3e92134f3b8fe7be62959fc3574e293f5ee81f87
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-02-20 16:12:44 +0000

    needs to include paths.h, now for _PATH_NOLOGIN

--- sbin/shutdown/pathnames.h
+++ sbin/shutdown/pathnames.h
@@ -14,8 +14,10 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)pathnames.h	5.3 (Berkeley) 02/15/90
+ *	@(#)pathnames.h	5.4 (Berkeley) 02/20/90
  */
+
+#include <paths.h>
 
 #define	_PATH_FASTBOOT	"/fastboot"
 #define	_PATH_HALT	"/sbin/halt"
