9ad8ee350b4dc5ba3e2458eec516c0cebd2c4635
marc <marc@65305748-3e16-e211-a854-8c89a5c6f760>
1990-02-23 11:16:33 +0000

    include machparam.h to get KERNTEXTOFF

--- old/adb/common_source/setup.c
+++ old/adb/common_source/setup.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char sccsid[] = "@(#)setup.c	5.1 (Berkeley) 01/16/89";
+static char sccsid[] = "@(#)setup.c	5.2 (Berkeley) 02/23/90";
 #endif
 
 /*
@@ -8,6 +8,7 @@ static char sccsid[] = "@(#)setup.c	5.1 (Berkeley) 01/16/89";
 
 #include "defs.h"
 #include <sys/file.h>
+#include <machine/machparam.h>
 
 static struct exec filhdr;
 
