8d82ba9eae6641f6aba3cb80156dfbe724bd687f
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-03-05 11:41:42 +0000

    fix h_errno for ANSI C variable scoping

--- usr.sbin/sendmail/src/domain.c
+++ usr.sbin/sendmail/src/domain.c
@@ -20,9 +20,9 @@
 
 #ifndef lint
 #ifdef NAMED_BIND
-static char sccsid[] = "@(#)domain.c	5.19 (Berkeley) 01/01/89 (with name server)";
+static char sccsid[] = "@(#)domain.c	5.20 (Berkeley) 03/05/90 (with name server)";
 #else
-static char sccsid[] = "@(#)domain.c	5.19 (Berkeley) 01/01/89 (without name server)";
+static char sccsid[] = "@(#)domain.c	5.20 (Berkeley) 03/05/90 (without name server)";
 #endif
 #endif /* not lint */
 
@@ -171,6 +171,7 @@ getcanonname(host, hbsize)
 	char *host;
 	int hbsize;
 {
+	extern int h_errno;
 	register u_char *eom, *cp;
 	register int n; 
 	HEADER *hp;
