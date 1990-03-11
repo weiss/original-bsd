980fee1a2e42c9951afe01be71b3706e5115751d
corbett <corbett@65305748-3e16-e211-a854-8c89a5c6f760>
1990-03-11 22:32:16 +0000

    I changed the banner to match the current revision of the skeleton. -rpc

--- usr.bin/yacc/skeleton.c
+++ usr.bin/yacc/skeleton.c
@@ -19,7 +19,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)skeleton.c	5.3 (Berkeley) 03/06/90";
+static char sccsid[] = "@(#)skeleton.c	5.4 (Berkeley) 03/11/90";
 #endif /* not lint */
 
 #include "defs.h"
@@ -34,7 +34,7 @@ static char sccsid[] = "@(#)skeleton.c	5.3 (Berkeley) 03/06/90";
 char *banner[] =
 {
     "#ifndef lint",
-    "char yysccsid[] = \"@(#)yaccpar	1.1 (Berkeley) 12/10/89\";",
+    "char yysccsid[] = \"@(#)yaccpar	1.4 (Berkeley) 02/25/90\";",
     "#endif",
     0
 };
