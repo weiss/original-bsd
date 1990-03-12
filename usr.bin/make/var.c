c5e74732bef4e026e9ee7c90fc7bc9130e41f9fd
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-03-12 09:55:02 +0000

    wire in DEF_OLD_VARS, still needs more work (oldVars variable)

--- usr.bin/make/var.c
+++ usr.bin/make/var.c
@@ -21,7 +21,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)var.c	5.3 (Berkeley) 03/12/90";
+static char sccsid[] = "@(#)var.c	5.4 (Berkeley) 03/12/90";
 #endif /* not lint */
 
 /*-
@@ -1725,12 +1725,6 @@ Var_Subst (str, ctxt, undefErr)
 		 * the string...
 		 */
 		if (oldVars) {
-#ifndef DEF_OLD_VARS
-		    if (!sysVmake && !amMake && undefErr && !errorReported) {
-			Parse_Error (PARSE_WARNING,
-				     "Undefined variable \"%.*s\"",length,str);
-		    }
-#endif
 		    str += length;
 		} else if (undefErr) {
 		    /*
