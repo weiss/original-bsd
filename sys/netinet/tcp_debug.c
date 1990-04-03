3b5bfb869a7fc2ef516d23e87732b5e36c0aa297
karels <karels@65305748-3e16-e211-a854-8c89a5c6f760>
1990-04-03 14:34:08 +0000

    in_pcb.h requires ip.h

--- sys/netinet/tcp_debug.c
+++ sys/netinet/tcp_debug.c
@@ -14,7 +14,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)tcp_debug.c	7.3 (Berkeley) 06/29/88
+ *	@(#)tcp_debug.c	7.4 (Berkeley) 04/03/90
  */
 
 #include "param.h"
@@ -30,9 +30,9 @@
 #include "../net/if.h"
 
 #include "in.h"
-#include "in_pcb.h"
 #include "in_systm.h"
 #include "ip.h"
+#include "in_pcb.h"
 #include "ip_var.h"
 #include "tcp.h"
 #define TCPSTATES
