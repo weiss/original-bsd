48670c793fd91f145e120a2cb84861283ab341f5
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-04-17 21:22:10 +0000

    date and time created 90/04/17 21:22:10 by bostic

---
+++ usr.bin/xargs/pathnames.h
@@ -0,0 +1,10 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pathnames.h	5.1 (Berkeley) 04/17/90
+ */
+
+#define	_PATH_ECHO	"/bin/echo"
