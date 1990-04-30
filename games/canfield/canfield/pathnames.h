23e40e1b4c3bc55e1f62f61764f6ca07e84fc4d1
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-04-30 21:55:37 +0000

    date and time created 90/04/30 21:55:37 by bostic

---
+++ games/canfield/canfield/pathnames.h
@@ -0,0 +1,11 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pathnames.h	5.1 (Berkeley) 04/30/90
+ */
+
+#define	_PATH_SCORE	"/var/games/cfscores"
+
