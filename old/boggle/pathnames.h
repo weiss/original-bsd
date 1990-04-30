7bbeb75285e7b366d53f8d25a854570ca459fd14
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-04-30 21:48:05 +0000

    date and time created 90/04/30 21:48:05 by bostic

---
+++ old/boggle/pathnames.h
@@ -0,0 +1,12 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pathnames.h	5.1 (Berkeley) 04/30/90
+ */
+
+#define	_PATH_DICTIONARY	"/usr/share/games/bogdict"
+
+
