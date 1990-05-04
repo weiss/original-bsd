c9b6b1331ff4636bcaa45419f04da548d9518f6b
sklower <sklower@65305748-3e16-e211-a854-8c89a5c6f760>
1990-05-04 18:48:28 +0000

    use generic machinery for llifaddrs to replace themeselves by protocol specific ifaddrs by not stomping on ifa_rtrequest in the ll ifaddr.

--- sys/netiso/if_eon.c
+++ sys/netiso/if_eon.c
@@ -27,7 +27,7 @@ SOFTWARE.
 /*
  * $Header: if_eon.c,v 1.4 88/07/19 15:53:59 hagens Exp $ 
  * $Source: /usr/argo/sys/netiso/RCS/if_eon.c,v $ 
- *	@(#)if_eon.c	7.6 (Berkeley) 04/05/90 *
+ *	@(#)if_eon.c	7.7 (Berkeley) 05/04/90 *
  *
  *	EON rfc 
  *  Layer between IP and CLNL
@@ -161,13 +161,15 @@ eonioctl(ifp, cmd, data)
 
 	switch (cmd) {
 		register struct ifaddr *ifa;
+		extern link_rtrequest();
 
 	case SIOCSIFADDR:
 		if (ifa = (struct ifaddr *)data) {
 			ifp->if_flags |= IFF_UP;
-			ifa->ifa_rtrequest = eonrtrequest;
+			if (ifa->ifa_addr->sa_family != AF_LINK)
+				ifa->ifa_rtrequest = eonrtrequest;
 			ifa->ifa_llinfolen = sizeof(struct eon_llinfo);
-	    }
+		}
 		break;
 	}
 	splx(s);
