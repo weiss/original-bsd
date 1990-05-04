dc5331bc535eba9b3e22fe7697471bf261a1a2e1
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1990-05-04 12:48:27 +0000

    delete unneeded headers

--- sbin/icheck/icheck.c
+++ sbin/icheck/icheck.c
@@ -5,7 +5,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)icheck.c	5.5 (Berkeley) 07/30/89";
+static char sccsid[] = "@(#)icheck.c	5.6 (Berkeley) 05/04/90";
 #endif not lint
 
 /*
@@ -16,9 +16,7 @@ static char sccsid[] = "@(#)icheck.c	5.5 (Berkeley) 07/30/89";
 #define	MAXNINDIR	(MAXBSIZE / sizeof (daddr_t))
 
 #include <sys/param.h>
-#include <sys/time.h>
-#include <sys/vnode.h>
-#include <ufs/inode.h>
+#include <ufs/dinode.h>
 #include <ufs/fs.h>
 #ifndef STANDALONE
 #include <stdio.h>
