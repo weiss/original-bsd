078beaed09021858099159dc66775c29beec6d6d
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1990-05-10 13:04:00 +0000

    get rid of #defines in user.h

--- sys/kern/kern_resource.c
+++ sys/kern/kern_resource.c
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)kern_resource.c	7.6 (Berkeley) 04/03/90
+ *	@(#)kern_resource.c	7.7 (Berkeley) 05/10/90
  */
 
 #include "param.h"
@@ -130,8 +130,8 @@ donice(p, n)
 	register int n;
 {
 
-	if (u.u_uid && u.u_ruid &&
-	    u.u_uid != p->p_uid && u.u_ruid != p->p_uid) {
+	if (u.u_uid && u.u_procp->p_ruid &&
+	    u.u_uid != p->p_uid && u.u_procp->p_ruid != p->p_uid) {
 		u.u_error = EPERM;
 		return;
 	}
