b3065d700288d4eb94e0eb52e4a3d5386d2b01a6
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-05-12 16:35:06 +0000

    don't use hash mark for comments.

--- lib/libc/sys/getgid.s
+++ lib/libc/sys/getgid.s
@@ -16,10 +16,10 @@
  */
 
 #if defined(SYSLIBC_SCCS) && !defined(lint)
-	.asciz "@(#)getgid.s	5.5 (Berkeley) 06/27/88"
+	.asciz "@(#)getgid.s	5.6 (Berkeley) 05/12/90"
 #endif /* SYSLIBC_SCCS and not lint */
 
 #include "SYS.h"
 
 SYSCALL(getgid)
-	ret		# gid = getgid();
+	ret		/* gid = getgid(); */
