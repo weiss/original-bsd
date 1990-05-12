778f9b1c767e993dd4877b78b76306494c47e6c3
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-05-12 16:40:34 +0000

    don't use hash mark for comments.

--- lib/libc/sys/sethostid.s
+++ lib/libc/sys/sethostid.s
@@ -16,10 +16,10 @@
  */
 
 #if defined(SYSLIBC_SCCS) && !defined(lint)
-	.asciz "@(#)sethostid.s	5.5 (Berkeley) 06/27/88"
+	.asciz "@(#)sethostid.s	5.6 (Berkeley) 05/12/90"
 #endif /* SYSLIBC_SCCS and not lint */
 
 #include "SYS.h"
 
 SYSCALL(sethostid)
-	ret		# sethostid(hostid)
+	ret		/* sethostid(hostid) */
