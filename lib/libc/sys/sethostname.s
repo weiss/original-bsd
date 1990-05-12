5c697522f2801ee639665cd1ce5d00bf73bca552
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-05-12 16:40:45 +0000

    don't use hash mark for comments.

--- lib/libc/sys/sethostname.s
+++ lib/libc/sys/sethostname.s
@@ -16,10 +16,10 @@
  */
 
 #if defined(SYSLIBC_SCCS) && !defined(lint)
-	.asciz "@(#)sethostname.s	5.5 (Berkeley) 06/27/88"
+	.asciz "@(#)sethostname.s	5.6 (Berkeley) 05/12/90"
 #endif /* SYSLIBC_SCCS and not lint */
 
 #include "SYS.h"
 
 SYSCALL(sethostname)
-	ret		# sethostname(name, len)
+	ret		/* sethostname(name, len) */
