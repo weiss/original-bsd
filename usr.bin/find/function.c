443cf18d92bdadb18231d5df2e5b7593c86e703c
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-05-12 15:28:33 +0000

    define strerror() return

--- usr.bin/find/function.c
+++ usr.bin/find/function.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)function.c	5.1 (Berkeley) 04/16/90";
+static char sccsid[] = "@(#)function.c	5.2 (Berkeley) 05/12/90";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -22,6 +22,7 @@ static char sccsid[] = "@(#)function.c	5.1 (Berkeley) 04/16/90";
 #include <unistd.h>
 #include <tzfile.h>
 #include <stdio.h>
+#include <strings.h>
 #include "find.h"
 
 #define	FIND_EQUAL	0
