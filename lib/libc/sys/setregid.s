2f5269608172ad36856eb94cd9a56ddb5fd5bb63
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-05-12 16:41:23 +0000

    don't use hash mark for comments.

--- lib/libc/sys/setregid.s
+++ lib/libc/sys/setregid.s
@@ -16,10 +16,10 @@
  */
 
 #if defined(SYSLIBC_SCCS) && !defined(lint)
-	.asciz "@(#)setregid.s	5.5 (Berkeley) 06/27/88"
+	.asciz "@(#)setregid.s	5.6 (Berkeley) 05/12/90"
 #endif /* SYSLIBC_SCCS and not lint */
 
 #include "SYS.h"
 
 SYSCALL(setregid)
-	ret		# setregid(rgid, egid)
+	ret		/* setregid(rgid, egid) */
