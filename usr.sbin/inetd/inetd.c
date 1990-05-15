6ceae2fb07d1d1bd49e125a1ed7efbb69d5638a6
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-05-15 20:03:52 +0000

    string.h is ANSI C include file

--- usr.sbin/inetd/inetd.c
+++ usr.sbin/inetd/inetd.c
@@ -22,7 +22,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)inetd.c	5.20 (Berkeley) 04/04/90";
+static char sccsid[] = "@(#)inetd.c	5.21 (Berkeley) 05/15/90";
 #endif /* not lint */
 
 /*
@@ -78,7 +78,7 @@ static char sccsid[] = "@(#)inetd.c	5.20 (Berkeley) 04/04/90";
 #include <syslog.h>
 #include <pwd.h>
 #include <stdio.h>
-#include <strings.h>
+#include <string.h>
 #include "pathnames.h"
 
 #define	TOOMANY		40		/* don't start more than TOOMANY */
