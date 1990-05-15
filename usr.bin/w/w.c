5092ff8c50d3e03be58817d5705fca56d99edaf6
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-05-15 19:54:55 +0000

    string.h is ANSI C include file

--- usr.bin/w/w.c
+++ usr.bin/w/w.c
@@ -11,7 +11,7 @@ char copyright[] =
 #endif not lint
 
 #ifndef lint
-static char sccsid[] = "@(#)w.c	5.20 (Berkeley) 05/04/90";
+static char sccsid[] = "@(#)w.c	5.21 (Berkeley) 05/15/90";
 #endif not lint
 
 /*
@@ -33,7 +33,7 @@ static char sccsid[] = "@(#)w.c	5.20 (Berkeley) 05/04/90";
 #include <kvm.h>
 #include <ctype.h>
 #include <paths.h>
-#include <strings.h>
+#include <string.h>
 #include <stdio.h>
 
 char	*program;
