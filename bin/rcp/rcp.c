e108010137033e5df5d784c5c4bb61c930f90218
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-05-15 19:40:35 +0000

    string.h is ANSI C include file

--- bin/rcp/rcp.c
+++ bin/rcp/rcp.c
@@ -22,7 +22,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rcp.c	5.24 (Berkeley) 05/12/90";
+static char sccsid[] = "@(#)rcp.c	5.25 (Berkeley) 05/15/90";
 #endif /* not lint */
 
 /*
@@ -39,7 +39,7 @@ static char sccsid[] = "@(#)rcp.c	5.24 (Berkeley) 05/12/90";
 #include <pwd.h>
 #include <netdb.h>
 #include <errno.h>
-#include <strings.h>
+#include <string.h>
 #include <stdio.h>
 #include <ctype.h>
 #include "pathnames.h"
