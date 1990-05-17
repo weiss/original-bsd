0c270cab9cebca93f79c7cfb67b80c64eff3596d
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-05-17 16:17:13 +0000

    mv x3j11.h -> machtypes.h

--- include/stddef.h
+++ include/stddef.h
@@ -4,12 +4,12 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)stddef.h	5.1 (Berkeley) 05/15/90
+ *	@(#)stddef.h	5.2 (Berkeley) 05/17/90
  */
 
 #ifndef _STDDEF_H_
 #define _STDDEF_H_
-#include <machine/x3j11.h>
+#include <machine/machtypes.h>
 
 typedef	_PTRDIFF_T_	ptrdiff_t;
 
