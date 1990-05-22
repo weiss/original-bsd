2e1f056867661d60a5f6d8c4838b8a84e31c34c0
marc <marc@65305748-3e16-e211-a854-8c89a5c6f760>
1990-05-22 16:09:20 +0000

    date and time created 90/05/22 16:09:20 by marc

---
+++ sys/sys/tprintf.h
@@ -0,0 +1,12 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)tprintf.h	1.1 (Berkeley) 05/22/90
+ */
+
+typedef struct session *tpr_t;
+
+tpr_t tpr_open();
