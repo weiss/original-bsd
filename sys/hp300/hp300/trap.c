fe365b5f322dc49866449e48e942747e5a4bb61d
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1990-05-29 13:12:15 +0000

    new calling convension for system calls

--- sys/hp300/hp300/trap.c
+++ sys/hp300/hp300/trap.c
@@ -11,7 +11,7 @@
  *
  * from: Utah $Hdr: trap.c 1.28 89/09/25$
  *
- *	@(#)trap.c	7.3 (Berkeley) 05/25/90
+ *	@(#)trap.c	7.4 (Berkeley) 05/29/90
  */
 
 #include "cpu.h"
@@ -595,7 +595,7 @@ syscall(code, frame)
 		error = notimp(code, callp->sy_narg);
 	else
 #endif
-	error = (*(callp->sy_call))(&u);
+	error = (*callp->sy_call)(u.u_procp, u.u_ap, &u.u_r.r_val1);
 	error = u.u_error;		/* XXX */
 	if (error == ERESTART)
 		frame.f_pc = opc;
