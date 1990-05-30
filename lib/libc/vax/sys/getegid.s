d240c8f679ce503f4ebdea53bbfbb983ffce8e37
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-05-30 13:07:16 +0000

    can't have spaces in macro args...

--- lib/libc/vax/sys/getegid.s
+++ lib/libc/vax/sys/getegid.s
@@ -8,9 +8,9 @@
 #include "SYS.h"
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	ASMSTR "@(#)getegid.s	5.7 (Berkeley) 05/30/90"
+	ASMSTR "@(#)getegid.s	5.8 (Berkeley) 05/30/90"
 #endif /* LIBC_SCCS and not lint */
 
-PSEUDO(getegid, getgid)
+PSEUDO(getegid,getgid)
 	movl	r1,r0
 	ret		/* egid = getegid(); */
