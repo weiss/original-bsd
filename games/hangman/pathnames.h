def0cdc4d2e8ccaacef351b4272cee9837986de8
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-06-01 13:10:01 +0000

    date and time created 90/06/01 13:10:01 by bostic

---
+++ games/hangman/pathnames.h
@@ -0,0 +1,10 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pathnames.h	5.1 (Berkeley) 06/01/90
+ */
+
+#define	_PATH_DICT	"/usr/share/dict/words"
