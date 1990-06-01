a61bd1d9513d39b255f64b4dcd617bb330aa18aa
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-06-01 13:15:30 +0000

    date and time created 90/06/01 13:15:30 by bostic

---
+++ games/rogue/pathnames.h
@@ -0,0 +1,12 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pathnames.h	5.1 (Berkeley) 06/01/90
+ */
+
+#define	_PATH_SCOREFILE	"/var/games/rogue.scores"
+#define	_PATH_LOCKFILE	"/var/games/rogue.lock"
+
