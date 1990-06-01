9f9a0d6d53827bdc3f4258ddb065ffcfe0d1f5ea
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-06-01 13:57:18 +0000

    date and time created 90/06/01 13:57:18 by bostic

---
+++ games/wump/pathnames.h
@@ -0,0 +1,11 @@
+/*-
+ * Copyright (c) 1989 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pathnames.h	5.1 (Berkeley) 06/01/90
+ */
+
+#define	_PATH_PAGER	"/usr/bin/more"
+#define	_PATH_WUMPINFO	"/usr/share/games/wump.info"
