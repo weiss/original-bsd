ab3488206b34a8a415cbf5a7cbc22e57d33cc999
karels <karels@65305748-3e16-e211-a854-8c89a5c6f760>
1990-06-21 19:08:16 +0000

    select routines don't return errors

--- sys/kern/tty_tty.c
+++ sys/kern/tty_tty.c
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)tty_tty.c	7.8 (Berkeley) 05/01/90
+ *	@(#)tty_tty.c	7.9 (Berkeley) 06/21/90
  */
 
 /*
@@ -102,6 +102,6 @@ syselect(dev, flag)
 	struct vnode *ttyvp = cttyvp(u.u_procp);
 
 	if (ttyvp == NULL)
-		return (ENXIO);
+		return (1);	/* try operation to get EOF/failure */
 	return (VOP_SELECT(ttyvp, flag, FREAD|FWRITE, NOCRED));
 }
