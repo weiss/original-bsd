6982dced7b712ffc7f2afaa7d5d4abb8fd83d6cd
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1990-06-22 17:17:29 +0000

    no more u.u_ap

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)init_main.c	7.28 (Berkeley) 05/24/90
+ *	@(#)init_main.c	7.29 (Berkeley) 06/22/90
  */
 
 #include "param.h"
@@ -98,7 +98,6 @@ main(firstaddr)
 	 * handled when copying the u. area in newproc().
 	 */
 	ndinit(&u.u_nd);
-	u.u_ap = u.u_arg;
 
 	u.u_cmask = cmask;
 	u.u_lastfile = -1;
