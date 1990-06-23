e45fb1252027182e65f6ad99f0bdb5f20e13fc94
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-06-23 14:53:58 +0000

    move lib.b to bc.library

--- usr.bin/bc/pathnames.h
+++ usr.bin/bc/pathnames.h
@@ -4,8 +4,8 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.2 (Berkeley) 06/01/90
+ *	@(#)pathnames.h	5.3 (Berkeley) 06/23/90
  */
 
-#define	_PATH_LIBB	"/usr/share/misc/lib.b"
+#define	_PATH_LIBB	"/usr/share/misc/bc.library"
 #define	_PATH_DC	"/usr/bin/dc"
