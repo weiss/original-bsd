884f979387dbfa40e9df733d2a09a77c6af2ee6a
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-06-24 21:18:51 +0000

    date and time created 90/06/24 21:18:51 by bostic

---
+++ libexec/ftpd/vers.c
@@ -0,0 +1,12 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)vers.c	5.1 (Berkeley) 06/24/90";
+#endif /* not lint */
+
+char version[] = "Version 5.60";
