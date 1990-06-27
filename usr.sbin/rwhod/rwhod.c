0d7d304402e03e8d9bf70e0e51c91cf83f3c8370
sklower <sklower@65305748-3e16-e211-a854-8c89a5c6f760>
1990-06-27 14:22:02 +0000

    tested wrong thing to see if new or old sockaddrs.

--- usr.sbin/rwhod/rwhod.c
+++ usr.sbin/rwhod/rwhod.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rwhod.c	5.17 (Berkeley) 06/26/90";
+static char sccsid[] = "@(#)rwhod.c	5.18 (Berkeley) 06/27/90";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -378,7 +378,7 @@ configure(s)
 		return (0);
 	}
 	ifr = ifc.ifc_req;
-#ifdef RTM_ADD
+#ifdef AF_LINK
 #define max(a, b) (a > b ? a : b)
 #define size(p)	max((p).sa_len, sizeof(p))
 #else
