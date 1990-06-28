9d566ef6a50ab759bb905277e5dab4e5bc2ce590
karels <karels@65305748-3e16-e211-a854-8c89a5c6f760>
1990-06-28 14:57:07 +0000

    protocol notify routines set errors, so can be soft

--- sys/netinet/in_pcb.c
+++ sys/netinet/in_pcb.c
@@ -14,7 +14,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)in_pcb.c	7.11 (Berkeley) 04/03/90
+ *	@(#)in_pcb.c	7.12 (Berkeley) 06/28/90
  */
 
 #include "param.h"
@@ -326,12 +326,10 @@ in_pcbnotify(head, dst, fport, laddr, lport, cmd, notify)
 			inp = inp->inp_next;
 			continue;
 		}
-		if (errno) 
-			inp->inp_socket->so_error = errno;
 		oinp = inp;
 		inp = inp->inp_next;
 		if (notify)
-			(*notify)(oinp);
+			(*notify)(oinp, errno);
 	}
 }
 
