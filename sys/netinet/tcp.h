43818b85fef790e1edcfda4ec22d86ddc582afee
karels <karels@65305748-3e16-e211-a854-8c89a5c6f760>
1990-06-28 14:34:57 +0000

    rm lint fix, just make TCP_MSS 512; define MAXWIN

--- sys/netinet/tcp.h
+++ sys/netinet/tcp.h
@@ -14,23 +14,8 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)tcp.h	7.4.1.2 (Berkeley) 02/17/89
+ *	@(#)tcp.h	7.6 (Berkeley) 06/28/90
  */
-#ifndef BYTE_ORDER
-/*
- * Definitions for byte order,
- * according to byte significance from low address to high.
- */
-#define	LITTLE_ENDIAN	1234	/* least-significant byte first (vax) */
-#define	BIG_ENDIAN	4321	/* most-significant byte first (IBM, net) */
-#define	PDP_ENDIAN	3412	/* LSB first in word, MSW first in long (pdp) */
-
-#ifdef vax
-#define	BYTE_ORDER	LITTLE_ENDIAN
-#else
-#define	BYTE_ORDER	BIG_ENDIAN	/* mc68000, tahoe, most others */
-#endif
-#endif BYTE_ORDER
 
 typedef	u_long	tcp_seq;
 /*
@@ -42,11 +27,11 @@ struct tcphdr {
 	u_short	th_dport;		/* destination port */
 	tcp_seq	th_seq;			/* sequence number */
 	tcp_seq	th_ack;			/* acknowledgement number */
-#if BYTE_ORDER == LITTLE_ENDIAN
+#if BYTE_ORDER == LITTLE_ENDIAN 
 	u_char	th_x2:4,		/* (unused) */
 		th_off:4;		/* data offset */
 #endif
-#if BYTE_ORDER == BIG_ENDIAN
+#if BYTE_ORDER == BIG_ENDIAN 
 	u_char	th_off:4,		/* data offset */
 		th_x2:4;		/* (unused) */
 #endif
@@ -70,15 +55,11 @@ struct tcphdr {
  * Default maximum segment size for TCP.
  * With an IP MSS of 576, this is 536,
  * but 512 is probably more convenient.
+ * This should be defined as MIN(512, IP_MSS - sizeof (struct tcpiphdr)).
  */
-#ifdef	lint
-#define	TCP_MSS	536
-#else
-#ifndef IP_MSS
-#define	IP_MSS	576
-#endif
-#define	TCP_MSS	MIN(512, IP_MSS - sizeof (struct tcpiphdr))
-#endif
+#define	TCP_MSS	512
+
+#define	TCP_MAXWIN	65535		/* largest value for window */
 
 /*
  * User-settable options (used with setsockopt).
