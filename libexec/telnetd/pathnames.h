ce93a3c82cc8f13d247b18eda914b09186e4e3a9
borman <borman@65305748-3e16-e211-a854-8c89a5c6f760>
1990-06-28 13:06:23 +0000

    Lots of bugfixes, add the ENVIRON and X-DISPLAY-LOC options, "-l user" to the command line, diagnostic flags.

--- libexec/telnetd/pathnames.h
+++ libexec/telnetd/pathnames.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.4 (Berkeley) 06/01/90
+ *	@(#)pathnames.h	5.5 (Berkeley) 06/28/90
  */
 
 #if BSD > 43
@@ -19,3 +19,7 @@
 # define	_PATH_LOGIN	"/bin/login"
 
 #endif
+
+#ifdef BFTPDAEMON
+#define		BFTPPATH	"/usr/ucb/bftp"
+#endif  /* BFTPDAEMON */
