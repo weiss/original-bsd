3c0ca27e75ec4d08f721a80a2c24a5c786caab25
marc <marc@65305748-3e16-e211-a854-8c89a5c6f760>
1990-06-29 00:29:42 +0000

    remove reference to ldisc.

--- usr.bin/ex/ex_cmds.c
+++ usr.bin/ex/ex_cmds.c
@@ -5,7 +5,7 @@
  */
 
 #ifndef lint
-static char *sccsid = "@(#)ex_cmds.c	7.12 (Berkeley) 01/02/88";
+static char *sccsid = "@(#)ex_cmds.c	7.13 (Berkeley) 06/29/90";
 #endif not lint
 
 #include "ex.h"
@@ -593,8 +593,6 @@ quit:
 			case 'u':
 				tail("suspend");
 suspend:
-				if (!ldisc)
-					error("Old tty driver|Not using new tty driver/shell");
 				c = exclam();
 				eol();
 				if (!c)
