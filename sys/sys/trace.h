c7f28305a0323aea8ed6fa2790ac23874fa8ba90
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1990-08-24 08:48:17 +0000

    fsid is now of type quad

--- sys/sys/trace.h
+++ sys/sys/trace.h
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)trace.h	7.2 (Berkeley) 03/06/90
+ *	@(#)trace.h	7.3 (Berkeley) 08/24/90
  */
 
 /*
@@ -78,7 +78,7 @@ struct	proc *traceproc;
 int	tracebuf[TRCSIZ];
 unsigned tracex;
 int	tracewhich;
-#define	pack(v,b)	(((v)->v_mount->m_stat.f_fsid[0])<<16)|(b)
+#define	pack(v,b)	(((v)->v_mount->m_stat.f_fsid.val[0])<<16)|(b)
 #define	trace(a,b,c)	if (traceflags[a]) trace1(a,b,c)
 #else
 #define	trace(a,b,b)	;
