3218594a73cc7afad6b67d1dc66aa990d15b4023
karels <karels@65305748-3e16-e211-a854-8c89a5c6f760>
1990-08-29 17:38:35 +0000

    from chris: exit doesn't return

--- sys/kern/kern_exit.c
+++ sys/kern/kern_exit.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kern_exit.c	7.26 (Berkeley) 06/28/90
+ *	@(#)kern_exit.c	7.27 (Berkeley) 08/29/90
  */
 
 #include "param.h"
@@ -40,7 +40,8 @@ rexit(p, uap, retval)
 	int *retval;
 {
 
-	return (exit(p, W_EXITCODE(uap->rval, 0)));
+	exit(p, W_EXITCODE(uap->rval, 0));
+	/* NOTREACHED */
 }
 
 /*
