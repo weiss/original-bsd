94f46ba8153a8880f9f0e46815920212eb672073
karels <karels@65305748-3e16-e211-a854-8c89a5c6f760>
1990-08-30 12:35:05 +0000

    add IFF_SIMPLEX to IFF_CANTCHANGE

--- sys/net/if.h
+++ sys/net/if.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)if.h	7.9 (Berkeley) 06/28/90
+ *	@(#)if.h	7.10 (Berkeley) 08/30/90
  */
 
 /*
@@ -106,7 +106,8 @@ struct ifnet {
 #define	IFF_SIMPLEX	0x800		/* can't hear own transmissions */
 
 /* flags set internally only: */
-#define	IFF_CANTCHANGE	(IFF_BROADCAST|IFF_POINTOPOINT|IFF_RUNNING|IFF_OACTIVE)
+#define	IFF_CANTCHANGE \
+	(IFF_BROADCAST|IFF_POINTOPOINT|IFF_RUNNING|IFF_OACTIVE|IFF_SIMPLEX)
 
 /*
  * Output queues (ifp->if_snd) and internetwork datagram level (pup level 1)
