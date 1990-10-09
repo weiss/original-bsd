e74d5520ab508965c459fb9b1b77b8955ff0a510
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-10-09 17:46:50 +0000

    new copyright notice

--- lib/libm/common/trig.h
+++ lib/libm/common/trig.h
@@ -4,12 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- * All recipients should regard themselves as participants in an ongoing
- * research project and hence should feel obligated to report their
- * experiences (good or bad) with these elementary function codes, using
- * the sendbug(8) program, to the authors.
- *
- *	@(#)trig.h	5.5 (Berkeley) 06/01/90
+ *	@(#)trig.h	5.6 (Berkeley) 10/09/90
  */
 
 #include "mathimpl.h"
