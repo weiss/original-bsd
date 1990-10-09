11cdaabf7c687e74bc8e6aed005f2b14b1d17c78
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-10-09 18:16:19 +0000

    new copyright notice

--- lib/libm/vax/infnan.s
+++ lib/libm/vax/infnan.s
@@ -4,17 +4,12 @@
  *
  * %sccs.include.redist.c%
  *
- * All recipients should regard themselves as participants in an ongoing
- * research project and hence should feel obligated to report their
- * experiences (good or bad) with these elementary function codes, using
- * the sendbug(8) program, to the authors.
- *
- *	@(#)infnan.s	5.4 (Berkeley) 06/01/90
+ *	@(#)infnan.s	5.5 (Berkeley) 10/09/90
  */
 	.data
 	.align	2
 _sccsid:
-.asciz	"@(#)infnan.s	1.1 (Berkeley) 8/21/85; 5.4 (ucb.elefunt) 06/01/90"
+.asciz	"@(#)infnan.s	1.1 (Berkeley) 8/21/85; 5.5 (ucb.elefunt) 10/09/90"
 
 /*
  * infnan(arg) int arg;
