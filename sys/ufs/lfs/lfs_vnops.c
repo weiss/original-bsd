e8b60e03df76216d972578a01cf93c3334dbf74b
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1990-10-19 15:26:43 +0000

    properly decrement reference count on directory

--- sys/ufs/lfs/lfs_vnops.c
+++ sys/ufs/lfs/lfs_vnops.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lfs_vnops.c	7.45 (Berkeley) 06/28/90
+ *	@(#)lfs_vnops.c	7.46 (Berkeley) 10/19/90
  */
 
 #include "param.h"
@@ -1036,6 +1036,16 @@ ufs_rename(fndp, tndp)
 		}
 		if (error = dirrewrite(dp, ip, tndp))
 			goto bad;
+		/*
+		 * If the target directory is in the same
+		 * directory as the source directory,
+		 * decrement the link count on the parent
+		 * of the target directory.
+		 */
+		 if (doingdirectory && !newparent) {
+			dp->i_nlink--;
+			dp->i_flag |= ICHG;
+		}
 		vput(ITOV(dp));
 		/*
 		 * Adjust the link count of the target to
