23a5cd61949ce8e0d7ade7959a691b0a200cde32
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1990-10-24 15:09:43 +0000

    update Utah source identifier

--- sys/hp/hpux/hpux_termio.h
+++ sys/hp/hpux/hpux_termio.h
@@ -9,9 +9,9 @@
  *
  * %sccs.include.redist.c%
  *
- * from: Utah $Hdr: hpux.h 1.15 89/09/25$
+ * from: Utah $Hdr: hpux_termio.h 1.1 90/07/09$
  *
- *	@(#)hpux_termio.h	7.2 (Berkeley) 06/27/90
+ *	@(#)hpux_termio.h	7.3 (Berkeley) 10/24/90
  */
 
 /* HP-UX termio stuff */
