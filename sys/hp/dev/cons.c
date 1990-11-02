6d18a544030327ab7b87a7442f4f836f8b429315
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1990-11-02 16:25:57 +0000

    update identifier from Utah

--- sys/hp/dev/cons.c
+++ sys/hp/dev/cons.c
@@ -9,9 +9,9 @@
  *
  * %sccs.include.redist.c%
  *
- * from: Utah $Hdr: cons.c 1.4 88/12/03$
+ * from: Utah $Hdr: cons.c 1.1 90/07/09$
  *
- *	@(#)cons.c	7.3 (Berkeley) 06/06/90
+ *	@(#)cons.c	7.4 (Berkeley) 11/02/90
  */
 
 #include "param.h"
