11bc9cb4a6b56b8fcf7b9629df3324d34a6f1996
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1990-11-04 19:36:55 +0000

    update identifier from Utah

--- sys/hp/dev/itevar.h
+++ sys/hp/dev/itevar.h
@@ -9,9 +9,9 @@
  *
  * %sccs.include.redist.c%
  *
- * from: Utah $Hdr: itevar.h 1.13 89/02/27$
+ * from: Utah $Hdr: itevar.h 1.1 90/07/09$
  *
- *	@(#)itevar.h	7.1 (Berkeley) 05/08/90
+ *	@(#)itevar.h	7.2 (Berkeley) 11/04/90
  */
 
 #define UNIT(dev)       minor(dev)
