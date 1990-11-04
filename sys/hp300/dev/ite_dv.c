f46e9e379f3817107232a0b92206ada138519021
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1990-11-04 19:33:57 +0000

    update identifier from Utah

--- sys/hp300/dev/ite_dv.c
+++ sys/hp300/dev/ite_dv.c
@@ -9,9 +9,9 @@
  *
  * %sccs.include.redist.c%
  *
- * from: Utah $Hdr: ite_dv.c 1.5 89/04/11$
+ * from: Utah $Hdr: ite_dv.c 1.1 90/07/09$
  *
- *	@(#)ite_dv.c	7.1 (Berkeley) 05/08/90
+ *	@(#)ite_dv.c	7.2 (Berkeley) 11/04/90
  */
 
 #include "ite.h"
