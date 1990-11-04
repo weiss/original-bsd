22c4f4adbac688708453a5a8a452a3067fd2838c
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1990-11-04 19:22:49 +0000

    update identifier from Utah

--- sys/dev/vnioctl.h
+++ sys/dev/vnioctl.h
@@ -9,9 +9,9 @@
  *
  * %sccs.include.redist.c%
  *
- * from: Utah $Hdr: fdioctl.h 1.1 89/11/20$
+ * from: Utah $Hdr: fdioctl.h 1.1 90/07/09$
  *
- *	@(#)vnioctl.h	7.1 (Berkeley) 05/08/90
+ *	@(#)vnioctl.h	7.2 (Berkeley) 11/04/90
  */
 
 /*
