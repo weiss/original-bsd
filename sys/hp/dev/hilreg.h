d6f29424f9923d21b33691c21f26d98d2ee7cf1c
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1990-11-04 19:31:46 +0000

    update identifier from Utah

--- sys/hp/dev/hilreg.h
+++ sys/hp/dev/hilreg.h
@@ -9,9 +9,9 @@
  *
  * %sccs.include.redist.c%
  *
- * from: Utah $Hdr: hilreg.h 1.8 89/08/24$
+ * from: Utah $Hdr: hilreg.h 1.1 90/07/09$
  *
- *	@(#)hilreg.h	7.1 (Berkeley) 05/08/90
+ *	@(#)hilreg.h	7.2 (Berkeley) 11/04/90
  */
 
 struct	hil_dev {
