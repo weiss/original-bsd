f51217e384601338e38eaec4f78b24edd477cbd1
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1990-11-04 19:32:14 +0000

    update identifier from Utah

--- sys/hp/dev/hilvar.h
+++ sys/hp/dev/hilvar.h
@@ -9,9 +9,9 @@
  *
  * %sccs.include.redist.c%
  *
- * from: Utah $Hdr: hilvar.h 1.1 89/08/22$
+ * from: Utah $Hdr: hilvar.h 1.1 90/07/09$
  *
- *	@(#)hilvar.h	7.1 (Berkeley) 05/08/90
+ *	@(#)hilvar.h	7.2 (Berkeley) 11/04/90
  */
 
 #ifndef TRUE
