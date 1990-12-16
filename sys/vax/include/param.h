dd262573a20df43399c71a8817b8b1bc199fc1db
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1990-12-16 16:52:02 +0000

    kernel reorg

--- sys/vax/include/param.h
+++ sys/vax/include/param.h
@@ -3,19 +3,13 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)param.h	7.16 (Berkeley) 05/10/90
+ *	@(#)param.h	7.17 (Berkeley) 12/16/90
  */
 
 /*
  * Machine dependent constants for VAX.
  */
 #define	MACHINE	"vax"
-
-#ifndef BYTE_ORDER
-#include <machine/endian.h>
-#endif
-
-#include <machine/machlimits.h>
 
 /*
  * Round p (pointer or byte index) up to a correctly-aligned value
