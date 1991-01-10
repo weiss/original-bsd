2c0844a6ff3bacd5c9fdfd6730e9eb19e049843b
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-01-10 17:25:25 +0000

    get rid of stupid error messages

--- usr.bin/make/parse.c
+++ usr.bin/make/parse.c
@@ -11,7 +11,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parse.c	5.16 (Berkeley) 06/29/90";
+static char sccsid[] = "@(#)parse.c	5.17 (Berkeley) 01/10/91";
 #endif /* not lint */
 
 /*-
@@ -2174,7 +2174,7 @@ Parse_MainName()
     main = Lst_Init (FALSE);
 
     if (mainNode == NILGNODE) {
-	Punt ("I don't know what to DO!\n");
+	Punt ("make: no target to make.\n");
     	/*NOTREACHED*/
     } else if (mainNode->type & OP_DOUBLEDEP) {
 	Lst_Concat(main, mainNode->cohorts, LST_CONCNEW);
