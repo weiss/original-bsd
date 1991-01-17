d04fa817c5c41cf0bc43db5ee28a2d44905d679a
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-01-17 14:39:32 +0000

    from scratch -- /bin/mail was a security problem, and not worth fixing

--- libexec/mail.local/pathnames.h
+++ libexec/mail.local/pathnames.h
@@ -4,10 +4,8 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.2 (Berkeley) 06/24/90
+ *	@(#)pathnames.h	5.3 (Berkeley) 01/17/91
  */
-
 #include <paths.h>
 
-#undef _PATH_TMP
-#define _PATH_TMP	"/tmp/maXXXXX"
+#define _PATH_LOCTMP	"/tmp/local.XXXXXX"
