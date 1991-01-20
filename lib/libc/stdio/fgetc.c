4c3b941008c66e77c5344b5e1a0f4b0eabd2f839
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-01-20 21:09:42 +0000

    new stdio

--- lib/libc/stdio/fgetc.c
+++ lib/libc/stdio/fgetc.c
@@ -1,11 +1,21 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Chris Torek.
+ *
+ * %sccs.include.redist.c%
+ */
+
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)fgetc.c	5.2 (Berkeley) 03/09/86";
-#endif LIBC_SCCS and not lint
+static char sccsid[] = "@(#)fgetc.c	5.3 (Berkeley) 01/20/91";
+#endif /* LIBC_SCCS and not lint */
 
 #include <stdio.h>
 
 fgetc(fp)
-FILE *fp;
+	FILE *fp;
 {
-	return(getc(fp));
+	return (__sgetc(fp));
 }
