a3050234fce4ac8570ee9c68f8f8bc7f0c82c28b
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-01-20 21:35:03 +0000

    new stdio

---
+++ lib/libc/stdio/tmpfile.c
@@ -0,0 +1,33 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Chris Torek.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+static char sccsid[] = "@(#)tmpfile.c	5.1 (Berkeley) 01/20/91";
+#endif /* LIBC_SCCS and not lint */
+
+#include <sys/param.h>
+#include <stdio.h>
+#include <errno.h>
+
+FILE *
+tmpfile()
+{
+	FILE *fp;
+	int e;
+	char *f, buf[MAXPATHLEN];
+
+	if ((f = tmpnam(buf)) == NULL)
+		return (NULL);
+	fp = fopen(f, "w+");
+	e = errno;
+	(void) unlink(f);
+	errno = e;
+	return (fp);
+}
---
+++ lib/libc/stdio/tmpnam.c
@@ -0,0 +1,29 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Chris Torek.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+static char sccsid[] = "@(#)tmpnam.c	5.1 (Berkeley) 01/20/91";
+#endif /* LIBC_SCCS and not lint */
+
+#include <sys/param.h>
+#include <stdio.h>
+
+char *
+tmpnam(s)
+	char *s;
+{
+	static char buf[MAXPATHLEN];
+	char *mktemp();
+
+	if (s == NULL)
+		s = buf;
+	(void) sprintf(s, "%s/XXXXXX", P_tmpdir);
+	return (mktemp(s));
+}
