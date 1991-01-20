647846d03334e27ba86485e3beb1bc9481ab1c39
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-01-20 21:26:19 +0000

    new stdio

--- lib/libc/stdio/putchar.c
+++ lib/libc/stdio/putchar.c
@@ -1,16 +1,28 @@
-#if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)putchar.c	5.2 (Berkeley) 03/09/86";
-#endif LIBC_SCCS and not lint
-
-/*
- * A subroutine version of the macro putchar
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Chris Torek.
+ *
+ * %sccs.include.redist.c%
  */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+static char sccsid[] = "@(#)putchar.c	5.3 (Berkeley) 01/20/91";
+#endif /* LIBC_SCCS and not lint */
+
 #include <stdio.h>
 
 #undef putchar
 
+/*
+ * A subroutine version of the macro putchar
+ */
 putchar(c)
-register c;
+	int c;
 {
-	putc(c, stdout);
+	register FILE *so = stdout;
+
+	return (__sputc(c, so));
 }
