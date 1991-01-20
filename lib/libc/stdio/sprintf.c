ff51ad61e5391a438cc1e71ad523e115a0a87f77
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-01-20 21:30:14 +0000

    new stdio

--- lib/libc/stdio/sprintf.c
+++ lib/libc/stdio/sprintf.c
@@ -1,27 +1,49 @@
-/*
- * Copyright (c) 1987 Regents of the University of California.
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
  * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Chris Torek.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)sprintf.c	5.6 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)sprintf.c	5.7 (Berkeley) 01/20/91";
 #endif /* LIBC_SCCS and not lint */
 
 #include <stdio.h>
+#if __STDC__
+#include <stdarg.h>
+#else
+#include <varargs.h>
+#endif
+#include <limits.h>
+#include "local.h"
 
-sprintf(str, fmt, args)
-	char *str, *fmt;
-	int args;
+#if __STDC__
+sprintf(char *str, char const *fmt, ...)
+#else
+sprintf(str, fmt, va_alist)
+	char *str;
+	char *fmt;
+	va_dcl
+#endif
 {
-	FILE _strbuf;
-	int len;
+	int ret;
+	va_list ap;
+	FILE f;
 
-	_strbuf._flag = _IOWRT+_IOSTRG;
-	_strbuf._ptr = str;
-	_strbuf._cnt = 32767;
-	len = _doprnt(fmt, &args, &_strbuf);
-	*_strbuf._ptr = 0;
-	return(len);
+	f._flags = __SWR | __SSTR;
+	f._bf._base = f._p = (unsigned char *)str;
+	f._bf._size = f._w = INT_MAX;
+#if __STDC__
+	va_start(ap, fmt);
+#else
+	va_start(ap);
+#endif
+	ret = vfprintf(&f, fmt, ap);
+	va_end(ap);
+	*f._p = 0;
+	return (ret);
 }
