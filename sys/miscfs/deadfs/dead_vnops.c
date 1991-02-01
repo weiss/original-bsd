e5a1a2432c3927d80dc0a33f99f2fed5c3a4aa9c
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1991-02-01 16:21:34 +0000

    add POSIX-style byte-level record locking

--- sys/miscfs/deadfs/dead_vnops.c
+++ sys/miscfs/deadfs/dead_vnops.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)dead_vnops.c	7.11 (Berkeley) 08/24/90
+ *	@(#)dead_vnops.c	7.12 (Berkeley) 02/01/91
  */
 
 #include "param.h"
@@ -61,6 +61,7 @@ struct vnodeops dead_vnodeops = {
 	dead_strategy,	/* strategy */
 	dead_print,	/* print */
 	dead_nullop,	/* islocked */
+	dead_ebadf,	/* advlock */
 };
 
 /*
