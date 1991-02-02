24b3ac300290d0548d2847437ff55ce8a2547c2e
torek <torek@65305748-3e16-e211-a854-8c89a5c6f760>
1991-02-02 18:18:02 +0000

    <sys/file.h> => <sys/types.h> + <fcntl.h>

--- usr.bin/tip/remcap.c
+++ usr.bin/tip/remcap.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)remcap.c	5.4 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)remcap.c	5.5 (Berkeley) 02/02/91";
 #endif /* not lint */
 
 /*
@@ -14,7 +14,8 @@ static char sccsid[] = "@(#)remcap.c	5.4 (Berkeley) 06/01/90";
  *
  * derived from termcap
  */
-#include <sys/file.h>
+#include <sys/types.h>
+#include <fcntl.h>
 #include <ctype.h>
 #include "pathnames.h"
 
