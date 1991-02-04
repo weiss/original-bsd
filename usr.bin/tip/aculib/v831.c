d8e3ca06583192ab5f1f5ebc1f25a8d970d48779
torek <torek@65305748-3e16-e211-a854-8c89a5c6f760>
1991-02-04 22:50:35 +0000

    tip.h now includes sys/time.h for us

--- usr.bin/tip/aculib/v831.c
+++ usr.bin/tip/aculib/v831.c
@@ -6,14 +6,12 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)v831.c	5.3 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)v831.c	5.4 (Berkeley) 02/04/91";
 #endif /* not lint */
 
 /*
  * Routines for dialing up on Vadic 831
  */
-#include <sys/time.h>
-
 #include "tip.h"
 
 int	v831_abort();
