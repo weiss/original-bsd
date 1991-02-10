2bccc615f2a9d1212ed65ccc24ecd372ddfa3094
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-02-10 18:07:49 +0000

    passwd -> chpass

--- usr.bin/chpass/pathnames.h
+++ usr.bin/chpass/pathnames.h
@@ -4,10 +4,10 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.3 (Berkeley) 06/01/90
+ *	@(#)pathnames.h	5.4 (Berkeley) 02/10/91
  */
 
 #include <paths.h>
 
 #undef _PATH_TMP
-#define	_PATH_TMP	"/tmp/passwd.XXXXXX"
+#define	_PATH_TMP	"/tmp/chpass.XXXXXX"
