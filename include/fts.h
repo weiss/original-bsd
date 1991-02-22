06b4dfc48525ed3a0893d3edcb9ff3638f9aa5e2
donn <donn@65305748-3e16-e211-a854-8c89a5c6f760>
1991-02-22 19:50:29 +0000

    const char ** => char * const *

--- include/fts.h
+++ include/fts.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)fts.h	5.10 (Berkeley) 02/12/91
+ *	@(#)fts.h	5.11 (Berkeley) 02/22/91
  */
 
 typedef struct {
@@ -69,7 +69,7 @@ __BEGIN_DECLS
 FTSENT	*fts_children __P((FTS *));
 int	 fts_close __P((FTS *));
 FTS	*fts_open
-	    __P((const char **, int, int (*)(const FTSENT *, const FTSENT *)));
+	    __P((char * const *, int, int (*)(const FTSENT *, const FTSENT *)));
 FTSENT	*fts_read __P((FTS *));
 int	 fts_set __P((FTS *, FTSENT *, int));
 __END_DECLS
