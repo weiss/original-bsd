5ba59b7e786e1c7f2648bd2060ede2fcbf7856c1
donn <donn@65305748-3e16-e211-a854-8c89a5c6f760>
1991-02-23 09:45:44 +0000

    Changes to accommodate prototype declarations.

--- lib/libc/gen/exec.c
+++ lib/libc/gen/exec.c
@@ -1,5 +1,5 @@
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)exec.c	5.3 (Berkeley) 04/24/88";
+static char sccsid[] = "@(#)exec.c	5.4 (Berkeley) 02/23/91";
 #endif LIBC_SCCS and not lint
 
 /*
@@ -7,20 +7,23 @@ static char sccsid[] = "@(#)exec.c	5.3 (Berkeley) 04/24/88";
  *	execvp(name, argv)	(like execv, but does path search)
  */
 #include <errno.h>
-#define	NULL	0
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 static	char shell[] =	"/bin/sh";
-char	*getenv();
-extern	errno;
 
+int
 execlp(name, argv)
-char *name, *argv;
+const char *name, *argv;
 {
 	return(execvp(name, &argv));
 }
 
+int
 execvp(name, argv)
-char *name, **argv;
+const char *name;
+char * const *argv;
 {
 	char *pathstr;
 	register char *cp;
