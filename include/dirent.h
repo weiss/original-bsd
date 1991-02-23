570af81e7cd2ccbb5e9679f5eb138ea2c6d6d9dd
donn <donn@65305748-3e16-e211-a854-8c89a5c6f760>
1991-02-23 18:52:51 +0000

    Adjust the type of scandir's function argument.

--- include/dirent.h
+++ include/dirent.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)dirent.h	5.17 (Berkeley) 02/22/91
+ *	@(#)dirent.h	5.18 (Berkeley) 02/23/91
  */
 
 #ifndef _DIRENT_H_
@@ -70,7 +70,7 @@ int closedir __P((DIR *));
 long telldir __P((const DIR *));
 void seekdir __P((DIR *, long));
 int scandir __P((const char *, struct dirent ***,
-    int (*)(struct dirent *), int (*)(void *, void *)));
+    int (*)(struct dirent *), int (*)(const void *, const void *)));
 int alphasort __P((const void *, const void *));
 int getdirentries __P((int, char *, int, long *));
 #endif /* not POSIX */
