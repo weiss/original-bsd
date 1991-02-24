efc267e5f8f42523421e98f63f17c267ded50c63
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-02-24 14:08:55 +0000

    put "." at the end of the default path

--- include/paths.h
+++ include/paths.h
@@ -4,10 +4,10 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)paths.h	5.11 (Berkeley) 02/23/91
+ *	@(#)paths.h	5.12 (Berkeley) 02/24/91
  */
 
-#define	_PATH_DEFPATH	":/bin:/usr/bin"
+#define	_PATH_DEFPATH	"/bin:/usr/bin:"
 
 #define	_PATH_BSHELL	"/bin/sh"
 #define	_PATH_CONSOLE	"/dev/console"
