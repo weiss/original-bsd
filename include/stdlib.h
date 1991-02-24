1219422aee7a919be41cbb6f783e7a7f911d5dcf
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-02-24 14:17:23 +0000

    include sys/types.h, no longer need to typedef size_t typo in RAND_MAX define

--- include/stdlib.h
+++ include/stdlib.h
@@ -4,17 +4,12 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)stdlib.h	5.9 (Berkeley) 02/22/91
+ *	@(#)stdlib.h	5.10 (Berkeley) 02/24/91
  */
 
 #ifndef _STDLIB_H_
 #define _STDLIB_H_
-#include <machine/types.h>
-
-#ifdef	_SIZE_T_
-typedef	_SIZE_T_	size_t;
-#undef	_SIZE_T_
-#endif
+#include <sys/types.h>
 
 #ifdef	_WCHAR_T_
 typedef	_WCHAR_T_	wchar_t;
@@ -33,7 +28,7 @@ typedef struct {
 #define	EXIT_FAILURE	1
 #define	EXIT_SUCCESS	0
 
-#define	RAND_MAX	0x7ffffffff
+#define	RAND_MAX	0x7fffffff
 
 #define	MB_CUR_MAX	1	/* XXX */
 
