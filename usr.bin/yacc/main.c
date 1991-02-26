34bfd575d54355f06ea5114d9603982e86fe7ee7
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-02-26 14:58:27 +0000

    ANSI fix

--- usr.bin/yacc/main.c
+++ usr.bin/yacc/main.c
@@ -15,7 +15,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	5.3 (Berkeley) 01/20/91";
+static char sccsid[] = "@(#)main.c	5.4 (Berkeley) 02/26/91";
 #endif /* not lint */
 
 #include <signal.h>
@@ -90,6 +90,7 @@ int k;
 }
 
 
+void
 onintr()
 {
     done(1);
