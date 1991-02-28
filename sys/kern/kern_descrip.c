5d4fae20c96ce44d7946a3c497912c6c9eacb19c
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1991-02-28 15:11:11 +0000

    have to set offset for SEEK_CUR in F_GETLK too (from scooter@gene.com)

--- sys/kern/kern_descrip.c
+++ sys/kern/kern_descrip.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kern_descrip.c	7.19 (Berkeley) 02/21/91
+ *	@(#)kern_descrip.c	7.20 (Berkeley) 02/28/91
  */
 
 #include "param.h"
@@ -227,6 +227,8 @@ fcntl(p, uap, retval)
 		error = copyin((caddr_t)uap->arg, (caddr_t)&fl, sizeof (fl));
 		if (error)
 			return (error);
+		if (fl.l_whence == SEEK_CUR)
+			fl.l_start += fp->f_offset;
 		if (error = VOP_ADVLOCK(vp, p, F_GETLK, &fl, F_POSIX))
 			return (error);
 		return (copyout((caddr_t)&fl, (caddr_t)uap->arg, sizeof (fl)));
