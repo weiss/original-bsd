bc7f19712cc056c50582fb614b82de7489959ac7
dab <dab@65305748-3e16-e211-a854-8c89a5c6f760>
1991-02-28 22:01:33 +0000

    date and time created 91/02/28 22:01:33 by dab

---
+++ lib/libtelnet/misc.h
@@ -0,0 +1,16 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)misc.h	5.1 (Berkeley) 02/28/91
+ */
+
+extern char *UserNameRequested;
+extern char *LocalHostName;
+extern char *RemoteHostName;
+extern int ConnectedCount;
+extern int ReservedPort;
+
+#include "misc-proto.h"
