e2b66ddc342d88c59df30be360bcc329b22e388b
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-03-02 15:10:01 +0000

    ANSI

--- old/vfilters/rvsort/rvsort.c
+++ old/vfilters/rvsort/rvsort.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rvsort.c	5.4 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)rvsort.c	5.5 (Berkeley) 03/02/91";
 #endif /* not lint */
 
 /*
@@ -258,11 +258,6 @@ getch()
 		return(c);
 	}
 	return(getc(in));
-}
-
-ungetc(c)
-{
-	peekc = c;
 }
 
 error(s)
