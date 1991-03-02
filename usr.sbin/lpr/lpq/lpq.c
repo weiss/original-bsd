a7f812df93ca2a1c98f8c82da807a0324d9aa8f2
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-03-02 15:02:42 +0000

    ANSI

--- usr.sbin/lpr/lpq/lpq.c
+++ usr.sbin/lpr/lpq/lpq.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)lpq.c	5.6 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)lpq.c	5.7 (Berkeley) 03/02/91";
 #endif /* not lint */
 
 /*
@@ -79,7 +79,6 @@ main(argc, argv)
 	exit(0);
 }
 
-static
 usage()
 {
 	puts("usage: lpq [-l] [-Pprinter] [user ...] [job ...]");
