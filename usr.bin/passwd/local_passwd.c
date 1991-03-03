9d7e4cf0387e7b6ea7b75ade0b90dd98c4bf7454
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-03-03 16:08:05 +0000

    redo the pw_copy calling convention

--- usr.bin/passwd/local_passwd.c
+++ usr.bin/passwd/local_passwd.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)local_passwd.c	5.1 (Berkeley) 02/12/91";
+static char sccsid[] = "@(#)local_passwd.c	5.2 (Berkeley) 03/03/91";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -48,7 +48,7 @@ local_passwd(uname)
 	 */
 	pw->pw_passwd = getnewpasswd(pw);
 	pw->pw_change = 0;
-	pw_copy(pfd, pw);
+	pw_copy(pfd, tfd, pw);
 
 	if (!pw_mkdb())
 		pw_error((char *)NULL, 0, 1);
