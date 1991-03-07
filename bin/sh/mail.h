86f518c73f468f5c249be7442975dc923741b5c1
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-03-07 20:27:38 +0000

    date and time created 91/03/07 20:27:38 by bostic

---
+++ bin/sh/mail.h
@@ -0,0 +1,17 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Kenneth Almquist.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)mail.h	5.1 (Berkeley) 03/07/91
+ */
+
+#ifdef __STDC__
+void chkmail(int);
+#else
+void chkmail();
+#endif
