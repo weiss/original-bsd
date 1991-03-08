4a367439ed9885845ba0ce0fb1cd62d3abaac402
donn <donn@65305748-3e16-e211-a854-8c89a5c6f760>
1991-03-08 11:54:10 +0000

    Don't permit '$' in identifiers, for the sake of the 386 assembler.

--- usr.bin/cpp/cpp.sh
+++ usr.bin/cpp/cpp.sh
@@ -9,14 +9,14 @@
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)cpp.sh	6.1 (Berkeley) 02/01/91
+#	@(#)cpp.sh	6.2 (Berkeley) 03/08/91
 #
 # Transitional front end to CCCP to make it behave like (Reiser) CCP:
 #	specifies -traditional
 #	doesn't search gcc-include
 #
 cpp="eval exec /usr/libexec/cpp"
-ALST="-traditional -D__GNUC__"
+ALST="-traditional -D__GNUC__ -$ "
 NSI=no
 ARGS=""
 INCS=""
