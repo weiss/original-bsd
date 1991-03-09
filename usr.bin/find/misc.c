01ce0332d94a3c3cfbbab16254fc65478e47ab59
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-03-09 18:44:03 +0000

    ANSI

--- usr.bin/find/misc.c
+++ usr.bin/find/misc.c
@@ -9,16 +9,16 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)misc.c	5.5 (Berkeley) 11/15/90";
+static char sccsid[] = "@(#)misc.c	5.6 (Berkeley) 03/09/91";
 #endif /* not lint */
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/errno.h>
 #include <stdio.h>
-#include "find.h"
 #include <stdlib.h>
 #include <string.h>
+#include "find.h"
  
 /*
  * brace_subst --
