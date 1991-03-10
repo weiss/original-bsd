8d0e61ecaca07e7fed1293d3e8cf47415b0b2b5c
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-03-10 15:36:29 +0000

    SKIP -> skipobj

--- usr.bin/ar/contents.c
+++ usr.bin/ar/contents.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)contents.c	5.2 (Berkeley) 01/21/91";
+static char sccsid[] = "@(#)contents.c	5.3 (Berkeley) 03/10/91";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -56,7 +56,7 @@ contents(argv)
 			if (!all && !*argv)
 				break;
 		}
-		SKIP(afd, chdr.size, archive);
+		skipobj(afd);
 	} 
 	eval = 0;
 	ORPHANS;
