dbe107591ec76f650623a8ce5cc2a1c5050e802b
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-03-14 16:24:12 +0000

    forgot to free at one point

--- bin/csh/glob.c
+++ bin/csh/glob.c
@@ -5,7 +5,7 @@
  */
 
 #ifndef lint
-static char *sccsid = "@(#)glob.c	5.7 (Berkeley) 03/14/91";
+static char *sccsid = "@(#)glob.c	5.8 (Berkeley) 03/14/91";
 #endif
 
 #include "sh.h"
@@ -380,6 +380,8 @@ globall(v)
 			gappend = GLOB_APPEND;
 		}
 		while (*++vl);
+		if (gflag & G_CSH)
+			blkfree(vo);
 		if (globv.gl_pathc)
 			vl = saveblk(globv.gl_pathv);
 		else
