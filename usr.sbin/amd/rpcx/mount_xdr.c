ccb54f2ace0aad76f43fb67d65964b0e9b8b105b
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1991-03-17 13:46:16 +0000

    from amd5.3 alpha11

--- usr.sbin/amd/rpcx/mount_xdr.c
+++ usr.sbin/amd/rpcx/mount_xdr.c
@@ -1,5 +1,5 @@
 /*
- * $Id: mount_xdr.c,v 5.2 90/06/23 22:20:21 jsp Rel $
+ * $Id: mount_xdr.c,v 5.2.1.1 90/10/21 22:30:21 jsp Exp $
  *
  * Copyright (c) 1989 Jan-Simon Pendry
  * Copyright (c) 1989 Imperial College of Science, Technology & Medicine
@@ -11,7 +11,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mount_xdr.c	5.1 (Berkeley) 07/19/90
+ *	@(#)mount_xdr.c	5.2 (Berkeley) 03/17/91
  */
 
 #include "am.h"
--- usr.sbin/amd/rpcx/nfs_prot_svc.c
+++ usr.sbin/amd/rpcx/nfs_prot_svc.c
@@ -1,5 +1,5 @@
 /*
- * $Id: nfs_prot_svc.c,v 5.2 90/06/23 22:20:25 jsp Rel $
+ * $Id: nfs_prot_svc.c,v 5.2.1.1 90/10/21 22:30:25 jsp Exp $
  *
  * Copyright (c) 1989 Jan-Simon Pendry
  * Copyright (c) 1989 Imperial College of Science, Technology & Medicine
@@ -11,7 +11,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)nfs_prot_svc.c	5.1 (Berkeley) 07/19/90
+ *	@(#)nfs_prot_svc.c	5.2 (Berkeley) 03/17/91
  */
 
 #include "am.h"
