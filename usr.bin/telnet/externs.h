82160b82c2c3f79e9c5e6ad309119eff0c476648
dab <dab@65305748-3e16-e211-a854-8c89a5c6f760>
1991-03-22 13:09:18 +0000

    Upgrade to the latest & greatest authentication/encryption

--- usr.bin/telnet/externs.h
+++ usr.bin/telnet/externs.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)externs.h	5.2 (Berkeley) 03/01/91
+ *	@(#)externs.h	5.3 (Berkeley) 03/22/91
  */
 
 #ifndef	BSD
@@ -70,6 +70,7 @@ extern int errno;		/* outside this world */
 
 extern int
     autologin,		/* Autologin enabled */
+    skiprc,		/* Don't process the ~/.telnetrc file */
     eight,		/* use eight bit mode (binary in and/or out */
     flushout,		/* flush output */
     connected,		/* Are we connected to the other side? */
