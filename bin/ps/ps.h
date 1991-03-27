0cd3261c1ca5f2fea7880b55208754381cccdc14
marc <marc@65305748-3e16-e211-a854-8c89a5c6f760>
1991-03-27 15:49:48 +0000

    its \not/ an error to specify a keyword twice

--- bin/ps/ps.h
+++ bin/ps/ps.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ps.h	5.1 (Berkeley) 02/08/91
+ *	@(#)ps.h	5.2 (Berkeley) 03/27/91
  */
 
 #define	UNLIMITED	0	/* unlimited terminal width */
@@ -49,7 +49,12 @@ typedef struct _var {
 	/*
 	 * glue to link selected fields together
 	 */
-	struct _var *next;
 } VAR;
 
-extern VAR var[], *vhead, *vtail;
+struct varent {
+	VAR *var;
+	struct varent *next;
+};
+
+extern VAR var[];
+extern struct varent *vhead, *vtail;
