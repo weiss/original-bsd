9de0275e3db8d0e9e8b201fd7046bfc211481e16
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-01 08:59:23 +0000

    ANSI

--- usr.bin/mail/popen.c
+++ usr.bin/mail/popen.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)popen.c	5.15 (Berkeley) 06/25/90";
+static char sccsid[] = "@(#)popen.c	5.16 (Berkeley) 04/01/91";
 #endif /* not lint */
 
 #include "rcv.h"
@@ -258,13 +258,15 @@ delchild(cp)
 	free((char *) cp);
 }
 
+void
 sigchild()
 {
 	int pid;
 	union wait status;
 	register struct child *cp;
 
-	while ((pid = wait3(&status, WNOHANG, (struct timeval *)0)) > 0) {
+	while ((pid =
+	    wait3((int *)&status, WNOHANG, (struct rusage *)0)) > 0) {
 		cp = findchild(pid);
 		if (cp->free)
 			delchild(cp);
