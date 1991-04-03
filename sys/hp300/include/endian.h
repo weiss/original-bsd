5d0f4898fd40cfe49191e825ce3de77e46afb365
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-03 17:14:28 +0000

    don't make machine type specific

--- sys/hp300/include/endian.h
+++ sys/hp300/include/endian.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)endian.h	7.5 (Berkeley) 06/28/90
+ *	@(#)endian.h	7.6 (Berkeley) 04/03/91
  */
 
 /*
@@ -15,7 +15,7 @@
 #define	BIG_ENDIAN	4321	/* most-significant byte first (IBM, net) */
 #define	PDP_ENDIAN	3412	/* LSB first in word, MSW first in long (pdp) */
 
-#define	BYTE_ORDER	BIG_ENDIAN	/* byte order on tahoe */
+#define	BYTE_ORDER	BIG_ENDIAN
 
 /*
  * Macros for network/external number representation conversion.
