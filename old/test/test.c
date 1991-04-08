fef403a85fa2315225fcc4c38dfc4d5e176f097f
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-08 16:46:06 +0000

    new copyright; att/bsd/shared

--- old/test/test.c
+++ old/test/test.c
@@ -1,6 +1,19 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
+ */
+
 #ifndef lint
-static char *sccsid = "@(#)test.c	4.2 (Berkeley) 05/11/86";
-#endif
+char copyright[] =
+"@(#) Copyright (c) 1991 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)test.c	5.1 (Berkeley) 04/08/91";
+#endif /* not lint */
 
 /*
  *	test expression
