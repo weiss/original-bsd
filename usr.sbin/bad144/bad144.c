5c9184fd881ecfc8970cded14e9208f478b913ce
sklower <sklower@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-11 11:15:02 +0000

    machine/dkio.h is extraneous

--- usr.sbin/bad144/bad144.c
+++ usr.sbin/bad144/bad144.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif not lint
 
 #ifndef lint
-static char sccsid[] = "@(#)bad144.c	5.18 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)bad144.c	5.19 (Berkeley) 04/11/91";
 #endif not lint
 
 /*
@@ -480,7 +480,6 @@ register struct bt_bad *bt;
 }
 
 #ifdef vax
-#include <machine/dkio.h>
 
 struct rp06hdr {
 	short	h_cyl;
