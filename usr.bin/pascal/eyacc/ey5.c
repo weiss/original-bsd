704299f3eebeafe892b28e78677c478736192816
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-12 16:47:21 +0000

    new copyright; att/bsd/shared

--- usr.bin/pascal/eyacc/ey5.c
+++ usr.bin/pascal/eyacc/ey5.c
@@ -1,12 +1,13 @@
-/*
- * Copyright (c) 1979 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1979 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)ey5.c	5.2 (Berkeley) 09/22/88";
-#endif not lint
+static char sccsid[] = "@(#)ey5.c	5.3 (Berkeley) 04/12/91";
+#endif /* not lint */
 
 /* fake portable I/O routines, for those
     sites so backward as to not have the
