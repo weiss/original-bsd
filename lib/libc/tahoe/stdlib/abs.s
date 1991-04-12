6ce252dc16795a2597e7d657ef1ba07f57719b57
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-12 12:56:31 +0000

    yank to rev 5.1

--- lib/libc/tahoe/stdlib/abs.s
+++ lib/libc/tahoe/stdlib/abs.s
@@ -9,7 +9,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)abs.s	1.4 (Berkeley) 06/01/90"
+	.asciz "@(#)abs.s	5.1 (Berkeley) 04/12/91"
 #endif /* LIBC_SCCS and not lint */
 
 /* abs - int absolute value */
