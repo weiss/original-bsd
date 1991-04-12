5be0f76f3cc232df659026078c12b8c35c95a565
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-12 16:09:21 +0000

    new copyright; att/bsd/shared

--- usr.bin/f77/pass1.vax/format.h
+++ usr.bin/f77/pass1.vax/format.h
@@ -1,9 +1,10 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)format.h	5.1 (Berkeley) 06/07/85
+ * %sccs.include.proprietary.c%
+ *
+ *	@(#)format.h	5.2 (Berkeley) 04/12/91
  */
 
 /*
--- usr.bin/f77/pass1.vax/pccdefs.h
+++ usr.bin/f77/pass1.vax/pccdefs.h
@@ -1,11 +1,14 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)pccdefs.h	5.1 (Berkeley) 06/07/85
+ * %sccs.include.proprietary.c%
+ *
+ *	@(#)pccdefs.h	5.2 (Berkeley) 04/12/91
  */
 
+#define P2BAD -1
+#define P2BAD -1
 #define P2BAD -1
 #define P2NAME 2
 #define P2ICON 4
