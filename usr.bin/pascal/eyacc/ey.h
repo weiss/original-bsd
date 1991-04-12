e59ef7aeda9e2544f1a5e38d617dcc89a199753d
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-12 16:47:47 +0000

    new copyright; att/bsd/shared

--- usr.bin/pascal/eyacc/ey.h
+++ usr.bin/pascal/eyacc/ey.h
@@ -1,9 +1,10 @@
-/*
- * Copyright (c) 1979 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1979 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)ey.h	5.2 (Berkeley) 09/22/88
+ * %sccs.include.proprietary.c%
+ *
+ *	@(#)ey.h	5.3 (Berkeley) 04/12/91
  */
 
 #include <stdio.h>
