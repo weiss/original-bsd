9189691cfbfe924dda4fdf27c042aefbb14ea061
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-12 14:33:42 +0000

    new copyright; att/bsd/shared

--- usr.bin/f77/f77.vax/f77.c
+++ usr.bin/f77/f77.vax/f77.c
@@ -1,12 +1,19 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1985 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static	char sccsid[] = "@(#)f77.c	5.7 (Berkeley) 04/04/90";
-#endif
+char copyright[] =
+"@(#) Copyright (c) 1985 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)f77.c	5.8 (Berkeley) 04/12/91";
+#endif /* not lint */
 
 /*
  * f77.c
