bc19982fbc3dc4af1df0a35a93870b7629390ab6
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-16 15:56:39 +0000

    new copyright; att/bsd/shared

--- usr.bin/pascal/pxp/yymain.c
+++ usr.bin/pascal/pxp/yymain.c
@@ -1,12 +1,13 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)yymain.c	5.3 (Berkeley) 12/04/87";
-#endif not lint
+static char sccsid[] = "@(#)yymain.c	5.4 (Berkeley) 04/16/91";
+#endif /* not lint */
 
 /*
  * pi - Pascal interpreter code translator
