0ade382785b87c87fa233d11e4cd3b122ddf4b5a
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-16 14:50:00 +0000

    new copyright; att/bsd/shared

--- usr.bin/pascal/pdx/vax/cerror.s
+++ usr.bin/pascal/pdx/vax/cerror.s
@@ -1,10 +1,11 @@
+# Copyright (c) 1982 The Regents of the University of California.
+# All rights reserved.
 #
-# Copyright (c) 1982 Regents of the University of California.
-# All rights reserved.  The Berkeley software License Agreement
-# specifies the terms and conditions for redistribution.
+# %sccs.include.redist.sh%
 #
-#	@(#)cerror.s	5.1 (Berkeley) 06/07/85
+#	@(#)cerror.s	5.2 (Berkeley) 04/16/91
 #
+
 # modified version of cerror
 #
 # The idea is that every time an error occurs in a system call
