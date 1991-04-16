799f22f86de9ae6b68cb9838f6feddaa3fd3c324
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-16 15:32:02 +0000

    new copyright; att/bsd/shared

--- usr.bin/pascal/pdx/machine/optab.c
+++ usr.bin/pascal/pdx/machine/optab.c
@@ -1,12 +1,13 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)optab.c	5.2 (Berkeley) 09/30/87";
-#endif not lint
+static char sccsid[] = "@(#)optab.c	5.3 (Berkeley) 04/16/91";
+#endif /* not lint */
 
 /*
  * px opcode table
--- usr.bin/pascal/pdx/machine/pxerrors.h
+++ usr.bin/pascal/pdx/machine/pxerrors.h
@@ -1,9 +1,10 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)pxerrors.h	5.2 (Berkeley) 04/07/87
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pxerrors.h	5.3 (Berkeley) 04/16/91
  */
 
 #define ESIGNAL		SIGTRAP
