c2c76ab533876c2ba572cd93af5835f93b1adde1
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-16 14:39:04 +0000

    Pascal is Berkeley code, add Berkeley specific copyright

--- usr.bin/pascal/pc3/pc3.h
+++ usr.bin/pascal/pc3/pc3.h
@@ -1,12 +1,11 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980, 1982, 1983 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)pc3.h	5.1 (Berkeley) 06/05/85
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pc3.h	5.2 (Berkeley) 04/16/91
  */
-
-    /*	static	char sccsid[] = "@(#)pc3.h 5.1 06/05/85"; */
 
     /*
      *	a symbol table entry.
