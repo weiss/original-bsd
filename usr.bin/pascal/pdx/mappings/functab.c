e12637f97957e4693cc28e1f51ab4f76a8d8fa82
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-16 15:26:26 +0000

    new copyright; att/bsd/shared

--- usr.bin/pascal/pdx/mappings/functab.c
+++ usr.bin/pascal/pdx/mappings/functab.c
@@ -1,12 +1,14 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)functab.c	5.2 (Berkeley) 04/06/87";
-#endif not lint
+static char sccsid[] = "@(#)functab.c	5.3 (Berkeley) 04/16/91";
+#endif /* not lint */
+
 /*
  * This file contains the implementation of a table for going
  * from object addresses to the functions in which they belong.
--- usr.bin/pascal/pdx/mappings/srcline.c
+++ usr.bin/pascal/pdx/mappings/srcline.c
@@ -1,12 +1,14 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)srcline.c	5.1 (Berkeley) 06/06/85";
-#endif not lint
+static char sccsid[] = "@(#)srcline.c	5.2 (Berkeley) 04/16/91";
+#endif /* not lint */
+
 /*
  * lookup the source line number nearest from below to an address
  */
