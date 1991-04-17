396bcbc20d3b105c4efd94f72256305dba9ad14f
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-17 17:12:33 +0000

    new copyright; att/bsd/shared

--- usr.bin/diction/diction/diction.sh
+++ usr.bin/diction/diction/diction.sh
@@ -1,7 +1,10 @@
-#! /bin/sh
+#!/bin/sh -
 #
-#	@(#)diction.sh	4.7	(Berkeley)	89/08/31
+# %sccs.include.proprietary.sh%
 #
+#	@(#)diction.sh	4.8 (Berkeley) 04/17/91
+#
+
 D=/usr/bin
 B=/usr/libexec
 echo $*
