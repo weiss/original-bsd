09f313baf040025275c7f77e5875cfb8066dea95
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-18 16:41:08 +0000

    new copyright; att/bsd/shared

--- old/refer/mkey/mkey2.c
+++ old/refer/mkey/mkey2.c
@@ -1,6 +1,10 @@
+/*-
+ * %sccs.include.proprietary.c%
+ */
+
 #ifndef lint
-static char *sccsid = "@(#)mkey2.c	4.2 (Berkeley) 04/24/88";
-#endif
+static char sccsid[] = "@(#)mkey2.c	4.3 (Berkeley) 04/18/91";
+#endif /* not lint */
 
 #include <stdio.h>
 #include <ctype.h>
