1dc204cfe648f243c6b4bc143139a76f5d55a206
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-18 18:09:24 +0000

    new copyright; att/bsd/shared

--- usr.bin/spline/spline.c
+++ usr.bin/spline/spline.c
@@ -1,6 +1,19 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
+ */
+
 #ifndef lint
-static char *sccsid = "@(#)spline.c	4.6 (Berkeley) 09/24/90";
-#endif
+char copyright[] =
+"@(#) Copyright (c) 1991 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)spline.c	4.7 (Berkeley) 04/18/91";
+#endif /* not lint */
 
 #include <stdio.h>
 #include <math.h>
