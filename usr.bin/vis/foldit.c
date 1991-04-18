c62162bb59ffbd194896bb10b0d9b27532cf8c48
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-18 18:41:31 +0000

    yank to 5.1, new copyright; att/bsd/shared

--- usr.bin/vis/foldit.c
+++ usr.bin/vis/foldit.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)foldit.c	1.1 (Berkeley) 05/23/90";
+static char sccsid[] = "@(#)foldit.c	5.1 (Berkeley) 04/18/91";
 #endif /* not lint */
 
 foldit(chunk, col, max)
