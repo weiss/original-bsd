b887afb54b304206d28a413017b4842232dc5c7a
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-19 13:02:21 +0000

    new copyright; att/bsd/shared

--- usr.sbin/config/mkubglue.c
+++ usr.sbin/config/mkubglue.c
@@ -1,12 +1,13 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)mkubglue.c	5.2 (Berkeley) 12/02/90";
-#endif not lint
+static char sccsid[] = "@(#)mkubglue.c	5.3 (Berkeley) 04/19/91";
+#endif /* not lint */
 
 /*
  * Make the uba interrupt file ubglue.s
