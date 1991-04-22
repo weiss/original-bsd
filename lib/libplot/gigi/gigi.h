f59b84313d59e1ebdfec1f2ee27a707ebb8b69a5
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-22 15:05:33 +0000

    new copyright; att/bsd/shared

--- lib/libplot/gigi/gigi.h
+++ lib/libplot/gigi/gigi.h
@@ -1,9 +1,10 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)gigi.h	5.1 (Berkeley) 05/07/85
+ * %sccs.include.proprietary.c%
+ *
+ *	@(#)gigi.h	5.2 (Berkeley) 04/22/91
  */
 
 /*
