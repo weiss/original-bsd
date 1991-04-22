466d507c7168e9a23c83ff77562bdbd670c8cfb8
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-22 14:57:16 +0000

    new copyright; att/bsd/shared

--- lib/libplot/bitgraph/bg.h
+++ lib/libplot/bitgraph/bg.h
@@ -1,9 +1,10 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)bg.h	5.1 (Berkeley) 04/30/85
+ * %sccs.include.proprietary.c%
+ *
+ *	@(#)bg.h	5.2 (Berkeley) 04/22/91
  */
 
 /*
