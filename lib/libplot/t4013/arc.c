9d3248a062d4f0c719e777e95412736528e86505
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-22 16:03:44 +0000

    new copyright; att/bsd/shared

--- lib/libplot/t4013/arc.c
+++ lib/libplot/t4013/arc.c
@@ -1,12 +1,13 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1985 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)arc.c	5.2 (Berkeley) 06/23/85";
-#endif not lint
+static char sccsid[] = "@(#)arc.c	5.3 (Berkeley) 04/22/91";
+#endif /* not lint */
 
 int del = 20;
 step(d){
