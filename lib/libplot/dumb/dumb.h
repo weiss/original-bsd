429e11f21b9ac6f916f1ef1199d2e6deab2a24c1
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-22 14:59:39 +0000

    new copyright; att/bsd/shared

--- lib/libplot/dumb/dumb.h
+++ lib/libplot/dumb/dumb.h
@@ -1,11 +1,13 @@
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
+ *
+ *	@(#)dumb.h	5.2 (Berkeley) 04/22/91
+ */
+
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
- *
- *	@(#)dumb.h	5.1 (Berkeley) 05/07/85
- *
- *
  * This accepts plot file formats and produces the appropriate plots
  * for dumb terminals.  It can also be used for printing terminals and
  * lineprinter listings, although there is no way to specify number of
