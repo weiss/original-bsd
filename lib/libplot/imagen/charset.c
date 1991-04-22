853c0a52fbe475a420b37e7a05f645fbd394516c
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-22 15:55:22 +0000

    new copyright; att/bsd/shared

--- lib/libplot/imagen/charset.c
+++ lib/libplot/imagen/charset.c
@@ -1,12 +1,13 @@
-/*
- * Copyright (c) 1985 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1985 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)charset.c	1.1 (Berkeley) 09/21/85";
-#endif not lint
+static char sccsid[] = "@(#)charset.c	1.2 (Berkeley) 04/22/91";
+#endif /* not lint */
 
 #include "imp.h"
 char *imP_charset = "cour07";
--- lib/libplot/imagen/close.c
+++ lib/libplot/imagen/close.c
@@ -1,12 +1,13 @@
-/*
- * Copyright (c) 1985 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1985 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)close.c	5.1 (Berkeley) 09/21/85";
-#endif not lint
+static char sccsid[] = "@(#)close.c	5.2 (Berkeley) 04/22/91";
+#endif /* not lint */
 
 #include "imp.h"
 #include "imPcodes.h"
