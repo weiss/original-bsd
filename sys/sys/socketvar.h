f600dd0913d44dc85a173dedcc767035221c12bd
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-23 18:18:02 +0000

    needed struct ucred and uio; applications include this

--- sys/sys/socketvar.h
+++ sys/sys/socketvar.h
@@ -16,7 +16,7 @@
  * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)socketvar.h	7.14 (Berkeley) 04/15/91
+ *	@(#)socketvar.h	7.15 (Berkeley) 04/23/91
  */
 
 /*
@@ -179,6 +179,8 @@ extern	char netio[], netcon[], netcls[];
  * Operations on sockets.
  */
 struct	file;
+struct	ucred;
+struct	uio;
 int	soo_read __P((			/* read a socket into a uio structure */
 		struct file *fp,
 		struct uio *uio,
