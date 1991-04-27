4d3d17a1deb213d2c62d000d929c70090e6e304a
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-27 16:34:12 +0000

    the .c's are really .h's

--- usr.bin/diction/style2/edict.c
+++ usr.bin/diction/style2/edict.c
@@ -1,10 +1,8 @@
 /*-
  * %sccs.include.proprietary.c%
+ *
+ *	@(#)edict.c	4.4 (Berkeley) 04/27/91
  */
-
-#ifndef lint
-static char sccsid[] = "@(#)edict.c	4.3 (Berkeley) 04/17/91";
-#endif /* not lint */
 
 struct dict able_d[] = {
 "noihsaf",'Y',
