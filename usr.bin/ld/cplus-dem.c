d359e8cac8788f0e0445b23699d0928673c8cdd1
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-04-30 14:30:29 +0000

    new copyright notice, specifically for GNU

--- usr.bin/ld/cplus-dem.c
+++ usr.bin/ld/cplus-dem.c
@@ -1,5 +1,12 @@
+/*-
+ * This code is derived from software copyrighted by the Free Software
+ * Foundation.
+ *
+ * Modified 1991 by Donn Seeley at UUNET Technologies, Inc.
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)cplus-dem.c	5.2 (Berkeley) 04/30/91";
+static char sccsid[] = "@(#)cplus-dem.c	5.3 (Berkeley) 04/30/91";
 #endif /* not lint */
 
 /* Demangler for GNU C++ 
--- usr.bin/ld/symseg.h
+++ usr.bin/ld/symseg.h
@@ -1,5 +1,11 @@
 /*-
- *	@(#)symseg.h	5.2 (Berkeley) 04/30/91
+ *
+ * This code is derived from software copyrighted by the Free Software
+ * Foundation.
+ *
+ * Modified 1991 by Donn Seeley at UUNET Technologies, Inc.
+ *
+ *	@(#)symseg.h	5.3 (Berkeley) 04/30/91
  */
 
 /* GDB symbol table format definitions.
