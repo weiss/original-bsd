98474ecafbde721c4c21e064561f6a6ca32fdffd
sklower <sklower@65305748-3e16-e211-a854-8c89a5c6f760>
1991-05-03 19:22:16 +0000

    minor fix to allow sendmsg with purely control data while confirming

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)uipc_socket.c	7.26 (Berkeley) 02/19/91
+ *	@(#)uipc_socket.c	7.27 (Berkeley) 05/03/91
  */
 
 #include "param.h"
@@ -315,7 +315,8 @@ restart:
 			snderr(so->so_error);
 		if ((so->so_state & SS_ISCONNECTED) == 0) {
 			if (so->so_proto->pr_flags & PR_CONNREQUIRED) {
-				if ((so->so_state & SS_ISCONFIRMING) == 0)
+				if ((so->so_state & SS_ISCONFIRMING) == 0 &&
+				    !(resid == 0 && clen != 0))
 					snderr(ENOTCONN);
 			} else if (addr == 0)
 				snderr(EDESTADDRREQ);
