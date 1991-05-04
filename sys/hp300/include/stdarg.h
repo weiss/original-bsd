3bf31f06ab118f67ead899ad65dda49bcd46a99f
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-05-04 11:02:09 +0000

    kernel has no abort() function

--- sys/hp300/include/stdarg.h
+++ sys/hp300/include/stdarg.h
@@ -4,14 +4,19 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)stdarg.h	7.1 (Berkeley) 04/03/91
+ *	@(#)stdarg.h	7.2 (Berkeley) 05/04/91
  */
 
 typedef char *va_list;
 
+#ifdef KERNEL
+#define	va_arg(ap, type) \
+	((type *)(ap += sizeof(type)))[-1]
+#else
 #define	va_arg(ap, type) \
 	((type *)(ap += sizeof(type) < sizeof(int) ? \
 		(abort(), 0) : sizeof(type)))[-1]
+#endif
 
 #define	va_end(ap)
 
