29f2c884561d1a371a12f91138a1d22d90d788f0
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-05-04 17:17:28 +0000

    new copyright; att/bsd/shared

--- sys/vax/stand/boot.c
+++ sys/vax/stand/boot.c
@@ -1,9 +1,10 @@
-/*
- * Copyright (c) 1982, 1986 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1982, 1986 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)boot.c	7.14 (Berkeley) 12/16/90
+ * %sccs.include.redist.c%
+ *
+ *	@(#)boot.c	7.15 (Berkeley) 05/04/91
  */
 
 #include "sys/param.h"
