705a23ccda2407296d9aefd7f1de19fe77338adf
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-05-05 13:31:56 +0000

    includes for standard stand source, READ, WRITE -> F_READ, FWRITE

--- sys/hp300/stand/hpib.c
+++ sys/hp300/stand/hpib.c
@@ -4,13 +4,14 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)hpib.c	7.3 (Berkeley) 12/16/90
+ *	@(#)hpib.c	7.4 (Berkeley) 05/05/91
  */
 
 /*
  * HPIB driver
  */
-#include "sys/reboot.h"
+#include <sys/param.h>
+#include <sys/reboot.h>
 #include "../dev/device.h"
 #include "hpibvar.h"
 
@@ -143,12 +144,12 @@ hpibgo(unit, sec, addr, count, flag)
 	slave = hpibslave(unit);
 	unit = hpibunit(unit);
 	if (hpib_softc[unit].sc_type == HPIBC)
-		if (flag == READ)
+		if (flag == F_READ)
 			fhpibrecv(unit, slave, sec, addr, count);
 		else
 			fhpibsend(unit, slave, sec, addr, count);
 	else
-		if (flag == READ)
+		if (flag == F_READ)
 			nhpibrecv(unit, slave, sec, addr, count);
 		else
 			nhpibsend(unit, slave, sec, addr, count);
