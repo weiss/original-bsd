c4490912d88c536bc8fefa212fd5e9e94d31ed68
karels <karels@65305748-3e16-e211-a854-8c89a5c6f760>
1991-05-06 13:00:57 +0000

    define selwait

--- sys/kern/sys_generic.c
+++ sys/kern/sys_generic.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sys_generic.c	7.28 (Berkeley) 04/16/91
+ *	@(#)sys_generic.c	7.29 (Berkeley) 05/06/91
  */
 
 #include "param.h"
@@ -447,7 +447,7 @@ ioctl(p, uap, retval)
 	return (error);
 }
 
-int	nselcoll;
+int	selwait, nselcoll;
 
 /*
  * Select system call.
