ec126aa46c65026dae664290f538d42f6dda1c7f
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-05-06 20:39:43 +0000

    new copyright; att/bsd/shared

--- lib/csu/hp300/crt0.c
+++ lib/csu/hp300/crt0.c
@@ -2,11 +2,11 @@
  * Copyright (c) 1991 The Regents of the University of California.
  * All rights reserved.
  *
- * %sccs.include.proprietary.c%
+ * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)crt0.c	5.3 (Berkeley) 04/28/91";
+static char sccsid[] = "@(#)crt0.c	5.4 (Berkeley) 05/06/91";
 #endif /* not lint */
 
 /*
