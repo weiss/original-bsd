76e182d3afa1ab277f6fe31e53180c641cc23f6a
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-05-08 14:41:14 +0000

    new copyright; att/bsd/shared

--- sys/tahoe/include/kdbparam.h
+++ sys/tahoe/include/kdbparam.h
@@ -1,9 +1,10 @@
-/*
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1988 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)kdbparam.h	7.8 (Berkeley) 05/03/90
+ * %sccs.include.proprietary.c%
+ *
+ *	@(#)kdbparam.h	7.9 (Berkeley) 05/08/91
  */
 
 #define DBNAME "kdb\n"
