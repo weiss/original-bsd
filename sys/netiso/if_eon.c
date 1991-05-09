7834b320c1904caf6685f5c218fa7fef42d44c95
sklower <sklower@65305748-3e16-e211-a854-8c89a5c6f760>
1991-05-09 23:31:49 +0000

    fix stupid fix

--- sys/netiso/if_eon.c
+++ sys/netiso/if_eon.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)if_eon.c	7.14 (Berkeley) 05/09/91
+ *	@(#)if_eon.c	7.15 (Berkeley) 05/09/91
  */
 
 /***********************************************************
@@ -288,7 +288,7 @@ register struct sockaddr *gate;
 	el->el_flags |= RTF_UP;
 	eoniphdr(&el->el_ei, ipaddrloc, &el->el_iproute, EON_NORMAL_ADDR, 0);
 	if (el->el_iproute.ro_rt)
-		rt->rt_rmx.rmx_mtu = el->el_iproute.ro_rt->rt_rmx_mtu
+		rt->rt_rmx.rmx_mtu = el->el_iproute.ro_rt->rt_rmx.rmx_mtu
 							- sizeof(el->el_ei);
 }
 
