4cc271e49c96e54b165c4b481a6a316d2ccd59c8
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-05-09 20:55:54 +0000

    new copyright; att/bsd/shared

--- sys/sys/ttydefaults.h
+++ sys/sys/ttydefaults.h
@@ -1,9 +1,10 @@
-/*
- * Copyright (c) 1982, 1986 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1982, 1986 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)ttydefaults.h	7.8 (Berkeley) 02/05/91
+ * %sccs.include.redist.c%
+ *
+ *	@(#)ttydefaults.h	7.9 (Berkeley) 05/09/91
  */
 
 /*
