9e5284e48165261ed10caade95975224ff7760d0
marc <marc@65305748-3e16-e211-a854-8c89a5c6f760>
1991-05-16 18:18:19 +0000

    pass flags from device close to l_close

--- sys/vax/uba/qd.c
+++ sys/vax/uba/qd.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)qd.c	1.17 (Berkeley) 12/16/90
+ *	@(#)qd.c	1.18 (Berkeley) 05/16/91
  */
 
 /************************************************************************
@@ -711,9 +711,10 @@ qdopen(dev, flag)
 } /* qdopen */
 
 /*ARGSUSED*/
-qdclose(dev, flag)
+qdclose(dev, flag, mode, p)
 	dev_t dev;
-	int flag;
+	int flag, mode;
+	struct proc *p;
 {
 	register struct tty *tp;
 	register struct qdmap *qd;
@@ -878,7 +879,7 @@ qdclose(dev, flag)
 		* this is the console 
 		*/
 		tp = &qd_tty[minor_dev];
-		(*linesw[tp->t_line].l_close)(tp);
+		(*linesw[tp->t_line].l_close)(tp, flag);
 		ttyclose(tp);
 		tp->t_state = 0;
 		qdflags[unit].inuse &= ~CONS_DEV;
--- sys/vax/uba/qv.c
+++ sys/vax/uba/qv.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)qv.c	1.15 (Berkeley) 12/16/90
+ *	@(#)qv.c	1.16 (Berkeley) 05/16/91
  */
 
 /*
@@ -391,9 +391,10 @@ qvopen(dev, flag)
  * Close a QVSS line.
  */
 /*ARGSUSED*/
-qvclose(dev, flag)
+qvclose(dev, flag, mode, p)
 	dev_t dev;
-	int flag;
+	int flag, mode;
+	struct proc *p;
 {
 	register struct tty *tp;
 	register unit;
@@ -416,7 +417,7 @@ qvclose(dev, flag)
 	 * otherwise clear the state flag, and put the keyboard into down/up.
 	 */
 	if (QVCHAN(unit) != QVMOUSECHAN) {
-		(*linesw[tp->t_line].l_close)(tp);
+		(*linesw[tp->t_line].l_close)(tp, flag);
 		error = ttyclose(tp);
 	} else {
 		mouseon = 0;
