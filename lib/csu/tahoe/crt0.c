bcd96dcee63b8dbeb2b5a0ef2dc9533830b397a6
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1991-05-22 11:21:46 +0000

    delete support for prof style profiling

--- lib/csu/tahoe/crt0.c
+++ lib/csu/tahoe/crt0.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)crt0.c	5.2 (Berkeley) 05/06/91";
+static char sccsid[] = "@(#)crt0.c	5.3 (Berkeley) 05/22/91";
 #endif /* not lint */
 
 /*
@@ -73,7 +73,7 @@ asm("eprol:");
 exit(code)
 	register int code;
 {
-	monitor(0);
+	_mcleanup();
 	_cleanup();
 	_exit(code);
 }
