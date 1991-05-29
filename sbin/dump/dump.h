fbc3f157f6649dfe32c3c981596ee0e7e16cca8d
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1991-05-29 19:29:52 +0000

    update for new header files

--- sbin/dump/dump.h
+++ sbin/dump/dump.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)dump.h	5.15 (Berkeley) 03/07/91
+ *	@(#)dump.h	5.16 (Berkeley) 05/29/91
  */
 
 #define MAXINOPB	(MAXBSIZE / sizeof(struct dinode))
@@ -155,8 +155,7 @@ void	sigterm();
 #ifndef __STDC__
 #include <sys/file.h>
 #define _PATH_FSTAB	"/etc/fstab"
-typedef int (*sig_t)();
-extern char *strdup();
+extern char *index(), *strdup();
 extern char *ctime();
 extern int errno;
 #endif
