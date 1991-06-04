610b8d828cbe774d81e8b70aa8de94d7fab66f5a
marc <marc@65305748-3e16-e211-a854-8c89a5c6f760>
1991-06-04 16:52:18 +0000

    add stub for hp300

--- usr.bin/systat/disks.c
+++ usr.bin/systat/disks.c
@@ -5,7 +5,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)disks.c	5.8 (Berkeley) 04/11/91";
+static char sccsid[] = "@(#)disks.c	5.9 (Berkeley) 06/04/91";
 #endif not lint
 
 #include "systat.h"
@@ -218,6 +218,10 @@ read_names()
 		     cp[0], cp[1], udev.ui_unit);
 	}
 }
+#endif
+
+#ifdef hp300
+read_names() {}
 #endif
 
 dkselect(args, truefalse, selections)
