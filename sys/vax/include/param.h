ed6f80fe2b51f6be7e0a639ee3fd353b2410dbaf
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-06-08 16:44:08 +0000

    don't put anything after the #else or #endif's, unless it's a comment

--- sys/vax/include/param.h
+++ sys/vax/include/param.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.proprietary.c%
  *
- *	@(#)param.h	7.19 (Berkeley) 05/08/91
+ *	@(#)param.h	7.20 (Berkeley) 06/08/91
  */
 
 /*
@@ -116,6 +116,6 @@ int	cpuspeed;
 #define	DELAY(n)	{ register int N = cpuspeed * (n); while (--N > 0); }
 #endif
 
-#else KERNEL
+#else
 #define	DELAY(n)	{ register int N = (n); while (--N > 0); }
-#endif KERNEL
+#endif
