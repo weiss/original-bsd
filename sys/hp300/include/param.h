9bbdd6332b6ffe26375558c8fb6fe258af04af1d
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-06-08 16:43:25 +0000

    don't put anything after the #else or #endif's, unless it's a comment

--- sys/hp300/include/param.h
+++ sys/hp300/include/param.h
@@ -11,7 +11,7 @@
  *
  * from: Utah $Hdr: machparam.h 1.11 89/08/14$
  *
- *	@(#)param.h	7.5 (Berkeley) 05/08/91
+ *	@(#)param.h	7.6 (Berkeley) 06/08/91
  */
 
 /*
@@ -149,9 +149,9 @@ int	cpuspeed;
 #define	DELAY(n)	{ register int N = cpuspeed * (n); while (--N > 0); }
 #endif
 
-#else KERNEL
+#else
 #define	DELAY(n)	{ register int N = (n); while (--N > 0); }
-#endif KERNEL
+#endif
 
 #ifdef HPUXCOMPAT
 /*
