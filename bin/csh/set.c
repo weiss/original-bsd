f3075a1376ce9a3b3ec16377ef5d4980d858702a
christos <christos@65305748-3e16-e211-a854-8c89a5c6f760>
1991-06-14 14:35:52 +0000

    setNS needed to save the argument to set(). Otherwise we would free memory we did not allocate

--- bin/csh/set.c
+++ bin/csh/set.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)set.c	5.11 (Berkeley) 06/08/91";
+static char sccsid[] = "@(#)set.c	5.12 (Berkeley) 06/14/91";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -586,7 +586,7 @@ void
 setNS(cp)
     Char   *cp;
 {
-    set(cp, STRNULL);
+    set(cp, Strsave(STRNULL));
 }
 
 void
