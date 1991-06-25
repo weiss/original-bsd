e82d3f1350e0b7b58c18f386abd3789b0cea5465
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1991-06-25 12:14:33 +0000

    select list before initializing the ofile entry

--- sys/kern/kern_descrip.c
+++ sys/kern/kern_descrip.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kern_descrip.c	7.26 (Berkeley) 06/03/91
+ *	@(#)kern_descrip.c	7.28 (Berkeley) 06/25/91
  */
 
 #include "param.h"
@@ -457,11 +457,11 @@ falloc(p, resultfp, resultfd)
 	 */
 	nfiles++;
 	MALLOC(fp, struct file *, sizeof(struct file), M_FILE, M_WAITOK);
-	p->p_fd->fd_ofiles[i] = fp;
 	if (fq = p->p_fd->fd_ofiles[0])
 		fpp = &fq->f_filef;
 	else
 		fpp = &filehead;
+	p->p_fd->fd_ofiles[i] = fp;
 	if (fq = *fpp)
 		fq->f_fileb = &fp->f_filef;
 	fp->f_filef = fq;
