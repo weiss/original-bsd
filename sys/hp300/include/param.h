4dc73d39f200cc53bfe3dc1e4cc3ce96df484e1f
sklower <sklower@65305748-3e16-e211-a854-8c89a5c6f760>
1991-06-26 10:51:05 +0000

    add ALIGN macro

--- sys/hp300/include/param.h
+++ sys/hp300/include/param.h
@@ -11,13 +11,20 @@
  *
  * from: Utah $Hdr: machparam.h 1.11 89/08/14$
  *
- *	@(#)param.h	7.6 (Berkeley) 06/08/91
+ *	@(#)param.h	7.7 (Berkeley) 06/26/91
  */
 
 /*
  * Machine dependent constants for HP9000 series 300.
  */
 #define	MACHINE "hp300"
+
+/*
+ * Round p (pointer or byte index) up to a correctly-aligned value
+ * for all data types (int, long, ...).   The result is u_int and
+ * must be cast to any desired pointer type.
+ */
+#define	ALIGN(p)	(((u_int)(p) + (sizeof(int) - 1)) &~ (sizeof(int) - 1))
 
 #define	NBPG		4096		/* bytes/page */
 #define	PGOFSET		(NBPG-1)	/* byte offset into page */
