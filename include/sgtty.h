5da3d24a901789896f09fdf1d796e217b4629a70
marc <marc@65305748-3e16-e211-a854-8c89a5c6f760>
1991-06-26 11:40:35 +0000

    don't test for _IOCTL_H_

--- include/sgtty.h
+++ include/sgtty.h
@@ -4,12 +4,10 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sgtty.h	5.1 (Berkeley) 02/05/91
+ *	@(#)sgtty.h	5.2 (Berkeley) 06/26/91
  */
 
-#ifndef	_IOCTL_H_
 #ifndef USE_OLD_TTY
 #define	USE_OLD_TTY
 #endif
 #include <sys/ioctl.h>
-#endif /* !_IOCTL_H_ */
