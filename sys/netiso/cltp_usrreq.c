e80ff5ab55c19a4155ebec80f14a9e140b76823e
sklower <sklower@65305748-3e16-e211-a854-8c89a5c6f760>
1991-06-27 22:31:40 +0000

    make compile under both new and old vm systems

--- sys/netiso/cltp_usrreq.c
+++ sys/netiso/cltp_usrreq.c
@@ -4,12 +4,11 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)cltp_usrreq.c	7.5 (Berkeley) 06/28/90
+ *	@(#)cltp_usrreq.c	7.6 (Berkeley) 06/27/91
  */
 
 #ifndef CLTPOVAL_SRC /* XXX -- till files gets changed */
 #include "param.h"
-#include "user.h"
 #include "malloc.h"
 #include "mbuf.h"
 #include "protosw.h"
