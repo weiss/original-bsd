1f8e00bc0e1eed22eaa6827868d1aced98ff654d
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1991-07-25 22:44:12 +0000

    vm_page_free_count moves to vm_stat.free_count

--- sys/i386/i386/machdep.c
+++ sys/i386/i386/machdep.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)machdep.c	7.4 (Berkeley) 06/03/91
+ *	@(#)machdep.c	7.5 (Berkeley) 07/25/91
  */
 
 
@@ -230,7 +230,7 @@ again:
 	for (i = 1; i < ncallout; i++)
 		callout[i-1].c_next = &callout[i];
 
-	printf("avail mem = %d\n", ptoa(vm_page_free_count));
+	printf("avail mem = %d\n", ptoa(vm_stat.free_count));
 	printf("using %d buffers containing %d bytes of memory\n",
 		nbuf, bufpages * CLBYTES);
 
