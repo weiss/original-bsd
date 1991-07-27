4f157b477420ad4aa14a747820521483de431bd1
karels <karels@65305748-3e16-e211-a854-8c89a5c6f760>
1991-07-27 15:01:11 +0000

    don't try to cd to "obj" unless it's a directory, or a symlink to a directory

--- usr.bin/make/main.c
+++ usr.bin/make/main.c
@@ -17,7 +17,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	5.26 (Berkeley) 07/20/91";
+static char sccsid[] = "@(#)main.c	5.27 (Berkeley) 07/27/91";
 #endif /* not lint */
 
 /*-
@@ -328,7 +328,8 @@ main(argc, argv)
 	 */
 	if (!(path = getenv("MAKEOBJDIR")))
 		path = _PATH_OBJDIR;
-	if (!lstat(path, &sb)) {
+	if (stat(path, &sb) == 0 && S_ISDIR(sb.st_mode) &&
+	    lstat(path, &sb) == 0) {
 		if (S_ISDIR(sb.st_mode))
 			curdir = "..";
 		else {
