46bf0326e88256cfe076792ac8dcc149c825c2bb
marc <marc@65305748-3e16-e211-a854-8c89a5c6f760>
1991-07-31 11:42:12 +0000

    missing \n

--- libexec/talkd/talkd.c
+++ libexec/talkd/talkd.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)talkd.c	5.8 (Berkeley) 02/26/91";
+static char sccsid[] = "@(#)talkd.c	5.9 (Berkeley) 07/31/91";
 #endif /* not lint */
 
 /*
@@ -55,7 +55,7 @@ main(argc, argv)
 	int cc;
 
 	if (getuid()) {
-		fprintf(stderr, "%s: getuid: not super-user", argv[0]);
+		fprintf(stderr, "%s: getuid: not super-user\n", argv[0]);
 		exit(1);
 	}
 	openlog("talkd", LOG_PID, LOG_DAEMON);
