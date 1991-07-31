552d461746328e2278b85837154841ce1725fac9
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-07-31 16:57:00 +0000

    workaround for "cc -ansi -c one.c two.c" GCC bug

--- include/stdio.h
+++ include/stdio.h
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)stdio.h	5.17 (Berkeley) 06/03/91
+ *	@(#)stdio.h	5.18 (Berkeley) 07/31/91
  */
 
 #ifndef	_STDIO_H_
@@ -284,7 +284,7 @@ __END_DECLS
  */
 #define	__sgetc(p) (--(p)->_r < 0 ? __srget(p) : (int)(*(p)->_p++))
 #if defined(__GNUC__) && defined(__STDC__)
-static inline int __sputc(int _c, FILE *_p) {
+static __inline int __sputc(int _c, FILE *_p) {
 	if (--_p->_w >= 0 || (_p->_w >= _p->_lbfsize && (char)_c != '\n'))
 		return (*_p->_p++ = _c);
 	else
