5a28f815d04d1402ccab7b5e78dc77b0b5e6c24b
christos <christos@65305748-3e16-e211-a854-8c89a5c6f760>
1991-08-01 12:42:17 +0000

    Avoid globbing NULL patterns in libglob. (`` returned nothing.)

--- bin/csh/glob.c
+++ bin/csh/glob.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)glob.c	5.25 (Berkeley) 07/28/91";
+static char sccsid[] = "@(#)glob.c	5.26 (Berkeley) 08/01/91";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -332,6 +332,9 @@ libglob(vl)
     glob_t  globv;
     char   *ptr;
     int     nonomatch = adrof(STRnonomatch) != 0, magic = 0, match = 0;
+
+    if (!vl || !vl[0])
+	return (vl);
 
     globv.gl_offs = 0;
     globv.gl_pathv = 0;
