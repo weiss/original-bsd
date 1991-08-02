fea81ae7063621809dc0ca1fb9168728678e9330
karels <karels@65305748-3e16-e211-a854-8c89a5c6f760>
1991-08-02 12:02:18 +0000

    apply fixes in 7.14.1.3 to main branch

--- sys/hp300/hp300/trap.c
+++ sys/hp300/hp300/trap.c
@@ -11,7 +11,7 @@
  *
  * from: Utah $Hdr: trap.c 1.32 91/04/06$
  *
- *	@(#)trap.c	7.14.1.3 (Berkeley) 08/02/91
+ *	@(#)trap.c	7.15 (Berkeley) 08/02/91
  */
 
 #include "param.h"
@@ -495,14 +495,6 @@ syscall(code, frame)
 	else
 #endif
 	error = (*callp->sy_call)(p, &args, rval);
-#ifdef DIAGNOSTIC
-	if (curproc->p_spare[0])
-		panic("syscall: M_NAMEI");
-	if (curproc->p_spare[1])
-		panic("syscall: STARTSAVE");
-	if (curproc->p_spare[2])
-		panic("syscall: LOCK COUNT");
-#endif
 	if (error == ERESTART)
 		frame.f_pc = opc;
 	else if (error != EJUSTRETURN) {
