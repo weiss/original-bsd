0563cb066197031fb23d3398c88f17bab69e1319
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1991-08-28 15:47:28 +0000

    vm_stat is merged into vmmeter structure

--- sys/vm/vm_pageout.h
+++ sys/vm/vm_pageout.h
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vm_pageout.h	7.4 (Berkeley) 07/25/91
+ *	@(#)vm_pageout.h	7.5 (Berkeley) 08/28/91
  *
  *
  * Copyright (c) 1987, 1990 Carnegie-Mellon University.
@@ -59,6 +59,6 @@ simple_lock_data_t	vm_pages_needed_lock;
 #define	VM_WAIT		{ \
 			simple_lock(&vm_pages_needed_lock); \
 			thread_wakeup((int)&vm_pages_needed); \
-			thread_sleep((int)&vm_stat.free_count, \
+			thread_sleep((int)&cnt.v_free_count, \
 				&vm_pages_needed_lock, FALSE); \
 			}
