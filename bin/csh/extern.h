063c38e35a422b9061289dd78870facc761fe1dd
christos <christos@65305748-3e16-e211-a854-8c89a5c6f760>
1991-08-30 21:23:49 +0000

    Bugfix for loops in aliases.

--- bin/csh/extern.h
+++ bin/csh/extern.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)extern.h	5.6 (Berkeley) 07/28/91
+ *	@(#)extern.h	5.7 (Berkeley) 08/30/91
  */
 
 #include <sys/cdefs.h>
@@ -152,7 +152,8 @@ void	savehist __P((struct wordent *));
  * lex.c
  */
 void	 addla __P((Char *));
-void	 bseek __P((off_t));
+void	 bseek __P((struct Ain *));
+void	 btell __P((struct Ain *));
 void	 btoeof __P((void));
 void	 copylex __P((struct wordent *, struct wordent *));
 Char	*domod __P((Char *, int));
