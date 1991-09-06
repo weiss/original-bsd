ae87f290a7b4573b6e81e61215f9f31cc45a2623
sklower <sklower@65305748-3e16-e211-a854-8c89a5c6f760>
1991-09-06 17:15:21 +0000

    adopt TCP's policies concerning acks & (among others) use the fast timeout mechanism and delayed acks.

--- sys/netiso/tp_stat.h
+++ sys/netiso/tp_stat.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)tp_stat.h	7.5 (Berkeley) 06/27/91
+ *	@(#)tp_stat.h	7.6 (Berkeley) 09/06/91
  */
 
 /***********************************************************
@@ -120,6 +120,8 @@ struct tp_stat {
 	u_long	ts_Cset;
 	u_long	ts_Ccan_act;
 	u_long	ts_Ccan_inact;
+	u_long	ts_Fdelack;
+	u_long	ts_Fpruned;
 
 	u_long	ts_concat_rcvd;
 
