a7617dc41d81c3cad56ef76a9725c4db7bf21b89
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-09-11 16:29:28 +0000

    date and time created 91/09/11 16:29:28 by bostic

---
+++ lib/libc/db/recno/recno.h
@@ -0,0 +1,13 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)recno.h	5.1 (Berkeley) 09/11/91
+ */
+
+enum SRCHOP { SDELETE, SINSERT, SEARCH};	/* Rec_search operation. */
+
+#include "../btree/btree.h"
+#include "extern.h"
