f4837b053778c755787d263b259076dd11d696e3
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-09-23 14:09:22 +0000

    hash_open -> dbopen

--- usr.sbin/pwd_mkdb/pwd_mkdb.c
+++ usr.sbin/pwd_mkdb/pwd_mkdb.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)pwd_mkdb.c	5.5 (Berkeley) 05/06/91";
+static char sccsid[] = "@(#)pwd_mkdb.c	5.6 (Berkeley) 09/23/91";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -88,14 +88,14 @@ main(argc, argv)
 
 	/* Open the temporary insecure password database. */
 	(void)sprintf(buf, "%s.tmp", _PATH_MP_DB);
-	dp = hash_open(buf, O_WRONLY|O_CREAT|O_EXCL, PERM_INSECURE, NULL);
+	dp = dbopen(buf, O_WRONLY|O_CREAT|O_EXCL, PERM_INSECURE, DB_HASH, NULL);
 	if (!dp)
 		error(buf);
 	clean = FILE_INSECURE;
 
 	/* Open the temporary encrypted password database. */
 	(void)sprintf(buf, "%s.tmp", _PATH_SMP_DB);
-	edp = hash_open(buf, O_WRONLY|O_CREAT|O_EXCL, PERM_SECURE, NULL);
+	edp = dbopen(buf, O_WRONLY|O_CREAT|O_EXCL, PERM_SECURE, DB_HASH, NULL);
 	if (!edp)
 		error(buf);
 	clean = FILE_SECURE;
