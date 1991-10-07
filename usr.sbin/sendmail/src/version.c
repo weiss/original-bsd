dd4da7c5a0319d0ad33e17e4f9457b1aed38d926
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1991-10-07 11:31:24 +0000

    have DNS return what matched on a T_CNAME query

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	5.76 (Berkeley) 10/05/91";
+static char sccsid[] = "@(#)version.c	5.77 (Berkeley) 10/07/91";
 #endif /* not lint */
 
-char	Version[] = "5.76";
+char	Version[] = "5.77";
