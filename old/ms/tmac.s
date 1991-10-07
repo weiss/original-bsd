f3d4f830289749be150d588d1b183d1f87f922f9
cael <cael@65305748-3e16-e211-a854-8c89a5c6f760>
1991-10-07 21:05:51 +0000

    modify pathname

--- old/ms/tmac.s
+++ old/ms/tmac.s
@@ -1,10 +1,10 @@
 .\" %sccs.include.proprietary.roff%
 .\"
-.\"	@(#)tmac.s	5.1 (Berkeley) 04/17/91
+.\"	@(#)tmac.s	5.2 (Berkeley) 10/07/91
 .\"
 .\" ancillary files reside in \*(//,
 .\" except local which are in \*(L/,
-.ds // /usr/share/ms/
+.ds // /usr/old/lib/ms/
 .ds L/ /usr/local/lib/tmac/
 .	\" IZ - initialize (before text begins)
 .de IZ
