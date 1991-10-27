f1812eb6895771e1051b91e5898805c8854d60a4
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-10-27 08:56:00 +0000

    add err prototype

--- bin/cp/extern.h
+++ bin/cp/extern.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)extern.h	5.2 (Berkeley) 04/03/91
+ *	@(#)extern.h	5.3 (Berkeley) 10/27/91
  */
 
 typedef struct {
@@ -17,6 +17,7 @@ extern char *progname;			/* program name */
 #include <sys/cdefs.h>
 
 __BEGIN_DECLS
+void	 err __P((const char *fmt, ...));
 int	 path_set __P((PATH_T *, char *));
 char	*path_append __P((PATH_T *, char *, int));
 char	*path_basename __P((PATH_T *));
