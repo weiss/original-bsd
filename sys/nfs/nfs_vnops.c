07d450e662dbb3eb4c6596ed30d5087779124518
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-11-01 16:49:49 +0000

    ufs moved down one level

--- sys/nfs/nfs_vnops.c
+++ sys/nfs/nfs_vnops.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)nfs_vnops.c	7.61 (Berkeley) 09/06/91
+ *	@(#)nfs_vnops.c	7.62 (Berkeley) 11/01/91
  */
 
 /*
@@ -29,9 +29,9 @@
 #include "fifo.h"
 #include "map.h"
 
-#include "../ufs/quota.h"
-#include "../ufs/inode.h"
-#include "../ufs/dir.h"
+#include "ufs/ufs/quota.h"
+#include "ufs/ufs/inode.h"
+#include "ufs/ufs/dir.h"
 
 #include "nfsv2.h"
 #include "nfs.h"
