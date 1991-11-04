0c749210d314ba5ef40c085d6dd7afb756a23f92
christos <christos@65305748-3e16-e211-a854-8c89a5c6f760>
1991-11-04 18:23:45 +0000

    Changed slightly the $! error message

--- bin/csh/err.c
+++ bin/csh/err.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)err.c	5.11 (Berkeley) 07/19/91";
+static char sccsid[] = "@(#)err.c	5.12 (Berkeley) 11/04/91";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -189,7 +189,7 @@ static char *errorlist[] =
 #define ERR_HISTUS	78
     "Usage: history [-rh] [# number of events]",
 #define ERR_SPDOLLT	79
-    "$ or < not allowed with $# or $?",
+    "$, ! or < not allowed with $# or $?",
 #define ERR_NEWLINE	80
     "Newline in variable name",
 #define ERR_SPSTAR	81
