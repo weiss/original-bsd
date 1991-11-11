6c0be1caeca29d6898c0700032e83e8da96d9f09
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-11-11 15:02:22 +0000

    date and time created 91/11/11 15:02:22 by bostic

---
+++ usr.bin/id/groups.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -
+#
+# Copyright (c) 1991 The Regents of the University of California.
+# All rights reserved.
+#
+# %sccs.include.redist.sh%
+#
+#	@(#)groups.sh	5.1 (Berkeley) 11/11/91
+#
+
+id -Gn $*
