938d96e530d52d3e9b114c0e74d97e1fa0422306
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1991-11-13 10:12:58 +0000

    redeclaration of err()

--- usr.bin/mesg/mesg.c
+++ usr.bin/mesg/mesg.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)mesg.c	5.1 (Berkeley) 08/17/91";
+static char sccsid[] = "@(#)mesg.c	5.2 (Berkeley) 11/13/91";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -23,8 +23,8 @@ static char sccsid[] = "@(#)mesg.c	5.1 (Berkeley) 08/17/91";
 #include <stdlib.h>
 #include <string.h>
 
-static void err __P((const char *fmt, ...));
-static void usage __P((void));
+void err __P((const char *fmt, ...));
+void usage __P((void));
 
 int
 main(argc, argv)
@@ -72,7 +72,7 @@ main(argc, argv)
 	/* NOTREACHED */
 }
 
-static void
+void
 usage()
 {
 	(void)fprintf(stderr, "usage: mesg [y | n]\n");
