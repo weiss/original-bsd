52dce3f64da84bf6711705a3bc3d5c6640543579
marc <marc@65305748-3e16-e211-a854-8c89a5c6f760>
1991-11-20 16:39:44 +0000

    3100 changes (from ralph)

--- sys/vm/vm_glue.c
+++ sys/vm/vm_glue.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vm_glue.c	7.11 (Berkeley) 08/28/91
+ *	@(#)vm_glue.c	7.12 (Berkeley) 11/20/91
  *
  *
  * Copyright (c) 1987, 1990 Carnegie-Mellon University.
@@ -427,6 +427,20 @@ swapout(p)
 			addr += NBPG;
 		}
 		addr = (vm_offset_t) p->p_addr;
+	}
+#endif
+#ifdef mips
+	/*
+	 * Be sure to save the floating point coprocessor state before
+	 * paging out the u-struct.
+	 */
+	{
+		extern struct proc *machFPCurProcPtr;
+
+		if (p == machFPCurProcPtr) {
+			MachSaveCurFPState(p);
+			machFPCurProcPtr = (struct proc *)0;
+		}
 	}
 #endif
 #ifndef	i386 /* temporary measure till we find spontaineous unwire of kstack */
