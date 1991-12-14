6da0c2802b2be39d938c0bb500561fabb65cb470
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1991-12-14 21:29:07 +0000

    statfs.f_bsize => statfs.f_iosize; statfs.f_fsize => statfs.f_bsize (for SunOS)

--- sys/ufs/lfs/lfs_vfsops.c
+++ sys/ufs/lfs/lfs_vfsops.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lfs_vfsops.c	7.66 (Berkeley) 12/13/91
+ *	@(#)lfs_vfsops.c	7.67 (Berkeley) 12/14/91
  */
 
 #include <sys/param.h>
@@ -401,8 +401,8 @@ lfs_statfs(mp, sbp, p)
 	if (fs->lfs_magic != LFS_MAGIC)
 		panic("lfs_statfs: magic");
 	sbp->f_type = MOUNT_LFS;
-	sbp->f_fsize = fs->lfs_bsize;
 	sbp->f_bsize = fs->lfs_bsize;
+	sbp->f_iosize = fs->lfs_bsize;
 	sbp->f_blocks = fs->lfs_dsize;
 	sbp->f_bfree = fs->lfs_bfree;
 	sbp->f_bavail = (fs->lfs_dsize * (100 - fs->lfs_minfree) / 100) -
