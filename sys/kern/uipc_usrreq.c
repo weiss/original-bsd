c2ce6d13527dcdef529e7e749a935589a7da6501
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1992-01-14 12:25:09 +0000

    lease check when binding unix domain sockets

--- sys/kern/uipc_usrreq.c
+++ sys/kern/uipc_usrreq.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)uipc_usrreq.c	7.27 (Berkeley) 12/19/91
+ *	@(#)uipc_usrreq.c	7.28 (Berkeley) 01/14/92
  */
 
 #include "param.h"
@@ -376,6 +376,7 @@ unp_bind(unp, nam, p)
 	VATTR_NULL(&vattr);
 	vattr.va_type = VSOCK;
 	vattr.va_mode = 0777;
+	LEASE_CHECK(ndp->ni_dvp, p, p->p_ucred, LEASE_WRITE);
 	if (error = VOP_CREATE(ndp, &vattr, p))
 		return (error);
 	vp = ndp->ni_vp;
