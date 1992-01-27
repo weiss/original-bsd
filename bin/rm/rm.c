602c33059d10c4637467bbce869a2b7ba8e0acf9
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-01-27 09:59:14 +0000

    fts_statb -> fts_statp

--- bin/rm/rm.c
+++ bin/rm/rm.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rm.c	4.26 (Berkeley) 03/10/91";
+static char sccsid[] = "@(#)rm.c	4.27 (Berkeley) 01/27/92";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -128,7 +128,7 @@ rmtree(argv)
 		/* Pre-order: give user chance to skip. */
 		case FTS_D:
 			if (iflag && !check(p->fts_path, p->fts_accpath,
-			    &p->fts_statb)) {
+			    p->fts_statp)) {
 				(void)fts_set(fts, p, FTS_SKIP);
 				p->fts_number = SKIPPED;
 			}
@@ -141,7 +141,7 @@ rmtree(argv)
 		}
 
 		if (!fflag &&
-		    !check(p->fts_path, p->fts_accpath, &p->fts_statb))
+		    !check(p->fts_path, p->fts_accpath, p->fts_statp))
 			continue;
 
 		/*
