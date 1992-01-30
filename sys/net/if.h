e6512618496d267f19b03dab743d9724c7c68b73
sklower <sklower@65305748-3e16-e211-a854-8c89a5c6f760>
1992-01-30 12:02:51 +0000

    further ansi C crap necessary to relieve ordering constraints

--- sys/net/if.h
+++ sys/net/if.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)if.h	7.14 (Berkeley) 01/30/92
+ *	@(#)if.h	7.15 (Berkeley) 01/30/92
  */
 
 /*
@@ -39,6 +39,12 @@
 #endif
 #endif
 
+#ifdef __STDC__
+/*
+ * Forward structure declarations for function prototypes [sic].
+ */
+struct rtentry;	
+#endif
 /*
  * Structure defining a queue for a network interface.
  *
