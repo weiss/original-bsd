10a41e898b10182f4ea7d7ca740d998cf8411fab
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1992-02-03 16:17:51 +0000

    check that mount structures are empty before deleteing them

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vfs_syscalls.c	7.76 (Berkeley) 01/22/92
+ *	@(#)vfs_syscalls.c	7.77 (Berkeley) 02/03/92
  */
 
 #include "param.h"
@@ -250,6 +250,8 @@ dounmount(mp, flags, p)
 	} else {
 		vrele(coveredvp);
 		vfs_remove(mp);
+		if (mp->mnt_mounth != NULL)
+			panic("unmount: dangling vnode");
 		free((caddr_t)mp, M_MOUNT);
 	}
 	return (error);
