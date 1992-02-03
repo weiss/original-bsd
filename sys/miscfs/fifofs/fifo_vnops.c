b8fc66326a2c1cccae7217ffde4e4380187c8e2f
heideman <heideman@65305748-3e16-e211-a854-8c89a5c6f760>
1992-02-03 23:27:23 +0000

    new calling convention for VOP ops

--- sys/miscfs/fifofs/fifo_vnops.c
+++ sys/miscfs/fifofs/fifo_vnops.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)fifo_vnops.c	7.8 (Berkeley) 11/05/91
+ *	@(#)fifo_vnops.c	7.9 (Berkeley) 02/03/92
  */
 
 #include "param.h"
@@ -79,14 +79,13 @@ struct vnodeops fifo_vnodeops = {
  * Trivial lookup routine that always fails.
  */
 /* ARGSUSED */
-fifo_lookup(vp, ndp, p)
-	struct vnode *vp;
-	struct nameidata *ndp;
-	struct proc *p;
+fifo_lookup(dvp, vpp, cnp)
+	struct vnode *dvp;
+	struct vnode **vpp;
+	struct componentname *cnp;
 {
-
-	ndp->ni_dvp = vp;
-	ndp->ni_vp = NULL;
+	
+	*vpp = NULL;
 	return (ENOTDIR);
 }
 
