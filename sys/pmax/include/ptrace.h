6c5347bd11d3ca016ae20d24e0b90caebf38e8b2
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1992-02-05 22:41:43 +0000

    date and time created 92/02/05 22:41:43 by mckusick

---
+++ sys/pmax/include/ptrace.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)ptrace.h	7.1 (Berkeley) 02/05/92
+ */
+
+/*
+ * Machine dependent trace commands.
+ *
+ * None for the pmax at this time.
+ */
