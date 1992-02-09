b7538d8ba1050b97131fcd90a5173c0c5563138d
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1992-02-09 08:44:36 +0000

    merge in new release

--- usr.sbin/amd/include/config.h
+++ usr.sbin/amd/include/config.h
@@ -9,9 +9,9 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)config.h	5.2 (Berkeley) 05/12/91
+ *	@(#)config.h	5.3 (Berkeley) 02/09/92
  *
- * $Id: config.h,v 5.2.1.4 91/05/07 22:19:20 jsp Alpha $
+ * $Id: config.h,v 5.2.2.1 1992/02/09 15:09:56 jsp beta $
  *
  */
 
