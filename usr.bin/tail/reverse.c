a2d4f174556ce73db07d344a2436b6d961a8006b
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-02-12 15:28:19 +0000

    fix so if falls off the beginning of the file doesn't discard first byte

--- usr.bin/tail/reverse.c
+++ usr.bin/tail/reverse.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)reverse.c	5.2 (Berkeley) 10/21/91";
+static char sccsid[] = "@(#)reverse.c	5.3 (Berkeley) 02/12/92";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -101,11 +101,13 @@ r_reg(fp, style, off, sbp)
 		if (*--p == '\n') {
 			WR(p + 1, llen);
 			llen = 0;
-			if (style == RLINES && !--off)
+			if (style == RLINES && !--off) {
+				++p;
 				break;
+			}
 		}
 	if (llen)
-		WR(p + 1, llen);
+		WR(p, llen);
 }
 
 typedef struct bf {
