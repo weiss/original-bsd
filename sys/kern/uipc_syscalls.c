eb10fef22b7ac333d2db2dc8dd71db68fc494619
torek <torek@65305748-3e16-e211-a854-8c89a5c6f760>
1992-02-14 17:47:24 +0000

    compatibility for COMPAT_SUNOS

--- sys/kern/uipc_syscalls.c
+++ sys/kern/uipc_syscalls.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)uipc_syscalls.c	7.24 (Berkeley) 06/03/91
+ *	@(#)uipc_syscalls.c	7.25 (Berkeley) 02/14/92
  */
 
 #include "param.h"
@@ -24,6 +24,9 @@
 /*
  * System call interface to the socket abstraction.
  */
+#if defined(COMPAT_43) || defined(COMPAT_SUNOS)
+#define COMPAT_OLDSOCK
+#endif
 
 extern	struct fileops socketops;
 
@@ -96,7 +99,7 @@ listen(p, uap, retval)
 	return (solisten((struct socket *)fp->f_data, uap->backlog));
 }
 
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 accept(p, uap, retval)
 	struct proc *p;
 	struct args {
@@ -126,7 +129,7 @@ oaccept(p, uap, retval)
 	uap->compat_43 = 1;
 	return (accept1(p, uap, retval));
 }
-#else /* COMPAT_43 */
+#else /* COMPAT_OLDSOCK */
 
 #define	accept1	accept
 #endif
@@ -137,7 +140,7 @@ accept1(p, uap, retval)
 		int	s;
 		caddr_t	name;
 		int	*anamelen;
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 		int	compat_43;
 #endif
 	} *uap;
@@ -196,7 +199,7 @@ accept1(p, uap, retval)
 	nam = m_get(M_WAIT, MT_SONAME);
 	(void) soaccept(so, nam);
 	if (uap->name) {
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 		if (uap->compat_43)
 			mtod(nam, struct osockaddr *)->sa_family =
 			    mtod(nam, struct sockaddr *)->sa_family;
@@ -341,7 +344,7 @@ sendto(p, uap, retval)
 	msg.msg_iov = &aiov;
 	msg.msg_iovlen = 1;
 	msg.msg_control = 0;
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 	msg.msg_flags = 0;
 #endif
 	aiov.iov_base = uap->buf;
@@ -349,7 +352,7 @@ sendto(p, uap, retval)
 	return (sendit(p, uap->s, &msg, uap->flags, retval));
 }
 
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 osend(p, uap, retval)
 	struct proc *p;
 	register struct args {
@@ -439,7 +442,7 @@ sendmsg(p, uap, retval)
 	    (unsigned)(msg.msg_iovlen * sizeof (struct iovec)))))
 		goto done;
 	msg.msg_iov = iov;
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 	msg.msg_flags = 0;
 #endif
 	error = sendit(p, uap->s, &msg, uap->flags, retval);
@@ -489,7 +492,7 @@ sendit(p, s, mp, flags, retsize)
 		to = 0;
 	if (mp->msg_control) {
 		if (mp->msg_controllen < sizeof(struct cmsghdr)
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 		    && mp->msg_flags != MSG_COMPAT
 #endif
 		) {
@@ -499,7 +502,7 @@ sendit(p, s, mp, flags, retsize)
 		if (error = sockargs(&control, mp->msg_control,
 		    mp->msg_controllen, MT_CONTROL))
 			goto bad;
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 		if (mp->msg_flags == MSG_COMPAT) {
 			register struct cmsghdr *cm;
 
@@ -550,7 +553,7 @@ bad:
 	return (error);
 }
 
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 orecvfrom(p, uap, retval)
 	struct proc *p;
 	struct args {
@@ -601,7 +604,7 @@ recvfrom(p, uap, retval)
 	return (recvit(p, uap->s, &msg, (caddr_t)uap->fromlenaddr, retval));
 }
 
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 orecv(p, uap, retval)
 	struct proc *p;
 	register struct args {
@@ -695,7 +698,7 @@ recvmsg(p, uap, retval)
 		       M_WAITOK);
 	} else
 		iov = aiov;
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 	msg.msg_flags = uap->flags &~ MSG_COMPAT;
 #else
 	msg.msg_flags = uap->flags;
@@ -779,7 +782,7 @@ recvit(p, s, mp, namelenp, retsize)
 		if (len <= 0 || from == 0)
 			len = 0;
 		else {
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 			if (mp->msg_flags & MSG_COMPAT)
 				mtod(from, struct osockaddr *)->sa_family =
 				    mtod(from, struct sockaddr *)->sa_family;
@@ -794,7 +797,7 @@ recvit(p, s, mp, namelenp, retsize)
 		mp->msg_namelen = len;
 		if (namelenp &&
 		    (error = copyout((caddr_t)&len, namelenp, sizeof (int)))) {
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 			if (mp->msg_flags & MSG_COMPAT)
 				error = 0;	/* old recvfrom didn't check */
 			else
@@ -803,7 +806,7 @@ recvit(p, s, mp, namelenp, retsize)
 		}
 	}
 	if (mp->msg_control) {
-#ifdef	COMPAT_43
+#ifdef COMPAT_OLDSOCK
 		/*
 		 * We assume that old recvmsg calls won't receive access
 		 * rights and other control info, esp. as control info
@@ -982,7 +985,7 @@ free1:
 /*
  * Get socket name.
  */
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 getsockname(p, uap, retval)
 	struct proc *p;
 	struct args {
@@ -1012,7 +1015,7 @@ ogetsockname(p, uap, retval)
 	uap->compat_43 = 1;
 	return (getsockname1(p, uap, retval));
 }
-#else /* COMPAT_43 */
+#else /* COMPAT_OLDSOCK */
 
 #define	getsockname1	getsockname
 #endif
@@ -1024,7 +1027,7 @@ getsockname1(p, uap, retval)
 		int	fdes;
 		caddr_t	asa;
 		int	*alen;
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 		int	compat_43;
 #endif
 	} *uap;
@@ -1047,7 +1050,7 @@ getsockname1(p, uap, retval)
 		goto bad;
 	if (len > m->m_len)
 		len = m->m_len;
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 	if (uap->compat_43)
 		mtod(m, struct osockaddr *)->sa_family =
 		    mtod(m, struct sockaddr *)->sa_family;
@@ -1064,7 +1067,7 @@ bad:
 /*
  * Get name of peer for connected socket.
  */
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 getpeername(p, uap, retval)
 	struct proc *p;
 	struct args {
@@ -1094,7 +1097,7 @@ ogetpeername(p, uap, retval)
 	uap->compat_43 = 1;
 	return (getpeername1(p, uap, retval));
 }
-#else /* COMPAT_43 */
+#else /* COMPAT_OLDSOCK */
 
 #define	getpeername1	getpeername
 #endif
@@ -1106,7 +1109,7 @@ getpeername1(p, uap, retval)
 		int	fdes;
 		caddr_t	asa;
 		int	*alen;
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 		int	compat_43;
 #endif
 	} *uap;
@@ -1131,7 +1134,7 @@ getpeername1(p, uap, retval)
 		goto bad;
 	if (len > m->m_len)
 		len = m->m_len;
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 	if (uap->compat_43)
 		mtod(m, struct osockaddr *)->sa_family =
 		    mtod(m, struct sockaddr *)->sa_family;
@@ -1153,7 +1156,7 @@ sockargs(mp, buf, buflen, type)
 	int error;
 
 	if ((u_int)buflen > MLEN) {
-#ifdef COMPAT_43
+#ifdef COMPAT_OLDSOCK
 		if (type == MT_SONAME && (u_int)buflen <= 112)
 			buflen = MLEN;		/* unix domain compat. hack */
 		else
@@ -1172,7 +1175,7 @@ sockargs(mp, buf, buflen, type)
 	if (type == MT_SONAME) {
 		register struct sockaddr *sa = mtod(m, struct sockaddr *);
 
-#if defined(COMPAT_43) && BYTE_ORDER != BIG_ENDIAN
+#if defined(COMPAT_OLDSOCK) && BYTE_ORDER != BIG_ENDIAN
 		if (sa->sa_family == 0 && sa->sa_len < AF_MAX)
 			sa->sa_family = sa->sa_len;
 #endif
