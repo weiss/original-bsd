c831401d02b28e508852068e6ad7a5d33e5068da
sklower <sklower@65305748-3e16-e211-a854-8c89a5c6f760>
1992-02-20 10:45:15 +0000

    shut up lint

--- sys/netinet/in.c
+++ sys/netinet/in.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)in.c	7.22 (Berkeley) 02/20/92
+ *	@(#)in.c	7.23 (Berkeley) 02/20/92
  */
 
 #include "param.h"
@@ -334,8 +334,8 @@ in_control(so, cmd, data, ifp)
 			return (EINVAL);
 		oldaddr = ia->ia_dstaddr;
 		ia->ia_dstaddr = *(struct sockaddr_in *)&ifr->ifr_dstaddr;
-		if (ifp->if_ioctl &&
-		    (error = (*ifp->if_ioctl)(ifp, SIOCSIFDSTADDR, ia))) {
+		if (ifp->if_ioctl && (error = (*ifp->if_ioctl)
+					(ifp, SIOCSIFDSTADDR, (caddr_t)ia))) {
 			ia->ia_dstaddr = oldaddr;
 			return (error);
 		}
