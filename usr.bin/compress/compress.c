27d623d971816e1ee1d04ccdec99d4d2b5643b01
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1992-03-01 15:20:27 +0000

    check errors on close (net2/usr.bin/2)

--- usr.bin/compress/compress.c
+++ usr.bin/compress/compress.c
@@ -16,7 +16,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)compress.c	5.19 (Berkeley) 03/18/91";
+static char sccsid[] = "@(#)compress.c	5.20 (Berkeley) 03/01/92";
 #endif /* not lint */
 
 /*
@@ -784,7 +784,7 @@ code_int  code;
 	}
 	offset = 0;
 	(void)fflush( stdout );
-	if( ferror( stdout ) )
+	if( ferror( stdout ) || (fclose( stdout ) == EOF) )
 		writeerr();
 #ifdef DEBUG
 	if ( verbose )
@@ -910,6 +910,8 @@ decompress() {
 	offset += nwritten;
 	n -= nwritten;
     }
+    if ((zcat_flg == 0) && (close(fileno(stdout)) == -1))
+	writeerr();
 }
 
 /*-
