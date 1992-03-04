da1b240b950b1cf98073a1d25f2cc3f8c8471206
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-03-04 14:54:51 +0000

    unsigned comparisons <= 0

--- usr.bin/tail/read.c
+++ usr.bin/tail/read.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)read.c	5.2 (Berkeley) 03/03/92";
+static char sccsid[] = "@(#)read.c	5.3 (Berkeley) 03/04/92";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -111,8 +111,7 @@ lines(fp, off)
 	} *lines;
 	register int ch;
 	register char *p;
-	u_int blen, cnt, recno;
-	int wrap;
+	int blen, cnt, recno, wrap;
 	char *sp;
 
 	if ((lines = malloc(off * sizeof(*lines))) == NULL)
