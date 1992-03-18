746f258e282858e900c583b439f3ada99f2041ae
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-03-18 11:06:24 +0000

    typo

--- sys/ufs/lfs/lfs.h
+++ sys/ufs/lfs/lfs.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lfs.h	7.9 (Berkeley) 01/08/92
+ *	@(#)lfs.h	7.10 (Berkeley) 03/18/92
  */
 
 typedef struct buf	BUF;
@@ -180,7 +180,7 @@ struct segsum {
 	u_long	ss_create;		/* creation time stamp */
 	u_long	ss_nfinfo;		/* number of file info structures */
 	u_long	ss_ninos;		/* number of inodes in summary */
-	/* FINFO's... */
+	/* FINFO's and inode daddr's... */
 };
 
 /* NINDIR is the number of indirects in a file system block. */
