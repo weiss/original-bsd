49f7d88839f377cccfd60cd39c8a8f881ba0403d
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-03-18 16:34:25 +0000

    date and time created 92/03/18 16:34:25 by bostic

---
+++ sys/pmax/include/reloc.h
@@ -0,0 +1,12 @@
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)reloc.h	7.1 (Berkeley) 03/18/92
+ */
+
+/*
+ * There is currently no relocation information for the MIPS.
+ */
