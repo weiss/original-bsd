b5ed8b481f85d4ceff814fde62e01f308d06f152
ralph <ralph@65305748-3e16-e211-a854-8c89a5c6f760>
1992-03-22 20:39:37 +0000

    fix name of __ctype to match ctype.h

--- sys/deprecated/kdb/kdb_ctype.c
+++ sys/deprecated/kdb/kdb_ctype.c
@@ -3,12 +3,12 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)kdb_ctype.c	7.2 (Berkeley) 12/15/86
+ *	@(#)kdb_ctype.c	7.3 (Berkeley) 03/22/92
  */
 
 #include	<ctype.h>
 
-char _ctype_[1 + 256] = {
+char __ctype[1 + 256] = {
 	0,
 	_C,	_C,	_C,	_C,	_C,	_C,	_C,	_C,
 	_C,	_C|_S,	_C|_S,	_C|_S,	_C|_S,	_C|_S,	_C,	_C,
