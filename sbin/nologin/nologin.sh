e7b4862661d58f8123dbf51dfed374124f6a7a91
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-04-02 14:05:25 +0000

    date and time created 92/04/02 14:05:25 by bostic

---
+++ sbin/nologin/nologin.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -
+#
+# Copyright (c) 1992 The Regents of the University of California.
+# All rights reserved.
+#
+# %sccs.include.redist.sh%
+#
+#	@(#)nologin.sh	5.1 (Berkeley) 04/02/92
+#
+
+echo 'This account is currently not available.'
+exit 1
