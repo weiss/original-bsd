2c94b2270dfa6f6f9829a993c20a4957d8a70c4f
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-04-14 09:55:15 +0000

    KNF

--- lib/libc/gen/popen.c
+++ lib/libc/gen/popen.c
@@ -9,7 +9,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)popen.c	5.15 (Berkeley) 02/23/91";
+static char sccsid[] = "@(#)popen.c	5.16 (Berkeley) 04/14/92";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
@@ -38,7 +38,7 @@ popen(program, type)
 	if (pids == NULL) {
 		if ((fds = getdtablesize()) <= 0)
 			return (NULL);
-		if ((pids = (pid_t *)malloc((u_int)(fds * sizeof(int)))) == NULL)
+		if ((pids = malloc((u_int)(fds * sizeof(int)))) == NULL)
 			return (NULL);
 		bzero((char *)pids, fds * sizeof(pid_t));
 	}
