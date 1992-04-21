625d63b2a0409837be4922a966571ae6e5adab8b
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1992-04-21 10:25:38 +0000

    vop_truncate takes off_t rather than u_long length op

--- sys/miscfs/fifofs/fifo.h
+++ sys/miscfs/fifofs/fifo.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)fifo.h	7.3 (Berkeley) 02/04/92
+ *	@(#)fifo.h	7.4 (Berkeley) 04/21/92
  */
 
 #ifdef FIFO
@@ -180,7 +180,7 @@ int	fifo_advlock __P((
 		int mode))) fifo_badop)
 #define fifo_truncate ((int (*) __P(( \
 		struct vnode *vp, \
-		u_long length, \
+		off_t length, \
 		int flags))) nullop)
 #define fifo_update ((int (*) __P(( \
 		struct vnode *vp, \
