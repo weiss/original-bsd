64b7759c10e98da62140e4165e7f053f2059c7fe
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1992-04-21 10:53:52 +0000

    ensure proper types for local variables and parameters

--- sys/nfs/nfsnode.h
+++ sys/nfs/nfsnode.h
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)nfsnode.h	7.17 (Berkeley) 01/22/92
+ *	@(#)nfsnode.h	7.18 (Berkeley) 04/21/92
  */
 
 /*
@@ -253,7 +253,7 @@ void	nfs_vfree __P((
 		int mode));
 int	nfs_truncate __P((
 		struct vnode *vp,
-		u_long length,
+		off_t length,
 		int flags));
 int	nfs_update __P((
 		struct vnode *vp,
