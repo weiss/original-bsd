55b72adc17e62786f8395dc74d7c0e29edbcd6a1
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1992-04-21 13:10:19 +0000

    VOP_TRUNCATE now takes a cred argument

--- sys/miscfs/deadfs/dead_vnops.c
+++ sys/miscfs/deadfs/dead_vnops.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)dead_vnops.c	7.16 (Berkeley) 04/21/92
+ *	@(#)dead_vnops.c	7.17 (Berkeley) 04/21/92
  */
 
 #include "param.h"
@@ -187,7 +187,8 @@ int	dead_print __P((
 #define dead_truncate ((int (*) __P(( \
 		struct vnode *vp, \
 		off_t length, \
-		int flags))) nullop)
+		int flags, \
+		struct ucred *cred))) nullop)
 #define dead_update ((int (*) __P(( \
 		struct vnode *vp, \
 		struct timeval *ta, \
