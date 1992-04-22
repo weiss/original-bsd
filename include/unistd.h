29778967472d8cd47bb05b57f296247e2b64741e
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1992-04-22 11:34:22 +0000

    for now lseek takes and returns a long

--- include/unistd.h
+++ include/unistd.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)unistd.h	5.15 (Berkeley) 03/02/92
+ *	@(#)unistd.h	5.16 (Berkeley) 04/22/92
  */
 
 #ifndef _UNISTD_H_
@@ -59,7 +59,7 @@ pid_t	 getppid __P((void));
 uid_t	 getuid __P((void));
 int	 isatty __P((int));
 int	 link __P((const char *, const char *));
-off_t	 lseek __P((int, off_t, int));
+long	 lseek __P((int, long, int));
 long	 pathconf __P((const char *, int));	/* not yet */
 int	 pause __P((void));
 int	 pipe __P((int *));
