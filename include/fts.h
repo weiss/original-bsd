ac7d1f7aa1df61600f797341991a81ab72c7e8cb
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-05-05 11:52:28 +0000

    typo

--- include/fts.h
+++ include/fts.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)fts.h	5.23 (Berkeley) 03/05/92
+ *	@(#)fts.h	5.24 (Berkeley) 05/05/92
  */
 
 #ifndef	_FTS_H_
@@ -72,7 +72,7 @@ typedef struct _ftsent {
 	u_short fts_info;		/* user flags for FTSENT structure */
 
 #define	FTS_DONTCHDIR	 0x01		/* don't chdir .. to the parent */
-#define	FTS_SYMFOLLOW	 0x01		/* followed a symlink to get here */
+#define	FTS_SYMFOLLOW	 0x02		/* followed a symlink to get here */
 	u_short fts_flags;		/* private flags for FTSENT structure */
 
 #define	FTS_AGAIN	 1		/* read node again */
