458405be572ee639906d4e91d5039b106f1764b9
heideman <heideman@65305748-3e16-e211-a854-8c89a5c6f760>
1992-05-13 18:10:10 +0000

    minor syntax change before vnode interface conversion

--- sys/ufs/ffs/ffs_extern.h
+++ sys/ufs/ffs/ffs_extern.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ffs_extern.h	7.6 (Berkeley) 04/21/92
+ *	@(#)ffs_extern.h	7.7 (Berkeley) 05/13/92
  */
 
 struct buf;
@@ -68,7 +68,8 @@ __END_DECLS
 
 extern int inside[], around[];
 extern u_char *fragtbl[];
-extern struct vnodeops ffs_vnodeops, ffs_specops;
+extern struct vnodeops ffs_vnodeops;
+extern struct vnodeops ffs_specops;
 #ifdef FIFO
 extern struct vnodeops ffs_fifoops;
 #define FFS_FIFOOPS &ffs_fifoops
