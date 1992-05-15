b05cacb668da2eca4938cc66d2dd8b883f5da1ab
heideman <heideman@65305748-3e16-e211-a854-8c89a5c6f760>
1992-05-15 12:20:24 +0000

    lfs_vfree now returns an int

--- sys/ufs/lfs/lfs_alloc.c
+++ sys/ufs/lfs/lfs_alloc.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lfs_alloc.c	7.43 (Berkeley) 05/14/92
+ *	@(#)lfs_alloc.c	7.44 (Berkeley) 05/15/92
  */
 
 #include <sys/param.h>
@@ -175,7 +175,7 @@ lfs_vcreate(mp, ino, vpp)
 
 /* Free an inode. */
 /* ARGUSED */
-void
+int
 lfs_vfree (ap)
 	struct vop_vfree_args *ap;
 #define vp (ap->a_pvp)
@@ -224,7 +224,10 @@ lfs_vfree (ap)
 	/* Set superblock modified bit and decrement file count. */
 	fs->lfs_fmod = 1;
 	--fs->lfs_nfiles;
+	return (0);
 }
 #undef vp
 #undef notused1
 #undef notused2
+
+
