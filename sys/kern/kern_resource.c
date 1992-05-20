437d062decad94a43124acfb4ef82af1b4523d26
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-05-20 17:03:47 +0000

    fencepost error in ruadd; from Pace Willisson (pace@blitz.com)

--- sys/kern/kern_resource.c
+++ sys/kern/kern_resource.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kern_resource.c	7.15 (Berkeley) 03/15/92
+ *	@(#)kern_resource.c	7.16 (Berkeley) 05/20/92
  */
 
 #include "param.h"
@@ -291,7 +291,7 @@ ruadd(ru, ru2)
 	if (ru->ru_maxrss < ru2->ru_maxrss)
 		ru->ru_maxrss = ru2->ru_maxrss;
 	ip = &ru->ru_first; ip2 = &ru2->ru_first;
-	for (i = &ru->ru_last - &ru->ru_first; i > 0; i--)
+	for (i = &ru->ru_last - &ru->ru_first; i >= 0; i--)
 		*ip++ += *ip2++;
 }
 
