40b777efcd1d33b611e8ea33c27d10f06fab0263
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1992-05-29 10:44:57 +0000

    make GECOS matching configurable, and look at aliases after a match

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	5.99 (Berkeley) 05/25/92";
+static char sccsid[] = "@(#)version.c	5.100 (Berkeley) 05/29/92";
 #endif /* not lint */
 
-char	Version[] = "5.99";
+char	Version[] = "5.100";
