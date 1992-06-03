faa29347b8c2074e11374a5406b75b7e6a79949a
elan <elan@65305748-3e16-e211-a854-8c89a5c6f760>
1992-06-03 16:46:57 +0000

    fixed copyright.

--- bin/test/stalloc.c
+++ bin/test/stalloc.c
@@ -9,13 +9,13 @@
  */
 
 #ifndef lint
-char copyright[] =
+static char copyright[] =
 "@(#) Copyright (c) 1988 The Regents of the University of California.\n\
  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)stalloc.c	1.1 (Berkeley) 06/03/92";
+static char sccsid[] = "@(#)stalloc.c	1.2 (Berkeley) 06/03/92";
 #endif /* not lint */
 
 #include <stdio.h>
