64fc4c7d71d416b7786aa8bc4c47b859aaf3e7c1
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-06-16 20:11:43 +0000

    initscr() does a savetty() call; new curses doesn't have savetty().

--- games/battlestar/fly.c
+++ games/battlestar/fly.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)fly.c	5.6 (Berkeley) 03/04/91";
+static char sccsid[] = "@(#)fly.c	5.7 (Berkeley) 06/16/92";
 #endif /* not lint */
 
 #include "externs.h"
@@ -42,7 +42,6 @@ visual()
 	void moveenemy();
 
 	destroyed = 0;
-	savetty();
 	if(initscr() == ERR){
 		puts("Whoops!  No more memory...");
 		return(0);
