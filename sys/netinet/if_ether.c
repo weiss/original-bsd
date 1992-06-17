3822996e12727659e11a4ce3cdc96bbe4fae57de
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1992-06-17 20:25:55 +0000

    null pointer bug from Leres

--- sys/netinet/if_ether.c
+++ sys/netinet/if_ether.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)if_ether.c	7.22 (Berkeley) 05/05/92
+ *	@(#)if_ether.c	7.23 (Berkeley) 06/17/92
  */
 
 /*
@@ -438,9 +438,10 @@ reply:
 		la = arplookup(itaddr.s_addr, 0, SIN_PROXY);
 		if (la == NULL)
 			goto out;
+		rt = la->la_rt;
 		bcopy((caddr_t)ea->arp_sha, (caddr_t)ea->arp_tha,
 		    sizeof(ea->arp_sha));
-		sdl = SDL(la->la_rt->rt_gateway);
+		sdl = SDL(rt->rt_gateway);
 		bcopy(LLADDR(sdl), (caddr_t)ea->arp_sha, sizeof(ea->arp_sha));
 		sendtrailers = rt->rt_flags & RTF_USETRAILERS;
 	}
