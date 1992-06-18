6ee9544f34f07f489c1c032fffb6c21dc7c1776f
hibler <hibler@65305748-3e16-e211-a854-8c89a5c6f760>
1992-06-18 21:04:12 +0000

    include time.h

--- sys/stand.att/saio.h
+++ sys/stand.att/saio.h
@@ -4,9 +4,10 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)saio.h	7.17 (Berkeley) 05/27/92
+ *	@(#)saio.h	7.18 (Berkeley) 06/18/92
  */
 
+#include <sys/time.h>
 #include <ufs/ffs/fs.h>
 #include <ufs/ufs/dinode.h>
 #include "saioctl.h"
