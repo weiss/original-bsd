488ce95726ba6f97e524e11f5b098594206da4af
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1992-06-19 17:45:53 +0000

    support for luna68k

--- include/setjmp.h
+++ include/setjmp.h
@@ -4,13 +4,13 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)setjmp.h	5.6 (Berkeley) 09/06/91
+ *	@(#)setjmp.h	5.7 (Berkeley) 06/19/92
  */
 
 #ifndef _SETJMP_H_
 #define _SETJMP_H_
 
-#if defined(hp300) || defined(__hp300__)
+#if defined(hp300) || defined(__hp300__) || defined(luna68k) || defined(__luna68k__)
 #define _JBLEN	17
 #endif
 
