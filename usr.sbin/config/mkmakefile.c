057802f0e39b75428ea5cfdd0264d90546759e69
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1992-06-19 17:54:16 +0000

    support for luna68k

--- usr.sbin/config/mkmakefile.c
+++ usr.sbin/config/mkmakefile.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)mkmakefile.c	5.36 (Berkeley) 02/18/92";
+static char sccsid[] = "@(#)mkmakefile.c	5.37 (Berkeley) 06/19/92";
 #endif /* not lint */
 
 /*
@@ -99,6 +99,7 @@ static	struct users {
 	{ 8, 2, 64 },			/* MACHINE_I386 */
 	{ 8, 2, 64 },			/* MACHINE_MIPS */
 	{ 8, 2, 64 },			/* MACHINE_PMAX */
+	{ 8, 2, 64 },			/* MACHINE_LUNA68K */
 };
 #define	NUSERS	(sizeof (users) / sizeof (users[0]))
 
