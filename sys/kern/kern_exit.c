7ef676f56b77866c8d9bb2936ed80fbdab57281a
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1992-06-20 11:57:47 +0000

    turn off profiling if on

--- sys/kern/kern_exit.c
+++ sys/kern/kern_exit.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kern_exit.c	7.43 (Berkeley) 02/15/92
+ *	@(#)kern_exit.c	7.44 (Berkeley) 06/20/92
  */
 
 #include "param.h"
@@ -67,6 +67,8 @@ exit(p, rv)
 #ifdef PGINPROF
 	vmsizmon();
 #endif
+	if (p->p_flag & SPROFIL)
+		stopprofclock(p);
 	MALLOC(p->p_ru, struct rusage *, sizeof(struct rusage),
 		M_ZOMBIE, M_WAITOK);
 	/*
