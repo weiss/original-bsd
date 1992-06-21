e6076ab4bc3f87ac08664fb367bbd7435626e23d
torek <torek@65305748-3e16-e211-a854-8c89a5c6f760>
1992-06-21 01:37:17 +0000

    get the types right, and get the parameters to locc in the right order!

--- sys/libkern/scanc.c
+++ sys/libkern/scanc.c
@@ -4,17 +4,19 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)scanc.c	7.1 (Berkeley) 06/20/92
+ *	@(#)scanc.c	7.2 (Berkeley) 06/21/92
  */
 
 int
-scanc(size, cp, table, mask)
+scanc(size, cp, table, mask0)
 	unsigned int size;
 	register unsigned char *cp, table[];
-	register unsigned char mask;
+	int mask0;
 {
 	register unsigned char *end;
+	register unsigned char mask;
 
+	mask = mask0;
 	for (end = &cp[size]; cp < end && (table[*cp] & mask) == 0; ++cp);
 	return (end - cp);
 }
