9640b10a4facd6348f34ab6e009ac755471d5269
andrew <andrew@65305748-3e16-e211-a854-8c89a5c6f760>
1992-06-27 16:52:12 +0000

    add paths for message files

--- libexec/ftpd/pathnames.h
+++ libexec/ftpd/pathnames.h
@@ -4,9 +4,11 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.2 (Berkeley) 06/01/90
+ *	@(#)pathnames.h	5.3 (Berkeley) 06/27/92
  */
 
 #include <paths.h>
 
-#define	_PATH_FTPUSERS	"/etc/ftpusers"
+#define	_PATH_FTPUSERS		"/etc/ftpusers"
+#define	_PATH_FTPWELCOME	"/etc/ftpwelcome"
+#define	_PATH_FTPLOGINMESG	"/etc/motd"
