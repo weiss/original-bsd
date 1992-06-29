f39a08ed38bb42b8af03fd5e11c3770c0049b34c
sklower <sklower@65305748-3e16-e211-a854-8c89a5c6f760>
1992-06-29 16:43:39 +0000

    get rid of spurious reference to stdio

--- sys/netiso/tp_astring.c
+++ sys/netiso/tp_astring.c
@@ -4,12 +4,9 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)tp_astring.c	7.4 (Berkeley) 05/06/91
+ *	@(#)tp_astring.c	7.5 (Berkeley) 06/29/92
  */
 
-#ifndef _NFILE
-#include <stdio.h>
-#endif _NFILE
 char *tp_sstring[] = {
 "ST_ERROR(0x0)",
 "TP_CLOSED(0x1)",
