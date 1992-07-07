4592210706abbec340ca12b1b204c60ff54b9071
torek <torek@65305748-3e16-e211-a854-8c89a5c6f760>
1992-07-07 16:49:53 +0000

    only need to block clock; volatile poisoning

--- sys/kern/sys_generic.c
+++ sys/kern/sys_generic.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sys_generic.c	7.34 (Berkeley) 06/23/92
+ *	@(#)sys_generic.c	7.35 (Berkeley) 07/07/92
  */
 
 #include "param.h"
@@ -496,8 +496,8 @@ select(p, uap, retval)
 			error = EINVAL;
 			goto done;
 		}
-		s = splhigh();
-		timevaladd(&atv, &time);
+		s = splclock();
+		timevaladd(&atv, (struct timeval *)&time);
 		timo = hzto(&atv);
 		splx(s);
 	} else
