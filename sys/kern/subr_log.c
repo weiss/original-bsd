8103babd54f6a5fe7d11b6adf982b2932cdb2423
torek <torek@65305748-3e16-e211-a854-8c89a5c6f760>
1992-07-07 19:12:09 +0000

    FIONREAD can only return 32-bit size

--- sys/kern/subr_log.c
+++ sys/kern/subr_log.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)subr_log.c	7.14 (Berkeley) 05/20/92
+ *	@(#)subr_log.c	7.15 (Berkeley) 07/07/92
  */
 
 /*
@@ -177,7 +177,7 @@ logioctl(dev, com, data, flag, p)
 		splx(s);
 		if (l < 0)
 			l += MSG_BSIZE;
-		*(off_t *)data = l;
+		*(int *)data = l;
 		break;
 
 	case FIONBIO:
