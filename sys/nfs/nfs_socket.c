a5ec86706fe747919e232ad29e7127cae7dc14b0
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1992-07-10 00:42:35 +0000

    eliminate random stuff

--- sys/nfs/nfs_socket.c
+++ sys/nfs/nfs_socket.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)nfs_socket.c	7.32 (Berkeley) 07/02/92
+ *	@(#)nfs_socket.c	7.33 (Berkeley) 07/10/92
  */
 
 /*
@@ -41,7 +41,6 @@
 #define	TRUE	1
 #define	FALSE	0
 
-int netnetnet = sizeof (struct netaddrhash);
 /*
  * Estimate rto for an nfs rpc sent via. an unreliable datagram.
  * Use the mean and mean deviation of rtt for the appropriate type of rpc
