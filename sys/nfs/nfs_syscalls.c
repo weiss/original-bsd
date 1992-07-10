06898ff672628d00ae094f46931ef574851bff57
torek <torek@65305748-3e16-e211-a854-8c89a5c6f760>
1992-07-10 20:46:10 +0000

    ANSIfy syscall args

--- sys/nfs/nfs_syscalls.c
+++ sys/nfs/nfs_syscalls.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)nfs_syscalls.c	7.31 (Berkeley) 05/06/92
+ *	@(#)nfs_syscalls.c	7.32 (Berkeley) 07/10/92
  */
 
 #include "param.h"
@@ -71,12 +71,13 @@ static int nfs_asyncdaemon[NFS_MAXASYNCDAEMON];
 /*
  * Get file handle system call
  */
+struct getfh_args {
+	char	*fname;
+	fhandle_t *fhp;
+};
 getfh(p, uap, retval)
 	struct proc *p;
-	register struct args {
-		char	*fname;
-		fhandle_t *fhp;
-	} *uap;
+	register struct getfh_args *uap;
 	int *retval;
 {
 	register struct vnode *vp;
@@ -112,12 +113,13 @@ static struct nfssvc_sock nfssvc_sockhead;
  * - remains in the kernel as an nfsd
  * - remains in the kernel as an nfsiod
  */
+struct nfssvc_args {
+	int flag;
+	caddr_t argp;
+};
 nfssvc(p, uap, retval)
 	struct proc *p;
-	register struct args {
-		int flag;
-		caddr_t argp;
-	} *uap;
+	register struct nfssvc_args *uap;
 	int *retval;
 {
 	struct nameidata nd;
