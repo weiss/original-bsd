c3ba38c1f5df4b5f343f9041a14a23844dbc314c
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-07-12 15:14:12 +0000

    move file systems into misclfs

--- old/mount_lofs/mount_lofs.c
+++ old/mount_lofs/mount_lofs.c
@@ -8,12 +8,12 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mount_lofs.c	5.2 (Berkeley) 07/05/92
+ *	@(#)mount_lofs.c	5.3 (Berkeley) 07/12/92
  */
 
 #include <sys/param.h>
 #include <sys/mount.h>
-#include <lofs/lofs.h>
+#include <miscfs/lofs/lofs.h>
 
 #include <errno.h>
 #include <stdio.h>
