76b22a8e7b4e62350b9cf11eb3223edb6f9f0227
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-07-12 15:48:46 +0000

    move misc file systems to miscfs

--- sbin/mount_null/mount_null.c
+++ sbin/mount_null/mount_null.c
@@ -8,12 +8,12 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mount_null.c	5.2 (Berkeley) 07/11/92
+ *	@(#)mount_null.c	5.3 (Berkeley) 07/12/92
  */
 
 #include <sys/param.h>
 #include <sys/mount.h>
-#include <nullfs/null.h>
+#include <miscfs/nullfs/null.h>
 
 #include <errno.h>
 #include <stdio.h>
