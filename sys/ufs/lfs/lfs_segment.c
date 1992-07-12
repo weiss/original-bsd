13ea45f885bce685a13601da6d8435328ca46f71
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1992-07-12 15:32:25 +0000

    new location for fifofs and specfs

--- sys/ufs/lfs/lfs_segment.c
+++ sys/ufs/lfs/lfs_segment.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lfs_segment.c	7.21 (Berkeley) 07/05/92
+ *	@(#)lfs_segment.c	7.22 (Berkeley) 07/12/92
  */
 
 #include <sys/param.h>
@@ -18,10 +18,11 @@
 #include <sys/proc.h>
 #include <sys/conf.h>
 #include <sys/vnode.h>
-#include <sys/specdev.h>
-#include <sys/fifo.h>
 #include <sys/malloc.h>
 #include <sys/mount.h>
+
+#include <miscfs/specfs/specdev.h>
+#include <miscfs/fifofs/fifo.h>
 
 #include <ufs/ufs/quota.h>
 #include <ufs/ufs/inode.h>
