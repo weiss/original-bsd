08842225eba6c05a5983e52c6ff38cb0b585a576
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1992-07-12 12:34:49 +0000

    change setproctitle so it will print sendmail: in 4.4bsd

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	5.104 (Berkeley) 07/12/92";
+static char sccsid[] = "@(#)version.c	5.105 (Berkeley) 07/12/92";
 #endif /* not lint */
 
-char	Version[] = "5.104";
+char	Version[] = "5.105";
