76ba244cd900bf18db71ff54469f24824d1c6527
marc <marc@65305748-3e16-e211-a854-8c89a5c6f760>
1992-07-15 16:12:19 +0000

    check for stopped jobs before exiting

--- bin/sh/jobs.h
+++ bin/sh/jobs.h
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)jobs.h	5.1 (Berkeley) 03/07/91
+ *	@(#)jobs.h	5.2 (Berkeley) 07/15/92
  */
 
 /* Mode argument to forkshell.  Don't change FORK_FG or FORK_BG. */
@@ -49,6 +49,7 @@ struct job {
 };
 
 extern short backgndpid;	/* pid of last background process */
+extern int job_warning;		/* user was warned about stopped jobs */
 
 
 #ifdef __STDC__
