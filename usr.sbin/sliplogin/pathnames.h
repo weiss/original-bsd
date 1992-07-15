3510a7eacf7481d4e8c1c3e8fbaae03f341e4069
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-07-15 13:31:52 +0000

    put slip down into /etc/sliphome

--- usr.sbin/sliplogin/pathnames.h
+++ usr.sbin/sliplogin/pathnames.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.3 (Berkeley) 07/15/92
+ *	@(#)pathnames.h	5.4 (Berkeley) 07/15/92
  */
 
 #ifndef COMPAT
@@ -13,8 +13,8 @@
 #define	_PATH_DEVNULL	"/dev/null"
 #endif
 
-#define	_PATH_ACCESS	"/etc/slip.hosts"
-#define	_PATH_LOGIN	"/etc/slip.login"
-#define	_PATH_LOGOUT	"/etc/slip.logout"
+#define	_PATH_ACCESS	"/etc/sliphome/slip.hosts"
+#define	_PATH_LOGIN	"/etc/sliphome/slip.login"
+#define	_PATH_LOGOUT	"/etc/sliphome/slip.logout"
 #define	_PATH_DEBUG	"/tmp/sliplogin.XXXXXX"
 
