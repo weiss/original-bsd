0a46a9a46a370f077d9a98665576c369cd2f801f
marc <marc@65305748-3e16-e211-a854-8c89a5c6f760>
1992-07-15 10:01:55 +0000

    propor declarations

--- bin/sh/input.c
+++ bin/sh/input.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)input.c	5.7 (Berkeley) 07/15/92";
+static char sccsid[] = "@(#)input.c	5.8 (Berkeley) 07/15/92";
 #endif /* not lint */
 
 /*
@@ -170,7 +170,7 @@ preadbuffer() {
 retry:
 	p = parsenextc = parsefile->buf;
 	if (parsefile->fd == 0 && el) {
-		char *rl_cp;
+		const char *rl_cp;
 		int len;
 
 		rl_cp = el_gets(el, &len);
