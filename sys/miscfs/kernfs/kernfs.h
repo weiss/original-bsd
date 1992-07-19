2a1e8850ec2e4122995a166b7275794a1745546d
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1992-07-19 04:56:06 +0000

    added raw root device support to kernfs

--- sys/miscfs/kernfs/kernfs.h
+++ sys/miscfs/kernfs/kernfs.h
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kernfs.h	7.1 (Berkeley) 07/18/92
+ *	@(#)kernfs.h	7.2 (Berkeley) 07/19/92
  */
 
 #ifdef KERNEL
@@ -25,4 +25,5 @@ struct kernfs_node {
 
 extern int (**kernfs_vnodeop_p)();
 extern struct vfsops kernfs_vfsops;
+extern struct vnode *rrootvp;
 #endif /* KERNEL */
