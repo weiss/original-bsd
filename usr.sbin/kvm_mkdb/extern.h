edef12e8ea8fb2bd4a629172fb57b72a62430be7
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-07-21 14:03:04 +0000

    date and time created 92/07/21 14:03:04 by bostic

---
+++ usr.sbin/kvm_mkdb/extern.h
@@ -0,0 +1,12 @@
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)extern.h	5.1 (Berkeley) 07/21/92
+ */
+
+void	create_knlist __P((char *, DB *));
+void	error __P((char *));
+int	testdb __P(());
