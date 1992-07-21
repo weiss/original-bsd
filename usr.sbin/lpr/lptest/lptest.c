001c16bd53ad4dd24cb0e77185b84f4f956da3f7
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-07-21 10:30:33 +0000

    prototype lpr to fix quad's

--- usr.sbin/lpr/lptest/lptest.c
+++ usr.sbin/lpr/lptest/lptest.c
@@ -12,15 +12,16 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)lptest.c	5.4 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)lptest.c	5.5 (Berkeley) 07/21/92";
 #endif /* not lint */
+
+#include <stdlib.h>
+#include <stdio.h>
 
 /*
  * lptest -- line printer test program (and other devices).
  */
-
-#include <stdio.h>
-
+int
 main(argc, argv)
 	int argc;
 	char **argv;
@@ -51,4 +52,5 @@ main(argc, argv)
 		putchar('\n');
 	}
 	(void) fflush(stdout);
+	exit(0);
 }
