6d6ea34a2164d5934a16789b98b78673dce9a2a9
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-07-21 16:50:40 +0000

    update the contribution notice; LBL requires acknowledgement

--- sys/sparc/rcons/rcons_kern.c
+++ sys/sparc/rcons/rcons_kern.c
@@ -6,9 +6,14 @@
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
  * contributed to Berkeley.
  *
+ * All advertising materials mentioning features or use of this software
+ * must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Lawrence Berkeley Laboratories.
+ *
  * %sccs.include.redist.c%
  *
- *	@(#)rcons_kern.c	7.1 (Berkeley) 07/13/92
+ *	@(#)rcons_kern.c	7.2 (Berkeley) 07/21/92
  *
  * from: $Header: rcons_kern.c,v 1.26 92/07/09 08:01:28 torek Exp $
  */
