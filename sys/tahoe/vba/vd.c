023a1e8a3171d0924799828171cdf1caf3146301
karels <karels@65305748-3e16-e211-a854-8c89a5c6f760>
1992-07-23 16:42:03 +0000

    parenthesis error

--- sys/tahoe/vba/vd.c
+++ sys/tahoe/vba/vd.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vd.c	7.14 (Berkeley) 04/01/91
+ *	@(#)vd.c	7.15 (Berkeley) 07/23/92
  */
 
 #include "dk.h"
@@ -1251,7 +1251,7 @@ vdreset_ctlr(vm)
 	if (vdinit_ctlr(vm, vd) == 0)
 		return;
 	for (unit = 0; unit < NDK; unit++)
-		if (vi = vddinfo[unit] && vi->ui_mi == vm && vi->ui_alive)
+		if ((vi = vddinfo[unit]) && vi->ui_mi == vm && vi->ui_alive)
 			(void) vdreset_drive(vi);
 }
 
