e90728dc30765e74825470007f6220cbc3bf9eb4
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-07-23 16:42:23 +0000

    Welcome to the Wonderful World of ANSI C!

--- libexec/rbootd/conf.c
+++ libexec/rbootd/conf.c
@@ -12,16 +12,20 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.c	5.1 (Berkeley) 07/23/92
+ *	@(#)conf.c	5.2 (Berkeley) 07/23/92
  *
  * Utah $Hdr: conf.c 3.1 92/07/06$
  * Author: Jeff Forys, University of Utah CSS
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	5.1 (Berkeley) 07/23/92";
+static char sccsid[] = "@(#)conf.c	5.2 (Berkeley) 07/23/92";
 #endif /* not lint */
 
+#include <sys/param.h>
+#include <sys/time.h>
+
+#include <stdio.h>
 #include "defs.h"
 #include "pathnames.h"
 
