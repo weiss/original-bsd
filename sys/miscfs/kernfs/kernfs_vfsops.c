2f629785778b7bd719d82d303bab88e8d071e514
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1992-08-01 04:23:09 +0000

    remove decl of spec_vnodeop_p

--- sys/miscfs/kernfs/kernfs_vfsops.c
+++ sys/miscfs/kernfs/kernfs_vfsops.c
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kernfs_vfsops.c	7.3 (Berkeley) 07/19/92
+ *	@(#)kernfs_vfsops.c	7.4 (Berkeley) 08/01/92
  */
 
 /*
@@ -26,7 +26,6 @@
 #include <sys/malloc.h>
 
 #include <miscfs/specfs/specdev.h>
-extern int (**spec_vnodeop_p)();
 #include <miscfs/kernfs/kernfs.h>
 
 struct vnode *rrootvp;
