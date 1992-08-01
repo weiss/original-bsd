54f67e2fd4101abc1d8136872f529f85105faa77
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-08-01 18:56:10 +0000

    fix the comment

--- lib/libc/string/strsep.c
+++ lib/libc/string/strsep.c
@@ -10,11 +10,11 @@
 #include <stdio.h>
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)strsep.c	5.5 (Berkeley) 11/16/91";
+static char sccsid[] = "@(#)strsep.c	5.6 (Berkeley) 08/01/92";
 #endif /* LIBC_SCCS and not lint */
 
 /*
- * Get next token from string *stringp, where tokens are nonempty
+ * Get next token from string *stringp, where tokens are possibly-empty
  * strings separated by characters from delim.  
  *
  * Writes NULs into the string at *stringp to end tokens.
@@ -22,7 +22,7 @@ static char sccsid[] = "@(#)strsep.c	5.5 (Berkeley) 11/16/91";
  * On return, *stringp points past the last NUL written (if there might
  * be further tokens), or is NULL (if there are definitely no more tokens).
  *
- * If *stringp is NULL, strtoken returns NULL.
+ * If *stringp is NULL, strsep returns NULL.
  */
 char *
 strsep(stringp, delim)
