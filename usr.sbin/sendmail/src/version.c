305d2714fec0eebe7b1c093f3c79e13711626421
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1992-08-09 15:21:09 +0000

    represent arpa dates with four digit years instead of two digits

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	5.113 (Berkeley) 07/20/92";
+static char sccsid[] = "@(#)version.c	5.114 (Berkeley) 08/09/92";
 #endif /* not lint */
 
-char	Version[] = "5.113";
+char	Version[] = "5.114";
