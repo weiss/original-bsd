0c702d09ea93f0afecbb6c42bdd1612fcfb0b304
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-08-23 11:48:16 +0000

    rename _putchar to __cputchar, lint, KNF

--- lib/libcurses/putchar.c
+++ lib/libcurses/putchar.c
@@ -6,17 +6,18 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)putchar.c	5.4 (Berkeley) 06/01/90";
-#endif /* not lint */
+static char sccsid[] = "@(#)putchar.c	5.5 (Berkeley) 08/23/92";
+#endif	/* not lint */
 
-# include	"curses.ext"
+#include <curses.h>
 
-char
-_putchar(c)
-reg char	c; {
+void
+__cputchar(ch)
+	int ch;
+{
 
-	putchar(c);
 #ifdef DEBUG
-	fprintf(outf, "_PUTCHAR(%s)\n", unctrl(c));
+	__TRACE("__cputchar: %s\n", unctrl(ch));
 #endif
+	(void)putchar(ch);
 }
