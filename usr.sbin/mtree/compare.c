13472e9e2f03070cbf96552487f72cad755017fd
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-09-01 13:52:53 +0000

    sizes are quad's, not

--- usr.sbin/mtree/compare.c
+++ usr.sbin/mtree/compare.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)compare.c	5.12 (Berkeley) 07/15/92";
+static char sccsid[] = "@(#)compare.c	5.13 (Berkeley) 09/01/92";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -135,7 +135,7 @@ typeerr:		LABEL;
 	}
 	if (s->flags & F_SIZE && s->st_size != p->fts_statp->st_size) {
 		LABEL;
-		(void)printf("%ssize (%ld, %ld)\n",
+		(void)printf("%ssize (%qd, %qd)\n",
 		    tab, s->st_size, p->fts_statp->st_size);
 		tab = "\t";
 	}
