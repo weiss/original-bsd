79a060072f6bdc48c80314a7593898d4dcc893fa
elan <elan@65305748-3e16-e211-a854-8c89a5c6f760>
1992-09-09 10:58:37 +0000

    Changed reference data size since tag was eliminated.

--- usr.bin/cap_mkdb/cap_mkdb.c
+++ usr.bin/cap_mkdb/cap_mkdb.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)cap_mkdb.c	1.8 (Berkeley) 09/08/92";
+static char sccsid[] = "@(#)cap_mkdb.c	1.9 (Berkeley) 09/09/92";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -171,7 +171,7 @@ db_build(inputfiles)
 		 */
 		(void)strcpy((char *)(data.data), nf);
 
-		data.size = key.size + 1;	/* need extra byte for tag */
+		data.size = key.size;
 		key.data = namebuf;
 		np = namebuf;
 		for (cp = nf; *cp != '\0'; cp++) {
