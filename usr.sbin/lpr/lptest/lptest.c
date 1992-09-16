e6fdcb1aeb557fa1e7197355bb28d10de81fe2e1
elan <elan@65305748-3e16-e211-a854-8c89a5c6f760>
1992-09-16 10:54:44 +0000

    copyright[] -> static

--- usr.sbin/lpr/lptest/lptest.c
+++ usr.sbin/lpr/lptest/lptest.c
@@ -7,13 +7,13 @@
  */
 
 #ifndef lint
-char copyright[] =
+static char copyright[] =
 "@(#) Copyright (c) 1983 The Regents of the University of California.\n\
  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)lptest.c	5.7 (Berkeley) 09/15/92";
+static char sccsid[] = "@(#)lptest.c	5.8 (Berkeley) 09/16/92";
 #endif /* not lint */
 
 #include <stdlib.h>
