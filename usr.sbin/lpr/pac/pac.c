618b231a37c5c43abc7abddfc3f8acee0986fda9
elan <elan@65305748-3e16-e211-a854-8c89a5c6f760>
1992-09-16 10:55:11 +0000

    copyright[] -> static.

--- usr.sbin/lpr/pac/pac.c
+++ usr.sbin/lpr/pac/pac.c
@@ -7,13 +7,13 @@
  */
 
 #ifndef lint
-char copyright[] =
+static char copyright[] =
 "@(#) Copyright (c) 1983 The Regents of the University of California.\n\
  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)pac.c	5.8 (Berkeley) 09/15/92";
+static char sccsid[] = "@(#)pac.c	5.9 (Berkeley) 09/16/92";
 #endif /* not lint */
 
 /*
