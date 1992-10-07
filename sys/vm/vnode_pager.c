1f1d28f3ba3887b6b73b99fd672e39d308c74fb2
margo <margo@65305748-3e16-e211-a854-8c89a5c6f760>
1992-10-07 17:18:54 +0000

    Add argument to VOP_BMAP.

--- sys/vm/vnode_pager.c
+++ sys/vm/vnode_pager.c
@@ -9,7 +9,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vnode_pager.c	7.14 (Berkeley) 10/01/92
+ *	@(#)vnode_pager.c	7.15 (Berkeley) 10/07/92
  */
 
 /*
@@ -261,7 +261,7 @@ vnode_pager_haspage(pager, offset)
 	 */
 	err = VOP_BMAP(vnp->vnp_vp,
 		       offset / vnp->vnp_vp->v_mount->mnt_stat.f_iosize,
-		       (struct vnode **)0, &bn);
+		       (struct vnode **)0, &bn, NULL);
 	if (err) {
 #ifdef DEBUG
 		if (vpagerdebug & VDB_FAIL)
