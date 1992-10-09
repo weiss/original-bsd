a60a9e82e4ff62931e6b1d4f32b5c536eb54c0b4
sklower <sklower@65305748-3e16-e211-a854-8c89a5c6f760>
1992-10-09 01:08:03 +0000

    correct defective copyright dates

--- sys/netiso/tuba_usrreq.c
+++ sys/netiso/tuba_usrreq.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1982, 1986, 1988 Regents of the University of California.
+ * Copyright (c) 1992 Regents of the University of California.
  * All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)tuba_usrreq.c	7.1 (Berkeley) 10/09/92
+ *	@(#)tuba_usrreq.c	7.2 (Berkeley) 10/09/92
  */
 
 #include "param.h"
