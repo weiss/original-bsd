0d68e7c9a7a113d05d14b024be8dff6bfac0636a
ralph <ralph@65305748-3e16-e211-a854-8c89a5c6f760>
1992-10-24 14:19:40 +0000

    use COPY_SIGCODE now.

--- sys/pmax/include/cpu.h
+++ sys/pmax/include/cpu.h
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)cpu.h	7.5 (Berkeley) 10/11/92
+ *	@(#)cpu.h	7.6 (Berkeley) 10/24/92
  */
 
 #ifndef _CPU_H_
@@ -23,7 +23,7 @@
  * definitions of cpu-dependent requirements
  * referenced in generic code
  */
-#undef	COPY_SIGCODE		/* copy sigcode above user stack in exec */
+#define	COPY_SIGCODE		/* copy sigcode above user stack in exec */
 
 #define	cpu_exec(p)	(p->p_md.md_ss_addr = 0) /* init single step */
 #define	cpu_wait(p)	/* nothing */
