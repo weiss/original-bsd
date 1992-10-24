7b65564928a3b1f5c5e375141aee28f709a73925
ralph <ralph@65305748-3e16-e211-a854-8c89a5c6f760>
1992-10-24 14:04:44 +0000

    remove unneeded include files

--- sys/pmax/dev/asc.c
+++ sys/pmax/dev/asc.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)asc.c	7.6 (Berkeley) 10/11/92
+ *	@(#)asc.c	7.7 (Berkeley) 10/24/92
  */
 
 /* 
@@ -96,9 +96,6 @@
 
 #include <sys/param.h>
 #include <sys/systm.h>
-#include <sys/dkstat.h>
-#include <sys/buf.h>
-#include <sys/conf.h>
 #include <sys/errno.h>
 
 #include <pmax/dev/device.h>
