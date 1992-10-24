a3739771e2a1017318fcbaef95f5090c6d91eec9
ralph <ralph@65305748-3e16-e211-a854-8c89a5c6f760>
1992-10-24 15:16:26 +0000

    fix gcc2 warning about register char array.

--- usr.bin/ar/archive.c
+++ usr.bin/ar/archive.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)archive.c	5.8 (Berkeley) 06/19/92";
+static char sccsid[] = "@(#)archive.c	5.9 (Berkeley) 10/24/92";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -103,7 +103,8 @@ get_arobj(fd)
 {
 	struct ar_hdr *hdr;
 	register int len, nr;
-	register char *p, buf[20];
+	register char *p;
+	char buf[20];
 
 	nr = read(fd, hb, sizeof(HDR));
 	if (nr != sizeof(HDR)) {
