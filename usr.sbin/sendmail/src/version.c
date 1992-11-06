5c516dd622667909e8f27df5628bfa48e086a442
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1992-11-06 11:28:04 +0000

    some debugging hooks for -bt mode

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	5.119 (Berkeley) 11/04/92";
+static char sccsid[] = "@(#)version.c	5.120 (Berkeley) 11/06/92";
 #endif /* not lint */
 
-char	Version[] = "5.119";
+char	Version[] = "5.120";
