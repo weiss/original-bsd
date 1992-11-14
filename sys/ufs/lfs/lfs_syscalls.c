7bbd7642c0bd7709fde4c3730009def58a354753
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1992-11-14 23:00:35 +0000

    ufs_iput is obsoleted by vput

--- sys/ufs/lfs/lfs_syscalls.c
+++ sys/ufs/lfs/lfs_syscalls.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lfs_syscalls.c	7.26 (Berkeley) 10/24/92
+ *	@(#)lfs_syscalls.c	7.27 (Berkeley) 11/14/92
  */
 
 #include <sys/param.h>
@@ -462,7 +462,7 @@ lfs_fastvget(mp, ino, daddr, vpp, dinp)
 			ufs_ihashrem(ip);
 
 			/* Unlock and discard unneeded inode. */
-			ufs_iput(ip);
+			vput(vp);
 			brelse(bp);
 			*vpp = NULL;
 			return (error);
@@ -479,7 +479,7 @@ lfs_fastvget(mp, ino, daddr, vpp, dinp)
 	 * cases re-init ip, the underlying vnode/inode may have changed.
 	 */
 	if (error = ufs_vinit(mp, lfs_specop_p, LFS_FIFOOPS, &vp)) {
-		ufs_iput(ip);
+		vput(vp);
 		*vpp = NULL;
 		return (error);
 	}
