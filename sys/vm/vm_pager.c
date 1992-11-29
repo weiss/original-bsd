d1a46e81a09baf9930d0674cee2da421e26c4081
hibler <hibler@65305748-3e16-e211-a854-8c89a5c6f760>
1992-11-29 10:22:20 +0000

    cleanup

--- sys/vm/vm_pager.c
+++ sys/vm/vm_pager.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vm_pager.c	7.9 (Berkeley) 10/01/92
+ *	@(#)vm_pager.c	7.10 (Berkeley) 11/29/92
  *
  *
  * Copyright (c) 1987, 1990 Carnegie-Mellon University.
@@ -190,7 +190,7 @@ vm_pager_map_page(m)
 	vm_offset_t kva;
 
 #ifdef DEBUG
-	if (!(m->flags & PG_BUSY) || (m->flags & PG_ACTIVE))
+	if ((m->flags & (PG_BUSY | PG_ACTIVE)) != PG_BUSY)
 		panic("vm_pager_map_page: page active or not busy");
 	if (m->flags & PG_PAGEROWNED)
 		printf("vm_pager_map_page: page %x already in pager\n", m);
