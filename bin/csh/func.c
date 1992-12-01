6a83f1d2b8cf6ed45498bae69f6df9c00cf74c29
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1992-12-01 13:43:26 +0000

    RLIMIT_OFILE => RLIMIT_NOFILE for compatibility with other UNIX's

--- bin/csh/func.c
+++ bin/csh/func.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)func.c	5.37 (Berkeley) 07/12/92";
+static char sccsid[] = "@(#)func.c	5.38 (Berkeley) 12/01/92";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -1106,7 +1106,7 @@ static struct limits {
     RLIMIT_RSS,		"memoryuse",	1024,	"kbytes",
     RLIMIT_MEMLOCK,	"memorylocked",	1024,	"kbytes",
     RLIMIT_NPROC,	"maxproc",	1,	"",
-    RLIMIT_OFILE,	"openfiles",	1,	"",
+    RLIMIT_NOFILE,	"openfiles",	1,	"",
     -1,			NULL,		0,	NULL
 };
 
