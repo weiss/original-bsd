de794f9c607f7f6e8fdcf0771d6f6a9ed98c87a5
ralph <ralph@65305748-3e16-e211-a854-8c89a5c6f760>
1992-12-05 12:25:20 +0000

    need to bump 'ap' up to the next double aligned address, then add

--- sys/pmax/include/varargs.h
+++ sys/pmax/include/varargs.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)varargs.h	7.1 (Berkeley) 11/04/92
+ *	@(#)varargs.h	7.2 (Berkeley) 12/05/92
  */
 
 #ifndef _VARARGS_H_
@@ -24,7 +24,7 @@ typedef char *va_list;
 #define	va_arg(ap, type) \
 	((type *)(ap += sizeof(type) == sizeof(int) ? sizeof(type) : \
 		sizeof(type) > sizeof(int) ? \
-		(-(int)(ap) & ~(sizeof(type) - 1)) + sizeof(type) : \
+		(-(int)(ap) & (sizeof(type) - 1)) + sizeof(type) : \
 		(abort(), 0)))[-1]
 #endif
 
