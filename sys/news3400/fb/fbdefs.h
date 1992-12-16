67bd5e641223d68724e389878ee27c4a62d4ca25
utashiro <utashiro@65305748-3e16-e211-a854-8c89a5c6f760>
1992-12-16 21:56:22 +0000

    WB_FLUSH macro calls MachEmptyWriteBuffer()

--- sys/news3400/fb/fbdefs.h
+++ sys/news3400/fb/fbdefs.h
@@ -9,7 +9,7 @@
  *
  * from: $Hdr: fbdefs.h,v 4.300 91/06/09 06:33:54 root Rel41 $ SONY
  *
- *	@(#)fbdefs.h	7.1 (Berkeley) 06/04/92
+ *	@(#)fbdefs.h	7.2 (Berkeley) 12/16/92
  */
 
 #ifndef mips
@@ -42,7 +42,7 @@
 #endif /* CPU_SINGLE */
 
 #if defined(news3400) || defined(news3700)
-#define WB_FLUSH	wb_flush()
+#define WB_FLUSH	MachEmptyWriteBuffer()
 #else
 #define WB_FLUSH
 #endif
