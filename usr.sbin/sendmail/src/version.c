d122a9f555bd1e176deda1b56f832360fce095ce
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1992-12-16 17:24:43 +0000

    clearly mark the version number as an alpha

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	5.134 (Berkeley) 12/15/92";
+static char sccsid[] = "@(#)version.c	5.135 (Berkeley) 12/16/92";
 #endif /* not lint */
 
-char	Version[] = "5.134";
+char	Version[] = "ALPHA-5.135";
