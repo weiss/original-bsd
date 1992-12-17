f6e583ac5e779a3c7f9fa6fad79a236f04fc5c24
utashiro <utashiro@65305748-3e16-e211-a854-8c89a5c6f760>
1992-12-17 02:44:13 +0000

    make kernel includes standard

--- sys/news3400/fb/fbdefs.h
+++ sys/news3400/fb/fbdefs.h
@@ -9,7 +9,7 @@
  *
  * from: $Hdr: fbdefs.h,v 4.300 91/06/09 06:33:54 root Rel41 $ SONY
  *
- *	@(#)fbdefs.h	7.2 (Berkeley) 12/16/92
+ *	@(#)fbdefs.h	7.3 (Berkeley) 12/17/92
  */
 
 #ifndef mips
@@ -34,7 +34,7 @@
 #ifdef IPC_MRX
 #define FB_LOCK
 #define FB_UNLOCK
-#include "../fb/fbsem.h" /* semaphore name */
+#include <news3400/fb/fbsem.h> /* semaphore name */
 #else /* IPC_MRX */
 #define FB_LOCK		wait(bitmap_use)
 #define FB_UNLOCK	signal(bitmap_use)
