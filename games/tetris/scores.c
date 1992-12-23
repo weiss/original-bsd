725a81688fdb6a6f8dca12628589fb89a5a72154
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1992-12-23 10:16:49 +0000

    update copyright notice with contrib notice

--- games/tetris/scores.c
+++ games/tetris/scores.c
@@ -2,13 +2,16 @@
  * Copyright (c) 1992 The Regents of the University of California.
  * All rights reserved.
  *
+ * This code is derived from software contributed to Berkeley by
+ * Chris Torek and Darren F. Provine.
+ *
  * %sccs.include.redist.c%
  *
- *	@(#)scores.c	5.1 (Berkeley) 12/22/92
+ *	@(#)scores.c	5.2 (Berkeley) 12/23/92
  */
 
 /*
- * score code for Tetris, by Darren Provine (kilroy@gboro.glassboro.edu)
+ * Score code for Tetris, by Darren Provine (kilroy@gboro.glassboro.edu)
  * modified 22 January 1992, to limit the number of entries any one
  * person has.
  *
--- games/tetris/tetris.c
+++ games/tetris/tetris.c
@@ -2,9 +2,12 @@
  * Copyright (c) 1992 The Regents of the University of California.
  * All rights reserved.
  *
+ * This code is derived from software contributed to Berkeley by
+ * Chris Torek and Darren F. Provine.
+ *
  * %sccs.include.redist.c%
  *
- *	@(#)tetris.c	5.1 (Berkeley) 12/22/92
+ *	@(#)tetris.c	5.2 (Berkeley) 12/23/92
  */
 
 #ifndef lint
