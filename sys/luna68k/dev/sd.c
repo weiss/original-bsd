8005f0a5c45e023cf68ee5fef6ad7fa726861e52
akito <akito@65305748-3e16-e211-a854-8c89a5c6f760>
1992-12-31 10:10:50 +0000

    add DIOCWLABEL DIOCSDINFO DIOCWDINFO

--- sys/luna68k/dev/sd.c
+++ sys/luna68k/dev/sd.c
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sd.c	7.4 (Berkeley) 10/11/92
+ *	@(#)sd.c	7.5 (Berkeley) 12/31/92
  */
 
 /*
@@ -540,6 +540,11 @@ sdioctl(dev, cmd, data, flag, p)
 		((struct partinfo *)data)->disklab = lp;
 		((struct partinfo *)data)->part =
 		    &lp->d_partitions[sdpart(dev)];
+		break;
+
+        case DIOCWLABEL:
+        case DIOCSDINFO:
+        case DIOCWDINFO:
 		break;
 
 	default:
