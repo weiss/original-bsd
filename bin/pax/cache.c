10b29097a77c2f4e7b24b717082aac2c3222d486
muller <muller@65305748-3e16-e211-a854-8c89a5c6f760>
1993-01-15 14:10:36 +0000

    fixed a typo

--- bin/pax/cache.c
+++ bin/pax/cache.c
@@ -10,7 +10,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)cache.c	1.1 (Berkeley) 12/17/92";
+static char sccsid[] = "@(#)cache.c	1.2 (Berkeley) 01/15/93";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -425,7 +425,7 @@ gid_name(name, gid)
 
 	if (!gropn) {
 		setgroupent(1);
-		++pwopn;
+		++gropn;
 	}
 	if (ptr == NULL)
 		ptr = (GIDC *)malloc(sizeof(GIDC));
