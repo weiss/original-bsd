6abb0563183dc810724122eae558a2893fca475d
torek <torek@65305748-3e16-e211-a854-8c89a5c6f760>
1993-01-15 00:06:29 +0000

    emit correct syntax

--- usr.sbin/config.new/mkioconf.c
+++ usr.sbin/config.new/mkioconf.c
@@ -13,7 +13,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mkioconf.c	5.1 (Berkeley) 01/12/93
+ *	@(#)mkioconf.c	5.2 (Berkeley) 01/15/93
  *
  * from: $Header: mkioconf.c,v 1.2 92/12/08 19:34:31 torek Exp $
  */
@@ -298,7 +298,7 @@ emitpseudo(fp)
 		return (1);
 	for (i = allpseudo; i != NULL; i = i->i_next) {
 		d = i->i_base;
-		if (fprintf(fp, "\t{ %sattach, %d }\n",
+		if (fprintf(fp, "\t{ %sattach, %d },\n",
 		    d->d_name, d->d_umax) < 0)
 			return (1);
 	}
