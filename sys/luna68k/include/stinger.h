aa727c3beee24aa5f7926df5c741f0b0c141d2a8
akito <akito@65305748-3e16-e211-a854-8c89a5c6f760>
1993-01-18 19:11:58 +0000

    add some member to struct KernInter

--- sys/luna68k/include/stinger.h
+++ sys/luna68k/include/stinger.h
@@ -8,24 +8,20 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)stinger.h	7.2 (Berkeley) 10/11/92
+ *	@(#)stinger.h	7.3 (Berkeley) 01/18/93
  */
 
 /*
  * stinger.h -- Stinger Kernel Interface Definitions
- * by A.Fujita, MAR-14-1992
- *
- * This file should be remade.
+ *   remade by A.Fujita, JAN-12-1993
  */
 
-#include <machine/rdvar.h>
-#include <machine/memory.h>
-
 struct KernInter {
-	caddr_t	  maxaddr;
-	struct prgmem prg;
-	int	  argc;
-	char	**argv;
-	struct rd_partition *rd;
+	caddr_t	maxaddr;
+	u_int	dipsw;
+	int	plane;
 };
 
+extern struct KernInter KernInter;
+
+#define	KIFF_DIPSW_NOBM		0x0002		/* not use bitmap display as console */
