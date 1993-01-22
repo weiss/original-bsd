72988bdb88e1fb4b2bcd6104041805f4355463a5
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-01-22 17:13:15 +0000

    cfree -> free

--- usr.bin/ctags/tree.c
+++ usr.bin/ctags/tree.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)tree.c	5.5 (Berkeley) 02/26/91";
+static char sccsid[] = "@(#)tree.c	5.6 (Berkeley) 01/22/93";
 #endif /* not lint */
 
 #include <errno.h>
@@ -104,7 +104,7 @@ free_tree(node)
 	while (node) {
 		if (node->right)
 			free_tree(node->right);
-		cfree(node);
+		free(node);
 		node = node->left;
 	}
 }
