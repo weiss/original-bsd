bfe700d38aaddc3a270267595cdb5ebc622b1a69
torek <torek@65305748-3e16-e211-a854-8c89a5c6f760>
1993-02-01 11:20:04 +0000

    rcsid

--- sys/dev/scsi/scsi_ioctl.h
+++ sys/dev/scsi/scsi_ioctl.h
@@ -13,9 +13,9 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)scsi_ioctl.h	5.2 (Berkeley) 07/23/92
+ *	@(#)scsi_ioctl.h	5.3 (Berkeley) 02/01/93
  *
- * from: $Header: scsi_ioctl.h,v 1.2 92/05/15 11:24:02 torek Exp $ (LBL)
+ * from: $Header: scsi_ioctl.h,v 1.3 92/12/02 03:54:19 torek Exp $ (LBL)
  */
 
 /*
