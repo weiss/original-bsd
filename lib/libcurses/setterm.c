869f2945fe439d9c6e1f43106e36c198bbf91363
elan <elan@65305748-3e16-e211-a854-8c89a5c6f760>
1993-02-03 16:41:18 +0000

    Fixed missing "db".

--- lib/libcurses/setterm.c
+++ lib/libcurses/setterm.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)setterm.c	5.14 (Berkeley) 01/11/93";
+static char sccsid[] = "@(#)setterm.c	5.15 (Berkeley) 02/03/93";
 #endif /* not lint */
 
 #include <sys/ioctl.h>
@@ -143,7 +143,7 @@ zap()
 #endif
 	tmp[2] = '\0';
 
-	namp = "ambsdadbeohchzinmimsncnsosulxbxnxtxsxx";
+	namp = "ambsdaeohchzinmimsncnsosulxbxnxtxsxx";
 	fp = sflags;
 	do {
 		*tmp = *namp;
