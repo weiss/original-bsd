76cf45ddbae8285855de516544647b04db69ceb8
elan <elan@65305748-3e16-e211-a854-8c89a5c6f760>
1993-02-03 17:30:05 +0000

    Do not do addstr("\n\n\n") rather 3 deleteln

--- games/hangman/endgame.c
+++ games/hangman/endgame.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)endgame.c	5.3 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)endgame.c	5.4 (Berkeley) 02/03/93";
 #endif /* not lint */
 
 # include	"hangman.h"
@@ -43,5 +43,17 @@ endgame()
 
 	leaveok(stdscr, TRUE);
 	move(MESGY, MESGX);
-	addstr("\n\n\n");
+	deleteln();
+	deleteln();
+	deleteln();
 }
+
+
+
+
+
+
+
+
+
+
