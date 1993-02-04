cb05bafbd817325e1791a307b485a2c8880f0d04
akito <akito@65305748-3e16-e211-a854-8c89a5c6f760>
1993-02-04 15:23:15 +0000

    reset Tx interrupt

--- sys/luna68k/dev/bmc.c
+++ sys/luna68k/dev/bmc.c
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)bmc.c	7.5 (Berkeley) 12/25/92
+ *	@(#)bmc.c	7.6 (Berkeley) 02/04/93
  */
 
 #define	BMC_NOCONSOLE
@@ -567,6 +567,10 @@ bmcintr(unit)
 		code = (c & KC_CHAR);
 		if ((tp->t_state & TS_ISOPEN) != 0)
 			(*linesw[tp->t_line].l_rint)(code, tp);
+	}
+
+	if (rr & RR_TXRDY) {
+		sio->sio_cmd = WR0_RSTPEND;
 	}
 }
 
