7d4580ee0aa5c6a6e6201f656ab82f485036cb57
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-02-10 20:38:43 +0000

    bpf doesn't need sys/dir.h

--- sys/net/bpf.c
+++ sys/net/bpf.c
@@ -9,7 +9,7 @@
  *
  * %sccs.include.redist.c%
  *
- *      @(#)bpf.c	7.12 (Berkeley) 07/16/92
+ *      @(#)bpf.c	7.13 (Berkeley) 02/10/93
  *
  * static char rcsid[] =
  * "$Header: bpf.c,v 1.33 91/10/27 21:21:58 mccanne Exp $";
@@ -29,7 +29,6 @@
 #include <sys/systm.h>
 #include <sys/mbuf.h>
 #include <sys/buf.h>
-#include <sys/dir.h>
 #include <sys/time.h>
 #include <sys/proc.h>
 #include <sys/user.h>
