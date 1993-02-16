68e0032f496d1e724afdda7ed661c93a0b796f20
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-02-16 12:33:14 +0000

    bcmp/bcopy/bzero -> memcmp/memmove/memset

--- lib/libc/db/test/dbtest.c
+++ lib/libc/db/test/dbtest.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)dbtest.c	5.9 (Berkeley) 01/10/93";
+static char sccsid[] = "@(#)dbtest.c	5.10 (Berkeley) 02/16/93";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -572,7 +572,7 @@ xmalloc(text, len)
 
 	if ((p = malloc(len)) == NULL)
 		err("%s", strerror(errno));
-	bcopy(text, p, len);
+	memmove(p, text, len);
 	return (p);
 }
 
