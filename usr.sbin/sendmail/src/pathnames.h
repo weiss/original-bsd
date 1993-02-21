245f7a0c22c76218f16dd784104f9be55773ffe9
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-02-21 14:43:19 +0000

    put pid file in /var/run on 4.4bsd (not /etc)

--- usr.sbin/sendmail/src/pathnames.h
+++ usr.sbin/sendmail/src/pathnames.h
@@ -4,9 +4,13 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	6.2 (Berkeley) 02/20/93
+ *	@(#)pathnames.h	6.3 (Berkeley) 02/21/93
  */
 
 #define	_PATH_SENDMAILCF	"/etc/sendmail.cf"
 #define	_PATH_SENDMAILFC	"/etc/sendmail.fc"
+#ifdef BSD4_4
+#define _PATH_SENDMAILPID	"/var/run/sendmail.pid"
+#else
 #define _PATH_SENDMAILPID	"/etc/sendmail.pid"
+#endif
