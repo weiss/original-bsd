41fc8ee13c1fe395b88b6e30bb5bbf01bbf138c4
torek <torek@65305748-3e16-e211-a854-8c89a5c6f760>
1993-02-23 18:38:10 +0000

    sys_siglist is declared in <signal.h> now; do not misdeclare it here

--- usr.bin/ex/ex_unix.c
+++ usr.bin/ex/ex_unix.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)ex_unix.c	7.11 (Berkeley) 04/17/91";
+static char sccsid[] = "@(#)ex_unix.c	7.12 (Berkeley) 02/23/93";
 #endif /* not lint */
 
 #include "ex.h"
@@ -339,7 +339,6 @@ waitfor()
 {
 	union wait stat, pstat;
 	int wpid;
-	extern char *sys_siglist[];
 
 	pstat.w_status = 0;
 	do {
