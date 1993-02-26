ca8545719fa01eb1c499e8f64a364b2f2fc23f14
ralph <ralph@65305748-3e16-e211-a854-8c89a5c6f760>
1993-02-26 19:08:58 +0000

    change name of minbrk and curbrk for profiling.

--- lib/libc/mips/sys/brk.s
+++ lib/libc/mips/sys/brk.s
@@ -11,20 +11,20 @@
 #include "SYS.h"
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	ASMSTR("@(#)brk.s	5.2 (Berkeley) 02/04/93")
+	ASMSTR("@(#)brk.s	5.3 (Berkeley) 02/26/93")
 #endif /* LIBC_SCCS and not lint */
 
 #define	SYS_brk		17
 
 LEAF(brk)
-	lw	v0, _minbrk
+	lw	v0, minbrk
 	bgeu	a0, v0, _brk
 	move	a0, v0		# dont allow break < minbrk
 ALEAF(_brk)
 	li	v0, SYS_brk
 	syscall
 	bne	a3, zero, 1f
-	sw	a0, _curbrk
+	sw	a0, curbrk
 	move	v0, zero
 	j	ra
 1:
