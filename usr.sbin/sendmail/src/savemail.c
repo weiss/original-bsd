9c89c67090f35e0358bd4e3195e56c3998a2ea8f
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-02-27 12:00:22 +0000

    fix from Eric Wassenaar for return message suppression

--- usr.sbin/sendmail/src/savemail.c
+++ usr.sbin/sendmail/src/savemail.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)savemail.c	6.13 (Berkeley) 02/24/93";
+static char sccsid[] = "@(#)savemail.c	6.14 (Berkeley) 02/27/93";
 #endif /* not lint */
 
 # include <sys/types.h>
@@ -63,7 +63,6 @@ savemail(e)
 
 	if (bitset(EF_RESPONSE, e->e_flags))
 		return;
-	ForceMail = TRUE;
 	e->e_flags &= ~EF_FATALERRS;
 
 	/*
@@ -194,6 +193,9 @@ savemail(e)
 			**	joe@x, which gives a response, etc.  Also force
 			**	the mail to be delivered even if a version of
 			**	it has already been sent to the sender.
+			**
+			**	Clever technique for computing rpath from
+			**	Eric Wassenaar <e07@nikhef.nl>.
 			*/
 
 			if (state == ESM_MAIL)
@@ -202,13 +204,11 @@ savemail(e)
 				{
 					char *rpath;
 
-					if (strcmp(e->e_returnpath, "<>") != 0)
+					if (e->e_returnpath != e->e_sender)
 						rpath = e->e_returnpath;
-					else if (strcmp(e->e_from.q_paddr, "<>") != 0)
-						rpath = e->e_from.q_paddr;
 					else
-						rpath = NULL;
-					if (rpath != NULL)
+						rpath = e->e_from.q_paddr;
+					if (strcmp(rpath, "<>") != 0)
 						(void) sendtolist(rpath,
 							  (ADDRESS *) NULL,
 							  &e->e_errorqueue, e);
