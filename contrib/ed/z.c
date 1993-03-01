f6c9e771c7ce1ad05cb36903321907a5860bc63e
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-01 17:04:38 +0000

    new version from Rodney

--- contrib/ed/z.c
+++ contrib/ed/z.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)z.c	5.3 (Berkeley) 02/28/93";
+static char sccsid[] = "@(#)z.c	5.4 (Berkeley) 03/01/93";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -55,7 +55,7 @@ z(inputt, errnum)
 			if (rol(inputt, errnum))
 				return;
 		}
-	if (start == NULL) {
+	if (top == NULL) {
 		strcpy(help_msg, "buffer empty");
 		*errnum = -1;
 		ungetc('\n', inputt);
@@ -72,6 +72,12 @@ z(inputt, errnum)
 		}
 	} else
 		start = End;
+	if (start == NULL) {
+		strcpy(help_msg, "bad address");
+		*errnum = -1;
+		ungetc('\n', inputt);
+		return;
+	}
 	start_default = End_default = 0;
 
 	current = start;
