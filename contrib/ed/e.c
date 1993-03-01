679b246b5a8c4ac2761d840b659732be2360afd1
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-01 16:56:26 +0000

    STDIO fix

--- contrib/ed/e.c
+++ contrib/ed/e.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)e.c	5.3 (Berkeley) 02/28/93";
+static char sccsid[] = "@(#)e.c	5.4 (Berkeley) 03/01/93";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -136,6 +136,7 @@ e2(inputt, errnum)
 	if (fhtmp == NULL) {
 		ed_exit(5); /* unable to create buffer */
 	}
+	fwrite("R", sizeof(char), 1, fhtmp);
 	file_seek = 0;
 #endif
 #ifdef DBI
