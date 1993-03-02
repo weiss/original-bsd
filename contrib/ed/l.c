18387032ceea87d3a70cdbdcbdfb652b96de7831
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-02 08:54:06 +0000

    typo

--- contrib/ed/l.c
+++ contrib/ed/l.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)l.c	5.3 (Berkeley) 02/28/93";
+static char sccsid[] = "@(#)l.c	5.4 (Berkeley) 03/02/93";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -28,7 +28,7 @@ static char sccsid[] = "@(#)l.c	5.3 (Berkeley) 02/28/93";
 
 /*
  * This is the list command. It's not wrapped in with n and p because
- * of the unambiguous printing needed.
+ * of the unambiguous printing need.
  */
 void
 l(inputt, errnum)
