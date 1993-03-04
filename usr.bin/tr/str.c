fe3424b387f622ebc19a9cadc9ab86ce86bce1d2
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-04 16:46:36 +0000

    revision 5.8 broke tr, this fix handles blah, blah, blah test case #3: echo 'foo' | obj/tr 'fo' '\aX'

--- usr.bin/tr/str.c
+++ usr.bin/tr/str.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)str.c	5.8 (Berkeley) 03/02/93";
+static char sccsid[] = "@(#)str.c	5.9 (Berkeley) 03/04/93";
 #endif /* not lint */
 
 #include <sys/cdefs.h>
@@ -290,6 +290,8 @@ backslash(s)
 	}
 	if (cnt)
 		return (val);
+	if (ch != '\0')
+		++s->str;
 	switch (ch) {
 		case 'a':			/* escape characters */
 			return ('\7');
