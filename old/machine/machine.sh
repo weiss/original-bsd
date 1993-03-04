516cdc94c9f8990985305a3f06df90783137a7ae
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-04 13:09:14 +0000

    date and time created 93/03/04 13:09:14 by bostic

---
+++ old/machine/machine.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -
+#
+# Copyright (c) 1993 The Regents of the University of California.
+# All rights reserved.
+#
+# %sccs.include.redist.sh%
+#
+#	@(#)machine.sh	5.1 (Berkeley) 03/04/93
+#
+
+uname -m
