30399aaf691f373958457908bfe82762fa602957
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-04 17:58:17 +0000

    change buffer lengths of sysctl(2) to be size_t's

--- sys/kern/kern_clock.c
+++ sys/kern/kern_clock.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kern_clock.c	7.29 (Berkeley) 02/04/93
+ *	@(#)kern_clock.c	7.30 (Berkeley) 03/04/93
  */
 
 #include <sys/param.h>
@@ -461,7 +461,7 @@ statclock(frame)
  */
 sysctl_clockrate(where, sizep)
 	register char *where;
-	int *sizep;
+	size_t *sizep;
 {
 	struct clockinfo clkinfo;
 
