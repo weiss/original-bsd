bbcbd63afc03eb7baf73950bf392c3d1e0a181cd
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-04 18:47:34 +0000

    stupid stdargs.h fix

--- usr.bin/rlogin/rlogin.c
+++ usr.bin/rlogin/rlogin.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rlogin.c	5.38 (Berkeley) 03/04/93";
+static char sccsid[] = "@(#)rlogin.c	5.39 (Berkeley) 03/04/93";
 #endif /* not lint */
 
 /*
@@ -838,7 +838,6 @@ warning(fmt, va_alist)
 #else
 	va_start(ap);
 #endif
-	fmt = va_arg(ap, char *);
 	vfprintf(stderr, fmt, ap);
 	va_end(ap);
 	(void)fprintf(stderr, ".\n");
