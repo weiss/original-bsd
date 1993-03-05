861c9927460dc70a4667dd7fcb0745b4f7ff44e2
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-05 17:23:38 +0000

    make utime(NULL) work for POSIX 1003.1 compatibility

--- sys/kern/vfs_subr.c
+++ sys/kern/vfs_subr.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vfs_subr.c	7.95 (Berkeley) 03/04/93
+ *	@(#)vfs_subr.c	7.96 (Berkeley) 03/05/93
  */
 
 /*
@@ -190,6 +190,7 @@ void vattr_null(vap)
 		vap->va_mtime.ts_sec = vap->va_mtime.ts_nsec =
 		vap->va_ctime.ts_sec = vap->va_ctime.ts_nsec =
 		vap->va_flags = vap->va_gen = VNOVAL;
+	vap->va_cflags = 0;
 }
 
 /*
