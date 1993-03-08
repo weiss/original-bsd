91515ac9d0e65239fe00cb004486cd514bb5de13
ralph <ralph@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-08 19:25:40 +0000

    change the substitute pattern to match the range pattern. some versions of sed are broken to match on the last occurance and can strip the 'U' off of 'U Ulabel'.

--- usr.bin/lorder/lorder.sh
+++ usr.bin/lorder/lorder.sh
@@ -5,7 +5,7 @@
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)lorder.sh	5.3 (Berkeley) 04/12/91
+#	@(#)lorder.sh	5.4 (Berkeley) 03/08/93
 #
 
 PATH=/bin:/usr/bin
@@ -44,12 +44,12 @@ nm -go $* | sed "
 		d
 	}
 	/ [TD] / {
-		s/:.* [TD]//
+		s/:.* [TD] / /
 		w $S
 		d
 	}
 	/ U / {
-		s/:.* U//
+		s/:.* U / /
 		w $R
 	}
 	d
