4a884f8becf6766ecf20d78cadd408f8f82597ec
utashiro <utashiro@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-09 23:51:27 +0000

    cleanup header and machine dependency.

--- sys/news3400/iop/fb.c
+++ sys/news3400/iop/fb.c
@@ -9,10 +9,8 @@
  *
  * from: $Hdr: fb.c,v 4.300 91/06/27 20:43:06 root Rel41 $ SONY
  *
- *	@(#)fb.c	7.2 (Berkeley) 12/17/92
+ *	@(#)fb.c	7.3 (Berkeley) 03/09/93
  */
-
-#include <machine/fix_machine_type.h>
 
 #include "fb.h"
 #if NFB > 0
