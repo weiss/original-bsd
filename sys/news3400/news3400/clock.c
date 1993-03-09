0dfcb6f643f5ae9bafa571720c14c7638924e7d7
utashiro <utashiro@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-09 23:53:18 +0000

    cleanup header and machine dependency.

--- sys/news3400/news3400/clock.c
+++ sys/news3400/news3400/clock.c
@@ -12,10 +12,9 @@
  *
  * from: Utah $Hdr: clock.c 1.18 91/01/21$
  *
- *	@(#)clock.c	7.6 (Berkeley) 01/21/93
+ *	@(#)clock.c	7.7 (Berkeley) 03/09/93
  */
 
-#include <machine/fix_machine_type.h>
 #include <machine/adrsmap.h>
 
 #include <sys/param.h>
