0d57202a7f1e99bd13eb4ae4374b071fa5fdd628
utashiro <utashiro@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-09 23:50:29 +0000

    cleanup header and machine dependency.

--- sys/news3400/iodev/sd_var.c
+++ sys/news3400/iodev/sd_var.c
@@ -9,7 +9,7 @@
  *
  * from: $Hdr: sd_var.c,v 4.300 91/06/09 06:38:23 root Rel41 $ SONY
  *
- *	@(#)sd_var.c	7.2 (Berkeley) 12/17/92
+ *	@(#)sd_var.c	7.3 (Berkeley) 03/09/93
  */
 
 /*
@@ -22,8 +22,6 @@
 
 #include "sd.h"
 #if NSD > 0
-
-#include <machine/fix_machine_type.h>
 
 #include <sys/param.h>
 #include <sys/buf.h>
