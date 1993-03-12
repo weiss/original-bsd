19a56368b4a65a3e6faf2483fc361d26585be1c5
andrew <andrew@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-12 18:06:20 +0000

    (by sklower) obvious bug fix in mbuf starvation case

--- sys/netinet/ip_output.c
+++ sys/netinet/ip_output.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ip_output.c	7.30 (Berkeley) 02/12/93
+ *	@(#)ip_output.c	7.31 (Berkeley) 03/12/93
  */
 
 #include <sys/param.h>
@@ -313,6 +313,7 @@ sendit:
 		mhip->ip_len = htons((u_short)(len + mhlen));
 		m->m_next = m_copy(m0, off, len);
 		if (m->m_next == 0) {
+			m_free(m);
 			error = ENOBUFS;	/* ??? */
 			ipstat.ips_odropped++;
 			goto sendorfree;
