3f712029c4a0370dfab0bb12d9352c81c164df82
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-14 08:30:36 +0000

    don't send all addresses through recipient rewriting in buildaddr;

--- usr.sbin/sendmail/src/parseaddr.c
+++ usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	6.26 (Berkeley) 03/14/93";
+static char sccsid[] = "@(#)parseaddr.c	6.27 (Berkeley) 03/14/93";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -194,6 +194,9 @@ allocaddr(a, copyf, paddr, delimptr)
 	char *delimptr;
 {
 	register MAILER *m = a->q_mailer;
+
+	if (tTd(24, 4))
+		printf("allocaddr(copyf=%d, paddr=%s)\n", copyf, paddr);
 
 	if (copyf > 0 && paddr != NULL)
 	{
@@ -1166,10 +1169,7 @@ buildaddr(tv, a)
 		a->q_flags |= QNOTREMOTE;
 	}
 
-	/* rewrite according recipient mailer rewriting rules */
-	rewrite(tv, 2);
-	if (m->m_re_rwset > 0)
-		rewrite(tv, m->m_re_rwset);
+	/* do cleanup of final address */
 	rewrite(tv, 4);
 
 	/* save the result for the command line/RCPT argument */
