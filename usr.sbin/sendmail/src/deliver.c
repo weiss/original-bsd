8dadacffca9a6fc448a44c4189b1d7cf016c4a73
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-16 18:06:12 +0000

    pass envelope to disconnect to avoid another use of CurEnv; make disconnect more POSIXish

--- usr.sbin/sendmail/src/deliver.c
+++ usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	6.44 (Berkeley) 03/16/93";
+static char sccsid[] = "@(#)deliver.c	6.45 (Berkeley) 03/16/93";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -1790,7 +1790,7 @@ sendenvelope(e, mode)
 			exit(EX_OK);
 
 		/* be sure we are immune from the terminal */
-		disconnect(FALSE);
+		disconnect(FALSE, e);
 
 # ifdef LOCKF
 		/*
