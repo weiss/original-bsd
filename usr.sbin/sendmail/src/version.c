d60351057f6b20b57b13de41dac4dac0b2288528
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-17 10:36:28 +0000

    early-bind headers; minor logging changes; handle temp errors better; HPUX/SystemV portability enhancements

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.37 (Berkeley) 03/16/93";
+static char sccsid[] = "@(#)version.c	6.38 (Berkeley) 03/17/93";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.37";
+char	Version[] = "ALPHA-6.38";
