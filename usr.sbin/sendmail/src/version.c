5b67958f1f9aa4215f2a95dd71291c0c29f311c6
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-18 11:51:38 +0000

    restore e_sender as undecorated envelope from address; OSF/1 on Alpha changes; some minor compilation problems

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.38 (Berkeley) 03/17/93";
+static char sccsid[] = "@(#)version.c	6.39 (Berkeley) 03/18/93";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.38";
+char	Version[] = "ALPHA-6.39";
