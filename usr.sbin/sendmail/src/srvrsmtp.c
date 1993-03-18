85f84f17d3b0feef4a0fb8590641d0a412f6f67c
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-18 14:18:11 +0000

    print queue id on final 250 (after DATA)

--- usr.sbin/sendmail/src/srvrsmtp.c
+++ usr.sbin/sendmail/src/srvrsmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)srvrsmtp.c	6.25 (Berkeley) 03/18/93 (with SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	6.26 (Berkeley) 03/18/93 (with SMTP)";
 #else
-static char sccsid[] = "@(#)srvrsmtp.c	6.25 (Berkeley) 03/18/93 (without SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	6.26 (Berkeley) 03/18/93 (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -106,6 +106,7 @@ smtp(e)
 	char *protocol;			/* sending protocol */
 	long msize;			/* approximate maximum message size */
 	auto char *delimptr;
+	char *id;
 	char inp[MAXLINE];
 	char cmdbuf[MAXLINE];
 	char hostbuf[MAXNAME];
@@ -446,6 +447,7 @@ smtp(e)
 			}
 			e->e_flags &= ~EF_FATALERRS;
 			e->e_xfp = freopen(queuename(e, 'x'), "w", e->e_xfp);
+			id = e->e_id;
 
 			/* send to all recipients */
 			sendall(e, SM_DEFAULT);
@@ -456,7 +458,7 @@ smtp(e)
 
 			/* issue success if appropriate and reset */
 			if (Errors == 0 || HoldErrs)
-				message("250 Ok");
+				message("250 %s OK", id);
 			else
 				e->e_flags &= ~EF_FATALERRS;
 
