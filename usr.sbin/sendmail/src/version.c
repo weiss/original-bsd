ca146fd6425326853f2497d47f9689292fd38a89
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-18 19:39:50 +0000

    clean up unnecessary creates of queue ids; be sure to instantiate split envelopes in the queue; fix problem with leading spaces on owner-aliases acting as delimiters

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.40 (Berkeley) 03/18/93";
+static char sccsid[] = "@(#)version.c	6.41 (Berkeley) 03/18/93";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.40";
+char	Version[] = "ALPHA-6.41";
