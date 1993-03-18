40e48bc1f7a3a538c493f9e67a341d2b53d0b288
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-18 08:16:05 +0000

    define TOBUFSIZE

--- usr.sbin/sendmail/src/conf.h
+++ usr.sbin/sendmail/src/conf.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.h	6.12 (Berkeley) 03/17/93
+ *	@(#)conf.h	6.13 (Berkeley) 03/18/93
  */
 
 /*
@@ -161,4 +161,14 @@ struct utsname
 
 # include <sys/file.h>
 
+#endif
+
+/*
+**  Size of tobuf (deliver.c)
+**	Tweak this to match your syslog implementation.  It will have to
+**	allow for the extra information printed.
+*/
+
+#ifndef TOBUFSIZE
+# define TOBUFSIZE (1024 - 256)
 #endif
