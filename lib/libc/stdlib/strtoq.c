f8d2eae31f708fec600aa2976a0884aff409624f
torek <torek@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-23 11:37:00 +0000

    shut gcc2 up

--- lib/libc/stdlib/strtoq.c
+++ lib/libc/stdlib/strtoq.c
@@ -6,7 +6,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)strtoq.c	5.1 (Berkeley) 06/26/92";
+static char sccsid[] = "@(#)strtoq.c	5.2 (Berkeley) 03/23/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -105,6 +105,6 @@ strtoq(nptr, endptr, base)
 	} else if (neg)
 		acc = -acc;
 	if (endptr != 0)
-		*endptr = any ? s - 1 : (char *)nptr;
+		*endptr = (char *)(any ? s - 1 : nptr);
 	return (acc);
 }
