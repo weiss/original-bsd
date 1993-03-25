50044b9fedce710d4cdccb8c96bc21e8bedcea84
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-03-25 07:31:34 +0000

    Jeff Forys' changes to keep from leaving two slashes in the pathname GNU Emacs doesn't like this

--- lib/libc/stdio/tmpnam.c
+++ lib/libc/stdio/tmpnam.c
@@ -9,7 +9,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)tmpnam.c	5.3 (Berkeley) 02/24/91";
+static char sccsid[] = "@(#)tmpnam.c	5.4 (Berkeley) 03/25/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <unistd.h>
@@ -23,6 +23,6 @@ tmpnam(s)
 
 	if (s == NULL)
 		s = buf;
-	(void)snprintf(s, L_tmpnam, "%s/tmp.XXXXXX", P_tmpdir);
+	(void)snprintf(s, L_tmpnam, "%stmp.XXXXXX", P_tmpdir);
 	return(mktemp(s));
 }
