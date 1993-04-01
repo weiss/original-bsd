5950cf49cbc5637e8e7261e6ea05b7f20ca6f5c4
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-04-01 13:34:21 +0000

    diagnose incorrect privacy flags; quote backslashes in _phrase_; some FallBackMX fixes; don't ignore rest of queue if one job is locked; always respond "quickly" to RCPT; accept email for mailing lists with errors on the list

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.48 (Berkeley) 03/30/93";
+static char sccsid[] = "@(#)version.c	6.49 (Berkeley) 04/01/93";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.48";
+char	Version[] = "ALPHA-6.49";
