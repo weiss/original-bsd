c0a12534a2b4e284b26a53670ecedd324e2416bc
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-04-01 13:29:34 +0000

    strip leading spaces off of aliases

--- usr.sbin/sendmail/src/alias.c
+++ usr.sbin/sendmail/src/alias.c
@@ -26,15 +26,15 @@ ERROR: DBM is no longer supported -- use NDBM instead.
 #ifndef lint
 #ifdef NEWDB
 #ifdef NDBM
-static char sccsid[] = "@(#)alias.c	6.29 (Berkeley) 03/30/93 (with NEWDB and NDBM)";
+static char sccsid[] = "@(#)alias.c	6.30 (Berkeley) 04/01/93 (with NEWDB and NDBM)";
 #else
-static char sccsid[] = "@(#)alias.c	6.29 (Berkeley) 03/30/93 (with NEWDB)";
+static char sccsid[] = "@(#)alias.c	6.30 (Berkeley) 04/01/93 (with NEWDB)";
 #endif
 #else
 #ifdef NDBM
-static char sccsid[] = "@(#)alias.c	6.29 (Berkeley) 03/30/93 (with NDBM)";
+static char sccsid[] = "@(#)alias.c	6.30 (Berkeley) 04/01/93 (with NDBM)";
 #else
-static char sccsid[] = "@(#)alias.c	6.29 (Berkeley) 03/30/93 (without NEWDB or NDBM)";
+static char sccsid[] = "@(#)alias.c	6.30 (Berkeley) 04/01/93 (without NEWDB or NDBM)";
 #endif
 #endif
 #endif /* not lint */
@@ -628,6 +628,8 @@ readaliases(aliasfile, init, e)
 		**	'p' points to the text of the RHS.
 		*/
 
+		while (isascii(*p) && isspace(*p))
+			p++;
 		rhs = p;
 		for (;;)
 		{
