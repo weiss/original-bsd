d33a0cf1fbb4854abc2a41c4a1b6a56cb43e69dc
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-04-01 13:31:15 +0000

    use e_statmsg for logging of OK status

--- usr.sbin/sendmail/src/usersmtp.c
+++ usr.sbin/sendmail/src/usersmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)usersmtp.c	6.20 (Berkeley) 03/30/93 (with SMTP)";
+static char sccsid[] = "@(#)usersmtp.c	6.21 (Berkeley) 04/01/93 (with SMTP)";
 #else
-static char sccsid[] = "@(#)usersmtp.c	6.20 (Berkeley) 03/30/93 (without SMTP)";
+static char sccsid[] = "@(#)usersmtp.c	6.21 (Berkeley) 04/01/93 (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -358,7 +358,7 @@ smtpdata(m, mci, e)
 		return (EX_TEMPFAIL);
 	}
 	mci->mci_state = MCIS_OPEN;
-	e->e_message = newstr(&SmtpReplyBuffer[4]);
+	e->e_statmsg = newstr(&SmtpReplyBuffer[4]);
 	if (REPLYTYPE(r) == 4)
 		return (EX_TEMPFAIL);
 	else if (r == 250)
