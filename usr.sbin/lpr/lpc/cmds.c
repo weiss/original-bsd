6a814acb7abaea4666646251d52d74c1926fcc91
ralph <ralph@65305748-3e16-e211-a854-8c89a5c6f760>
1993-04-05 21:53:32 +0000

    change start to startcmd to avoid conflict with start in crt0.o

--- usr.sbin/lpr/lpc/cmds.c
+++ usr.sbin/lpr/lpc/cmds.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)cmds.c	5.11 (Berkeley) 09/16/92";
+static char sccsid[] = "@(#)cmds.c	5.12 (Berkeley) 04/05/93";
 #endif /* not lint */
 
 /*
@@ -635,7 +635,7 @@ restart(argc, argv)
  * Enable printing on the specified printer and startup the daemon.
  */
 void
-start(argc, argv)
+startcmd(argc, argv)
 	int argc;
 	char *argv[];
 {
