22ae566c60014744bce1def79b209647de1bce12
torek <torek@65305748-3e16-e211-a854-8c89a5c6f760>
1993-04-06 19:40:20 +0000

    spelling

--- sys/sparc/sunos/sun_disklabel.h
+++ sys/sparc/sunos/sun_disklabel.h
@@ -9,11 +9,11 @@
  * All advertising materials mentioning features or use of this software
  * must display the following acknowledgement:
  *	This product includes software developed by the University of
- *	California, Lawrence Berkeley Laboratories.
+ *	California, Lawrence Berkeley Laboratory.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sun_disklabel.h	7.2 (Berkeley) 07/21/92
+ *	@(#)sun_disklabel.h	7.3 (Berkeley) 04/06/93
  *
  * from: $Header: sun_disklabel.h,v 1.4 92/06/17 07:04:13 torek Exp $
  */
