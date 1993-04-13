24a839b2abc7f323c653d09b6289aaacb15e8bfe
elan <elan@65305748-3e16-e211-a854-8c89a5c6f760>
1993-04-13 15:01:44 +0000

    Move from line turns of __ISPASTEOL.

--- lib/libcurses/move.c
+++ lib/libcurses/move.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)move.c	5.11 (Berkeley) 01/11/93";
+static char sccsid[] = "@(#)move.c	5.12 (Berkeley) 04/13/93";
 #endif	/* not lint */
 
 #include <curses.h>
@@ -29,6 +29,7 @@ wmove(win, y, x)
 	if (x >= win->maxx || y >= win->maxy)
 		return (ERR);
 	win->curx = x;
+	win->lines[win->cury]->flags &= ~__ISPASTEOL;
 	win->cury = y;
 	win->lines[y]->flags &= ~__ISPASTEOL;
 	return (OK);
