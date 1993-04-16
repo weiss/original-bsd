4f324003824cea43fce5a99363d04b2b3d6b4f49
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1993-04-16 15:01:09 +0000

    ufs_free_netcred needs a return value

--- sys/ufs/ffs/ufs_vfsops.c
+++ sys/ufs/ffs/ufs_vfsops.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ufs_vfsops.c	7.67 (Berkeley) 04/08/93
+ *	@(#)ufs_vfsops.c	7.68 (Berkeley) 04/16/93
  */
 
 #include <sys/param.h>
@@ -220,6 +220,7 @@ ufs_free_netcred(rn, w)
 	caddr_t w;
 {
 	free((caddr_t)rn, M_NETADDR);
+	return (0);
 }
 	
 
