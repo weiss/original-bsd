99adf8ced98ee5ae221ad2ab8285041f6533631d
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-04-18 09:33:16 +0000

    use UCB standard for __P

--- usr.sbin/sendmail/src/useful.h
+++ usr.sbin/sendmail/src/useful.h
@@ -4,16 +4,16 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)useful.h	6.3 (Berkeley) 04/18/93
+ *	@(#)useful.h	6.4 (Berkeley) 04/18/93
  */
 
 # include <sys/types.h>
 
 /* support for ANSI prototypes (or not, as the case may be) */
-#if defined(__STDC__) && defined(_FORGIVING_CC_)
-#define P(protos)	protos
+#if defined(__STDC__) && defined(_FORGIVING_CC_) && !defined(__P)
+#define __P(protos)	protos
 #else
-#define P(protos)	()
+#define __P(protos)	()
 #endif
 
 /* support for bool type */
