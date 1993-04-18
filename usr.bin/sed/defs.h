7b5d131138d3d1a72eaa5d265cf45763b58dd3da
torek <torek@65305748-3e16-e211-a854-8c89a5c6f760>
1993-04-18 03:41:40 +0000

    fix broken label handling (rewrite label code completely)

--- usr.bin/sed/defs.h
+++ usr.bin/sed/defs.h
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)defs.h	5.4 (Berkeley) 04/14/93
+ *	@(#)defs.h	5.5 (Berkeley) 04/18/93
  */
 
 /*
@@ -64,7 +64,6 @@ struct s_command {
 	char code;				/* Command code */
 	u_int nonsel:1;				/* True if ! */
 	u_int inrange:1;			/* True if in range */
-	u_int lused:1;				/* True if label used. */
 };
 
 /*
