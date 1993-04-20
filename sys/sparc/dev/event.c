15c8d4883ecf5426e73ed10aa0305452fa742775
torek <torek@65305748-3e16-e211-a854-8c89a5c6f760>
1993-04-20 05:48:10 +0000

    spelling

--- sys/sparc/dev/event.c
+++ sys/sparc/dev/event.c
@@ -9,13 +9,13 @@
  * All advertising materials mentioning features or use of this software
  * must display the following acknowledgement:
  *	This product includes software developed by the University of
- *	California, Lawrence Berkeley Laboratories.
+ *	California, Lawrence Berkeley Laboratory.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)event.c	7.3 (Berkeley) 10/11/92
+ *	@(#)event.c	7.4 (Berkeley) 04/20/93
  *
- * from: $Header: event.c,v 1.4 92/06/17 05:35:45 torek Exp $ (LBL)
+ * from: $Header: event.c,v 1.5 92/11/26 01:10:44 torek Exp $ (LBL)
  */
 
 /*
