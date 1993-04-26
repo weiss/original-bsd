f5446f54efca040de7e139ca997a64521709326b
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-04-26 10:34:44 +0000

    ANSI C, prototypes, lint, reformatting to KNF still doesn't work, but at least it compiles cleanly

--- games/cribbage/pathnames.h
+++ games/cribbage/pathnames.h
@@ -4,9 +4,9 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.1 (Berkeley) 05/01/90
+ *	@(#)pathnames.h	5.2 (Berkeley) 04/26/93
  */
 
+#define	_PATH_INSTR	"/usr/share/games/cribbage.instr"
 #define	_PATH_LOG	"/var/games/criblog"
 #define	_PATH_MORE	"/usr/bin/more"
-#define	_PATH_INSTR	"/usr/share/games/cribbage.instr"
