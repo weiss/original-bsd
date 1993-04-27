301ae90072650679ac6b2b6bc3e63457d425742a
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1993-04-27 15:07:00 +0000

    kinfo_proc merged into user.h

--- sys/sys/user.h
+++ sys/sys/user.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)user.h	7.21 (Berkeley) 06/19/92
+ *	@(#)user.h	7.22 (Berkeley) 04/27/93
  */
 
 #include <machine/pcb.h>
@@ -19,7 +19,7 @@
 #include <sys/resourcevar.h>
 #include <sys/signalvar.h>
 #include <vm/vm.h>		/* XXX */
-#include <sys/kinfo_proc.h>
+#include <sys/sysctl.h>
 
 
 /*
