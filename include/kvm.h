bafb69071fb395f28a4cc232dfe3a1de0215f3f8
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-04-27 11:12:39 +0000

    add kvm_getloadavg

--- include/kvm.h
+++ include/kvm.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kvm.h	5.10 (Berkeley) 07/11/92
+ *	@(#)kvm.h	5.11 (Berkeley) 04/27/93
  */
 
 #ifndef _KVM_H_
@@ -26,6 +26,7 @@ int	  kvm_close __P((kvm_t *));
 char	**kvm_getargv __P((kvm_t *, const struct kinfo_proc *, int));
 char	**kvm_getenvv __P((kvm_t *, const struct kinfo_proc *, int));
 char	 *kvm_geterr __P((kvm_t *));
+int	  kvm_getloadavg __P((kvm_t *, double [], int));
 char	 *kvm_getfiles __P((kvm_t *, int, int, int *));
 struct kinfo_proc *
 	  kvm_getprocs __P((kvm_t *, int, int, int *));
