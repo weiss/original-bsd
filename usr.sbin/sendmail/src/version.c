edc7d77158d58dd15479b0058d7a88efb5d7ce95
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-04-27 07:16:57 +0000

    adjust ctladdr on Q runs; fix lost CNAME problem; getmxrr match against $=w instead of $j; "a" mailer flag + ESMTP enhancements; extend "b" option to include max message size; NeXT & HPUX compat changes; up default buffer sizes; fix backslash scanning problem; misc cleanup

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.54 (Berkeley) 04/19/93";
+static char sccsid[] = "@(#)version.c	6.55 (Berkeley) 04/27/93";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.54";
+char	Version[] = "ALPHA-6.55";
