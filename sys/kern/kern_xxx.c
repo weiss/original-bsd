f9b3adc0c65a676f06109363424dd22bb46b4044
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1993-04-27 15:13:11 +0000

    sysctl.h needs vm/vm.h

--- sys/kern/kern_xxx.c
+++ sys/kern/kern_xxx.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kern_xxx.c	7.23 (Berkeley) 04/05/93
+ *	@(#)kern_xxx.c	7.24 (Berkeley) 04/27/93
  */
 
 #include <sys/param.h>
@@ -12,6 +12,7 @@
 #include <sys/kernel.h>
 #include <sys/proc.h>
 #include <sys/reboot.h>
+#include <vm/vm.h>
 #include <sys/sysctl.h>
 
 struct reboot_args {
