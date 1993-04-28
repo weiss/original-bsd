2cbfb730130aab54592fb68439b60ec1fcca1d1f
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-04-28 18:18:23 +0000

    isatty(3) declared by unistd.h

--- contrib/ed/main.c
+++ contrib/ed/main.c
@@ -15,19 +15,20 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	5.8 (Berkeley) 04/28/93";
+static char sccsid[] = "@(#)main.c	5.9 (Berkeley) 04/28/93";
 #endif /* not lint */
 
 #include <sys/types.h>
 #include <sys/ioctl.h>
 
+#include <limits.h>
 #include <regex.h>
 #include <setjmp.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <limits.h>
+#include <unistd.h>
 
 #ifdef DBI
 #include <db.h>
