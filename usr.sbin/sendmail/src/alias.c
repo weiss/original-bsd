47b98ee7e7b079d052e4eccd45095fa373ea97b3
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-01 09:22:35 +0000

    strerror => errstring

--- usr.sbin/sendmail/src/alias.c
+++ usr.sbin/sendmail/src/alias.c
@@ -25,15 +25,15 @@ ERROR: DBM is no longer supported -- use NDBM instead.
 #ifndef lint
 #ifdef NEWDB
 #ifdef NDBM
-static char sccsid[] = "@(#)alias.c	6.34 (Berkeley) 04/30/93 (with NEWDB and NDBM)";
+static char sccsid[] = "@(#)alias.c	6.35 (Berkeley) 05/01/93 (with NEWDB and NDBM)";
 #else
-static char sccsid[] = "@(#)alias.c	6.34 (Berkeley) 04/30/93 (with NEWDB)";
+static char sccsid[] = "@(#)alias.c	6.35 (Berkeley) 05/01/93 (with NEWDB)";
 #endif
 #else
 #ifdef NDBM
-static char sccsid[] = "@(#)alias.c	6.34 (Berkeley) 04/30/93 (with NDBM)";
+static char sccsid[] = "@(#)alias.c	6.35 (Berkeley) 05/01/93 (with NDBM)";
 #else
-static char sccsid[] = "@(#)alias.c	6.34 (Berkeley) 04/30/93 (without NEWDB or NDBM)";
+static char sccsid[] = "@(#)alias.c	6.35 (Berkeley) 05/01/93 (without NEWDB or NDBM)";
 #endif
 #endif
 #endif /* not lint */
@@ -920,7 +920,7 @@ forward(user, sendq, e)
 				syslog(LOG_NOTICE, "%s: forward %s: transient error: %e",
 					e->e_id, buf);
 #endif
-			message("%s: %s: message queued", buf, strerror(err));
+			message("%s: %s: message queued", buf, errstring(err));
 			user->q_flags |= QQUEUEUP|QDONTSEND;
 			return;
 		}
