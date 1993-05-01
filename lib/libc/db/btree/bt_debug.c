1c522a8597dc0597ea5c046ab2151ae432970a82
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-01 15:31:22 +0000

    recognize different byte orders automatically; m_lorder no longer used

--- lib/libc/db/btree/bt_debug.c
+++ lib/libc/db/btree/bt_debug.c
@@ -9,7 +9,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)bt_debug.c	5.7 (Berkeley) 02/14/93";
+static char sccsid[] = "@(#)bt_debug.c	5.8 (Berkeley) 05/01/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
@@ -100,7 +100,6 @@ __bt_dmpage(h)
 		X(BTF_RECNO,	"RECNO");
 		(void)fprintf(stderr, ")");
 	}
-	(void)fprintf(stderr, "\nlorder %lu\n", m->m_lorder);
 }
 
 /*
