bf66a50218d8c72c17b2e5e8cd41f0b4753e310d
akito <akito@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-02 05:43:33 +0000

    add PORT related macro

--- sys/luna68k/dev/sioreg.h
+++ sys/luna68k/dev/sioreg.h
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sioreg.h	7.2 (Berkeley) 11/17/92
+ *	@(#)sioreg.h	7.3 (Berkeley) 05/02/93
  */
 
 /* 
@@ -129,3 +129,7 @@ struct siodevice {
 
 
 #define SIO_HARDADDR	0x51000000			/* build-in serial-interface address */
+
+#define	NPORT		2
+#define	SIO_PORT	0
+#define	BMC_PORT	1
