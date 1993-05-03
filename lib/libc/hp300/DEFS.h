d922fb78c033e5a015dc40e390e82edebea66021
hibler <hibler@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-03 16:10:39 +0000

    add STDC versions for both gcc1 and gcc2, eliminate non-gcc versions

--- lib/libc/hp300/DEFS.h
+++ lib/libc/hp300/DEFS.h
@@ -8,20 +8,31 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)DEFS.h	5.1 (Berkeley) 05/12/90
+ *	@(#)DEFS.h	5.2 (Berkeley) 05/03/93
  */
 
+#ifdef __STDC__
 #ifdef PROF
-#ifdef __GNUC__
+#if __GNUC__ >= 2
+#define	ENTRY(x) \
+	.globl _ ## x; .even; _ ## x:; .data; PROF ## x:; .long 0; .text; \
+	link a6,\#0; lea PROF ## x,a0; jsr mcount; unlk a6
+#else
+#define	ENTRY(x) \
+	.globl _ ## x; .even; _ ## x:; .data; PROF ## x:; .long 0; .text; \
+	link a6,#0; lea PROF ## x,a0; jsr mcount; unlk a6
+#endif
+#else
+#define	ENTRY(x) \
+	.globl _ ## x; .even; _ ## x:
+#endif
+#else
+#ifdef PROF
 #define	ENTRY(x) \
 	.globl _/**/x; .even; _/**/x:; .data; PROF/**/x:; .long 0; .text; \
 	link a6,#0; lea PROF/**/x,a0; jsr mcount; unlk a6
 #else
 #define	ENTRY(x) \
-	.globl _/**/x; .even; _/**/x:; .data; PROF/**/x:; .long 0; .text; \
-	lea PROF/**/x,a0; jsr mcount
-#endif
-#else
-#define	ENTRY(x) \
 	.globl _/**/x; .even; _/**/x:
+#endif
 #endif
