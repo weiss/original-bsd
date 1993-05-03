0d18d2805c0da7a286a0838e10d1fad44078a77b
hibler <hibler@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-03 15:07:48 +0000

    disallow creation of filesystem on partition marked FS_BOOT

--- sbin/newfs/newfs.c
+++ sbin/newfs/newfs.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)newfs.c	6.32 (Berkeley) 07/23/92";
+static char sccsid[] = "@(#)newfs.c	6.33 (Berkeley) 05/03/93";
 #endif /* not lint */
 
 #ifndef lint
@@ -374,6 +374,9 @@ main(argc, argv)
 		if (pp->p_size == 0)
 			fatal("%s: `%c' partition is unavailable",
 			    argv[0], *cp);
+		if (pp->p_fstype == FS_BOOT)
+			fatal("%s: `%c' partition overlaps boot program",
+			      argv[0], *cp);
 	}
 	if (fssize == 0)
 		fssize = pp->p_size;
