1bd38a44263d3bb74ad00629ec1741b4e70ac55c
torek <torek@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-06 22:41:47 +0000

    add __swhatbuf, internal routine for determining "proper" buffering

--- lib/libc/stdio/local.h
+++ lib/libc/stdio/local.h
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)local.h	5.2 (Berkeley) 02/05/91
+ *	@(#)local.h	5.3 (Berkeley) 05/06/93
  */
 
 /*
@@ -26,6 +26,7 @@ void	__sinit __P((void));
 void	_cleanup __P((void));
 void	(*__cleanup) __P((void));
 void	__smakebuf __P((FILE *));
+int	__swhatbuf __P((FILE *, size_t *, int *));
 int	_fwalk __P((int (*)(FILE *)));
 int	__swsetup __P((FILE *));
 int	__sflags __P((const char *, int *));
