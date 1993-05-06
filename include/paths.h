a4adafca0637d630265f3a09c5ed81e0438f9be4
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-06 15:05:48 +0000

    make _PATH_STDPATH include *everything*

--- include/paths.h
+++ include/paths.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)paths.h	5.18 (Berkeley) 05/03/93
+ *	@(#)paths.h	5.19 (Berkeley) 05/06/93
  */
 
 #ifndef _PATHS_H_
@@ -13,7 +13,8 @@
 /* Default search path. */
 #define	_PATH_DEFPATH	"/usr/bin:/bin"
 /* All standard utilities path. */
-#define	_PATH_STDPATH	"/usr/bin:/bin:/usr/sbin"
+#define	_PATH_STDPATH \
+	"/usr/bin:/bin:/usr/sbin:/sbin:/usr/contrib/bin:/usr/old/bin"
 
 #define	_PATH_BSHELL	"/bin/sh"
 #define	_PATH_CONSOLE	"/dev/console"
