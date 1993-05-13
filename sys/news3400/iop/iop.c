ff39075dbbf598de482e2f36011b11ee8ea804ba
utashiro <utashiro@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-13 19:45:39 +0000

    fixed small  compiler problem.

--- sys/news3400/iop/iop.c
+++ sys/news3400/iop/iop.c
@@ -9,7 +9,7 @@
  *
  * from: $Hdr: iop.c,v 4.300 91/06/09 06:42:37 root Rel41 $ SONY
  *
- *	@(#)iop.c	7.3 (Berkeley) 03/09/93
+ *	@(#)iop.c	7.4 (Berkeley) 05/13/93
  */
 
 /*
@@ -144,7 +144,7 @@ iopsetup(bp, map, nmap)
 		panic("iopsetup: user address is not allowed");
 	}
 #else /* mips */
-	ERROR!  This code doesn't work. /* KU:XXX */
+	ERROR!  This code does not work. /* KU:XXX */
 	pte = buftopte(bp);
 	while (--npf >= 0) {
 		if (pte->pg_pfnum == 0)
