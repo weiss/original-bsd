1b4a04e8f24eb8327b47129db33322da366d8d68
torek <torek@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-16 16:10:21 +0000

    date and time created 93/05/16 16:10:21 by torek

---
+++ usr.bin/tftp/extern.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 1993 Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)extern.h	5.1 (Berkeley) 05/16/93
+ */
+
+void	recvfile __P((int, char *, char *));
+void	sendfile __P((int, char *, char *));
