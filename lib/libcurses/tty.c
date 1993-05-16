24690439b666421280a0239b94d4a3f21cfb0a33
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-16 21:37:04 +0000

    some preprocessors try to redefine the mvcur prototype to be __mvcur which fails miserably; don't use mvcur internally, either

--- lib/libcurses/tty.c
+++ lib/libcurses/tty.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)tty.c	5.11 (Berkeley) 05/12/93";
+static char sccsid[] = "@(#)tty.c	5.12 (Berkeley) 05/16/93";
 #endif /* not lint */
 
 /*
@@ -159,7 +159,7 @@ endwin()
 			tputs(SE, 0, __cputchar);
 			curscr->flags &= ~__WSTANDOUT;
 		}
-		mvcur(curscr->cury, curscr->cury, curscr->maxy - 1, 0);
+		__mvcur(curscr->cury, curscr->cury, curscr->maxy - 1, 0);
 	}
 
 	(void)tputs(VE, 0, __cputchar);
