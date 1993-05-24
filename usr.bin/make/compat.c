55a30904c094bb32532890be3261d5b86bc28601
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-24 18:21:29 +0000

    merge in rev 5.7->5.8 change

--- usr.bin/make/compat.c
+++ usr.bin/make/compat.c
@@ -11,7 +11,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)compat.c	5.10 (Berkeley) 05/24/93";
+static char sccsid[] = "@(#)compat.c	5.11 (Berkeley) 05/24/93";
 #endif /* not lint */
 
 /*-
@@ -27,12 +27,14 @@ static char sccsid[] = "@(#)compat.c	5.10 (Berkeley) 05/24/93";
  *	    	  	    thems as need creatin'
  */
 
-#include    <stdio.h>
 #include    <sys/types.h>
-#include    <sys/signal.h>
 #include    <sys/wait.h>
-#include    <sys/errno.h>
+
 #include    <ctype.h>
+#include    <errno.h>
+#include    <signal.h>
+#include    <stdio.h>
+
 #include    "make.h"
 #include    "hash.h"
 #include    "dir.h"
