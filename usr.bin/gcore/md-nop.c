1cd82203c055fbbd61120ca99f9901d0819e394d
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-25 23:34:08 +0000

    kinfo_proc => sysctl

--- usr.bin/gcore/md-nop.c
+++ usr.bin/gcore/md-nop.c
@@ -6,11 +6,11 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)md-nop.c	5.4 (Berkeley) 03/31/93";
+static char sccsid[] = "@(#)md-nop.c	5.5 (Berkeley) 05/25/93";
 #endif /* not lint */
 
 #include <sys/param.h>
-#include <sys/kinfo_proc.h>
+#include <sys/sysctl.h>
 
 #include <stdio.h>
 #include <kvm.h>
