6d2238770dba734a31f5c7e482393b4450cf0db1
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-31 16:20:31 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- old/boggle/pathnames.h
+++ old/boggle/pathnames.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.2 (Berkeley) 02/27/91
+ *	@(#)pathnames.h	8.1 (Berkeley) 05/31/93
  */
 
 #define	_PATH_BOGLOG		"/var/games/boglog"
