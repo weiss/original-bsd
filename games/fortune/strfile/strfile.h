2047fd460c9029d3fc6e50231c82f316e7cabacf
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-31 17:02:26 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- games/fortune/strfile/strfile.h
+++ games/fortune/strfile/strfile.h
@@ -1,13 +1,13 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ken Arnold.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)strfile.h	5.8 (Berkeley) 04/08/91
+ *	@(#)strfile.h	8.1 (Berkeley) 05/31/93
  */
 
 #define	STR_ENDSTRING(line,tbl) \
