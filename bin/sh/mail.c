c8b5c0a6bd777df31402834c362b7b793030e7ef
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-31 15:44:36 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- bin/sh/mail.c
+++ bin/sh/mail.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Kenneth Almquist.
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)mail.c	5.1 (Berkeley) 03/07/91";
+static char sccsid[] = "@(#)mail.c	8.1 (Berkeley) 05/31/93";
 #endif /* not lint */
 
 /*
