83548b42f6cdf6bcde1408939f2c62e3b39e49c1
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-31 15:40:37 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- bin/sh/alias.h
+++ bin/sh/alias.h
@@ -1,13 +1,13 @@
 /*-
- * Copyright (c) 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Kenneth Almquist.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)alias.h	1.2 (Berkeley) 05/31/93
+ *	@(#)alias.h	8.1 (Berkeley) 05/31/93
  */
 
 #define ALIASINUSE	1
