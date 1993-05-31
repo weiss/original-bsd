bec2b5838f4f909219d8b5aa58d52d25305ef161
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-31 17:45:59 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- games/robots/score.c
+++ games/robots/score.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)score.c	5.6 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)score.c	8.1 (Berkeley) 05/31/93";
 #endif /* not lint */
 
 # include	"robots.h"
