a51a1dd15d085cf404d38f7f17046c7cc1a5d7b9
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-31 17:27:22 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- games/monop/deck.h
+++ games/monop/deck.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)deck.h	5.3 (Berkeley) 06/01/90
+ *	@(#)deck.h	8.1 (Berkeley) 05/31/93
  */
 
 # define	bool	char
--- games/monop/trade.c
+++ games/monop/trade.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)trade.c	5.5 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)trade.c	8.1 (Berkeley) 05/31/93";
 #endif /* not lint */
 
 # include	"monop.ext"
