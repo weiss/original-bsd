8b636e0b2cf1dd3a4f2727bfeb5a8295b3f526fc
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-31 17:52:40 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- games/snake/snake/snake.h
+++ games/snake/snake/snake.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)snake.h	5.6 (Berkeley) 02/03/93
+ *	@(#)snake.h	8.1 (Berkeley) 05/31/93
  */
 
 # include <stdio.h>
