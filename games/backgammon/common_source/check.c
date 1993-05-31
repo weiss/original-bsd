954a9e4395845f4af5c4991f1dae3d78a0d778f2
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-31 16:10:20 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- games/backgammon/common_source/check.c
+++ games/backgammon/common_source/check.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)check.c	5.4 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)check.c	8.1 (Berkeley) 05/31/93";
 #endif /* not lint */
 
 #include "back.h"
