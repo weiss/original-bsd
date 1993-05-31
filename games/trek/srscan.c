bcd9e7a362e95ff5ce22f98f04d01e3bebc5a3b1
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-31 17:58:23 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- games/trek/srscan.c
+++ games/trek/srscan.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)srscan.c	5.4 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)srscan.c	8.1 (Berkeley) 05/31/93";
 #endif /* not lint */
 
 # include	"trek.h"
