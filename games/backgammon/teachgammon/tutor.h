d7688a5f310f24da491c013b39809e7deec4de5d
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-05-31 16:11:27 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- games/backgammon/teachgammon/tutor.h
+++ games/backgammon/teachgammon/tutor.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)tutor.h	5.4 (Berkeley) 06/01/90
+ *	@(#)tutor.h	8.1 (Berkeley) 05/31/93
  */
 
 struct situatn  {
