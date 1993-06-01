0763b53bfd13dc6af66af0d99b516b4804e68422
dab <dab@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-01 11:49:30 +0000

    Fix the hton() declaration to be an ANSI type declaration, so that it matches what is in machine/endian.h

--- usr.bin/telnet/commands.c
+++ usr.bin/telnet/commands.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)commands.c	5.12 (Berkeley) 05/20/93";
+static char sccsid[] = "@(#)commands.c	5.13 (Berkeley) 06/01/93";
 #endif /* not lint */
 
 #if	defined(unix)
@@ -2316,7 +2316,7 @@ tn(argc, argv)
 	    }
 	} else {
 #if	!defined(htons)
-	    u_short htons();
+	    u_short htons P((unsigned short));
 #endif	/* !defined(htons) */
 	    sin.sin_port = htons(sin.sin_port);
 	}
