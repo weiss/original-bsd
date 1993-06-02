03c00e8cd2ce4958943282d7fd4b34e6460e89b1
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-02 12:45:54 +0000

    Christos removed the comment clause in ParseReadLine this breaks for "# foo:; bar" lines in the Makefile

--- usr.bin/make/parse.c
+++ usr.bin/make/parse.c
@@ -11,7 +11,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parse.c	5.20 (Berkeley) 05/28/93";
+static char sccsid[] = "@(#)parse.c	5.21 (Berkeley) 06/02/93";
 #endif /* not lint */
 
 /*-
@@ -2049,6 +2049,9 @@ ParseReadLine ()
 	    break;
 	} else if (c == '\n') {
 	    lineno++;
+	} else if (c == '#') {
+		ungetc(c, curFILE);
+		break;
 	} else {
 	    /*
 	     * Anything else breaks out without doing anything
