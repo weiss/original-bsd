3cccd19b30eef6ad43dc447861869f4f222ff969
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-02 20:04:17 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- include/protocols/dumprestore.h
+++ include/protocols/dumprestore.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)dumprestore.h	5.9 (Berkeley) 03/09/93
+ *	@(#)dumprestore.h	8.1 (Berkeley) 06/02/93
  */
 
 #ifndef _DUMPRESTORE_H_
