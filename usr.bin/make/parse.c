4ad62e999c475801c692c09039c33cd8da0f78ca
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-02 20:25:59 +0000

    from Christos, use `ungetc' routine ParseUnreadc

--- usr.bin/make/parse.c
+++ usr.bin/make/parse.c
@@ -11,7 +11,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parse.c	5.21 (Berkeley) 06/02/93";
+static char sccsid[] = "@(#)parse.c	5.22 (Berkeley) 06/02/93";
 #endif /* not lint */
 
 /*-
@@ -2050,7 +2050,7 @@ ParseReadLine ()
 	} else if (c == '\n') {
 	    lineno++;
 	} else if (c == '#') {
-		ungetc(c, curFILE);
+		ParseUnreadc(c);
 		break;
 	} else {
 	    /*
