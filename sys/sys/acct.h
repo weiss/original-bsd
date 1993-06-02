c178896bcb527381f44c2a959b5a197d9c7c9943
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-02 19:52:21 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/sys/acct.h
+++ sys/sys/acct.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)acct.h	7.3 (Berkeley) 02/15/91
+ *	@(#)acct.h	8.1 (Berkeley) 06/02/93
  */
 
 /*
