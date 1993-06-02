4bc53171a82188adc8bedb5f809f99cc7f0da162
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-02 18:32:28 +0000

    getbsize moved into libc

--- usr.bin/du/du.c
+++ usr.bin/du/du.c
@@ -15,7 +15,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)du.c	5.21 (Berkeley) 04/28/93";
+static char sccsid[] = "@(#)du.c	5.22 (Berkeley) 06/02/93";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -29,7 +29,6 @@ static char sccsid[] = "@(#)du.c	5.21 (Berkeley) 04/28/93";
 #include <stdlib.h>
 #include <string.h>
 
-char	*getbsize __P((int *, long *));
 int	 linkchk __P((FTSENT *));
 void	 usage __P((void));
 
