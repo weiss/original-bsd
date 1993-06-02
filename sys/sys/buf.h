de1ccdb7a320425c54e095d8af984821278b3e1c
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-02 19:52:42 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/sys/buf.h
+++ sys/sys/buf.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1982, 1986, 1989 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)buf.h	7.22 (Berkeley) 02/02/93
+ *	@(#)buf.h	8.1 (Berkeley) 06/02/93
  */
 
 #ifndef _BUF_H_
--- sys/sys/cdefs.h
+++ sys/sys/cdefs.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)cdefs.h	7.7 (Berkeley) 03/02/92
+ *	@(#)cdefs.h	8.1 (Berkeley) 06/02/93
  */
 
 #ifndef	_CDEFS_H_
