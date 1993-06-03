536e6bf05e47e8e2ba697e9b3dcf3837dc9e1fad
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-03 13:49:24 +0000

    have to cast SYS_mmap to quad_t when using __indir

--- lib/libc/sys/mmap.c
+++ lib/libc/sys/mmap.c
@@ -6,7 +6,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)mmap.c	5.2 (Berkeley) 06/01/93";
+static char sccsid[] = "@(#)mmap.c	5.3 (Berkeley) 06/03/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -27,6 +27,6 @@ mmap(addr, len, prot, flags, fd, offset)
 	off_t	offset;
 {
 
-	return((caddr_t)__indir(SYS_mmap, addr, len, prot, flags, fd, 0,
+	return((caddr_t)__indir((quad_t)SYS_mmap, addr, len, prot, flags, fd, 0,
 		offset));
 }
