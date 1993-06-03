7f0a8b44f237c331008ef2dfe647759abd92a510
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-03 17:52:38 +0000

    usage statement needs a newline

--- bin/stty/stty.c
+++ bin/stty/stty.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)stty.c	5.33 (Berkeley) 04/29/93";
+static char sccsid[] = "@(#)stty.c	5.34 (Berkeley) 06/03/93";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -130,6 +130,6 @@ void
 usage()
 {
 	(void)fprintf(stderr,
-	    "usage: stty: [-a|-e|-g] [-f file] [options]");
+	    "usage: stty: [-a|-e|-g] [-f file] [options]\n");
 	exit (1);
 }
