c5233e6de1311b90504274eac5829dc764aaac82
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-03 15:40:44 +0000

    add another parameter to sfgets

--- usr.sbin/sendmail/src/sendmail.h
+++ usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	6.71 (Berkeley) 06/03/93
+ *	@(#)sendmail.h	6.72 (Berkeley) 06/03/93
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	6.71		06/03/93";
+static char SmailSccsId[] =	"@(#)sendmail.h	6.72		06/03/93";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -872,7 +872,7 @@ extern void		clearenvelope __P((ENVELOPE *, int));
 extern char		*username __P(());
 extern MCI		*mci_get __P((char *, MAILER *));
 extern char		*pintvl __P((time_t, int));
-extern char		*sfgets __P((char *, int, FILE *, time_t));
+extern char		*sfgets __P((char *, int, FILE *, time_t, char *));
 extern char		*map_rewrite __P((MAP *, char *, int, char **));
 extern ADDRESS		*getctladdr __P((ADDRESS *));
 extern char		*anynet_ntoa __P((SOCKADDR *));
