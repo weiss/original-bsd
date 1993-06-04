03b84a57a0686e8a077875be93f690ab08023a16
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 17:19:47 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/liby/main.c
+++ lib/liby/main.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	5.3 (Berkeley) 01/13/91";
+static char sccsid[] = "@(#)main.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 main()
--- lib/liby/yyerror.c
+++ lib/liby/yyerror.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)yyerror.c	5.2 (Berkeley) 05/15/90";
+static char sccsid[] = "@(#)yyerror.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 #include <stdio.h>
