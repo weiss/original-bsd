43f3741d018133615b12907392a293dd59401e78
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 16:06:29 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libc/stdlib/strtod.c
+++ lib/libc/stdlib/strtod.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)strtod.c	5.3 (Berkeley) 06/04/93";
+static char sccsid[] = "@(#)strtod.c	8.1 (Berkeley) 06/04/93";
 #endif /* LIBC_SCCS and not lint */
 
 /****************************************************************
