c0ca25c533842d1fdaf2affdd5459a211885531d
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 15:22:39 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libc/db/hash/hash_log2.c
+++ lib/libc/db/hash/hash_log2.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Margo Seltzer.
@@ -9,7 +9,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)hash_log2.c	5.4 (Berkeley) 09/26/91";
+static char sccsid[] = "@(#)hash_log2.c	8.1 (Berkeley) 06/04/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
--- lib/libc/db/hash/search.h
+++ lib/libc/db/hash/search.h
@@ -1,13 +1,13 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Margo Seltzer.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)search.h	5.2 (Berkeley) 09/04/91
+ *	@(#)search.h	8.1 (Berkeley) 06/04/93
  */
 
 /* Backward compatibility to hsearch interface. */
