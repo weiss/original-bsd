bbe79a3c9d4d0dcabdf5c1f09cdcabce591e0c8c
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 16:27:36 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libcompat/4.3/tahoe/nargs.s
+++ lib/libcompat/4.3/tahoe/nargs.s
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
@@ -9,7 +9,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)nargs.s	1.5 (Berkeley) 11/26/91"
+	.asciz "@(#)nargs.s	8.1 (Berkeley) 06/04/93"
 #endif /* LIBC_SCCS and not lint */
 
 /* C library -- nargs */
