445429a5748c4716f174ae57e781f98016e7dd96
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 13:23:17 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libc/tahoe/stdlib/abs.s
+++ lib/libc/tahoe/stdlib/abs.s
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
@@ -9,7 +9,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)abs.s	5.1 (Berkeley) 04/12/91"
+	.asciz "@(#)abs.s	8.1 (Berkeley) 06/04/93"
 #endif /* LIBC_SCCS and not lint */
 
 /* abs - int absolute value */
