bb43379cc66d1315ca32c55f0ab4fceb01292396
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 12:58:26 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libc/regex/regexec.c
+++ lib/libc/regex/regexec.c
@@ -1,18 +1,18 @@
 /*-
  * Copyright (c) 1992 Henry Spencer.
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Henry Spencer of the University of Toronto.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)regexec.c	5.4 (Berkeley) 05/21/93
+ *	@(#)regexec.c	8.1 (Berkeley) 06/04/93
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)regexec.c	5.4 (Berkeley) 05/21/93";
+static char sccsid[] = "@(#)regexec.c	8.1 (Berkeley) 06/04/93";
 #endif /* LIBC_SCCS and not lint */
 
 /*
