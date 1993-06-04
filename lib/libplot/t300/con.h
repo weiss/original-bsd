13b2c6e0372442be6f4873acfc1cb57fe31b882f
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 17:49:35 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libplot/t300/con.h
+++ lib/libplot/t300/con.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  *
- *	@(#)con.h	4.2 (Berkeley) 04/22/91
+ *	@(#)con.h	8.1 (Berkeley) 06/04/93
  */
 
 #include <sgtty.h>
