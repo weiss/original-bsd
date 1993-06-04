1dcb39f9df1b42084a772eb6681e0bc2ac2b3d7e
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 16:49:27 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libcurses/mvwin.c
+++ lib/libcurses/mvwin.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1981 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1981, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)mvwin.c	5.9 (Berkeley) 01/11/93";
+static char sccsid[] = "@(#)mvwin.c	8.1 (Berkeley) 06/04/93";
 #endif	/* not lint */
 
 #include <curses.h>
