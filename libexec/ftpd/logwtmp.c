31ad06d1c4d8dcb05f266d7a7fb5cdc0837e7f71
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 18:48:47 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- libexec/ftpd/logwtmp.c
+++ libexec/ftpd/logwtmp.c
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)logwtmp.c	5.8 (Berkeley) 06/27/92";
+static char sccsid[] = "@(#)logwtmp.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 #include <sys/types.h>
