633a6655459b077a82576628d18efc35a4e7cdfd
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 16:59:16 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libm/common/trig.h
+++ lib/libm/common/trig.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1987 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1987, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)trig.h	5.6 (Berkeley) 10/09/90
+ *	@(#)trig.h	8.1 (Berkeley) 06/04/93
  */
 
 #include "mathimpl.h"
