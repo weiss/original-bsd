964f26d21397893812a0ec0f52cae0329c906720
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 17:35:19 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libtelnet/auth.h
+++ lib/libtelnet/auth.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)auth.h	5.1 (Berkeley) 02/28/91
+ *	@(#)auth.h	8.1 (Berkeley) 06/04/93
  */
 
 /*
