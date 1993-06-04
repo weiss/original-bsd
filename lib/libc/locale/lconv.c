d3aa1d2b4233f9ec013e21f35dfcd20a4b099028
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 12:43:25 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libc/locale/lconv.c
+++ lib/libc/locale/lconv.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)lconv.c	5.3 (Berkeley) 01/12/93";
+static char sccsid[] = "@(#)lconv.c	8.1 (Berkeley) 06/04/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <limits.h>
--- lib/libc/locale/localeconv.c
+++ lib/libc/locale/localeconv.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)localeconv.c	5.1 (Berkeley) 02/18/91";
+static char sccsid[] = "@(#)localeconv.c	8.1 (Berkeley) 06/04/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <locale.h>
