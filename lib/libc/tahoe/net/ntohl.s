967dd0d949c0b464a09c9b10de150373da69d593
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 13:19:27 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libc/tahoe/net/ntohl.s
+++ lib/libc/tahoe/net/ntohl.s
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)ntohl.s	5.1 (Berkeley) 04/12/91"
+	.asciz "@(#)ntohl.s	8.1 (Berkeley) 06/04/93"
 #endif /* LIBC_SCCS and not lint */
 
 /* hostorder = ntohl(netorder) */
