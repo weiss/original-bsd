7921151cfb4dc50f35fc11b818861bc3a5213b4e
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 19:08:03 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- libexec/uucpd/uucpd.c
+++ libexec/uucpd/uucpd.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Rick Adams.
@@ -9,13 +9,13 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1985 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1985, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)uucpd.c	5.10 (Berkeley) 02/26/91";
+static char sccsid[] = "@(#)uucpd.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 /*
