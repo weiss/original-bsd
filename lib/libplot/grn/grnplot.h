6a4f45a9194d7b101b3374aca357f2fd835cb173
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 17:41:28 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libplot/grn/grnplot.h
+++ lib/libplot/grn/grnplot.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1980, 1986 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  *
- *	@(#)grnplot.h	6.2 (Berkeley) 04/22/91
+ *	@(#)grnplot.h	8.1 (Berkeley) 06/04/93
  */
 
 /*
--- lib/libplot/grn/linemod.c
+++ lib/libplot/grn/linemod.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1980, 1986 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)linemod.c	6.2 (Berkeley) 04/22/91";
+static char sccsid[] = "@(#)linemod.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 #include "grnplot.h"
