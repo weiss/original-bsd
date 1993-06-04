c6f2c7182c869fef72d1891ca3fad883462e04d6
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 17:37:43 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libplot/gigi/gigi.h
+++ lib/libplot/gigi/gigi.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  *
- *	@(#)gigi.h	5.2 (Berkeley) 04/22/91
+ *	@(#)gigi.h	8.1 (Berkeley) 06/04/93
  */
 
 /*
--- lib/libtelnet/read_password.c
+++ lib/libtelnet/read_password.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)read_password.c	5.1 (Berkeley) 01/20/93";
+static char sccsid[] = "@(#)read_password.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 /*
