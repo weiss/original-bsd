a01c16c2cd346dcdb457e94eb0f5e8fb6b33713d
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 17:35:40 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libtelnet/encrypt.h
+++ lib/libtelnet/encrypt.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)encrypt.h	5.4 (Berkeley) 05/20/93
+ *	@(#)encrypt.h	8.1 (Berkeley) 06/04/93
  */
 
 /*
--- lib/libtelnet/genget.c
+++ lib/libtelnet/genget.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)genget.c	5.1 (Berkeley) 02/28/91";
+static char sccsid[] = "@(#)genget.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 
