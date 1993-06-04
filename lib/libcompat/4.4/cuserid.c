a32fa803a5ea3f25fb017733b7abe46d7dde11fb
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 16:30:14 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libcompat/4.4/cuserid.c
+++ lib/libcompat/4.4/cuserid.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)cuserid.c	5.1 (Berkeley) 01/12/93";
+static char sccsid[] = "@(#)cuserid.c	8.1 (Berkeley) 06/04/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <pwd.h>
