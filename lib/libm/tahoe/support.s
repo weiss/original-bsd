0b383cce6f811cf6faa7202d65605f5c9d6c4e74
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 17:08:56 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libm/tahoe/support.s
+++ lib/libm/tahoe/support.s
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 1987 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1987, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 	.data
 	.align	2
 _sccsid:
-	.asciz	"@(#)support.s	5.6	(ucb.elefunt)	10/09/90"
+	.asciz	"@(#)support.s	8.1	(ucb.elefunt)	06/04/93"
 /*
  * copysign(x,y),
  * logb(x),
