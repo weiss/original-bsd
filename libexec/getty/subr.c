bd3461e172d4b3b9675e24d2359e65bc297bd818
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-04 18:50:44 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- libexec/getty/subr.c
+++ libexec/getty/subr.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)subr.c	5.13 (Berkeley) 05/27/93";
+static char sccsid[] = "@(#)subr.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 /*
