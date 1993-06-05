9363c57430a02d92485205428ee5c96f86e9e88a
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-05 11:09:06 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sbin/nologin/nologin.sh
+++ sbin/nologin/nologin.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -
 #
-# Copyright (c) 1992 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1992, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)nologin.sh	5.1 (Berkeley) 04/02/92
+#	@(#)nologin.sh	8.1 (Berkeley) 06/05/93
 #
 
 echo 'This account is currently not available.'
