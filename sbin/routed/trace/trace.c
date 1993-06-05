e456422dc46d929a0328952ce82c0e0b63b45b68
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-05 11:17:09 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sbin/routed/trace/trace.c
+++ sbin/routed/trace/trace.c
@@ -1,18 +1,18 @@
 /*-
- * Copyright (c) 1983, 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1983, 1988 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1983, 1988, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)trace.c	5.9 (Berkeley) 04/16/91";
+static char sccsid[] = "@(#)trace.c	8.1 (Berkeley) 06/05/93";
 #endif /* not lint */
 
 #include <sys/param.h>
