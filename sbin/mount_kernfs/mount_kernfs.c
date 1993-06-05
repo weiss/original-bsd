6e05713d860dae7d6f859fa40abd1b596d7bac26
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-05 11:02:18 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sbin/mount_kernfs/mount_kernfs.c
+++ sbin/mount_kernfs/mount_kernfs.c
@@ -1,14 +1,14 @@
 /*
  * Copyright (c) 1990, 1992 Jan-Simon Pendry
- * Copyright (c) 1992 The Regents of the University of California
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Jan-Simon Pendry.
  *
  * %sccs.redist.c.%
  *
- *	@(#)mount_kernfs.c	5.1 (Berkeley) 07/18/92
+ *	@(#)mount_kernfs.c	8.1 (Berkeley) 06/05/93
  */
 
 #include <sys/param.h>
