06380a9e8e9b869c5712c8bb48bf2f5af01a206c
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 14:38:03 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/error/error.h
+++ usr.bin/error/error.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)error.h	5.5 (Berkeley) 06/01/90
+ *	@(#)error.h	8.1 (Berkeley) 06/06/93
  */
 
 typedef	int	boolean;
