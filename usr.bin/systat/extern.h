be2d7a65b86d9f60d82f2773f7240148818bf680
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 16:28:53 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/systat/extern.h
+++ usr.bin/systat/extern.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *      @(#)extern.h	5.1 (Berkeley) 07/23/92
+ *      @(#)extern.h	8.1 (Berkeley) 06/06/93
  */
 
 #include <sys/cdefs.h>
