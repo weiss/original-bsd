71a524e9e7385b6ecdbd7e2d30e0eeb8cb3cb9be
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 16:29:03 +0000

    4.4BSD snapshot (revision 8.1)

--- usr.bin/struct/struct/1.incl.h
+++ usr.bin/struct/struct/1.incl.h
@@ -1,7 +1,7 @@
 /*-
  * %sccs.include.proprietary.c%
  *
- *	@(#)1.incl.h	4.3 (Berkeley) 04/16/91
+ *	@(#)1.incl.h	8.1 (Berkeley) 06/06/93
  */
 
 #define maxlsw		10	/* max number of switches and labels per statement */
