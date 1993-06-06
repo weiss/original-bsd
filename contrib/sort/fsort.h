6c63385004d86696926ca8ba44c2f9023e744c02
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 16:23:14 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- contrib/sort/fsort.h
+++ contrib/sort/fsort.h
@@ -1,13 +1,13 @@
 /*-
- * Copyright (c) 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Peter McIlroy.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)fsort.h	5.1 (Berkeley) 06/01/93
+ *	@(#)fsort.h	8.1 (Berkeley) 06/06/93
  */
 
 #define POW 20			/* exponent for buffer size */
