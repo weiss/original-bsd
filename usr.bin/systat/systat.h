c7083d75d2bd2132846bb56c6ce9802498399ac5
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 16:29:46 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/systat/systat.h
+++ usr.bin/systat/systat.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1980, 1989, 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1989, 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)systat.h	5.10 (Berkeley) 05/21/93
+ *	@(#)systat.h	8.1 (Berkeley) 06/06/93
  */
 
 #include <curses.h>
