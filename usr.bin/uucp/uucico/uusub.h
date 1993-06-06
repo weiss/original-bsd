886b23cdf2aac26c3cb12a188e8fdd5cbdde55a3
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 22:14:00 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/uucp/uucico/uusub.h
+++ usr.bin/uucp/uucico/uusub.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  *
- *	@(#)uusub.h	4.2 (Berkeley) 04/24/91
+ *	@(#)uusub.h	8.1 (Berkeley) 06/06/93
  */
 
 #define UB_SST(a)	 
