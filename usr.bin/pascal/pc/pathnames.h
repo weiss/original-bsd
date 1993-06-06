27447010f429cd33c1cc35697c652f987308532a
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 15:27:18 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/pascal/pc/pathnames.h
+++ usr.bin/pascal/pc/pathnames.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.2 (Berkeley) 03/07/92
+ *	@(#)pathnames.h	8.1 (Berkeley) 06/06/93
  */
 
 #define	_PATH_PC0	"/usr/libexec/pascal/pc0"
