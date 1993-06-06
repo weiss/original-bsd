8216efb64d780ecf38a05f8b27cd94f2a23ef1ee
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 22:11:39 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/uucp/libuu/logent.c
+++ usr.bin/uucp/libuu/logent.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)logent.c	5.11 (Berkeley) 05/17/93";
+static char sccsid[] = "@(#)logent.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include "uucp.h"
--- usr.bin/uucp/libuu/mailst.c
+++ usr.bin/uucp/libuu/mailst.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)mailst.c	5.8 (Berkeley) 04/24/91";
+static char sccsid[] = "@(#)mailst.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include <signal.h>
