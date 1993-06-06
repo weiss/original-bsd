2a9425ec4e3d51713138e33807c683706f7d0f44
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 14:38:11 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/error/subr.c
+++ usr.bin/error/subr.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)subr.c	5.5 (Berkeley) 02/26/91";
+static char sccsid[] = "@(#)subr.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include <stdio.h>
