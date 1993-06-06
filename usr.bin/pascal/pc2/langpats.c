ab4cc0494610438ffaeed0c278b0d89d56b21c7e
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 15:28:39 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/pascal/pc2/langpats.c
+++ usr.bin/pascal/pc2/langpats.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1979, 1984 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1979, 1984, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)langpats.c	5.6 (Berkeley) 04/16/91";
+static char sccsid[] = "@(#)langpats.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include "inline.h"
