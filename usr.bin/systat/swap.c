3cfdf273740b13c23c6cfd04125a24f01554610f
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 16:29:36 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/systat/swap.c
+++ usr.bin/systat/swap.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1980, 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)swap.c	5.15 (Berkeley) 06/02/93";
+static char sccsid[] = "@(#)swap.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 /*
