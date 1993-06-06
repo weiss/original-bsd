bd8ab2c2574184cc833b7543bc5ffb559961831d
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 22:22:13 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/vis/foldit.c
+++ usr.bin/vis/foldit.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)foldit.c	5.1 (Berkeley) 04/18/91";
+static char sccsid[] = "@(#)foldit.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 foldit(chunk, col, max)
