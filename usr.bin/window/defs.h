e85b52521225988b182d12001fd4141f7215594a
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 22:28:35 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/window/defs.h
+++ usr.bin/window/defs.h
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Edward Wang at The University of California, Berkeley.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)defs.h	3.21 (Berkeley) 06/06/90
+ *	@(#)defs.h	8.1 (Berkeley) 06/06/93
  */
 
 #include "ww.h"
