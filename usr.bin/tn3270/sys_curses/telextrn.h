085744799294e273eaaaa428568ad6a5c17f5d18
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 16:53:06 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/tn3270/sys_curses/telextrn.h
+++ usr.bin/tn3270/sys_curses/telextrn.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)telextrn.h	4.3 (Berkeley) 05/11/93
+ *	@(#)telextrn.h	8.1 (Berkeley) 06/06/93
  */
 
 /*
