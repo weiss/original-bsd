ec6998359b5a49a0a2eec3c9738f82ad7043ed55
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 14:04:49 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.sbin/pwd_mkdb/pw_scan.c
+++ usr.sbin/pwd_mkdb/pw_scan.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)pw_scan.c	5.1 (Berkeley) 02/12/91";
+static char sccsid[] = "@(#)pw_scan.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 /*
