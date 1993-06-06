1452592af13f3db99bbc1d5cab524aa1aeabc84e
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 14:29:11 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/compress/zcat.sh
+++ usr.bin/compress/zcat.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -
 #
-# Copyright (c) 1992 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1992, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)zcat.sh	5.1 (Berkeley) 12/02/92
+#	@(#)zcat.sh	8.1 (Berkeley) 06/06/93
 #
 
 uncompress -c $*
