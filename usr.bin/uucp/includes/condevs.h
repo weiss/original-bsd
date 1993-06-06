170d87a45a6561607fa510deaf972db09f0ffa59
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 22:08:18 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/uucp/includes/condevs.h
+++ usr.bin/uucp/includes/condevs.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  *
- *	@(#)condevs.h	4.10 (Berkeley) 05/16/93
+ *	@(#)condevs.h	8.1 (Berkeley) 06/06/93
  */
 
 #include "uucp.h"
