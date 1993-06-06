306c4fb00f2442b4ba93e5281e975737e24c942e
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 14:41:30 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/ftp/ftp_var.h
+++ usr.bin/ftp/ftp_var.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1985, 1989 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ftp_var.h	5.9 (Berkeley) 06/01/90
+ *	@(#)ftp_var.h	8.1 (Berkeley) 06/06/93
  */
 
 /*
