c0cef8471e0603068a14110a2a8b86602915b43d
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 14:48:43 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/head/head.c
+++ usr.bin/head/head.c
@@ -1,18 +1,18 @@
 /*
- * Copyright (c) 1980, 1987, 1992 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1987, 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1980, 1987, 1992 Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1980, 1987, 1992, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)head.c	5.7 (Berkeley) 03/04/92";
+static char sccsid[] = "@(#)head.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include <sys/types.h>
