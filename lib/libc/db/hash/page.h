15685cec4f4269d42883fd49f01faaebb89244c5
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 22:55:30 +0000

    The short at page[2] contains the flag value that identifies long keys, long data or overflow pages, not page[1].

--- lib/libc/db/hash/page.h
+++ lib/libc/db/hash/page.h
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)page.h	5.2 (Berkeley) 09/04/91
+ *	@(#)page.h	5.3 (Berkeley) 06/06/93
  */
 
 /*
@@ -54,7 +54,7 @@
 #define FREESPACE(P)	((P)[(P)[0]+1])
 #define	OFFSET(P)	((P)[(P)[0]+2])
 #define PAIRFITS(P,K,D) \
-	(((P)[1] >= REAL_KEY) && \
+	(((P)[2] >= REAL_KEY) && \
 	    (PAIRSIZE((K),(D)) + OVFLSIZE) <= FREESPACE((P)))
 #define PAGE_META(N)	(((N)+3) * sizeof(u_short))
 
