0a2a6b7c8e63ad847a2006dd17f2b5ac8833caba
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 13:58:37 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.sbin/kvm_mkdb/nlist.c
+++ usr.sbin/kvm_mkdb/nlist.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1990, 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)nlist.c	5.8 (Berkeley) 04/10/93";
+static char sccsid[] = "@(#)nlist.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include <sys/param.h>
--- usr.sbin/kvm_mkdb/testdb.c
+++ usr.sbin/kvm_mkdb/testdb.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)testdb.c	5.2 (Berkeley) 07/01/92";
+static char sccsid[] = "@(#)testdb.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include <sys/param.h>
