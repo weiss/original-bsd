0cdf7ca3f39be215399e5817c471b9dbf0b552f2
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 14:53:30 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/ktrace/subr.c
+++ usr.bin/ktrace/subr.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)subr.c	5.2 (Berkeley) 04/15/92";
+static char sccsid[] = "@(#)subr.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include <sys/param.h>
