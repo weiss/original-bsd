47c3919028e7e2ae3d5ebbce392cd10ef09953eb
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 15:58:13 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/pascal/px/utilities.c
+++ usr.bin/pascal/px/utilities.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)utilities.c	5.3 (Berkeley) 04/16/91";
+static char sccsid[] = "@(#)utilities.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include	<signal.h>
