d2182bd856b4c7301410a9fb87df93a26b98fed5
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 13:57:13 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.sbin/eeprom/pathnames.h
+++ usr.sbin/eeprom/pathnames.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This software was developed by the Computer Systems Engineering group
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
@@ -13,7 +13,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.1 (Berkeley) 04/27/93
+ *	@(#)pathnames.h	8.1 (Berkeley) 06/06/93
  */
 
 #define _PATH_OPENPROM "/dev/openprom"
