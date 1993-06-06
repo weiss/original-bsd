84fbe84a051355836f29314556901b799abfe40f
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 12:07:37 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.sbin/amd/rpcx/nfs_prot_xdr.c
+++ usr.sbin/amd/rpcx/nfs_prot_xdr.c
@@ -1,15 +1,15 @@
 /*
  * Copyright (c) 1989 Jan-Simon Pendry
  * Copyright (c) 1989 Imperial College of Science, Technology & Medicine
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Jan-Simon Pendry at Imperial College, London.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)nfs_prot_xdr.c	5.4 (Berkeley) 02/09/92
+ *	@(#)nfs_prot_xdr.c	8.1 (Berkeley) 06/06/93
  *
  * $Id: nfs_prot_xdr.c,v 5.2.2.1 1992/02/09 15:09:32 jsp beta $
  *
