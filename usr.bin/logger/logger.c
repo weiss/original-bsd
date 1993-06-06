bee56ece91d0bed0791a7b577835d6ef60cf56d0
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 15:01:15 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/logger/logger.c
+++ usr.bin/logger/logger.c
@@ -1,18 +1,18 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1983 Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1983, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)logger.c	6.16 (Berkeley) 06/24/92";
+static char sccsid[] = "@(#)logger.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include <errno.h>
