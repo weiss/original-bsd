802e2b6730ccb97411a66e69041dd1da7d25dced
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 16:38:19 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/tn3270/ascii/map3270.h
+++ usr.bin/tn3270/ascii/map3270.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)map3270.h	4.2 (Berkeley) 04/26/91
+ *	@(#)map3270.h	8.1 (Berkeley) 06/06/93
  */
 
 /*
