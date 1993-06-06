e1753b8eefe829d12f7bc561577856cbcf1ea801
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 14:51:14 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/id/groups.sh
+++ usr.bin/id/groups.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -
 #
-# Copyright (c) 1991 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1991, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)groups.sh	5.1 (Berkeley) 11/11/91
+#	@(#)groups.sh	8.1 (Berkeley) 06/06/93
 #
 
 id -Gn $*
--- usr.bin/id/whoami.sh
+++ usr.bin/id/whoami.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -
 #
-# Copyright (c) 1991 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1991, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)whoami.sh	5.1 (Berkeley) 11/11/91
+#	@(#)whoami.sh	8.1 (Berkeley) 06/06/93
 #
 
 id -un
