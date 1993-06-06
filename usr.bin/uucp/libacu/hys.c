5d8c40825e298deda52f6f37e535398171e254ca
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 22:09:48 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/uucp/libacu/hys.c
+++ usr.bin/uucp/libacu/hys.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)hys.c	4.9 (Berkeley) 04/24/91";
+static char sccsid[] = "@(#)hys.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include "condevs.h"
