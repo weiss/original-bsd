057a892c8aafd35e22d8be80ada053b772392ec9
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 14:57:04 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/learn/learn/maktee.c
+++ usr.bin/learn/learn/maktee.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1986 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)maktee.c	4.3 (Berkeley) 04/17/91";
+static char sccsid[] = "@(#)maktee.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include "stdio.h"
--- usr.bin/learn/learn/selsub.c
+++ usr.bin/learn/learn/selsub.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1986 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)selsub.c	4.8 (Berkeley) 04/17/91";
+static char sccsid[] = "@(#)selsub.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include <sys/types.h>
