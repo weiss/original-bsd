768e4033631c96628121322a2cf09cd9c111428b
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 14:59:46 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/locate/locate/locate.h
+++ usr.bin/locate/locate/locate.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)locate.h	5.3 (Berkeley) 06/01/90
+ *	@(#)locate.h	8.1 (Berkeley) 06/06/93
  */
 
 /* Symbolic constants shared by locate.c and code.c */
