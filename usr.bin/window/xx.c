82b6afa4edf2cad3384d048c764cddecb26a4302
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-06 22:33:38 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/window/xx.c
+++ usr.bin/window/xx.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Edward Wang at The University of California, Berkeley.
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)xx.c	3.7 (Berkeley) 11/10/92";
+static char sccsid[] = "@(#)xx.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include "ww.h"
