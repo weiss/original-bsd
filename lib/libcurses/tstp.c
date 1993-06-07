5f05742aed11341962f8f428e349c3380fed954f
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-07 12:24:34 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libcurses/tstp.c
+++ lib/libcurses/tstp.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1981 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1981, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)tstp.c	5.10 (Berkeley) 06/07/93";
+static char sccsid[] = "@(#)tstp.c	8.1 (Berkeley) 06/07/93";
 #endif /* not lint */
 
 #include <curses.h>
