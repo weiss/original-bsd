85a5c1761de9af89b00a6cbfb30d29f855e7f97f
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-07 12:23:07 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libcurses/tty.c
+++ lib/libcurses/tty.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)tty.c	5.18 (Berkeley) 06/07/93";
+static char sccsid[] = "@(#)tty.c	8.1 (Berkeley) 06/07/93";
 #endif /* not lint */
 
 #include <sys/ioctl.h>
