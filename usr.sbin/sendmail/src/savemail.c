b2a700c95235d5bdbbe11528d1b8e14a257a06c5
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-07 10:31:58 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.sbin/sendmail/src/savemail.c
+++ usr.sbin/sendmail/src/savemail.c
@@ -1,13 +1,13 @@
 /*
  * Copyright (c) 1983 Eric P. Allman
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)savemail.c	6.44 (Berkeley) 05/27/93";
+static char sccsid[] = "@(#)savemail.c	8.1 (Berkeley) 06/07/93";
 #endif /* not lint */
 
 # include <pwd.h>
