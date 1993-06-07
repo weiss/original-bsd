dfb98916f88ce0af5a74d35eb9904a822536cd13
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-07 10:30:41 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.sbin/sendmail/src/macro.c
+++ usr.sbin/sendmail/src/macro.c
@@ -1,13 +1,13 @@
 /*
  * Copyright (c) 1983 Eric P. Allman
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)macro.c	6.8 (Berkeley) 05/27/93";
+static char sccsid[] = "@(#)macro.c	8.1 (Berkeley) 06/07/93";
 #endif /* not lint */
 
 # include "sendmail.h"
--- usr.sbin/sendmail/src/mailstats.h
+++ usr.sbin/sendmail/src/mailstats.h
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 1983 Eric P. Allman
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mailstats.h	6.1 (Berkeley) 12/21/92
+ *	@(#)mailstats.h	8.1 (Berkeley) 06/07/93
  */
 
 /*
