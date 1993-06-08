3ac543070a3c3f8e5d66af84e5386d8f2bb86369
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-08 17:56:42 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- include/dirent.h
+++ include/dirent.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)dirent.h	5.20 (Berkeley) 06/08/93
+ *	@(#)dirent.h	8.1 (Berkeley) 06/08/93
  */
 
 #ifndef _DIRENT_H_
