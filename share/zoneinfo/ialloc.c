b568cb4f42eac5ae29f6bba15af23824e37e717b
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-08 11:21:28 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- share/zoneinfo/ialloc.c
+++ share/zoneinfo/ialloc.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Arthur David Olson of the National Cancer Institute.
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)ialloc.c	5.3 (Berkeley) 04/20/91";
+static char sccsid[] = "@(#)ialloc.c	8.1 (Berkeley) 06/08/93";
 #endif /* not lint */
 
 #ifdef notdef
