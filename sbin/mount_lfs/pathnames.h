768df6b299387bb5abf71eb29ad2f5b7343df6af
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-08 18:48:15 +0000

    date and time created 93/06/08 18:48:15 by bostic

---
+++ sbin/mount_lfs/pathnames.h
@@ -0,0 +1,10 @@
+/*-
+ * Copyright (c) 1993 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pathnames.h	5.1 (Berkeley) 06/08/93
+ */
+
+#define _PATH_LFS_CLEANERD	"/usr/libexec/lfs_cleanerd"
