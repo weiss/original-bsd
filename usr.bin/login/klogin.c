bdf4b16563dd658261ea308a2989745317c1aa9f
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-09 22:43:45 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/login/klogin.c
+++ usr.bin/login/klogin.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)klogin.c	5.11 (Berkeley) 07/12/92";
+static char sccsid[] = "@(#)klogin.c	8.1 (Berkeley) 06/09/93";
 #endif /* not lint */
 
 #ifdef KERBEROS
