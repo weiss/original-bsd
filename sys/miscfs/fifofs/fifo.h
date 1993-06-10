23e4b82d84debf76b922756cb4df2e1b51f6fd53
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-10 23:36:28 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/miscfs/fifofs/fifo.h
+++ sys/miscfs/fifofs/fifo.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)fifo.h	7.8 (Berkeley) 05/25/93
+ *	@(#)fifo.h	8.1 (Berkeley) 06/10/93
  */
 
 #ifdef FIFO
--- sys/miscfs/fifofs/fifo_vnops.c
+++ sys/miscfs/fifofs/fifo_vnops.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)fifo_vnops.c	7.19 (Berkeley) 05/26/93
+ *	@(#)fifo_vnops.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/param.h>
