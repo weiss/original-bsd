ee5af505e7206148fa57f27d0febed8993a17c6a
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-10 23:40:06 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/nfs/nfsrtt.h
+++ sys/nfs/nfsrtt.h
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Rick Macklem at The University of Guelph.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)nfsrtt.h	7.2 (Berkeley) 02/02/93
+ *	@(#)nfsrtt.h	8.1 (Berkeley) 06/10/93
  */
 
 /*
