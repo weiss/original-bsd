a69cfb4b184068cc8ddaf1934ab24c9d96f34a9e
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-10 22:07:46 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/libkern/libkern.h
+++ sys/libkern/libkern.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)libkern.h	7.5 (Berkeley) 11/17/92
+ *	@(#)libkern.h	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/types.h>
--- sys/libkern/locc.c
+++ sys/libkern/locc.c
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)locc.c	7.4 (Berkeley) 10/11/92
+ *	@(#)locc.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <libkern/libkern.h>
