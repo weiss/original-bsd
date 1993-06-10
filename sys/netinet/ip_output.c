2696d48b26e80a26abe623d70bd467210124a404
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-10 23:08:56 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/netinet/ip_output.c
+++ sys/netinet/ip_output.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1982, 1986, 1988, 1990, 1993 Regents of the University
- * of California.  All rights reserved.
+ * Copyright (c) 1982, 1986, 1988, 1990, 1993
+ *	Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ip_output.c	7.34 (Berkeley) 06/04/93
+ *	@(#)ip_output.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/param.h>
