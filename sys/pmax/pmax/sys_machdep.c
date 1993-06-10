3195adac7b91aa6d06b76726db3b479d6a2d7c74
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-10 23:08:05 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/pmax/pmax/sys_machdep.c
+++ sys/pmax/pmax/sys_machdep.c
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 1992 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ralph Campbell.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sys_machdep.c	7.3 (Berkeley) 10/11/92
+ *	@(#)sys_machdep.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/param.h>
