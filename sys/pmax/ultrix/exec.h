08cb0e45ca8d5809c30a3179ebad04a6edeac43b
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-10 23:33:43 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/pmax/ultrix/exec.h
+++ sys/pmax/ultrix/exec.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)exec.h	7.1 (Berkeley) 05/24/93
+ *	@(#)exec.h	8.1 (Berkeley) 06/10/93
  */
 
 /*
