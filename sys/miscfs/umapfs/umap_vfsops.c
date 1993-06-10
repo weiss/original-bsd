78502ddf984260121f4a2bd92bf823dd44ca9444
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-10 23:47:33 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/miscfs/umapfs/umap_vfsops.c
+++ sys/miscfs/umapfs/umap_vfsops.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California
- * Copyright (c) 1990, 1992 Jan-Simon Pendry
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  * All rights reserved.
  *
  * This code is derived from software donated to Berkeley by
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)umap_vfsops.c	7.2 (Berkeley) 07/12/92
+ *	@(#)umap_vfsops.c	8.1 (Berkeley) 06/10/93
  *
  * @(#)null_vfsops.c       1.5 (Berkeley) 7/10/92
  */
--- sys/miscfs/umapfs/umap_vnops.c
+++ sys/miscfs/umapfs/umap_vnops.c
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software donated to Berkeley by
  * the UCLA Ficus project.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)umap_vnops.c	7.1 (Berkeley) 07/23/92
+ *	@(#)umap_vnops.c	8.1 (Berkeley) 06/10/93
  */
 
 /*
