00666c5d1d795e1c2e328dab2fc8c76465c51cac
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-10 22:27:00 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/luna68k/luna68k/sys_machdep.c
+++ sys/luna68k/luna68k/sys_machdep.c
@@ -1,13 +1,13 @@
 /*
  * Copyright (c) 1992 OMRON Corporation.
- * Copyright (c) 1982, 1986 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
  * from: hp300/hp300/sys_machdep.c	7.11 (Berkeley) 12/27/92
  *
- *	@(#)sys_machdep.c	7.4 (Berkeley) 05/12/93
+ *	@(#)sys_machdep.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/param.h>
