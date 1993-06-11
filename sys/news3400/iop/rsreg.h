4f00418da45ad0d6fc39e63c7c51abfa85368f1d
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-11 15:01:06 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/news3400/iop/rsreg.h
+++ sys/news3400/iop/rsreg.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Sony Corp. and Kazumasa Utashiro of Software Research Associates, Inc.
@@ -9,7 +9,7 @@
  *
  * from: $Hdr: rsreg.h,v 4.300 91/06/09 06:43:04 root Rel41 $ SONY
  *
- *	@(#)rsreg.h	7.1 (Berkeley) 06/04/92
+ *	@(#)rsreg.h	8.1 (Berkeley) 06/11/93
  */
 
 #ifndef __RSREG__
