db00d1dee99b9f2d561abd9b168e18dc6bd915d6
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-11 15:08:44 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/sparc/dev/bsd_audio.c
+++ sys/sparc/dev/bsd_audio.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1991, 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This software was developed by the Computer Systems Engineering group
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
@@ -13,7 +13,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)bsd_audio.c	7.5 (Berkeley) 04/27/93
+ *	@(#)bsd_audio.c	8.1 (Berkeley) 06/11/93
  *
  * from: $Header: bsd_audio.c,v 1.18 93/04/24 16:20:35 leres Exp $ (LBL)
  */
