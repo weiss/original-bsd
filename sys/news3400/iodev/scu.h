0dc589e4e479931ca6ce9e6caf2a12005ef9f32d
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-11 15:00:02 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/news3400/iodev/scu.h
+++ sys/news3400/iodev/scu.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Sony Corp. and Kazumasa Utashiro of Software Research Associates, Inc.
@@ -9,7 +9,7 @@
  *
  * from: $Hdr: scu.h,v 4.300 91/07/02 16:38:38 root Rel41 $ SONY
  *
- *	@(#)scu.h	7.2 (Berkeley) 12/17/92
+ *	@(#)scu.h	8.1 (Berkeley) 06/11/93
  */
 
 /*
--- sys/news3400/iodev/sddefs.c
+++ sys/news3400/iodev/sddefs.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Sony Corp. and Kazumasa Utashiro of Software Research Associates, Inc.
@@ -9,7 +9,7 @@
  *
  * from: $Hdr: sddefs.c,v 4.300 91/06/09 06:38:25 root Rel41 $ SONY
  *
- *	@(#)sddefs.c	7.2 (Berkeley) 12/17/92
+ *	@(#)sddefs.c	8.1 (Berkeley) 06/11/93
  */
 
 #include "sd.h"
