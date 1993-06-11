260cce23867e8d1acc9285db18d802093358c087
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-11 00:43:30 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/news3400/fb/fbdefs.h
+++ sys/news3400/fb/fbdefs.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Sony Corp. and Kazumasa Utashiro of Software Research Associates, Inc.
@@ -9,7 +9,7 @@
  *
  * from: $Hdr: fbdefs.h,v 4.300 91/06/09 06:33:54 root Rel41 $ SONY
  *
- *	@(#)fbdefs.h	7.4 (Berkeley) 03/09/93
+ *	@(#)fbdefs.h	8.1 (Berkeley) 06/11/93
  */
 
 #ifndef mips
