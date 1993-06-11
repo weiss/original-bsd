6e6d992d7317921dc74dcfb6c0bfae815092f6c8
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-11 16:29:31 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/ufs/ufs/ufs_vnops.c
+++ sys/ufs/ufs/ufs_vnops.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1982, 1986, 1989 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ufs_vnops.c	7.120 (Berkeley) 05/25/93
+ *	@(#)ufs_vnops.c	8.1 (Berkeley) 06/11/93
  */
 
 #include <sys/param.h>
