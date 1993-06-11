c02ddb1606c0e047e39715f3c73e8068184cc385
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-11 15:02:59 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/news3400/news3400/cons.c
+++ sys/news3400/news3400/cons.c
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Kazumasa Utashiro of Software Research Associates, Inc.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)cons.c	7.3 (Berkeley) 12/17/92
+ *	@(#)cons.c	8.1 (Berkeley) 06/11/93
  */
 
 #include <sys/param.h>
--- sys/news3400/news3400/genassym.c
+++ sys/news3400/news3400/genassym.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)genassym.c	7.7 (Berkeley) 05/13/93
+ *	@(#)genassym.c	8.1 (Berkeley) 06/11/93
  */
 
 #define KERNEL
