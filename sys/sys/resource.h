7228837aae3b6b4843dd43fea54eee45afc57c3c
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-14 15:48:49 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/sys/resource.h
+++ sys/sys/resource.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1982, 1986 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)resource.h	7.10 (Berkeley) 06/14/93
+ *	@(#)resource.h	8.1 (Berkeley) 06/14/93
  */
 
 #ifndef _RESOURCE_H_
