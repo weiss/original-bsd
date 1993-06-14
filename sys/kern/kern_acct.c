16ca53a00ed4cd75e0c38998acb9937420f4bc27
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-14 15:27:05 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/kern/kern_acct.c
+++ sys/kern/kern_acct.c
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  *
- *	@(#)kern_acct.c	7.29 (Berkeley) 06/14/93
+ *	@(#)kern_acct.c	8.1 (Berkeley) 06/14/93
  */
 
 #include <sys/param.h>
