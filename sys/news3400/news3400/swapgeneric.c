b8b5ca7bb7670f8cf83a09fd9d2f4ee093498b73
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-16 19:32:20 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/news3400/news3400/swapgeneric.c
+++ sys/news3400/news3400/swapgeneric.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ralph Campbell, and Kazumasa Utashiro of Software Research
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)swapgeneric.c	7.4 (Berkeley) 06/16/93
+ *	@(#)swapgeneric.c	8.1 (Berkeley) 06/16/93
  */
 
 #include <sysparam.h>
