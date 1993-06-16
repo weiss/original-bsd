91de41c1f02e02bac2585e9848c399d55178df13
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-16 19:29:02 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/pmax/pmax/swapgeneric.c
+++ sys/pmax/pmax/swapgeneric.c
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 1992 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ralph Campbell.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)swapgeneric.c	7.4 (Berkeley) 06/16/93
+ *	@(#)swapgeneric.c	8.1 (Berkeley) 06/16/93
  */
 
 #include <sys/param.h>
