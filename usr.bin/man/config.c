b9d6d80604824e37331c486594a6d7a65140f6fb
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-17 11:46:09 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/man/config.c
+++ usr.bin/man/config.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)config.c	5.9 (Berkeley) 06/17/93";
+static char sccsid[] = "@(#)config.c	8.1 (Berkeley) 06/17/93";
 #endif /* not lint */
 
 #include <sys/param.h>
