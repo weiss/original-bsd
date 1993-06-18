c6ce5125b1b0eca190bd0db11b17afe26be25b47
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-18 17:08:26 +0000

    lint

--- sys/miscfs/fdesc/fdesc_vnops.c
+++ sys/miscfs/fdesc/fdesc_vnops.c
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)fdesc_vnops.c	7.8 (Berkeley) 04/30/93
+ *	@(#)fdesc_vnops.c	7.9 (Berkeley) 06/18/93
  *
  * $Id: fdesc_vnops.c,v 1.12 1993/04/06 16:17:17 jsp Exp $
  */
@@ -382,7 +382,7 @@ fdesc_getattr(ap)
 	struct vnode *vp = ap->a_vp;
 	struct vattr *vap = ap->a_vap;
 	unsigned fd;
-	int error;
+	int error = 0;
 
 #ifdef FDESC_DIAGNOSTIC
 	printf("fdesc_getattr: stat type = %d\n", VTOFDESC(vp)->fd_type);
