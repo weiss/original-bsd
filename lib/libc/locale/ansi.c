2fa2bcb3f9fe60090629bf66f0e1015a8b2b7a6b
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-27 19:22:41 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libc/locale/ansi.c
+++ lib/libc/locale/ansi.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Paul Borman at Krystal Technologies.
@@ -9,7 +9,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)ansi.c	5.2 (Berkeley) 05/27/93";
+static char sccsid[] = "@(#)ansi.c	8.1 (Berkeley) 06/27/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <stdlib.h>
