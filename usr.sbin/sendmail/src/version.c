07804493129a9e7cd51f7b4ae96bfc02d9b85e71
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-27 18:27:39 +0000

    4.4BSD snapshot (revision 8.1)

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.68 (Berkeley) 06/24/93";
+static char sccsid[] = "@(#)version.c	8.1 (Berkeley) 06/27/93";
 #endif /* not lint */
 
 char	Version[] = "8.1C";
