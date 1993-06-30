014c0886fae94a617e5fc09adacd4469eb3049ba
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1993-06-30 11:18:32 +0000

    no .TH for old man pages when doing -t

--- libexec/getNAME/getNAME.c
+++ libexec/getNAME/getNAME.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)getNAME.c	5.7 (Berkeley) 06/18/93";
+static char sccsid[] = "@(#)getNAME.c	5.8 (Berkeley) 06/30/93";
 #endif /* not lint */
 
 /*
@@ -123,7 +123,7 @@ getfrom(pathname)
 	trimln(headbuf);
 	if (tocrc)
 		doname(name);
-	if (!intro)
+	if (!tocrc && !intro)
 		printf("%s\t", headbuf);
 	linbuf[0] = '\0';
 	for (;;) {
