bcb45a7e729a57b1f62e149bc298edbf376a41b6
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-07-15 16:59:07 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/vm/vm_swap.c
+++ sys/vm/vm_swap.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1982, 1986, 1989 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vm_swap.c	7.30 (Berkeley) 07/15/93
+ *	@(#)vm_swap.c	8.1 (Berkeley) 07/15/93
  */
 
 #include <sys/param.h>
