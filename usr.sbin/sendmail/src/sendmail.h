773c5935253759c802c08177e30e79419116fdbf
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-07-18 07:21:08 +0000

    make metamac.metaval type u_char (some compilers complain)

--- usr.sbin/sendmail/src/sendmail.h
+++ usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.5 (Berkeley) 07/17/93
+ *	@(#)sendmail.h	8.6 (Berkeley) 07/18/93
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.5		07/17/93";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.6		07/18/93";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -390,7 +390,7 @@ EXTERN struct rewrite	*RewriteRules[MAXRWSETS];
 struct metamac
 {
 	char	metaname;	/* external code (after $) */
-	char	metaval;	/* internal code (as above) */
+	u_char	metaval;	/* internal code (as above) */
 };
 /*
 **  Information about currently open connections to mailers, or to
