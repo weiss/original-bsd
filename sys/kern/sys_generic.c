27a38ea8fe5dfe8e46d0bdc2d6d5bdeb5a3d0a95
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-07-19 00:17:37 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/kern/sys_generic.c
+++ sys/kern/sys_generic.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1982, 1986, 1989 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sys_generic.c	7.39 (Berkeley) 07/19/93
+ *	@(#)sys_generic.c	8.1 (Berkeley) 07/19/93
  */
 
 #include <sys/param.h>
