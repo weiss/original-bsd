e3fb50233824571ffe19fb2c61462e0b8368ed8f
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-07-20 11:36:57 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/kern/uipc_mbuf.c
+++ sys/kern/uipc_mbuf.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1982, 1986, 1988, 1991 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1988, 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)uipc_mbuf.c	7.26 (Berkeley) 07/20/93
+ *	@(#)uipc_mbuf.c	8.1 (Berkeley) 07/20/93
  */
 
 #include <sys/param.h>
