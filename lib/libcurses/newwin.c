ba2c28ba94e38d6a7ac0fabad84a6ba0375ae7dd
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-07-20 07:24:02 +0000

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libcurses/newwin.c
+++ lib/libcurses/newwin.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1981 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1981, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)newwin.c	5.19 (Berkeley) 07/20/93";
+static char sccsid[] = "@(#)newwin.c	8.1 (Berkeley) 07/20/93";
 #endif	/* not lint */
 
 #include <curses.h>
--- lib/libcurses/refresh.c
+++ lib/libcurses/refresh.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1981 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1981, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)refresh.c	5.47 (Berkeley) 07/20/93";
+static char sccsid[] = "@(#)refresh.c	8.1 (Berkeley) 07/20/93";
 #endif /* not lint */
 
 #include <curses.h>
