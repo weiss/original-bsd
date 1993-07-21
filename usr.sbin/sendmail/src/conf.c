b81e582a1b5ecc601725a57ac6976ba95d5fd777
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-07-21 20:06:31 +0000

    bulk clear flock structure -- some systems apparently have some additional fields that will cause "bad argument" messages

--- usr.sbin/sendmail/src/conf.c
+++ usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.8 (Berkeley) 07/21/93";
+static char sccsid[] = "@(#)conf.c	8.9 (Berkeley) 07/21/93";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1453,6 +1453,7 @@ lockfile(fd, filename, type)
 	int action;
 	struct flock lfd;
 
+	bzero(&lfd, sizeof lfd);
 	if (bitset(LOCK_UN, type))
 		lfd.l_type = F_UNLCK;
 	else if (bitset(LOCK_EX, type))
@@ -1464,8 +1465,6 @@ lockfile(fd, filename, type)
 		action = F_SETLK;
 	else
 		action = F_SETLKW;
-
-	lfd.l_whence = lfd.l_start = lfd.l_len = 0;
 
 	if (fcntl(fd, action, &lfd) >= 0)
 		return TRUE;
