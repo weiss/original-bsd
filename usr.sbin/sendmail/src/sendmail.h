4d50702a5aa48730d5658d9b2285232823649185
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-07-22 11:27:57 +0000

    fix bogus error message with open timeout in :include: -- this also fixes a bug that caused resolver errnos to conflict with real errnos

--- usr.sbin/sendmail/src/sendmail.h
+++ usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.7 (Berkeley) 07/21/93
+ *	@(#)sendmail.h	8.8 (Berkeley) 07/22/93
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.7		07/21/93";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.8		07/22/93";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -648,10 +648,6 @@ EXTERN char	OpMode;		/* operation mode, see below */
 /*
 **  Additional definitions
 */
-
-
-/* Offset used to ensure that name server error * codes are unique */
-#define	MAX_ERRNO	100
 
 
 /*
