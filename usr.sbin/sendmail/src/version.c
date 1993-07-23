bc2072b62d226b68591a85a4626e21b22e919af1
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-07-23 14:53:01 +0000

    command line mail to bad recipients didn't return error message; don't call getcanonname from getmxrr if you know it will fail; don't claim that all messages were received from localhost in error messages

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	8.4 (Berkeley) 07/22/93";
+static char sccsid[] = "@(#)version.c	8.5 (Berkeley) 07/23/93";
 #endif /* not lint */
 
-char	Version[] = "8.4";
+char	Version[] = "8.5";
