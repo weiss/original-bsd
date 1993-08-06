944b493c779d75363db1f794a87ca773c24bcce1
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-08-06 21:20:11 +0000

    Solaris and NeXT portability

--- usr.sbin/sendmail/src/conf.c
+++ usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.13 (Berkeley) 07/27/93";
+static char sccsid[] = "@(#)conf.c	8.14 (Berkeley) 08/06/93";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1120,7 +1120,6 @@ char	**nargv,
 	static char	*place = EMSG;	/* option letter processing */
 	static char	atend = 0;
 	register char	*oli;		/* option letter list index */
-	char	*index();
 
 	if (atend) {
 		atend = 0;
@@ -1137,7 +1136,7 @@ char	**nargv,
 			return(EOF);
 		}
 	}				/* option letter okay? */
-	if ((optopt = (int)*place++) == (int)':' || !(oli = index(ostr,optopt))) {
+	if ((optopt = (int)*place++) == (int)':' || !(oli = strchr(ostr,optopt))) {
 		if(!*place) ++optind;
 		tell(": illegal option -- ");
 	}
