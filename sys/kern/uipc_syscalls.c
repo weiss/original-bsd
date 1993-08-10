34dfe9df31f11397c277394141702147c491f50a
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1993-08-10 16:07:32 +0000

    avoid mbuf leak

--- sys/kern/uipc_syscalls.c
+++ sys/kern/uipc_syscalls.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)uipc_syscalls.c	8.1 (Berkeley) 06/10/93
+ *	@(#)uipc_syscalls.c	8.2 (Berkeley) 08/10/93
  */
 
 #include <sys/param.h>
@@ -1114,11 +1114,11 @@ getpeername1(p, uap, retval)
 	so = (struct socket *)fp->f_data;
 	if ((so->so_state & (SS_ISCONNECTED|SS_ISCONFIRMING)) == 0)
 		return (ENOTCONN);
+	if (error = copyin((caddr_t)uap->alen, (caddr_t)&len, sizeof (len)))
+		return (error);
 	m = m_getclr(M_WAIT, MT_SONAME);
 	if (m == NULL)
 		return (ENOBUFS);
-	if (error = copyin((caddr_t)uap->alen, (caddr_t)&len, sizeof (len)))
-		return (error);
 	if (error = (*so->so_proto->pr_usrreq)(so, PRU_PEERADDR, 0, m, 0))
 		goto bad;
 	if (len > m->m_len)
