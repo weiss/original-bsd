4f6cf211854732312a683d049db72d149d054afd
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-09-05 09:43:15 +0000

    Cleanups for 4.4BSD-Lite

--- sys/sys/vnode.h
+++ sys/sys/vnode.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vnode.h	8.1 (Berkeley) 06/02/93
+ *	@(#)vnode.h	8.2 (Berkeley) 09/05/93
  */
 
 #include <sys/queue.h>
@@ -181,7 +181,7 @@ void	vref __P((struct vnode *));
 /*
  * Global vnode data.
  */
-extern	struct vnode *rootdir;		/* root (i.e. "/") vnode */
+extern	struct vnode *rootvnode;	/* root (i.e. "/") vnode */
 extern	int desiredvnodes;		/* number of vnodes desired */
 extern	struct vattr va_null;		/* predefined null vattr structure */
 
