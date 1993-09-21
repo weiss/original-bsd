f2c4281693d5ad92b0e49359004b54a23af5cadf
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1993-09-21 07:35:34 +0000

    changes for 4.4BSD-Lite requested by USL

--- sys/kern/kern_exit.c
+++ sys/kern/kern_exit.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kern_exit.c	8.1 (Berkeley) 06/14/93
+ *	@(#)kern_exit.c	8.2 (Berkeley) 09/21/93
  */
 
 #include <sys/param.h>
@@ -294,11 +294,7 @@ wait4(p, uap, retval)
 #define	wait1	wait4
 #endif
 
-/*
- * Wait: check child processes to see if any have exited,
- * stopped under trace, or (optionally) stopped by a signal.
- * Pass back status and deallocate exited child's proc structure.
- */
+int
 wait1(q, uap, retval)
 	register struct proc *q;
 	register struct wait_args *uap;
