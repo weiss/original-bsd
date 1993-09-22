477d0e3c8dcbcd54e23dab7a0a898fc03cd98316
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-09-22 09:02:55 +0000

    get sendmail.cf name from a routine (so it can autoconfigure on NeXt)

--- usr.sbin/sendmail/src/conf.c
+++ usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.35 (Berkeley) 09/19/93";
+static char sccsid[] = "@(#)conf.c	8.36 (Berkeley) 09/22/93";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -101,7 +101,6 @@ struct hdrinfo	HdrInfo[] =
 **  Location of system files/databases/etc.
 */
 
-char	*ConfFile =	_PATH_SENDMAILCF;	/* runtime configuration */
 char	*PidFile =	_PATH_SENDMAILPID;	/* stores daemon proc id */
 
 
@@ -1612,4 +1611,17 @@ lockfile(fd, filename, ext, type)
 	if (tTd(55, 60))
 		printf("FAIL\n");
 	return FALSE;
+}
+/*
+**  GETCFNAME -- return the name of the .cf file.
+**
+**	Some systems (e.g., NeXT) determine this dynamically.
+*/
+
+char *
+getcfname()
+{
+	if (ConfFile != NULL)
+		return ConfFile;
+	return _PATH_SENDMAILCF;
 }
--- usr.sbin/sendmail/src/sendmail.h
+++ usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.19 (Berkeley) 09/18/93
+ *	@(#)sendmail.h	8.20 (Berkeley) 09/22/93
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.19		09/18/93";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.20		09/22/93";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -777,7 +777,7 @@ EXTERN bool	UseErrorsTo;	/* use Errors-To: header (back compat) */
 EXTERN bool	TryNullMXList;	/* if we are the best MX, try host directly */
 EXTERN char	SpaceSub;	/* substitution for <lwsp> */
 EXTERN int	PrivacyFlags;	/* privacy flags */
-extern char	*ConfFile;	/* location of configuration file [conf.c] */
+EXTERN char	*ConfFile;	/* location of configuration file [conf.c] */
 extern char	*PidFile;	/* location of proc id file [conf.c] */
 extern ADDRESS	NullAddress;	/* a null (template) address [main.c] */
 EXTERN long	WkClassFact;	/* multiplier for message class -> priority */
