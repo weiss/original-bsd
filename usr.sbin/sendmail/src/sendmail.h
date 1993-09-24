3310d6eeb619407ae44bce7afbad85a1ea718744
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-09-24 06:46:49 +0000

    add CheckLoopBack variable

--- usr.sbin/sendmail/src/sendmail.h
+++ usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.21 (Berkeley) 09/22/93
+ *	@(#)sendmail.h	8.22 (Berkeley) 09/24/93
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.21		09/22/93";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.22		09/24/93";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -775,6 +775,7 @@ EXTERN bool	SendMIMEErrors;	/* send error messages in MIME format */
 EXTERN bool	MatchGecos;	/* look for user names in gecos field */
 EXTERN bool	UseErrorsTo;	/* use Errors-To: header (back compat) */
 EXTERN bool	TryNullMXList;	/* if we are the best MX, try host directly */
+EXTERN bool	CheckLoopBack;	/* check for loopback on HELO packet */
 EXTERN char	SpaceSub;	/* substitution for <lwsp> */
 EXTERN int	PrivacyFlags;	/* privacy flags */
 EXTERN char	*ConfFile;	/* location of configuration file [conf.c] */
