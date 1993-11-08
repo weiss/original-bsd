278038724ec7db6a752f24bf8966f201498e2ff6
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-11-08 09:46:05 +0000

    general portability changes

--- usr.sbin/sendmail/src/err.c
+++ usr.sbin/sendmail/src/err.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)err.c	8.14 (Berkeley) 10/29/93";
+static char sccsid[] = "@(#)err.c	8.15 (Berkeley) 11/08/93";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -133,8 +133,6 @@ usrerr(fmt, va_alist)
 #endif
 {
 	VA_LOCAL_DECL
-	extern char SuprErrs;
-	extern int errno;
 
 	if (SuprErrs)
 		return;
