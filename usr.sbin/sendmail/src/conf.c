b1a6d5c2fc5986d04b50b26a639ce7c4e383306e
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-11-20 07:15:05 +0000

    clarify setvendor()

--- usr.sbin/sendmail/src/conf.c
+++ usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.47 (Berkeley) 11/17/93";
+static char sccsid[] = "@(#)conf.c	8.48 (Berkeley) 11/20/93";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1718,13 +1718,24 @@ getcfname()
 **	Returns:
 **		TRUE -- if ok.
 **		FALSE -- if vendor code could not be processed.
+**
+**	Side Effects:
+**		It is reasonable to set mode flags here to tweak
+**		processing in other parts of the code if necessary.
+**		For example, if you are a vendor that uses $%y to
+**		indicate YP lookups, you could enable that here.
 */
 
 bool
 setvendor(vendor)
 	char *vendor;
 {
-	return (strcasecmp(vendor, "Berkeley") == 0);
+	if (strcasecmp(vendor, "Berkeley") == 0)
+		return TRUE;
+
+	/* add vendor extensions here */
+
+	return FALSE;
 }
 /*
 **  STRTOL -- convert string to long integer
