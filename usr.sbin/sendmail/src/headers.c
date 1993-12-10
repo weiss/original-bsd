e9a342121e5fd0af56adaad6e55c376c404a03b1
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-12-10 12:31:19 +0000

    accept Full-Name: header on input

--- usr.sbin/sendmail/src/headers.c
+++ usr.sbin/sendmail/src/headers.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)headers.c	8.14 (Berkeley) 11/29/93";
+static char sccsid[] = "@(#)headers.c	8.15 (Berkeley) 12/10/93";
 #endif /* not lint */
 
 # include <errno.h>
@@ -345,6 +345,11 @@ eatheader(e, full)
 	else
 		define('u', NULL, e);
 
+	/* full name of from person */
+	p = hvalue("full-name", e);
+	if (p != NULL)
+		define('x', p, e);
+
 	if (tTd(32, 1))
 		printf("----- collected header -----\n");
 	msgid = "<none>";
@@ -426,11 +431,6 @@ eatheader(e, full)
 		e->e_msgpriority = e->e_msgsize
 				 - e->e_class * WkClassFact
 				 + e->e_nrcpts * WkRecipFact;
-
-	/* full name of from person */
-	p = hvalue("full-name", e);
-	if (p != NULL)
-		define('x', p, e);
 
 	/* date message originated */
 	p = hvalue("posted-date", e);
