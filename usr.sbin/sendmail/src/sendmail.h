ea5fc1a6ff2f5efa8a292b75c06a568bc758bf9a
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-12-10 17:11:04 +0000

    add a size parameter to prescan()

--- usr.sbin/sendmail/src/sendmail.h
+++ usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.31 (Berkeley) 12/10/93
+ *	@(#)sendmail.h	8.32 (Berkeley) 12/10/93
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.31		12/10/93";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.32		12/10/93";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -898,7 +898,7 @@ extern const char	*errstring __P((int));
 extern void		expand __P((char *, char *, char *, ENVELOPE *));
 extern void		define __P((int, char *, ENVELOPE *));
 extern char		*macvalue __P((int, ENVELOPE *));
-extern char		**prescan __P((char *, int, char[], char **));
+extern char		**prescan __P((char *, int, char[], int, char **));
 extern char		*fgetfolded __P((char *, int, FILE *));
 extern ADDRESS		*recipient __P((ADDRESS *, ADDRESS **, ENVELOPE *));
 extern ENVELOPE		*newenvelope __P((ENVELOPE *, ENVELOPE *));
