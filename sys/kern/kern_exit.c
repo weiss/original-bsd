8ca4e71a67f14f0664fafdcc64484fc92612cb11
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1993-12-13 12:21:44 +0000

    add p_textvp to track vnode of executable (for procfs)

--- sys/kern/kern_exit.c
+++ sys/kern/kern_exit.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kern_exit.c	8.3 (Berkeley) 09/23/93
+ *	@(#)kern_exit.c	8.4 (Berkeley) 12/13/93
  */
 
 #include <sys/param.h>
@@ -360,6 +360,12 @@ loop:
 				crfree(p->p_cred->pc_ucred);
 				FREE(p->p_cred, M_SUBPROC);
 			}
+
+			/*
+			 * Release reference to text vnode
+			 */
+			if (p->p_textvp)
+				vrele(p->p_textvp);
 
 			/*
 			 * Finally finished with old proc entry.
