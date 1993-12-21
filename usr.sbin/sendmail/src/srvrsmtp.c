dad1d4480c84422f87237d4228b1b6675474a05f
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-12-21 14:41:43 +0000

    make the "go away" message more polite

--- usr.sbin/sendmail/src/srvrsmtp.c
+++ usr.sbin/sendmail/src/srvrsmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)srvrsmtp.c	8.22 (Berkeley) 12/11/93 (with SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.23 (Berkeley) 12/21/93 (with SMTP)";
 #else
-static char sccsid[] = "@(#)srvrsmtp.c	8.22 (Berkeley) 12/11/93 (without SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.23 (Berkeley) 12/21/93 (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -603,7 +603,7 @@ smtp(e)
 				if (vrfy)
 					message("252 Who's to say?");
 				else
-					message("502 That's none of your business");
+					message("502 Sorry, we do not allow this operation");
 #ifdef LOG
 				if (LogLevel > 5)
 					syslog(LOG_INFO, "%s: %s [rejected]",
