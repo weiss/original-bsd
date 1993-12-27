46f12a1acf62db6112576711f539c90e1a02848e
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1993-12-27 14:54:50 +0000

    minor comment typo

--- usr.sbin/sendmail/src/conf.c
+++ usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.57 (Berkeley) 12/26/93";
+static char sccsid[] = "@(#)conf.c	8.58 (Berkeley) 12/27/93";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1342,7 +1342,7 @@ vsprintf(s, fmt, ap)
 
 #endif
 /*
-**  usershellok -- tell if a user's shell is ok for unrestricted use
+**  USERSHELLOK -- tell if a user's shell is ok for unrestricted use
 **
 **	Parameters:
 **		shell -- the user's shell from /etc/passwd
