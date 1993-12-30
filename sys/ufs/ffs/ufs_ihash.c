96e1a8103fa58dc8c0653bf8283d3f2709897e24
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1993-12-30 16:12:05 +0000

    vget now locks optionally

--- sys/ufs/ffs/ufs_ihash.c
+++ sys/ufs/ffs/ufs_ihash.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ufs_ihash.c	8.3 (Berkeley) 09/23/93
+ *	@(#)ufs_ihash.c	8.4 (Berkeley) 12/30/93
  */
 
 #include <sys/param.h>
@@ -77,7 +77,7 @@ ufs_ihashget(device, inum)
 					break;
 				}
 				vp = ITOV(ip);
-				if (!vget(vp))
+				if (!vget(vp, 1))
 					return (vp);
 				break;
 			}
