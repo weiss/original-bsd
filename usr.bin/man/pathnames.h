ff2b201d9a6b1bee132dad6274b86b522586fdf0
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-02 10:41:47 +0000

    add a way to specify the config file (-C)

--- usr.bin/man/pathnames.h
+++ usr.bin/man/pathnames.h
@@ -4,14 +4,10 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	8.2 (Berkeley) 01/02/94
+ *	@(#)pathnames.h	8.3 (Berkeley) 01/02/94
  */
 
-#ifdef DEBUG
-#define	_PATH_MANCONF	"./man.conf"
-#else
 #define	_PATH_MANCONF	"/etc/man.conf"
-#endif
 #define	_PATH_PAGER	"/usr/bin/more -s"
-#define	_PATH_WHATIS	"whatis.db"
 #define	_PATH_TMP	"/tmp/man.XXXXXX"
+#define	_PATH_WHATIS	"whatis.db"
