558da3cfc05b11fa9a58f58a945d09ce42589c39
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-04 22:28:45 +0000

    lint

--- sys/hp300/dev/grf_tc.c
+++ sys/hp300/dev/grf_tc.c
@@ -11,7 +11,7 @@
  *
  * from: Utah $Hdr: grf_tc.c 1.20 93/08/13$
  *
- *	@(#)grf_tc.c	8.2 (Berkeley) 09/09/93
+ *	@(#)grf_tc.c	8.3 (Berkeley) 01/04/94
  */
 
 #include "grf.h"
@@ -125,7 +125,7 @@ tc_mode(gp, cmd, data)
 	{
 		struct grf_fbinfo *fi = (struct grf_fbinfo *)data;
 		struct grfinfo *gi = &gp->g_display;
-		int i, j;
+		int i;
 
 		/* feed it what HP-UX expects */
 		fi->id = gi->gd_id;
