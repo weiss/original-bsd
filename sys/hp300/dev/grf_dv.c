3d3e03f7d044103c7c7e8b1ae0607d8a082a72fa
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-04 22:27:54 +0000

    lint

--- sys/hp300/dev/grf_dv.c
+++ sys/hp300/dev/grf_dv.c
@@ -11,7 +11,7 @@
  *
  * from: Utah $Hdr: grf_dv.c 1.12 93/08/13$
  *
- *	@(#)grf_dv.c	8.2 (Berkeley) 09/09/93
+ *	@(#)grf_dv.c	8.3 (Berkeley) 01/04/94
  */
 
 #include "grf.h"
@@ -178,7 +178,7 @@ dv_mode(gp, cmd, data)
 	{
 		struct grf_fbinfo *fi = (struct grf_fbinfo *)data;
 		struct grfinfo *gi = &gp->g_display;
-		int i, j;
+		int i;
 
 		/* feed it what HP-UX expects */
 		fi->id = gi->gd_id;
