22147370dcd51742521aa909add29baa4b0c7150
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-06 10:01:17 +0000

    fix name of filesystem for "df"

--- sys/miscfs/procfs/procfs_vfsops.c
+++ sys/miscfs/procfs/procfs_vfsops.c
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)procfs_vfsops.c	8.1 (Berkeley) 01/05/94
+ *	@(#)procfs_vfsops.c	8.2 (Berkeley) 01/06/94
  *
  * From:
  *	$Id: procfs_vfsops.c,v 3.1 1993/12/15 09:40:17 jsp Exp $
@@ -61,8 +61,8 @@ procfs_mount(mp, path, data, ndp, p)
 	(void) copyinstr(path, (caddr_t)mp->mnt_stat.f_mntonname, MNAMELEN, &size);
 	bzero(mp->mnt_stat.f_mntonname + size, MNAMELEN - size);
 
-	size = sizeof("proc") - 1;
-	bcopy("proc", mp->mnt_stat.f_mntfromname, size);
+	size = sizeof("procfs") - 1;
+	bcopy("procfs", mp->mnt_stat.f_mntfromname, size);
 	bzero(mp->mnt_stat.f_mntfromname + size, MNAMELEN - size);
 
 	return (0);
