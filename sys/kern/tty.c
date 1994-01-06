c12066089ed675ec4be7fc39a5912fed0b7552d8
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-06 16:42:03 +0000

    update to approximate process resident set size (from hibler and cgd)

--- sys/kern/tty.c
+++ sys/kern/tty.c
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)tty.c	8.6 (Berkeley) 01/02/94
+ *	@(#)tty.c	8.7 (Berkeley) 01/06/94
  */
 
 #include <sys/param.h>
@@ -1764,7 +1764,12 @@ ttyinfo(tp)
 		ttyprintf(tp, "%d%% %dk\n",
 		    tmp / 100,
 		    pick->p_stat == SIDL || pick->p_stat == SZOMB ? 0 :
-			pgtok(pick->p_vmspace->vm_rssize));
+#ifdef pmap_resident_count
+			pgtok(pmap_resident_count(&pick->p_vmspace->vm_pmap))
+#else
+			pgtok(pick->p_vmspace->vm_rssize)
+#endif
+			);
 	}
 	tp->t_rocount = 0;	/* so pending input will be retyped if BS */
 }
