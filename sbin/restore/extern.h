2a22fd6c4852be0db4c896d773faec2828ed6626
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-07 15:05:48 +0000

    use C library error functions, not local one

--- sbin/restore/extern.h
+++ sbin/restore/extern.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)extern.h	8.1 (Berkeley) 06/05/93
+ *	@(#)extern.h	8.2 (Berkeley) 01/07/94
  */
 
 struct entry	*addentry __P((char *, ino_t, int));
@@ -21,7 +21,6 @@ void		 deleteino __P((ino_t));
 ino_t		 dirlookup __P((const char *));
 __dead void 	 done __P((int));
 void		 dumpsymtable __P((char *, long));
-void		 err __P((const char *, ...));
 void	 	 extractdirs __P((int));
 int		 extractfile __P((char *));
 void		 findunreflinks __P((void));
