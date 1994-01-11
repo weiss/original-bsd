a7a3d8869ac75e4dffdb86ed7e9c3a90aebfd4cf
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-11 21:18:33 +0000

    use standard promotion rules

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)tcp_input.c	8.3 (Berkeley) 01/04/94
+ *	@(#)tcp_input.c	8.4 (Berkeley) 01/11/94
  */
 
 #ifndef TUBA_INCLUDE
@@ -1490,7 +1490,7 @@ tcp_xmit_timer(tp, rtt)
 int
 tcp_mss(tp, offer)
 	register struct tcpcb *tp;
-	u_short offer;
+	u_int offer;
 {
 	struct route *ro;
 	register struct rtentry *rt;
