b8319d9793be3e9d0c7380acdd226d1c9a2da769
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-11 16:01:49 +0000

    fix error message being partially printed on two different terminals (from ralph)

--- sys/pmax/dev/tz.c
+++ sys/pmax/dev/tz.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)tz.c	8.3 (Berkeley) 01/04/94
+ *	@(#)tz.c	8.4 (Berkeley) 01/11/94
  *
  * from: $Header: /sprite/src/kernel/dev/RCS/devSCSITape.c,
  *	v 8.14 89/07/31 17:26:13 mendel Exp $ SPRITE (Berkeley)
@@ -416,7 +416,7 @@ tzdone(unit, error, resid, status)
 
 			default:
 			prerr:
-				tprintf(sc->sc_ctty, "tz%d: ", unit);
+				printf("tz%d: ", unit);
 				scsiPrintSense((ScsiClass7Sense *)
 					sc->sc_sense.sense,
 					sizeof(sc->sc_sense.sense) - resid);
