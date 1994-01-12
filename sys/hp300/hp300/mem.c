30cdc8ae2f774eef934ef890f89ddfd7ebab3923
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-12 09:46:10 +0000

    lint

--- sys/hp300/hp300/mem.c
+++ sys/hp300/hp300/mem.c
@@ -11,7 +11,7 @@
  *
  * from: Utah $Hdr: mem.c 1.14 90/10/12$
  *
- *	@(#)mem.c	8.2 (Berkeley) 11/14/93
+ *	@(#)mem.c	8.3 (Berkeley) 01/12/94
  */
 
 /*
@@ -135,6 +135,7 @@ mmrw(dev, uio, flags)
 
 mmmap(dev, off, prot)
 	dev_t dev;
+	int off, prot;
 {
 	/*
 	 * /dev/mem is the only one that makes sense through this
