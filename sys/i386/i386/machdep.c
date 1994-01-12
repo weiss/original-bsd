a6813757ff35fed54f713d06388ad32094d38cba
hibler <hibler@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-12 14:29:54 +0000

    entries_pageable set TRUE for buffer map (set vm_map.c change).

--- sys/i386/i386/machdep.c
+++ sys/i386/i386/machdep.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)machdep.c	8.1 (Berkeley) 06/11/93
+ *	@(#)machdep.c	8.2 (Berkeley) 01/12/94
  */
 
 #include <sys/param.h>
@@ -183,7 +183,7 @@ again:
 	 */
 	size = MAXBSIZE * nbuf;
 	buffer_map = kmem_suballoc(kernel_map, (vm_offset_t)&buffers,
-				   &maxaddr, size, FALSE);
+				   &maxaddr, size, TRUE);
 	minaddr = (vm_offset_t)buffers;
 	if (vm_map_find(buffer_map, vm_object_allocate(size), (vm_offset_t)0,
 			&minaddr, size, FALSE) != KERN_SUCCESS)
