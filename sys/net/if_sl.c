616b08715b332dc88f14452a8232a09e4fde00db
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-12 09:31:21 +0000

    lint

--- sys/net/if_sl.c
+++ sys/net/if_sl.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)if_sl.c	8.4 (Berkeley) 01/07/94
+ *	@(#)if_sl.c	8.5 (Berkeley) 01/12/94
  */
 
 /*
@@ -278,7 +278,6 @@ sltioctl(tp, cmd, data, flag)
 	int flag;
 {
 	struct sl_softc *sc = (struct sl_softc *)tp->t_sc;
-	int s;
 
 	switch (cmd) {
 	case SLIOCGUNIT:
