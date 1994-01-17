21e913607c87b5323670fbe6b069f4306dbffe0d
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-17 14:44:17 +0000

    update contrib line

--- sys/miscfs/procfs/procfs_mem.c
+++ sys/miscfs/procfs/procfs_mem.c
@@ -4,11 +4,11 @@
  * All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
- * Jan-Simon Pendry.
+ * Jan-Simon Pendry and Sean Eric Fagan.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)procfs_mem.c	8.1 (Berkeley) 01/05/94
+ *	@(#)procfs_mem.c	8.2 (Berkeley) 01/17/94
  *
  * From:
  *	$Id: procfs_mem.c,v 3.2 1993/12/15 09:40:17 jsp Exp $
