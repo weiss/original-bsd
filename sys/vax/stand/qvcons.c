ec21962e509268e2ac978e307366a2dada7abf01
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-21 17:32:11 +0000

    add USL's copyright notice

--- sys/vax/stand/qvcons.c
+++ sys/vax/stand/qvcons.c
@@ -1,10 +1,15 @@
 /*
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988
+ *	The Regents of the University of California.  All rights reserved.
+ * (c) UNIX System Laboratories, Inc.
+ * All or some portions of this file are derived from material licensed
+ * to the University of California by American Telephone and Telegraph
+ * Co. or Unix System Laboratories, Inc. and are reproduced herein with
+ * the permission of UNIX System Laboratories, Inc.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)qvcons.c	7.7 (Berkeley) 12/16/90
+ *	@(#)qvcons.c	7.8 (Berkeley) 01/21/94
  */
 
 /*
