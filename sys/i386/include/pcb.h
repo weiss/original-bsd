eae503c9cc04c9a2f05eafae54922755317f426c
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-21 19:45:52 +0000

    copyright typo

--- sys/i386/include/pcb.h
+++ sys/i386/include/pcb.h
@@ -1,13 +1,13 @@
 /*-
- * Copyright (c) 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * William Jolitz.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pcb.h	8.1 (Berkeley) 06/11/93
+ *	@(#)pcb.h	8.2 (Berkeley) 01/21/94
  */
 
 /*
