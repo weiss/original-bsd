dc08275ac2e78ef7c957709a274145cf4126196e
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-21 19:56:26 +0000

    copyright typo

--- sys/miscfs/kernfs/kernfs_vnops.c
+++ sys/miscfs/kernfs/kernfs_vnops.c
@@ -1,14 +1,13 @@
 /*
  * Copyright (c) 1992, 1993
  *	The Regents of the University of California.  All rights reserved.
- * All rights reserved.
  *
  * This code is derived from software donated to Berkeley by
  * Jan-Simon Pendry.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kernfs_vnops.c	8.4 (Berkeley) 01/14/94
+ *	@(#)kernfs_vnops.c	8.5 (Berkeley) 01/21/94
  */
 
 /*
