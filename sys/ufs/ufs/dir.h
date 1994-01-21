a257ce3784ad7bdc4c4ad956a3cb386438ac0dea
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-21 17:30:59 +0000

    add USL's copyright notice

--- sys/ufs/ufs/dir.h
+++ sys/ufs/ufs/dir.h
@@ -1,10 +1,15 @@
 /*
  * Copyright (c) 1982, 1986, 1989, 1993
  *	The Regents of the University of California.  All rights reserved.
+ * (c) UNIX System Laboratories, Inc.
+ * All or some portions of this file are derived from material licensed
+ * to the University of California by American Telephone and Telegraph
+ * Co. or Unix System Laboratories, Inc. and are reproduced herein with
+ * the permission of UNIX System Laboratories, Inc.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)dir.h	8.1 (Berkeley) 06/11/93
+ *	@(#)dir.h	8.2 (Berkeley) 01/21/94
  */
 
 #ifndef _DIR_H_
