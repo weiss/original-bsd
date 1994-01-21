6074975084eb0f5eb8eebfee68f8ac0c57643e55
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-21 17:25:25 +0000

    add USL's copyright notice

--- sys/sys/signal.h
+++ sys/sys/signal.h
@@ -1,10 +1,15 @@
 /*
  * Copyright (c) 1982, 1986, 1989, 1991, 1993
  *	The Regents of the University of California.  All rights reserved.
+ * (c) UNIX System Laboratories, Inc.
+ * All or some portions of this file are derived from material licensed
+ * to the University of California by American Telephone and Telegraph
+ * Co. or Unix System Laboratories, Inc. and are reproduced herein with
+ * the permission of UNIX System Laboratories, Inc.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)signal.h	8.1 (Berkeley) 06/02/93
+ *	@(#)signal.h	8.2 (Berkeley) 01/21/94
  */
 
 #ifndef	_SYS_SIGNAL_H_
