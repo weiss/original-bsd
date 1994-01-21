83e429b645e82115aab7867a061f70ce66c73433
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-21 19:55:30 +0000

    copyright typo

--- sys/miscfs/umapfs/umap_vfsops.c
+++ sys/miscfs/umapfs/umap_vfsops.c
@@ -1,14 +1,13 @@
 /*
  * Copyright (c) 1992, 1993
  *	The Regents of the University of California.  All rights reserved.
- * All rights reserved.
  *
  * This code is derived from software donated to Berkeley by
  * the UCLA Ficus project.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)umap_vfsops.c	8.2 (Berkeley) 01/13/94
+ *	@(#)umap_vfsops.c	8.3 (Berkeley) 01/21/94
  *
  * @(#)null_vfsops.c       1.5 (Berkeley) 7/10/92
  */
