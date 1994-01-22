75570149e4c73eb191454bcf0771f610d1019d05
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-22 09:01:35 +0000

    fix problem that caused loss of "invalid HELO arg" authentication warnings

--- usr.sbin/sendmail/src/srvrsmtp.c
+++ usr.sbin/sendmail/src/srvrsmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)srvrsmtp.c	8.24 (Berkeley) 01/15/94 (with SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.25 (Berkeley) 01/22/94 (with SMTP)";
 #else
-static char sccsid[] = "@(#)srvrsmtp.c	8.24 (Berkeley) 01/15/94 (without SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.25 (Berkeley) 01/22/94 (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -238,14 +238,6 @@ smtp(e)
 				SmtpPhase = "server HELO";
 			}
 			sendinghost = newstr(p);
-			if (strcasecmp(p, RealHostName) != 0 &&
-			    (strcasecmp(RealHostName, "localhost") != 0 ||
-			     strcasecmp(p, MyHostName) != 0))
-			{
-				auth_warning(e, "Host %s claimed to be %s",
-					RealHostName, p);
-			}
-
 			gothello = TRUE;
 			if (c->cmdcode != CMDEHLO)
 			{
@@ -306,6 +298,14 @@ smtp(e)
 					"Host %s didn't use HELO protocol",
 					RealHostName);
 			}
+			if (strcasecmp(sendinghost, RealHostName) != 0 &&
+			    (strcasecmp(RealHostName, "localhost") != 0 ||
+			     strcasecmp(sendinghost, MyHostName) != 0))
+			{
+				auth_warning(e, "Host %s claimed to be %s",
+					RealHostName, sendinghost);
+			}
+
 			if (protocol == NULL)
 				protocol = "SMTP";
 			define('r', protocol, e);
