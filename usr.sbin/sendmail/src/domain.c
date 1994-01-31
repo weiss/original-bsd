423c850f26010ac79fda203e93162a4e2beb47e1
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1994-01-31 17:34:00 +0000

    use better MX hash function

--- usr.sbin/sendmail/src/domain.c
+++ usr.sbin/sendmail/src/domain.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef NAMED_BIND
-static char sccsid[] = "@(#)domain.c	8.11 (Berkeley) 01/15/94 (with name server)";
+static char sccsid[] = "@(#)domain.c	8.12 (Berkeley) 01/31/94 (with name server)";
 #else
-static char sccsid[] = "@(#)domain.c	8.11 (Berkeley) 01/15/94 (without name server)";
+static char sccsid[] = "@(#)domain.c	8.12 (Berkeley) 01/31/94 (without name server)";
 #endif
 #endif /* not lint */
 
@@ -343,7 +343,7 @@ mxrand(host)
 
 		if (isascii(c) && isupper(c))
 			c = tolower(c);
-		hfunc = ((hfunc << 1) + c) % 2003;
+		hfunc = ((hfunc << 1) ^ c) % 2003;
 	}
 
 	hfunc &= 0xff;
