562ec14760539bf8b1da1dc5a31a8e28c8f71a91
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1994-02-06 12:52:38 +0000

    try to limit the overhead on split envelopes

--- usr.sbin/sendmail/src/deliver.c
+++ usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	8.68 (Berkeley) 02/06/94";
+static char sccsid[] = "@(#)deliver.c	8.69 (Berkeley) 02/06/94";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -238,9 +238,6 @@ sendall(e, mode)
 						e->e_df, ee->e_df);
 				}
 			}
-
-			if (mode != SM_VERIFY)
-				openxscript(ee);
 #ifdef LOG
 			if (LogLevel > 4)
 				syslog(LOG_INFO, "%s: clone %s",
@@ -284,15 +281,15 @@ sendall(e, mode)
 		for (ee = splitenv; ee != NULL; ee = ee->e_sibling)
 		{
 			CurEnv = ee;
+			if (mode != SM_VERIFY)
+				openxscript(ee);
 			sendenvelope(ee, mode);
+			dropenvelope(ee);
 		}
 
 		CurEnv = e;
 	}
 	sendenvelope(e, mode);
-
-	for (; splitenv != NULL; splitenv = splitenv->e_sibling)
-		dropenvelope(splitenv);
 }
 
 sendenvelope(e, mode)
@@ -595,8 +592,8 @@ deliver(e, firstto)
 	SmtpError[0] = '\0';
 
 	if (tTd(10, 1))
-		printf("\n--deliver, mailer=%d, host=`%s', first user=`%s'\n",
-			m->m_mno, host, to->q_user);
+		printf("\n--deliver, id=%s, mailer=%s, host=`%s', first user=`%s'\n",
+			e->e_id, m->m_name, host, to->q_user);
 	if (tTd(10, 100))
 		printopenfds(FALSE);
 
