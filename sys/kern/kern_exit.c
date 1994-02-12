72ef5adfb2d49783723c4b950d8118ac0206d634
karels <karels@65305748-3e16-e211-a854-8c89a5c6f760>
1994-02-12 09:59:36 +0000

    fix race in exit of session leader: if tty is revoked by spec_close or other revoke while we wait for output to drain, ttyvp will be released and null

--- sys/kern/kern_exit.c
+++ sys/kern/kern_exit.c
@@ -9,7 +9,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kern_exit.c	8.6 (Berkeley) 01/21/94
+ *	@(#)kern_exit.c	8.7 (Berkeley) 02/12/94
  */
 
 #include <sys/param.h>
@@ -132,9 +132,15 @@ exit1(p, rv)
 				if (sp->s_ttyp->t_pgrp)
 					pgsignal(sp->s_ttyp->t_pgrp, SIGHUP, 1);
 				(void) ttywait(sp->s_ttyp);
-				vgoneall(sp->s_ttyvp);
+				/*
+				 * The tty could have been revoked
+				 * if we blocked.
+				 */
+				if (sp->s_ttyvp)
+					vgoneall(sp->s_ttyvp);
 			}
-			vrele(sp->s_ttyvp);
+			if (sp->s_ttyvp)
+				vrele(sp->s_ttyvp);
 			sp->s_ttyvp = NULL;
 			/*
 			 * s_ttyp is not zero'd; we use this to indicate
