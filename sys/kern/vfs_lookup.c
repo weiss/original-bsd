98db06caaf8a1a8fe232c692736f79b1b3b19013
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1994-02-16 23:29:12 +0000

    vref -> VREF

--- sys/kern/vfs_lookup.c
+++ sys/kern/vfs_lookup.c
@@ -9,7 +9,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vfs_lookup.c	8.3 (Berkeley) 01/21/94
+ *	@(#)vfs_lookup.c	8.4 (Berkeley) 02/16/94
  */
 
 #include <sys/param.h>
@@ -302,7 +302,7 @@ dirloop:
 		}
 		if (wantparent) {
 			ndp->ni_dvp = dp;
-			vref(dp);
+			VREF(dp);
 		}
 		ndp->ni_vp = dp;
 		if (!(cnp->cn_flags & (LOCKPARENT | LOCKLEAF)))
