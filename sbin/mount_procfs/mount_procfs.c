7390e9728c59a2ebab4f1f9cdcafea64b1be7db6
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1994-02-18 10:16:14 +0000

    fix error message

--- sbin/mount_procfs/mount_procfs.c
+++ sbin/mount_procfs/mount_procfs.c
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mount_procfs.c	8.1 (Berkeley) 01/11/94
+ *	@(#)mount_procfs.c	8.2 (Berkeley) 02/18/94
  */
 
 #include <sys/param.h>
@@ -46,7 +46,7 @@ main(argc, argv)
 		usage();
 
 	if (mount(MOUNT_PROCFS, argv[1], mntflags, NULL)) {
-		(void)fprintf(stderr, "mount_fdesc: %s\n", strerror(errno));
+		(void)fprintf(stderr, "mount_procfs: %s\n", strerror(errno));
 		exit(1);
 	}
 	exit(0);
