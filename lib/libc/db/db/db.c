55a2be2d0bc9cdc4a58bd4408bca51fb811ec7c9
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-02-21 15:07:29 +0000

    move __DBINTERFACE_PRIVATE into Makefile.inc

--- lib/libc/db/db/db.c
+++ lib/libc/db/db/db.c
@@ -6,7 +6,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)db.c	8.3 (Berkeley) 09/13/93";
+static char sccsid[] = "@(#)db.c	8.4 (Berkeley) 02/21/94";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -16,7 +16,6 @@ static char sccsid[] = "@(#)db.c	8.3 (Berkeley) 09/13/93";
 #include <stddef.h>
 #include <stdio.h>
 
-#define	__DBINTERFACE_PRIVATE
 #include <db.h>
 
 DB *
