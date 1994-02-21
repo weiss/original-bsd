289f38185b603e63221e42391c8efe3bc72ec2ee
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-02-21 15:12:48 +0000

    first pass at making DB use basic integral types (the Alpha port)

--- lib/libc/db/recno/extern.h
+++ lib/libc/db/recno/extern.h
@@ -4,14 +4,14 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)extern.h	8.1 (Berkeley) 06/04/93
+ *	@(#)extern.h	8.2 (Berkeley) 02/21/94
  */
 
 #include "../btree/extern.h"
 
 int	 __rec_close __P((DB *));
 int	 __rec_delete __P((const DB *, const DBT *, u_int));
-int	 __rec_dleaf __P((BTREE *, PAGE *, int));
+int	 __rec_dleaf __P((BTREE *, PAGE *, indx_t));
 int	 __rec_fd __P((const DB *));
 int	 __rec_fmap __P((BTREE *, recno_t));
 int	 __rec_fout __P((BTREE *));
