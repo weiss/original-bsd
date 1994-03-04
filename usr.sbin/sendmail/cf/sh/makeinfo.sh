b61a2b6d4fcad83aeaea84bfdba2813812a10303
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1994-03-04 07:32:59 +0000

    patch to handle automounter

--- usr.sbin/sendmail/cf/sh/makeinfo.sh
+++ usr.sbin/sendmail/cf/sh/makeinfo.sh
@@ -6,7 +6,7 @@
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)makeinfo.sh	8.3 (Berkeley) 07/23/93
+#	@(#)makeinfo.sh	8.4 (Berkeley) 03/04/94
 #
 
 usewhoami=0
@@ -47,5 +47,5 @@ then
 else
 	host=`uname -n`
 fi
-echo '#####' built by $user on `date`
-echo '#####' in `pwd` on $host
+echo '#####' built by $user@$host on `date`
+echo '#####' in `pwd` | sed 's/\/tmp_mnt//'
