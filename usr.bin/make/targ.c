7f150dabb43b79538d71568a60526b21392abd78
christos <christos@65305748-3e16-e211-a854-8c89a5c6f760>
1994-03-19 15:57:53 +0000

    fix indentation

--- usr.bin/make/targ.c
+++ usr.bin/make/targ.c
@@ -11,7 +11,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)targ.c	8.1 (Berkeley) 06/06/93";
+static char sccsid[] = "@(#)targ.c	8.2 (Berkeley) 03/19/94";
 #endif /* not lint */
 
 /*-
@@ -123,7 +123,7 @@ Targ_NewGN (name)
     gn->preds =     	Lst_Init(FALSE);
     gn->context =   	Lst_Init (FALSE);
     gn->commands =  	Lst_Init (FALSE);
-    gn->suffix = 	NULL;
+    gn->suffix =	NULL;
 
     return (gn);
 }
