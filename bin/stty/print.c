605a46e62697a0d6c458cd692531328c1c76838e
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-03-26 06:12:30 +0000

    prettiness police

--- bin/stty/print.c
+++ bin/stty/print.c
@@ -6,13 +6,15 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)print.c	8.2 (Berkeley) 03/17/94";
+static char sccsid[] = "@(#)print.c	8.3 (Berkeley) 03/26/94";
 #endif /* not lint */
 
 #include <sys/types.h>
+
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
+
 #include "stty.h"
 #include "extern.h"
 
