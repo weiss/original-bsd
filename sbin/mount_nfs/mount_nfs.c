63b682f04bc3c6c2d18f51e8700c91a5d81eacae
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-03-27 15:42:34 +0000

    Rick Macklem notes that NFS should get MNT_RDONLY and MNT_UPDATE add MNT_FORCE so that MNT_UPDATE can be forced

--- sbin/mount_nfs/mount_nfs.c
+++ sbin/mount_nfs/mount_nfs.c
@@ -15,7 +15,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)mount_nfs.c	8.2 (Berkeley) 03/27/94";
+static char sccsid[] = "@(#)mount_nfs.c	8.3 (Berkeley) 03/27/94";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -60,16 +60,9 @@ static char sccsid[] = "@(#)mount_nfs.c	8.2 (Berkeley) 03/27/94";
 
 #include "mntopts.h"
 
-/*
- * XXX
- * This is without question incorrect -- not sure what the right
- * values are.
- */
 struct mntopt mopts[] = {
 	MOPT_STDOPTS,
-	MOPT_ASYNC,
 	MOPT_FORCE,
-	MOPT_SYNCHRONOUS,
 	MOPT_UPDATE,
 	{ NULL }
 };
