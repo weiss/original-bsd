fe80bce7f5e5f5219957f3d5136a187b10b25249
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1994-03-27 07:44:04 +0000

    remove _PATH_EXECDIR, add _PATH_SBIN, _PATH_USRSBIN

--- sbin/mount/pathnames.h
+++ sbin/mount/pathnames.h
@@ -1,11 +1,12 @@
 /*
- * Copyright (c) 1989, 1993
+ * Copyright (c) 1989, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	8.1 (Berkeley) 06/05/93
+ *	@(#)pathnames.h	8.2 (Berkeley) 03/27/94
  */
 
-#define _PATH_EXECDIR	"/sbin"
+#define _PATH_SBIN	"/sbin"
+#define _PATH_USRSBIN	"/usr/sbin"
 #define	_PATH_MOUNTDPID	"/var/run/mountd.pid"
