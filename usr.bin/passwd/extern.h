49c4040e723194b54f7f98633e82ea15a5d7d472
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1994-04-02 09:50:32 +0000

    date and time created 94/04/02 09:50:32 by pendry

---
+++ usr.bin/passwd/extern.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 1994
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)extern.h	8.1 (Berkeley) 04/02/94
+ */
+
+int	krb_passwd __P((void));
+int	local_passwd __P((char *));
