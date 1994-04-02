5e3ce5bf39f4b726e0d19121576606cc70693588
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1994-04-02 10:07:23 +0000

    add 1994 copyright

--- usr.bin/cmp/extern.h
+++ usr.bin/cmp/extern.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1991, 1993
+ * Copyright (c) 1991, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)extern.h	8.2 (Berkeley) 04/01/94
+ *	@(#)extern.h	8.3 (Berkeley) 04/02/94
  */
 
 #define OK_EXIT		0
