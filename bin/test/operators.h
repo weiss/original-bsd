1b400eb660fc41f8abbb0b655282ef354675d4b0
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1994-04-02 10:03:02 +0000

    add 1994 copyright

--- bin/test/operators.h
+++ bin/test/operators.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1993
+ * Copyright (c) 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)operators.h	8.2 (Berkeley) 04/01/94
+ *	@(#)operators.h	8.3 (Berkeley) 04/02/94
  */
 
 #define	NOT		0
