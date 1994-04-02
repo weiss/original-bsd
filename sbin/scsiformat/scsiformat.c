71146bdb7d695c2a6113ab71abf0f4db41b9b2e0
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-04-02 10:00:59 +0000

    copyright cleanup

--- sbin/scsiformat/scsiformat.c
+++ sbin/scsiformat/scsiformat.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This software was developed by the Computer Systems Engineering group
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
@@ -8,17 +8,17 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)scsiformat.c	5.4 (Berkeley) 06/05/93
+ *	@(#)scsiformat.c	5.5 (Berkeley) 04/02/94
  */
 
 #ifndef lint
 char copyright[] =
-"@(#) Copyright (c) 1993 The Regents of the University of California.\n\
- All rights reserved.\n";
+"@(#) Copyright (c) 1992, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)scsiformat.c	5.4 (Berkeley) 06/05/93";
+static char sccsid[] = "@(#)scsiformat.c	5.5 (Berkeley) 04/02/94";
 #endif /* not lint */
 
 #include <sys/param.h>
