58ad177029dfaca087d8791c46de118a04df252f
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1994-04-02 10:02:40 +0000

    add 1994 copyright

--- bin/stty/modes.c
+++ bin/stty/modes.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1991, 1993
+ * Copyright (c) 1991, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)modes.c	8.2 (Berkeley) 04/01/94";
+static char sccsid[] = "@(#)modes.c	8.3 (Berkeley) 04/02/94";
 #endif /* not lint */
 
 #include <sys/types.h>
