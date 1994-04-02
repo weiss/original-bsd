0838c4196b9a57212e550cbc1973f5d0ee9b528e
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1994-04-02 10:14:14 +0000

    copyright cleanup

--- usr.bin/uuencode/uuencode.c
+++ usr.bin/uuencode/uuencode.c
@@ -1,4 +1,4 @@
-/*
+/*-
  * Copyright (c) 1983, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
@@ -6,7 +6,13 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)uuencode.c	8.1 (Berkeley) 06/06/93";
+char copyright[] =
+"@(#) Copyright (c) 1983, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)uuencode.c	8.2 (Berkeley) 04/02/94";
 #endif /* not lint */
 
 /*
@@ -16,11 +22,13 @@ static char sccsid[] = "@(#)uuencode.c	8.1 (Berkeley) 06/06/93";
  */
 #include <sys/types.h>
 #include <sys/stat.h>
+
 #include <stdio.h>
 
+int
 main(argc, argv)
 	int argc;
-	char **argv;
+	char *argv[];
 {
 	extern int optind;
 	extern int errno;
