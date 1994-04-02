19f1d5c2e801e3080b8b9128d24de09d791b7c04
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1994-04-02 09:10:06 +0000

    date and time created 94/04/02 09:10:06 by pendry

---
+++ usr.bin/chpass/pw_copy.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 1994
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pw_copy.h	8.1 (Berkeley) 04/02/94
+ */
+
+void	 pw_copy __P((int, int, struct passwd *));
