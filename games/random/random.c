6d21f9dd235f66b0276b1b37f15ae0e7a592c874
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1994-04-05 07:18:40 +0000

    random wasn't.  now uses integer compare with 0, which does

--- games/random/random.c
+++ games/random/random.c
@@ -15,7 +15,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)random.c	8.3 (Berkeley) 03/31/94";
+static char sccsid[] = "@(#)random.c	8.4 (Berkeley) 04/05/94";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -95,7 +95,7 @@ main(argc, argv)
 	 * 0 (which has a 1 / denom chance of being true), we select the
 	 * line.
 	 */
-	selected = !((denom * random()) / LONG_MAX);
+	selected = !(int)((denom * random()) / LONG_MAX);
 	while ((ch = getchar()) != EOF) {
 		if (selected)
 			(void)putchar(ch);
@@ -105,7 +105,7 @@ main(argc, argv)
 				err(2, "stdout");
 
 			/* Now see if the next line is to be printed. */
-			selected = !((denom * random()) / LONG_MAX);
+			selected = !(int)((denom * random()) / LONG_MAX);
 		}
 	}
 	if (ferror(stdin))
@@ -116,6 +116,7 @@ main(argc, argv)
 void
 usage()
 {
+
 	(void)fprintf(stderr, "usage: random [-er] [denominator]\n");
 	exit(1);
 }
