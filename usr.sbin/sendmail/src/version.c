0608ae6120992f5030bdf81d92e4893bf6771f65
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1994-04-18 22:18:09 +0000

    see ../RELEASE_NOTES

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	8.6.8.1 (Berkeley) 03/18/94";
+static char sccsid[] = "@(#)version.c	8.6.9.1 (Berkeley) 04/18/94";
 #endif /* not lint */
 
-char	Version[] = "8.6.8";
+char	Version[] = "8.6.9";
