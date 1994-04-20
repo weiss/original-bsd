5902eba7e639b7e0d043d39e2589656558ed73ca
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1994-04-20 09:43:44 +0000

    lint

--- sys/hp300/hp300/machdep.c
+++ sys/hp300/hp300/machdep.c
@@ -11,7 +11,7 @@
  *
  * from: Utah $Hdr: machdep.c 1.74 92/12/20$
  *
- *	@(#)machdep.c	8.9 (Berkeley) 04/18/94
+ *	@(#)machdep.c	8.10 (Berkeley) 04/20/94
  */
 
 #include <sys/param.h>
@@ -426,9 +426,8 @@ setregs(p, entry, retval)
 /*
  * Info for CTL_HW
  */
-extern	char machine[];
 char	cpu_model[120];
-extern	char ostype[], osrelease[], version[];
+extern	char version[];
 
 identifycpu()
 {
