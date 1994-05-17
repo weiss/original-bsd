8bf2e7d704d3f8f149d8cafae2ebc81f05464cc0
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1994-05-17 19:29:57 +0000

    abstract copyup function

--- sys/miscfs/union/union.h
+++ sys/miscfs/union/union.h
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)union.h	8.4 (Berkeley) 05/07/94
+ *	@(#)union.h	8.5 (Berkeley) 05/17/94
  */
 
 struct union_args {
@@ -69,8 +69,10 @@ extern int union_allocvp __P((struct vnode **, struct mount *,
 				struct vnode *, struct vnode *,
 				struct componentname *, struct vnode *,
 				struct vnode *));
-extern int union_copyfile __P((struct proc *, struct ucred *,
-				struct vnode *, struct vnode *));
+extern int union_copyfile __P((struct vnode *, struct vnode *,
+					struct ucred *, struct proc *));
+extern int union_copyup __P((struct union_node *, int, struct ucred *,
+				struct proc *));
 extern int union_mkshadow __P((struct union_mount *, struct vnode *,
 				struct componentname *, struct vnode **));
 extern int union_vn_create __P((struct vnode **, struct union_node *,
