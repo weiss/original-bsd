11a2ec72d8ae4fb98d59f7692384c58bdb96677b
hibler <hibler@65305748-3e16-e211-a854-8c89a5c6f760>
1994-05-27 09:12:14 +0000

    improper parameter to pmap_changebit (should be PA not VA) (from holmgren@tenon.com)

--- sys/hp300/hp300/pmap.c
+++ sys/hp300/hp300/pmap.c
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pmap.c	8.5 (Berkeley) 03/21/94
+ *	@(#)pmap.c	8.6 (Berkeley) 05/27/94
  */
 
 /*
@@ -2240,7 +2240,7 @@ pmap_enter_ptpage(pmap, va)
 #ifdef DEBUG
 			if (dowriteback && dokwriteback)
 #endif
-			pmap_changebit((vm_offset_t)pmap->pm_stab, PG_CCB, 0);
+			pmap_changebit((vm_offset_t)pmap->pm_stpa, PG_CCB, 0);
 			pmap->pm_stfree = protostfree;
 		}
 #endif
