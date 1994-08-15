492781b9bdf04fe3c7ab3a6409057106dd02b928
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1994-08-15 10:14:24 +0000

    don't send error file with return receipts

--- usr.sbin/sendmail/src/savemail.c
+++ usr.sbin/sendmail/src/savemail.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)savemail.c	8.33 (Berkeley) 07/23/94";
+static char sccsid[] = "@(#)savemail.c	8.34 (Berkeley) 08/15/94";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -637,7 +637,7 @@ errbody(mci, e, separator)
 	**  Output error message header (if specified and available).
 	*/
 
-	if (ErrMsgFile != NULL)
+	if (ErrMsgFile != NULL && !bitset(EF_SENDRECEIPT, e->e_parent->e_flags))
 	{
 		if (*ErrMsgFile == '/')
 		{
