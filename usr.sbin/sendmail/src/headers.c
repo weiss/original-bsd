dcaf65b665ae428efdbaeac71c7da715df71025b
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1994-08-17 08:10:02 +0000

    add H_CTE for content-transfer-encoding; don't output it when you are going to be doing 8to7 bit MIME translation, which will add one

--- usr.sbin/sendmail/src/headers.c
+++ usr.sbin/sendmail/src/headers.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)headers.c	8.34 (Berkeley) 07/23/94";
+static char sccsid[] = "@(#)headers.c	8.35 (Berkeley) 08/17/94";
 #endif /* not lint */
 
 # include <errno.h>
@@ -100,7 +100,7 @@ chompheader(line, def, e)
 		if (hi->hi_field == NULL)
 			printf("no header match\n");
 		else
-			printf("header match, hi_flags=%o\n", hi->hi_flags);
+			printf("header match, hi_flags=%x\n", hi->hi_flags);
 	}
 
 	/* see if this is a resent message */
@@ -938,6 +938,15 @@ putheader(mci, h, e)
 		{
 			if (tTd(34, 11))
 				printf(" (skipped (receipt))\n");
+			continue;
+		}
+
+		/* suppress Content-Transfer-Encoding: if we are MIMEing */
+		if (bitset(H_CTE, h->h_flags) &&
+		    bitset(MCIF_CVT8TO7, mci->mci_flags))
+		{
+			if (tTd(34, 11))
+				printf(" (skipped (content-transfer-encoding))\n");
 			continue;
 		}
 
