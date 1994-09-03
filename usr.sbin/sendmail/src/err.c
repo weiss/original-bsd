477d62d9e80190b6109e5fe5b9ae35841ae5e927
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1994-09-03 08:43:58 +0000

    avoid dropping message submitted on stdin if the address resolves to a $#error address with a tempfail code

--- usr.sbin/sendmail/src/err.c
+++ usr.sbin/sendmail/src/err.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)err.c	8.27 (Berkeley) 04/18/94";
+static char sccsid[] = "@(#)err.c	8.28 (Berkeley) 09/03/94";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -339,7 +339,8 @@ puterrmsg(msg)
 		/* notify the postmaster */
 		CurEnv->e_flags |= EF_PM_NOTIFY;
 	}
-	else if (msgcode == '5' && bitset(EF_GLOBALERRS, CurEnv->e_flags))
+	else if ((msgcode == '4' || msgcode == '5') &&
+		 bitset(EF_GLOBALERRS, CurEnv->e_flags))
 	{
 		/* mark long-term fatal errors */
 		CurEnv->e_flags |= EF_FATALERRS;
