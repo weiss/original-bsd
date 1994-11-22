093a63188498bae4b7732a6e2fec0474c94c835f
pendry <pendry@65305748-3e16-e211-a854-8c89a5c6f760>
1994-11-22 11:47:37 +0000

    get rid of usecount mount restriction (needed for union mounts)

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -9,7 +9,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vfs_syscalls.c	8.25 (Berkeley) 11/21/94
+ *	@(#)vfs_syscalls.c	8.26 (Berkeley) 11/22/94
  */
 
 #include <sys/param.h>
@@ -135,7 +135,7 @@ mount(p, uap, retval)
 		vput(vp);
 		return (ENODEV);
 	}
-	if (vp->v_usecount > 1 || vp->v_mountedhere != NULL) {
+	if (vp->v_mountedhere != NULL) {
 		vput(vp);
 		return (EBUSY);
 	}
