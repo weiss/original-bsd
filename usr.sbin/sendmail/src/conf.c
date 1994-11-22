5467ad09f0d14ee80238ea4b1e91c1ed709e9b0c
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1994-11-22 09:35:45 +0000

    "switch" class is not rebuildable -- only the underlying types are

--- usr.sbin/sendmail/src/conf.c
+++ usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.119 (Berkeley) 11/22/94";
+static char sccsid[] = "@(#)conf.c	8.120 (Berkeley) 11/22/94";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -372,7 +372,7 @@ setupmaps()
 		seq_map_lookup, seq_map_store);
 
 	/* switched interface to sequenced maps */
-	MAPDEF("switch", NULL, MCF_ALIASOK|MCF_REBUILDABLE,
+	MAPDEF("switch", NULL, MCF_ALIASOK,
 		map_parseargs, switch_map_open, seq_map_close,
 		seq_map_lookup, seq_map_store);
 }
