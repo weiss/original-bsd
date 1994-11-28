28377034d19a61f486e9dbca96915dab09434f74
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1994-11-28 09:16:47 +0000

    more DSN tweaking

--- usr.sbin/sendmail/src/savemail.c
+++ usr.sbin/sendmail/src/savemail.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)savemail.c	8.46 (Berkeley) 11/27/94";
+static char sccsid[] = "@(#)savemail.c	8.47 (Berkeley) 11/28/94";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -503,7 +503,18 @@ returntosender(msg, returnq, sendbody, e)
 			e->e_id, ee->e_id, msg);
 # endif
 
-	if (strncmp(msg, "Warning:", 8) == 0)
+	if (SendMIMEErrors)
+	{
+		addheader("MIME-Version", "1.0", &ee->e_header);
+		(void) sprintf(buf, "%s.%ld/%s",
+			ee->e_id, curtime(), MyHostName);
+		ee->e_msgboundary = newstr(buf);
+		(void) sprintf(buf,
+			"multipart/report; report-type=delivery-status; boundary=\"%s\"",
+			ee->e_msgboundary);
+		addheader("Content-Type", buf, &ee->e_header);
+	}
+	else if (strncmp(msg, "Warning:", 8) == 0)
 	{
 		addheader("Subject", msg, ee);
 		addheader("Precedence", "autoreply warning-timeout", &ee->e_header);
@@ -518,17 +529,6 @@ returntosender(msg, returnq, sendbody, e)
 		sprintf(buf, "Returned mail: %.*s", sizeof buf - 20, msg);
 		addheader("Subject", buf, &ee->e_header);
 		addheader("Precedence", "autoreply failure-message", &ee->e_header);
-	}
-	if (SendMIMEErrors)
-	{
-		addheader("MIME-Version", "1.0", &ee->e_header);
-		(void) sprintf(buf, "%s.%ld/%s",
-			ee->e_id, curtime(), MyHostName);
-		ee->e_msgboundary = newstr(buf);
-		(void) sprintf(buf,
-			"multipart/report; report-type=delivery-status; boundary=\"%s\"",
-			ee->e_msgboundary);
-		addheader("Content-Type", buf, &ee->e_header);
 	}
 
 	/* fake up an address header for the from person */
@@ -875,7 +875,12 @@ errbody(mci, e, separator, flags)
 			/* Final-Recipient: -- the name from the RCPT command */
 			for (r = q; r->q_alias != NULL; r = r->q_alias)
 				continue;
-			(void) sprintf(buf, "Final-Recipient: %s", r->q_paddr);
+			if (strchr(r->q_user, '@') == NULL)
+				(void) sprintf(buf, "Final-Recipient: %s@%s",
+					r->q_user, MyHostName);
+			else
+				(void) sprintf(buf, "Final-Recipient: %s",
+					r->q_user);
 			putline(buf, mci);
 
 			/* Final-Status: -- same as Status?  XXX */
