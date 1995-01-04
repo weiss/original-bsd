fc510d0439ebb3177cef42eca1849f9840a57972
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1995-01-04 16:32:13 +0000

    diagnose unknown options (on a debug flag)

--- usr.sbin/sendmail/src/readcf.c
+++ usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.57 (Berkeley) 12/30/94";
+static char sccsid[] = "@(#)readcf.c	8.58 (Berkeley) 01/04/95";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1220,7 +1220,9 @@ struct optioninfo
 #define O_MQA		0x83
 	"MinQueueAge",		O_MQA,		TRUE,
 #define O_MHSA		0x84
+/*
 	"MaxHostStatAge",	O_MHSA,		TRUE,
+*/
 #define O_DEFCHARSET	0x85
 	"DefaultCharSet",	O_DEFCHARSET,	TRUE,
 #define O_SSFILE	0x86
@@ -1836,6 +1838,13 @@ setoption(opt, val, safe, sticky, e)
 		break;
 
 	  default:
+		if (tTd(37, 1))
+		{
+			if (isascii(opt) && isprint(opt))
+				printf("Warning: option %c unknown\n", opt);
+			else
+				printf("Warning: option 0x%x unknown\n", opt);
+		}
 		break;
 	}
 	if (sticky)
