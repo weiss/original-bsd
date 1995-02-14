8b45a99d7ce0baa8e953653484616b77a504676b
cgd <cgd@65305748-3e16-e211-a854-8c89a5c6f760>
1995-02-14 10:36:26 +0000

    hack to make this work, for now.

--- sys/kern/kern_exit.c
+++ sys/kern/kern_exit.c
@@ -9,7 +9,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kern_exit.c	8.8 (Berkeley) 08/22/94
+ *	@(#)kern_exit.c	8.9 (Berkeley) 02/14/95
  */
 
 #include <sys/param.h>
@@ -251,7 +251,7 @@ struct wait_args {
 #define GETPS(rp)	(rp)[PS]
 #endif
 
-owait(p, uap, retval)
+compat_43_wait(p, uap, retval)
 	struct proc *p;
 	register struct wait_args *uap;
 	int *retval;
