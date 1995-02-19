74207d8ba04151695f16bedf3f15393d45bf461b
cgd <cgd@65305748-3e16-e211-a854-8c89a5c6f760>
1995-02-19 07:45:55 +0000

    fix external declarations to match prototyped return types

--- sys/hp300/dev/if_le.c
+++ sys/hp300/dev/if_le.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)if_le.c	8.1 (Berkeley) 06/10/93
+ *	@(#)if_le.c	8.2 (Berkeley) 02/19/95
  */
 
 #include "le.h"
@@ -51,7 +51,8 @@
 #if defined (CCITT) && defined (LLC)
 #include <sys/socketvar.h>
 #include <netccitt/x25.h>
-extern llc_ctlinput(), cons_rtrequest();
+extern int llc_ctlinput();
+extern void cons_rtrequest();
 #endif
 
 #include <machine/cpu.h>
