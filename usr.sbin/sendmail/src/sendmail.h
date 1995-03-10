4757536c519bbf63bd6d47ff44cea99619a66b81
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1995-03-10 10:30:47 +0000

    add SFF_OPENASROOT for safefopen

--- usr.sbin/sendmail/src/sendmail.h
+++ usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.99 (Berkeley) 03/10/95
+ *	@(#)sendmail.h	8.100 (Berkeley) 03/10/95
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.99		03/10/95";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.100		03/10/95";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -813,6 +813,9 @@ struct prival
 #define SFF_SETUIDOK		0x0020	/* setuid files are ok */
 #define SFF_CREAT		0x0040	/* ok to create file if necessary */
 #define SFF_REGONLY		0x0080	/* regular files only */
+
+/* flags that are actually specific to safefopen */
+#define SFF_OPENASROOT		0x1000	/* open as root instead of real user */
 
 
 /*
