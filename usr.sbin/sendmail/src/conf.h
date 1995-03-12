a3829f38e8e5cb8e592027f484259fd27cbf4c5f
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1995-03-12 09:02:04 +0000

    flock(2) is broken in Linux

--- usr.sbin/sendmail/src/conf.h
+++ usr.sbin/sendmail/src/conf.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.h	8.140 (Berkeley) 03/11/95
+ *	@(#)conf.h	8.141 (Berkeley) 03/12/95
  */
 
 /*
@@ -708,6 +708,9 @@ extern void		*malloc();
 # define ERRLIST_PREDEFINED	/* don't declare sys_errlist */
 # define GIDSET_T	gid_t	/* from <linux/types.h> */
 # define HASGETUSERSHELL 0	/* getusershell(3) broken in Slackware 2.0 */
+# ifndef HASFLOCK
+#  define HASFLOCK	0	/* flock(2) is broken after 0.99.13 */
+# endif
 # ifndef LA_TYPE
 #  define LA_TYPE	LA_PROCSTR
 # endif
