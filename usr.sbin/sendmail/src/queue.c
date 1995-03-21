2b335cec993979eb723cdcd12daf0336439c9658
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1995-03-21 06:46:16 +0000

    back out E qf line

--- usr.sbin/sendmail/src/queue.c
+++ usr.sbin/sendmail/src/queue.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef QUEUE
-static char sccsid[] = "@(#)queue.c	8.68 (Berkeley) 03/14/95 (with queueing)";
+static char sccsid[] = "@(#)queue.c	8.69 (Berkeley) 03/21/95 (with queueing)";
 #else
-static char sccsid[] = "@(#)queue.c	8.68 (Berkeley) 03/14/95 (without queueing)";
+static char sccsid[] = "@(#)queue.c	8.69 (Berkeley) 03/21/95 (without queueing)";
 #endif
 #endif /* not lint */
 
@@ -249,18 +249,8 @@ queueup(e, queueall, announce)
 	if (e->e_envid != NULL)
 		fprintf(tfp, "Z%s\n", denlstring(e->e_envid, TRUE, FALSE));
 
-	/* output list of error recipients */
-	printctladdr(NULL, NULL);
-	for (q = e->e_errorqueue; q != NULL; q = q->q_next)
-	{
-		if (!bitset(QDONTSEND|QBADADDR, q->q_flags))
-		{
-			printctladdr(q, tfp);
-			fprintf(tfp, "E%s\n", denlstring(q->q_paddr, TRUE, FALSE));
-		}
-	}
-
 	/* output list of recipient addresses */
+	printctladdr(NULL, NULL);
 	for (q = e->e_sendqueue; q != NULL; q = q->q_next)
 	{
 		if (bitset(QQUEUEUP, q->q_flags) ||
@@ -1352,7 +1342,7 @@ readqf(e)
 			break;
 
 		  case 'E':		/* specify error recipient */
-			(void) sendtolist(&bp[1], ctladdr, &e->e_errorqueue, 0, e);
+			/* no longer used */
 			break;
 
 		  case 'H':		/* header */
