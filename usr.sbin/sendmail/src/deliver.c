ac5a49f94265237b252299a79546600a2158e3a9
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1995-03-23 20:03:31 +0000

    avoid running out of file descriptors problem on vfork systems

--- usr.sbin/sendmail/src/deliver.c
+++ usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	8.134 (Berkeley) 03/22/95";
+static char sccsid[] = "@(#)deliver.c	8.135 (Berkeley) 03/23/95";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -1225,10 +1225,7 @@ tryhost:
 			extern int DtableSize;
 
 			if (e->e_lockfp != NULL)
-			{
-				fclose(e->e_lockfp);
-				e->e_lockfp = NULL;
-			}
+				(void) close(fileno(e->e_lockfp));
 
 			/* child -- set up input & exec mailer */
 			(void) setsignal(SIGINT, SIG_IGN);
