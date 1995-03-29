d480a7ad86b2818740f788155abc813e65f1e882
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1995-03-29 00:28:57 +0000

    add einval for invalid operations

--- sys/kern/subr_xxx.c
+++ sys/kern/subr_xxx.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)subr_xxx.c	8.2 (Berkeley) 02/14/95
+ *	@(#)subr_xxx.c	8.3 (Berkeley) 03/29/95
  */
 
 /*
@@ -67,6 +67,17 @@ eopnotsupp()
 {
 
 	return (EOPNOTSUPP);
+}
+
+/*
+ * Return error for an inval operation
+ * on a specific object or file type.
+ */
+int
+einval()
+{
+
+	return (EINVAL);
 }
 
 /*
