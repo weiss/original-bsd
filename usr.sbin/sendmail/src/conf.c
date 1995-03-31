549fe2fd0cf3efda1f06f4bab95182f8e59f9ede
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1995-03-31 15:35:20 +0000

    more type glitching for maximum portability

--- usr.sbin/sendmail/src/conf.c
+++ usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.145 (Berkeley) 03/31/95";
+static char sccsid[] = "@(#)conf.c	8.146 (Berkeley) 03/31/95";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -2792,7 +2792,7 @@ sm_gethostbyaddr(addr, len, type)
 
 struct passwd *
 sm_getpwnam(user)
-	const char *user;
+	char *user;
 {
 	return getpwnam(user);
 }
--- usr.sbin/sendmail/src/sendmail.h
+++ usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.111 (Berkeley) 03/31/95
+ *	@(#)sendmail.h	8.112 (Berkeley) 03/31/95
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.111		03/31/95";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.112		03/31/95";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -1096,7 +1096,7 @@ extern void		setsender __P((char *, ENVELOPE *, char **, bool));
 extern FILE		*safefopen __P((char *, int, int, int));
 extern struct hostent	*sm_gethostbyname __P((const char *));
 extern struct hostent	*sm_gethostbyaddr __P((const char *, int, int));
-extern struct passwd	*sm_getpwnam __P((const char *));
+extern struct passwd	*sm_getpwnam __P((char *));
 extern struct passwd	*sm_getpwuid __P((uid_t));
 
 /* ellipsis is a different case though */
