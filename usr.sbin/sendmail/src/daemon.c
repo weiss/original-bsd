3edb2e005c1d88e7b9d840081e2d73dbbb5059df
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1995-04-08 14:02:57 +0000

    just log failure to find "smtp" service instead of syserring

--- usr.sbin/sendmail/src/daemon.c
+++ usr.sbin/sendmail/src/daemon.c
@@ -11,9 +11,9 @@
 
 #ifndef lint
 #ifdef DAEMON
-static char sccsid[] = "@(#)daemon.c	8.76 (Berkeley) 03/31/95 (with daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.77 (Berkeley) 04/08/95 (with daemon mode)";
 #else
-static char sccsid[] = "@(#)daemon.c	8.76 (Berkeley) 03/31/95 (without daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.77 (Berkeley) 04/08/95 (without daemon mode)";
 #endif
 #endif /* not lint */
 
@@ -745,7 +745,10 @@ gothostent:
 
 		if (sp == NULL)
 		{
-			syserr("554 makeconnection: service \"smtp\" unknown");
+#ifdef LOG
+			if (LogLevel > 2)
+				syslog(LOG_ERR, "makeconnection: service \"smtp\" unknown");
+#endif
 			port = htons(25);
 		}
 		else
