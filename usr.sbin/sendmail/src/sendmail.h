bb8555015edfe6d2dca3642ad7fef647de271f61
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1995-04-10 08:26:12 +0000

    eliminate BrokenSmtpPeers; go for default one line greeting

--- usr.sbin/sendmail/src/sendmail.h
+++ usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.119 (Berkeley) 04/09/95
+ *	@(#)sendmail.h	8.120 (Berkeley) 04/10/95
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.119		04/09/95";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.120		04/10/95";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -967,7 +967,6 @@ EXTERN int	DeliveryNiceness;	/* how nice to be during delivery */
 EXTERN char	*PostMasterCopy;	/* address to get errs cc's */
 EXTERN int	CheckpointInterval;	/* queue file checkpoint interval */
 EXTERN bool	DontPruneRoutes;	/* don't prune source routes */
-EXTERN bool	BrokenSmtpPeers;	/* peers can't handle 2-line greeting */
 EXTERN int	MaxMciCache;		/* maximum entries in MCI cache */
 EXTERN time_t	MciCacheTimeout;	/* maximum idle time on connections */
 EXTERN char	*QueueLimitRecipient;	/* limit queue runs to this recipient */
