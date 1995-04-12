87c5886ba29bd1d484d90df32ba75671649cbd12
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1995-04-12 07:46:56 +0000

    define USERDB if either NEWDB or HESIOD are defined

--- usr.sbin/sendmail/src/conf.h
+++ usr.sbin/sendmail/src/conf.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.h	8.147 (Berkeley) 04/09/95
+ *	@(#)conf.h	8.148 (Berkeley) 04/12/95
  */
 
 /*
@@ -61,8 +61,8 @@
 # define NETINET	1	/* include internet support */
 # define MATCHGECOS	1	/* match user names from gecos field */
 # define XDEBUG		1	/* enable extended debugging */
-# ifdef NEWDB
-# define USERDB		1	/* look in user database (requires NEWDB) */
+# if (defined(NEWDB) || defined(HESIOD)) && !defined(USERDB)
+# define USERDB		1	/* look in user database */
 # endif
 
 /**********************************************************************
