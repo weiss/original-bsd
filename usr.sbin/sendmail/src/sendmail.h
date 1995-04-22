a2bf81898bcd1ce8fd8f699187aad984f0c17349
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1995-04-22 14:21:22 +0000

    add e_status

--- usr.sbin/sendmail/src/sendmail.h
+++ usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.124 (Berkeley) 04/22/95
+ *	@(#)sendmail.h	8.125 (Berkeley) 04/22/95
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.124		04/22/95";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.125		04/22/95";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -378,6 +378,7 @@ ENVELOPE
 	char		*e_msgboundary;	/* MIME-style message part boundary */
 	char		*e_origrcpt;	/* original recipient (one only) */
 	char		*e_envid;	/* envelope id from MAIL FROM: line */
+	char		*e_status;	/* DSN status for this message */
 	time_t		e_dtime;	/* time of last delivery attempt */
 	int		e_ntries;	/* number of delivery attempts */
 	dev_t		e_dfdev;	/* df file's device, for crash recov */
