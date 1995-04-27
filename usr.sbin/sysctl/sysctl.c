7cd463d7d3d7840d5ee6615bcfab3dfd15c23b60
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1995-04-27 15:41:06 +0000

    lint From: Chris Torek <torek@BSDI.COM>

--- usr.sbin/sysctl/sysctl.c
+++ usr.sbin/sysctl/sysctl.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)sysctl.c	8.3 (Berkeley) 04/27/95";
+static char sccsid[] = "@(#)sysctl.c	8.4 (Berkeley) 04/27/95";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -451,7 +451,8 @@ parse(string, flags)
  */
 debuginit()
 {
-	int mib[3], size, loc, i;
+	int mib[3], loc, i;
+	size_t size;
 
 	if (secondlevel[CTL_DEBUG].list != 0)
 		return;
