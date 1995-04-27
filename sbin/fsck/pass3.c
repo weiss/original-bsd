5998596524ef043386c13c76de4479a7e75957cd
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1995-04-27 11:33:09 +0000

    add function prototypes

--- sbin/fsck/pass3.c
+++ sbin/fsck/pass3.c
@@ -6,15 +6,18 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)pass3.c	8.1 (Berkeley) 06/05/93";
+static char sccsid[] = "@(#)pass3.c	8.2 (Berkeley) 04/27/95";
 #endif /* not lint */
 
 #include <sys/param.h>
 #include <sys/time.h>
+
 #include <ufs/ufs/dinode.h>
 #include <ufs/ffs/fs.h>
+
 #include "fsck.h"
 
+void
 pass3()
 {
 	register struct inoinfo **inpp, *inp;
