db149aaac761e3bd89d3c4435691341b6aabaf08
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1995-04-27 14:50:19 +0000

    script messed up the copyright notices

--- usr.sbin/bad144/bad144.c
+++ usr.sbin/bad144/bad144.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1993, 198019861988
+ * Copyright (c) 1980, 1986, 1988, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
@@ -7,12 +7,12 @@
 
 #ifndef lint
 static char copyright[] =
-"@(#) Copyright (c) 1993, 198019861988\n\
+"@(#) Copyright (c) 1980, 1986, 1988, 1993\n\
 	The Regents of the University of California.  All rights reserved.\n";
 #endif not lint
 
 #ifndef lint
-static char sccsid[] = "@(#)bad144.c	8.1 (Berkeley) 06/06/93";
+static char sccsid[] = "@(#)bad144.c	8.2 (Berkeley) 04/27/95";
 #endif not lint
 
 /*
