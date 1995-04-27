d6e0261caf62d58e91e544eef524d75691f85545
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1995-04-27 16:37:17 +0000

    error() wasn't printing the error string, even though it should always (well, unless write returns short) contain an interesting value. From: Charles Hannum <mycroft@NetBSD.ORG>

--- usr.bin/ar/misc.c
+++ usr.bin/ar/misc.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)misc.c	8.3 (Berkeley) 04/02/94";
+static char sccsid[] = "@(#)misc.c	8.4 (Berkeley) 04/27/95";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -110,7 +110,8 @@ void
 badfmt()
 {
 
-	errx(1, "%s: %s", archive, strerror(EFTYPE));
+	errno = EFTYPE;
+	err(1, "%s", archive);
 }
 
 void
@@ -118,5 +119,5 @@ error(name)
 	char *name;
 {
 
-	errx(1, "%s", name);
+	err(1, "%s", name);
 }
