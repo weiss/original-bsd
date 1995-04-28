5746698afe4c55aea0ce1e4407542e5f72bd82b1
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1995-04-28 16:15:38 +0000

    Now that file descriptor limits are soft, and can be larger than FD_SETSIZE, using the value of getdtablesize() as an input to select() does not work.  We fixed this in our RPC code a while back.  Here's a patch to make window(1) work more correctly. From: Charles Hannum <mycroft@NetBSD.ORG>

--- usr.bin/window/wwopen.c
+++ usr.bin/window/wwopen.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)wwopen.c	8.1 (Berkeley) 06/06/93";
+static char sccsid[] = "@(#)wwopen.c	8.2 (Berkeley) 04/28/95";
 #endif /* not lint */
 
 #include "ww.h"
@@ -90,6 +90,8 @@ wwopen(flags, nrow, ncol, row, col, nline)
 		}
 		w->ww_obe = w->ww_ob + 512;
 		w->ww_obp = w->ww_obq = w->ww_ob;
+		if (w->ww_pty >= wwdtablesize)
+			wwdtablesize = w->ww_pty + 1;
 	}
 
 	w->ww_win = wwalloc(w->ww_w.t, w->ww_w.l,
