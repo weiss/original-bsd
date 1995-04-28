e31bdd083a25b39d7ccdf299242a4f8282991278
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1995-04-28 14:11:17 +0000

    We lost when dealing with mappings with unequal ranges, for example 'tr A-Z N-ZA-M', because we didn't skip over the terminating character of the range when processing it.  This works when the ranges line up since they map terminating character to terminating character correctly, but fails when one range maps into two, or with other odd combinations. From: Chris Torek <torek@BSDI.COM>

--- usr.bin/tr/str.c
+++ usr.bin/tr/str.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)str.c	8.1 (Berkeley) 06/06/93";
+static char sccsid[] = "@(#)str.c	8.2 (Berkeley) 04/28/95";
 #endif /* not lint */
 
 #include <sys/cdefs.h>
@@ -215,8 +215,8 @@ genrange(s)
 	char *savestart;
 
 	savestart = s->str;
-	stopval = *++s->str == '\\' ? backslash(s) : *s->str;
-	if (stopval < s->lastch) {
+	stopval = *++s->str == '\\' ? backslash(s) : *s->str++;
+	if (stopval < (u_char)s->lastch) {
 		s->str = savestart;
 		return (0);
 	}
