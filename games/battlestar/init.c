498a88736f12e9af3aaa053beb0fa67ecedd02a6
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1995-04-30 09:10:19 +0000

    fix NULL pointer bug. From: ralphc@pyramid.com (Ralph Campbell)

--- games/battlestar/init.c
+++ games/battlestar/init.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)init.c	8.2 (Berkeley) 04/28/95";
+static char sccsid[] = "@(#)init.c	8.3 (Berkeley) 04/30/95";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -22,12 +22,12 @@ initialize(startup)
 	puts("Version 4.2, fall 1984.");
 	puts("First Adventure game written by His Lordship, the honorable");
 	puts("Admiral D.W. Riggle\n");
+	location = dayfile;
 	srand(getpid());
-	getutmp(uname);
-	wiz = wizard(uname);
+	getutmp(usrname);
+	wiz = wizard(usrname);
 	wordinit();
 	if (startup) {
-		location = dayfile;
 		direction = NORTH;
 		time = 0;
 		snooze = CYCLE * 1.5;
