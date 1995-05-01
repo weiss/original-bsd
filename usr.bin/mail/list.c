49a2a068c648b7e592c5b6aee302b7c7921e06a7
dab <dab@65305748-3e16-e211-a854-8c89a5c6f760>
1995-05-01 06:37:49 +0000

    Make "/:y" and abbreviation for "/subject:y".

--- usr.bin/mail/list.c
+++ usr.bin/mail/list.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)list.c	8.3 (Berkeley) 04/20/95";
+static char sccsid[] = "@(#)list.c	8.4 (Berkeley) 05/01/95";
 #endif /* not lint */
 
 #include "rcv.h"
@@ -718,7 +718,7 @@ matchsubj(str, mesg)
 		    str[2] == ':')
 			return(matchto(cp, mesg));
 		*cp++ = '\0';
-		cp2 = hfield(str, mp);
+		cp2 = hfield(*str ? str : "subject", mp);
 		cp[-1] = ':';
 		str = cp;
 	} else {
