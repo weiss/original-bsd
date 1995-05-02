6eafd81ff4733c1c0b9e81acbb2025bcfecba61a
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1995-05-02 00:01:04 +0000

    delete unused prototype

--- sys/netinet/if_ether.h
+++ sys/netinet/if_ether.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)if_ether.h	8.2 (Berkeley) 04/28/95
+ *	@(#)if_ether.h	8.3 (Berkeley) 05/02/95
  */
 
 /*
@@ -116,7 +116,6 @@ u_char	ether_ipmulticast_min[6];
 u_char	ether_ipmulticast_max[6];
 struct	ifqueue arpintrq;
 
-struct	llinfo_arp *arptnew __P((struct in_addr *));
 struct	llinfo_arp llinfo_arp;		/* head of the llinfo queue */
 
 void	arp_rtrequest __P((int, struct rtentry *, struct sockaddr *));
