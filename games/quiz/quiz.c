1228ee38463098cd53d948e326ea8816845a3c65
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1995-05-04 15:29:11 +0000

    optarg/optind moved to unistd.h

--- games/quiz/quiz.c
+++ games/quiz/quiz.c
@@ -16,16 +16,19 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)quiz.c	8.2 (Berkeley) 01/03/94";
+static char sccsid[] = "@(#)quiz.c	8.3 (Berkeley) 05/04/95";
 #endif /* not lint */
 
 #include <sys/types.h>
+
+#include <ctype.h>
 #include <errno.h>
-#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <time.h>
+#include <unistd.h>
+
 #include "quiz.h"
 #include "pathnames.h"
 
