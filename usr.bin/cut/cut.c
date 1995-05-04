e59a263f11bdc1b5d5f28b042d2f0b5a8d060c16
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1995-05-04 15:35:29 +0000

    optarg/optind moved to unistd.h

--- usr.bin/cut/cut.c
+++ usr.bin/cut/cut.c
@@ -15,7 +15,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)cut.c	8.2 (Berkeley) 04/28/95";
+static char sccsid[] = "@(#)cut.c	8.3 (Berkeley) 05/04/95";
 #endif /* not lint */
 
 #include <ctype.h>
@@ -24,6 +24,7 @@ static char sccsid[] = "@(#)cut.c	8.2 (Berkeley) 04/28/95";
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int	cflag;
 char	dchar;
