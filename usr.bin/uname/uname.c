a423a4a977a5b7465d3cd9b8e2063c7b71a409d2
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1995-05-04 15:48:13 +0000

    optarg/optind moved to unistd.h

--- usr.bin/uname/uname.c
+++ usr.bin/uname/uname.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)uname.c	8.1 (Berkeley) 06/06/93";
+static char sccsid[] = "@(#)uname.c	8.2 (Berkeley) 05/04/95";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -21,6 +21,7 @@ static char sccsid[] = "@(#)uname.c	8.1 (Berkeley) 06/06/93";
 #include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 void usage __P((void));
 
