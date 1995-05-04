6843d855fd7216444b27d771ce49e13c4be4f072
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1995-05-04 15:34:32 +0000

    optarg/optind moved to unistd.h

--- usr.bin/colrm/colrm.c
+++ usr.bin/colrm/colrm.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)colrm.c	8.1 (Berkeley) 06/06/93";
+static char sccsid[] = "@(#)colrm.c	8.2 (Berkeley) 05/04/95";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -21,6 +21,7 @@ static char sccsid[] = "@(#)colrm.c	8.1 (Berkeley) 06/06/93";
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define	TAB	8
 
