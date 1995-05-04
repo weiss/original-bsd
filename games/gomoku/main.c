f7a488222e09f375facb3143e11b3d4d9a20a8b8
bostic <bostic@65305748-3e16-e211-a854-8c89a5c6f760>
1995-05-04 15:27:57 +0000

    optarg/optind moved to unistd.h

--- games/gomoku/main.c
+++ games/gomoku/main.c
@@ -15,15 +15,16 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.3 (Berkeley) 05/03/95";
+static char sccsid[] = "@(#)main.c	8.4 (Berkeley) 05/04/95";
 #endif /* not lint */
 
+#include <curses.h>
+#include <err.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <signal.h>
-#include <curses.h>
-#include <err.h>
+#include <unistd.h>
 
 #include "gomoku.h"
 
