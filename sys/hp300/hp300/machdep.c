2d78f81524980661877a7e23e9f482b656aaa343
mckusick <mckusick@65305748-3e16-e211-a854-8c89a5c6f760>
1995-05-09 12:10:44 +0000

    put in request to unmount all filesystems when rebooting

--- sys/hp300/hp300/machdep.c
+++ sys/hp300/hp300/machdep.c
@@ -11,7 +11,7 @@
  *
  * from: Utah $Hdr: machdep.c 1.74 92/12/20$
  *
- *	@(#)machdep.c	8.11 (Berkeley) 01/09/95
+ *	@(#)machdep.c	8.12 (Berkeley) 05/09/95
  */
 
 #include <sys/param.h>
@@ -1061,6 +1061,11 @@ boot(howto)
 #endif
 #endif
 		sync(&proc0, (void *)NULL, (int *)NULL);
+		/*
+		 * Unmount filesystems
+		 */
+		if (panicstr == 0)
+			vfs_unmountall();
 
 		for (iter = 0; iter < 20; iter++) {
 			nbusy = 0;
