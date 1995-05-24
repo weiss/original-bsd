cf68af923cf0b267213159e52f8d74c4e79be44e
karels <karels@65305748-3e16-e211-a854-8c89a5c6f760>
1995-05-24 05:56:06 +0000

    two versions of a fix for an mbuf leak.  The first one is probably right, but a BSDI customer has not confirmed that problems went away.  The second version should be safe.  The mbuf leak happens when RST is received on a connection after sosend checks the state, then drops spl while doing a copyin.  A send of [100-208] bytes on a local connection would always hit this if the other end had closed.

--- sys/netinet/tcp_usrreq.c
+++ sys/netinet/tcp_usrreq.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1982, 1986, 1988, 1993
+ * Copyright (c) 1982, 1986, 1988, 1993, 1995
  *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)tcp_usrreq.c	8.3 (Berkeley) 01/09/95
+ *	@(#)tcp_usrreq.c	8.4 (Berkeley) 05/24/95
  */
 
 #include <sys/param.h>
@@ -75,6 +75,18 @@ tcp_usrreq(so, req, m, nam, control)
 	 */
 	if (inp == 0 && req != PRU_ATTACH) {
 		splx(s);
+#if 0
+		/*
+		 * The following corrects an mbuf leak under rare
+		 * circumstances, but has not been fully tested.
+		 */
+		if (m && req != PRU_SENSE)
+			m_freem(m);
+#else
+		/* safer version of fix for mbuf leak */
+		if (m && (req == PRU_SEND || req == PRU_SENDOOB))
+			m_freem(m);
+#endif
 		return (EINVAL);		/* XXX */
 	}
 	if (inp) {
