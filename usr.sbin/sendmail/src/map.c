d8ce95c2364798d85e6d0440232f181d4ff9a372
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1995-06-12 17:26:37 +0000

    keep looking for a dotted address in text_ and nis_getcanonname

--- usr.sbin/sendmail/src/map.c
+++ usr.sbin/sendmail/src/map.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)map.c	8.75 (Berkeley) 06/12/95";
+static char sccsid[] = "@(#)map.c	8.76 (Berkeley) 06/12/95";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -608,7 +608,7 @@ extract_canonname(name, line, cbuf)
 	if (line[0] == '#')
 		return FALSE;
 
-	for (i = 1; !found; i++)
+	for (i = 1; ; i++)
 	{
 		char nbuf[MAXNAME + 1];
 
