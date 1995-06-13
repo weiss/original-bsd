92051585cb8fb5c6112c780426ed31604a05469a
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1995-06-13 08:11:20 +0000

    fix possible null pointer dereference

--- usr.sbin/sendmail/src/deliver.c
+++ usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	8.156 (Berkeley) 06/10/95";
+static char sccsid[] = "@(#)deliver.c	8.157 (Berkeley) 06/13/95";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -1256,7 +1256,7 @@ tryhost:
 			(void) setsignal(SIGHUP, SIG_IGN);
 			(void) setsignal(SIGTERM, SIG_DFL);
 
-			if (m != FileMailer || stat(to->q_user, &stb) < 0)
+			if (m != FileMailer || stat(tochain->q_user, &stb) < 0)
 				stb.st_mode = 0;
 
 			/* tweak niceness */
