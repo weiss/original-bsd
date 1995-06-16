b4dac3b6f8b07229bf2c2cdb23d6664fd2393434
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1995-06-16 12:03:01 +0000

    Beta.5

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	8.7.Beta (Berkeley) 06/11/95";
+static char sccsid[] = "@(#)version.c	8.7.Beta (Berkeley) 06/16/95";
 #endif /* not lint */
 
-char	Version[] = "8.7.Beta.4";
+char	Version[] = "8.7.Beta.5";
