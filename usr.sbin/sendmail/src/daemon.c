d1b5947a8f8ce74fb50497666ecb32344897ac14
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1995-06-20 09:37:37 +0000

    better error message

--- usr.sbin/sendmail/src/daemon.c
+++ usr.sbin/sendmail/src/daemon.c
@@ -11,9 +11,9 @@
 
 #ifndef lint
 #ifdef DAEMON
-static char sccsid[] = "@(#)daemon.c	8.103 (Berkeley) 06/15/95 (with daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.104 (Berkeley) 06/20/95 (with daemon mode)";
 #else
-static char sccsid[] = "@(#)daemon.c	8.103 (Berkeley) 06/15/95 (without daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.104 (Berkeley) 06/20/95 (without daemon mode)";
 #endif
 #endif /* not lint */
 
@@ -813,7 +813,7 @@ gothostent:
 		if (s < 0)
 		{
 			sav_errno = errno;
-			syserr("makeconnection: no socket");
+			syserr("makeconnection: cannot create socket");
 			goto failure;
 		}
 
