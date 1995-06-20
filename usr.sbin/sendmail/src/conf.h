ca418389650c76ff84b64d6f3739e9e897291c34
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1995-06-20 06:14:41 +0000

    woops -- generic vendor paths need to be ifdefed

--- usr.sbin/sendmail/src/conf.h
+++ usr.sbin/sendmail/src/conf.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.h	8.192 (Berkeley) 06/20/95
+ *	@(#)conf.h	8.193 (Berkeley) 06/20/95
  */
 
 /*
@@ -1303,7 +1303,9 @@ extern int	errno;
 # ifndef _PATH_UNIX
 #  define _PATH_UNIX		"/unix"
 # endif
-# define _PATH_VENDOR_CF	"/usr/ucblib/sendmail.cf"
+# ifndef _PATH_VENDOR_CF
+#  define _PATH_VENDOR_CF	"/usr/ucblib/sendmail.cf"
+# endif
 # ifndef _PATH_SENDMAILPID
 #  define _PATH_SENDMAILPID	"/usr/ucblib/sendmail.pid"
 # endif
