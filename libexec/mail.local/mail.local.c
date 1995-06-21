2aab2b7ef231f6023ee7b7040b8daad59da36370
eric <eric@65305748-3e16-e211-a854-8c89a5c6f760>
1995-06-21 08:24:22 +0000

    close passwd file before setuid -- for paranoia

--- libexec/mail.local/mail.local.c
+++ libexec/mail.local/mail.local.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)mail.local.c	8.21 (Berkeley) 06/20/95";
+static char sccsid[] = "@(#)mail.local.c	8.22 (Berkeley) 06/21/95";
 #endif /* not lint */
 
 /*
@@ -254,6 +254,7 @@ deliver(fd, name)
 		warn("unknown name: %s", name);
 		return;
 	}
+	endpwent();
 
 	/*
 	 * Keep name reasonably short to avoid buffer overruns.
